7574a320aede1c30dc7f4a5203b170ca52335fe6
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-12-30 20:23:09 -0800

    small fixes

--- set_env_testnet.sh
+++ set_env_testnet.sh
@@ -1,6 +1,6 @@
 # set firebase env
-TLS=$(docker exec -i -t lnd_btc base64 /root/.lnd/tls.cert | tr -d '\n\r')
-MACAROON=$(docker exec -i -t lnd_btc base64 /root/.lnd/data/chain/bitcoin/testnet/admin.macaroon | tr -d '\n\r')
+TLS=$(docker exec -t lnd_btc base64 /root/.lnd/tls.cert | tr -d '\n\r')
+MACAROON=$(docker exec -t lnd_btc base64 /root/.lnd/data/chain/bitcoin/testnet/admin.macaroon | tr -d '\n\r')
 LNDADDR="set_ip_address"
 
 firebase functions:config:set lnd.testnet.TLS="$TLS" lnd.testnet.MACAROON="$MACAROON" lnd.testnet.LNDADDR="$LNDADDR"
