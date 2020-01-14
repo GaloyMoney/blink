c22633b4b29f341e445e0d0a323c666db38f974e
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-01-14 12:47:49 -0800

    fetch users phone with Twilio

--- src/index.ts
+++ src/index.ts
@@ -135,7 +135,6 @@ exports.sendPubKey = functions.https.onCall(async (data, context) => {
     })
 })
 
-
 exports.sendDeviceToken = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
         throw new functions.https.HttpsError('failed-precondition', 
@@ -162,19 +161,19 @@ exports.openChannel = functions.https.onCall(async (data, context) => {
 
     return firestore.doc(`/users/${context.auth.uid}`).get()
     .then(async doc => {
-        if (doc.exists) {
-            const pubkey = doc.data()!.lightning.pubkey
-    
-            // const is_private = network === "mainnet"
-            const is_private = true
-
-            const funding_tx = await lnService.openChannel({lnd, local_tokens, partner_public_key: pubkey, is_private})
-    
-            return {funding_tx}
-        } else {
+        if (!doc.exists) {
             throw new functions.https.HttpsError('not-found', 
             `Can't find associated user`);
         }
+
+        const pubkey = doc.data()!.lightning.pubkey
+
+        // const is_private = network === "mainnet"
+        const is_private = true
+
+        const funding_tx = await lnService.openChannel({lnd, local_tokens, partner_public_key: pubkey, is_private})
+
+        return {funding_tx}
     })
     .catch((err) => {
         console.error(err)
@@ -205,6 +204,20 @@ const initLnd = () => {
     const {lnd} = lnService.authenticatedLndGrpc(auth_lnd);
     return lnd
 }
+
+const twilioPhoneNumber = "***REMOVED***"
+const getTwilioClient = () => {
+    const accountSID = "***REMOVED***"
+    const authToken = "***REMOVED***"
+    
+    const client = require('twilio')(
+        accountSID,
+        authToken
+    );
+
+    return client
+}
+
 
 exports.quoteBTC = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
@@ -517,22 +530,10 @@ exports.incomingChannel = functions.https.onRequest(async (req, res) => {
         return { success: false, reason: 'phone number undefined' };
     }
 
-    console.log(phoneNumber)
-
-    /// FIXME
-    const accountSID = "***REMOVED***"
-    const authToken = "***REMOVED***"
-    const twilioPhoneNumber = "***REMOVED***"
-
-    const client = require('twilio')(
-        accountSID,
-        authToken
-      );
-    
     console.log(`sending message to ${phoneNumber} for channel creation`)
     
     try {
-        await client.messages.create({
+        await getTwilioClient().messages.create({
             from: twilioPhoneNumber,
             to: phoneNumber,
             body: `your channel is ready! open Galoy app to get and spend your micro reward`
@@ -545,9 +546,11 @@ exports.incomingChannel = functions.https.onRequest(async (req, res) => {
     return res.status(200).send({response: 'ok', phoneNumber})
 });
 
-exports.onUserCreation = functions.auth.user().onCreate((user) => {
+exports.onUserCreation = functions.auth.user().onCreate(async (user) => {
+    //TODO clean up returns
 
     const prepopulate = false
+    const lookup = true
 
     if (prepopulate) {
         const randomTxs = transactions_template.filter((item) => Math.random() > 0.5 )
@@ -562,7 +565,25 @@ exports.onUserCreation = functions.auth.user().onCreate((user) => {
         })
     }
 
-    return 'no prepopulation'
+    if (lookup) {
+
+        const phoneNumber = (await admin.auth().getUser(user.uid)).phoneNumber
+        console.log(phoneNumber)
+
+        const callerInfo = await getTwilioClient().lookups.phoneNumbers(phoneNumber)
+                                .fetch({type: ['caller-name', 'carrier']})
+
+        return firestore.doc(`/users/${user.uid}`).set({
+            callerInfo}, { merge: true }
+        ).then(writeResult => {
+            return {result: `Transaction succesfully added ${writeResult}`}
+        })
+        .catch((err) => {
+            console.error(err)
+            return err
+        })
+
+    }
 })
 
 exports.setGlobalInfo = functions.https.onCall(async (data, context) => {
