f42c05923da68370b5fc3ecff2f582420e6d82bc
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-04-20 11:24:46 -0700

    refactoring WIP/ pushing to create a github issue.

--- src/interface.ts
+++ src/interface.ts
@@ -1,13 +1,15 @@
+import { IAddInvoiceRequest } from "../../../../common/types"
 
 export interface IWallet {
-  getCurrency(): any // TODO
-  getBalance(): any // TODO
+  getCurrency(): string
+  getBalance(): Promise<number>
   getTransactions(): any // TODO
+  getInfo(): Promise<object>
 }
 
 export interface ILightningWallet extends IWallet {
-  payInvoice(invoice: string): boolean
-  getAddress(): any // TODO
+  payInvoice({invoice: string}): Promise<any> // TODO
+  addInvoice(IAddInvoiceRequest): Promise<any> // TODO
 }
 
 export interface FiatTransaction {
--- src/lightning.ts
+++ src/lightning.ts
@@ -1,10 +1,10 @@
-import * as functions from 'firebase-functions'
-import { checkNonAnonymous, checkAuth, uidToPubkey } from "./utils"
-const lnService = require('ln-service')
 import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
+import { OnboardingRewards, IAddInvoiceRequest, IAddInvoiceResponse, IPayInvoice } from "../../../../common/types"
+import { FiatTransaction, ILightningWallet } from "./interface"
+import { checkAuth, checkNonAnonymous, uidToPubkey } from "./utils"
+const lnService = require('ln-service')
 const firestore = admin.firestore()
-import { OnboardingRewards } from "../../../../common/types"
-import { FiatTransaction } from "./interface" 
 
 interface Auth {
     macaroon: string,
@@ -12,6 +12,72 @@ interface Auth {
     socket: string,
 }
 
+class LightningWallet implements ILightningWallet {
+    protected lnd: object
+    constructor(auth: Auth) {
+        this.lnd = lnService.authenticatedLndGrpc(auth).lnd
+    }
+
+    getCurrency() { return "BTC" }
+    
+    async getBalance() {
+      const balanceInChannels = (await lnService.getChannelBalance({lnd: this.lnd})).channel_balance;
+      return balanceInChannels
+    }
+
+    async getTransactions() {
+        const {payments} = await lnService.getPayments({lnd: this.lnd})
+        const {invoices} = await lnService.getInvoices({lnd: this.lnd})
+
+        console.log({payments, invoices})
+        // TODO
+    }
+
+    async payInvoice({invoice}) {
+        const {response} = await lnService.pay(
+            {lnd: this.lnd, 
+            request: invoice,
+        })
+        return {response}
+    }
+
+    async addInvoice(invoiceRequest: IAddInvoiceRequest) {
+        const {request} = await lnService.createInvoice(
+            {lnd: this.lnd, 
+            tokens: invoiceRequest.value,
+            description: invoiceRequest.memo,
+            // expires_at: validUntil.toISOString(),
+        })
+        return {request}
+    }
+
+    async getInfo() {
+        return await lnService.getWalletInfo({lnd: this.lnd});
+    }
+  }
+
+
+class LightningWalletAuthed extends LightningWallet {
+    constructor() {
+        let auth_lnd: Auth
+        let network: string
+
+        try {
+            network = process.env.NETWORK ?? functions.config().lnd.network
+            const cert = process.env.TLS ?? functions.config().lnd[network].tls
+            const macaroon = process.env.MACAROON ?? functions.config().lnd[network].macaroon
+            const lndaddr = process.env.LNDADDR ?? functions.config().lnd[network].lndaddr
+        
+            const socket = `${lndaddr}:10009`
+            auth_lnd = {macaroon, cert, socket}
+        } catch (err) {
+            throw new functions.https.HttpsError('failed-precondition', 
+                `neither env nor functions.config() are set` + err)
+        }
+
+        super(auth_lnd)
+    }
+}
 
 export const initLnd = () => {
     // TODO verify wallet is unlock?
@@ -38,22 +104,31 @@ export const initLnd = () => {
 }
 
 
+exports.addInvoice = functions.https.onCall(async (data: IAddInvoiceRequest, context): Promise<IAddInvoiceResponse> => {
+    // checkAuth(context)
+    const wallet = new LightningWalletAuthed()
+    return await wallet.addInvoice(data)
+})
 
-exports.addInvoice = functions.https.onCall(async (data, context) => {
+exports.getLightningInfo = functions.https.onCall(async (data, context) => {
+    // checkAuth(context)
+    const wallet = new LightningWalletAuthed()
+    return await wallet.getInfo()
+})
+
+exports.payInvoice = functions.https.onCall(async (data: IPayInvoice, context) => {
     checkAuth(context)
 
     const lnd = initLnd()
 
-    const {request} = await lnService.createInvoice(
+    const {response} = await lnService.payInvoice(
         {lnd, 
-        tokens: data.value,
-        description: data.memo,
-        // expires_at: validUntil.toISOString(),
+        request: data.invoice,
     });
 
-    console.log({request})
+    console.log({response})
 
-    return {request}
+    return {response}
 })
 
 
