6ae86a02ef2addaf437bb7a3760d18cc351cf6a2
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-05-04 08:58:06 -0700

    - use pending instead of settled for more clarity (@based on @kartik feedback) - add updatePendingPayment method (untested)

--- src/LightningImpl.ts
+++ src/LightningImpl.ts
@@ -84,7 +84,7 @@ export class LightningWallet extends Wallet implements ILightningWallet {
                 _id: id,
                 type,
                 user: this.uid,
-                settled: false, 
+                pending: true, 
             }).save()
         } catch (err) {
             // TODO
@@ -168,27 +168,45 @@ export class LightningWallet extends Wallet implements ILightningWallet {
         console.log({destination})
         
         // probe for Route
+        // TODO add private route from invoice
         const {route} = await lnService.probeForRoute({destination, lnd: this.lnd, tokens});
         console.log(util.inspect({route}, {showHidden: false, depth: null}))
 
         if (route.length === 0) {
             throw new functions.https.HttpsError('internal', `there is no route for this payment`)
-        }        
+        }
 
-        // TODO manage is the user is trying to pay an invoice twice
-        // { MongoError: E11000 duplicate key error collection ... }
-        // await this.addHash({type: "payment", id})
+        // we are confident enough that there is a possible payment route. let's move forward
 
+        try {
+            // we associated the hash to the user.
+            // and we used this table to known whether a payment is still pending
+            await this.addHash({type: "payment", id})
+        } catch (err) {
+            // TODO manage is the user is trying to pay an invoice twice
+            // { MongoError: E11000 duplicate key error collection ... }
+            throw new functions.https.HttpsError('internal', err.message)
+        }
 
+        // reduce balance from customer first
+        // TODO this should use a reference from balance computed above
+        // and fail is balance has changed in the meantime to prevent race condition
         const entry = await MainBook.entry('Payment paid')
         .debit('Assets:Reserve', tokens, {currency: this.currency})
         .credit(this.customerPath, tokens, {currency: this.currency})
         .commit()
 
+        // there is 3 scenarios for a payment.
+        // 1/ payment succeed is less than TIMEOUT_PAYMENT
+        // 2/ the payment fails. we are reverting it. this including voiding prior transaction
+        // 3/ payment is still pending after TIMEOUT_PAYMENT.
+        // we are timing out the request for UX purpose, so that the client can show the payment is pending
+        // even if the payment is still ongoing from lnd.
+        // to clean pending payments, another cron-job loop will run in the background.
         try {
-
+            const TIMEOUT_PAYMENT = 5000
             const promise = lnService.payViaRoutes({lnd: this.lnd, routes: [route], id})
-            await Timeout.wrap(promise, 5000, 'Timeout');
+            await Timeout.wrap(promise, TIMEOUT_PAYMENT, 'Timeout');
 
             // FIXME
             // return this.payDetail({
@@ -214,19 +232,55 @@ export class LightningWallet extends Wallet implements ILightningWallet {
             try {
                 // TODO transaction
                 await MainBook.void(entry._id, err[1])
-                await HashUser.findOneAndUpdate({_id: id}, {settled: true, error: err[1]})
+                await HashUser.findOneAndUpdate({_id: id}, {pending: false, error: err[1]})
             } catch (err_db) {
-                throw new functions.https.HttpsError('internal', `error canceling payment entry ${util.inspect({err_db})}`)
+                const err_message = `error canceling payment entry ${util.inspect({err_db})}`
+                console.error(err_message)
+                throw new functions.https.HttpsError('internal', err_message)
             }
 
             throw new functions.https.HttpsError('internal', `error paying invoice ${util.inspect({err})}`)
         }
         
         // success
-        await HashUser.findOneAndUpdate({_id: id}, {settled: true})
+        await HashUser.findOneAndUpdate({_id: id}, {pending: false})
         return {result: true}
     }
     
+    // should be run regularly with a cronjob
+    // TODO: move to an "admin/ops" wallet
+    async updatePendingPayment() {
+        
+        const HashUser = await createHashUser()
+        const hashArray = await HashUser.find({user: this.uid, type: "payment", pending: true})
+
+        for (const hash of hashArray) {
+
+            let result
+            try {
+                result = await lnService.getPayment({ lnd: this.lnd, id: hash.id })
+            } catch (err) {
+                throw Error('issue fetching payment: ' + err.toString())
+            }
+
+            if (result.is_confirmed) {
+                // success
+                await HashUser.findOneAndUpdate({_id: hash.id }, {pending: false})
+            }
+
+            if (result.is_failed) {
+                try {
+                    const MainBook = await this.getMainBook()
+                    // TODO mongodb transaction
+                    await MainBook.void(hash.id, result.failed)
+                    await HashUser.findOneAndUpdate({_id: hash.id}, {pending: false, error: result.failed})
+                } catch (err) {
+                    throw new functions.https.HttpsError('internal', `error canceling payment entry ${util.inspect({err})}`)
+                }
+            }
+        }
+    }
+
     async addInvoice({value, memo}: IAddInvoiceRequest) {
         const { request, id } = await lnService.createInvoice({
             lnd: this.lnd,
@@ -239,17 +293,16 @@ export class LightningWallet extends Wallet implements ILightningWallet {
         return { request }
     }
 
-    async updateSettledInvoices() {
+    // should be run regularly with a cronjob
+    // TODO: move to an "admin/ops" wallet
+    async updatePendingInvoices() {
     
-        const hashArray = await this.getHashes()
         const MainBook = await this.getMainBook()
 
         const HashUser = await createHashUser()
+        const hashArray = await HashUser.find({user: this.uid, type: "invoice", pending: true})
             
         for (const hash of hashArray) {
-            if (hash.settled) {
-                continue // TODO filter at the getHashes mongodb request level instead
-            }
             
             let result
             try {
@@ -266,7 +319,7 @@ export class LightningWallet extends Wallet implements ILightningWallet {
 
                 try {
 
-                    await HashUser.findOneAndUpdate({_id: hash.id}, {settled: true})
+                    await HashUser.findOneAndUpdate({_id: hash.id}, {pending: false})
 
                     await MainBook.entry('Invoice paid') // TODO replace by invoice memo
                     .credit('Assets:Reserve', result.tokens, {currency: "BTC", hash: hash.id }) 
