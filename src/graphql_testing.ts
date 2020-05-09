c8963a673b70a0162d19b4cba5b7df9e4fe2db20
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-05-09 16:53:21 -0700

    very first POC

---
+++ src/graphql_testing.ts
@@ -0,0 +1,51 @@
+import { graphql, buildSchema } from "graphql"
+import { LightningWalletAuthed } from "./LightningUserWallet";
+const express = require("express")
+const graphqlHTTP = require("express-graphql")
+import { Price } from "./priceImpl";
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Transaction {
+    t: Int
+    v: Float
+  }
+
+  type Price {
+    t: Int
+    o: Float
+  }
+
+  type Query {
+    prices: [Price]
+    balance(uid: String): String
+    transactions(uid: String): [Transaction]
+  }
+`);
+
+let lightningWallet
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  balance: ({uid}) => {
+    lightningWallet = new LightningWalletAuthed({uid})
+    return lightningWallet.getBalance()
+  },
+  transactions: ({uid}) => {
+    lightningWallet = new LightningWalletAuthed({uid})
+    return lightningWallet.getBalance()
+  },
+  prices: () => {
+    const price = new Price()
+    return price.lastCached()
+  }
+};
+
+const app = express()
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+app.listen(4000)
+console.log("now running...")
