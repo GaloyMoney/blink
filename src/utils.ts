1782f56737ac24acfe7b8a5ead0be49b024d4383
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-05-30 17:42:08 -0700

    removing firebase function from the backend

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,6 @@
-import * as functions from 'firebase-functions'
 const lnService = require('ln-service')
 import * as moment from 'moment'
 
-
-/**
- * @param lnd 
- * @param partner_public_key 
- * @returns array of channels
- */
-export const channelsWithPubkey = async (lnd: any, partner_public_key: string): Promise<Array<Object>> => {
-    // only send when first channel is being opened
-    const { channels } = await lnService.getChannels({lnd})
-    return channels.filter((item: any) => item.partner_public_key === partner_public_key)
-}
 
 export const btc2sat = (btc: number) => {
     return btc * Math.pow(10, 8)
@@ -21,25 +9,6 @@ export const btc2sat = (btc: number) => {
 export const sat2btc = (sat: number) => {
     return sat / Math.pow(10, 8)
 }
-
-
-export const checkAuth = (context: any) => { // FIXME any
-    if (!context.auth) {
-        throw new functions.https.HttpsError('failed-precondition', 
-            'The function must be called while authenticated.')
-    }
-}
-
-export const checkNonAnonymous = (context: any) => { // FIXME any
-    checkAuth(context)
-
-    if (context.auth.token.provider_id === "anonymous") {
-        throw new functions.https.HttpsError('failed-precondition', 
-            `This function must be while authenticate and not anonymous`)
-    }
-}
-
-export const checkBankingEnabled = checkNonAnonymous // TODO
 
 
 export const validate = require("validate.js")
