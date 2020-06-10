9b1a1e6da5f3e5fa74b0ab1d863bc91d76052ce8
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-06-09 17:55:56 -0700

    noop

--- src/index.ts
+++ src/index.ts
@@ -146,17 +146,17 @@ const resolvers = {
 
 const price = new Price()
 
-var CronJob = require('cron').CronJob;
-var job = new CronJob(
-	'5 * * * * *',
+// TODO this should be done in another pod
+const CronJob = require('cron').CronJob;
+new CronJob(
+	'0 * * * * *',
 	async function() {
-    console.log('You will see this message every minute');
     await price.update()
 	},
 	null,
 	true,
 	'America/Los_Angeles'
-);
+)
 
 function getUid(ctx: ContextParameters) {
   
