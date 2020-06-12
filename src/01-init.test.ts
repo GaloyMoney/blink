a72fc1a9fe0324dca07431d3712effd49853462b
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-06-12 12:25:04 -0700

    update init testing

--- src/01-init.test.ts
+++ src/01-init.test.ts
@@ -5,7 +5,8 @@ import { setupMongoConnection } from "./db"
 // this import needs to be before medici
 
 import { LightningAdminWallet } from "./LightningAdminImpl"
-import { sleep } from "./utils"
+import { sleep, getAuth } from "./utils"
+const mongoose = require("mongoose");
 
 //TODO: Choose between camel case or underscores for variable naming
 const BitcoindClient = require('bitcoin-core')
@@ -14,24 +15,15 @@ const cert = process.env.TLS
 
 const RANDOM_ADDRESS = "2N1AdXp9qihogpSmSBXSSfgeUFgTYyjVWqo"
 
-let macaroon1 = process.env.MACAROON
 let macaroon2 = process.env.MACAROONOUTSIDE1
 let macaroon3 = process.env.MACAROONOUTSIDE2
-let lnd1
-let lndOutside1
-let lndOutside2
 
-let lnd1_wallet_addr
-let lndOutside1_wallet_addr
-
-let lnd_addr = process.env.LNDIP
 let lnd_outside_1_addr = process.env.LNDOUTSIDE1ADDR
 let lnd_outside_2_addr = process.env.LNDOUTSIDE2ADDR
 
 let bitcoind_addr = process.env.BITCOINDADDR
 let bitcoind_port = process.env.BITCOINDPORT
 
-let lnd_rpc_port = process.env.LNDRPCPORT
 let lnd_outside_1_rpc_port = process.env.LNDOUTSIDE1RPCPORT
 let lnd_outside_2_rpc_port = process.env.LNDOUTSIDE2RPCPORT
 // let lnd_addr = 'lnd-service'
@@ -42,6 +34,16 @@ let lnd_outside_2_rpc_port = process.env.LNDOUTSIDE2RPCPORT
 // let lnd_rpc_port, lnd_outside_1_rpc_port  = 10009, 10009
 
 let bitcoindClient
+
+let lnd1
+let lndOutside1
+let lndOutside2
+
+let bank_address
+let lndOutside1_wallet_addr
+
+let admin_uid
+let User
 
 
 async function waitForNodeSync(lnd) {
@@ -57,12 +59,6 @@ async function waitForNodeSync(lnd) {
 }
 
 beforeAll(async () => {
-	lnd1 = lnService.authenticatedLndGrpc({
-		cert,
-		macaroon: macaroon1,
-		socket: `${lnd_addr}:${lnd_rpc_port}`,
-	}).lnd;
-
 	lndOutside1 = lnService.authenticatedLndGrpc({
 		cert,
 		macaroon: macaroon2,
@@ -76,77 +72,98 @@ beforeAll(async () => {
 	}).lnd;
 
 	await setupMongoConnection()
+	await mongoose.connection.dropCollection('users')
+	User = mongoose.model("User")
 })
 
 it('I can connect to bitcoind', async () => {
 	const connection_obj = { 
 		network: 'regtest', username: 'rpcuser', password: 'rpcpass',
 		host: bitcoind_addr, port: bitcoind_port }
-	console.log({connection_obj})
 	bitcoindClient = new BitcoindClient(connection_obj)
-	expect(await bitcoindClient.getBlockchainInfo().chain).toEqual('regtest')
+	const { chain } = await bitcoindClient.getBlockchainInfo()
+	expect(chain).toEqual('regtest')
 })
 
-it('I can connect to lnds', async () => {
-	console.log({lnd1})
-	const walletinfo = await lnService.getWalletInfo({ lnd: lnd1 })
-	console.log({walletinfo})
-	// expect(await bitcoindClient.getBlockchainInfo().chain).toEqual('regtest')
+it('I can connect to bank lnd', async () => {
+	// TODO
 })
 
-it('funding the bank', async () => {
-	// funding the bank
-	const adminWallet = new LightningAdminWallet({uid: "admin"})
-	lnd1_wallet_addr = adminWallet.getOnChainAddress()
-	console.log({lnd1_wallet_addr})
+it('I can connect to outside lnds', async () => {
+	const lnds = [lndOutside1, lndOutside2]
+	for (const lnd of lnds) {
+		const {current_block_height} = await lnService.getWalletInfo({ lnd })
+		expect(current_block_height).toBe(0)
+	}
+})
+
+it('I can connect to mongodb', async () => {
+	const users = await User.find({})
+	expect(users).toStrictEqual([])
+})
+
+it('creating bank "admin" user', async () => {
+	// FIXME there should be an API for this
+	await new User({}).save()
+	const users = await User.find({})
+	expect(users.length).toBe(1)
+
+	admin_uid = users[0]._id
+})
+
+it('getting bank address', async () => {
+	const users = await User.find({})
+	admin_uid = users[0]._id
+
+	const adminWallet = new LightningAdminWallet({uid: admin_uid})
+	bank_address = await adminWallet.getOnChainAddress()
+	expect(bank_address.substr(0, 4)).toBe("bcrt")
 })
 
 it('funding lndOutside 1', async () => {
-	lndOutside1_wallet_addr = (await lnService.createChainAddress({ format: 'np2wpkh', lnd: lndOutside1 })).address
-	console.log({lndOutside1_wallet_addr})
+	lndOutside1_wallet_addr = (await lnService.createChainAddress({ format: 'p2wpkh', lnd: lndOutside1 })).address
+	expect(lndOutside1_wallet_addr.substr(0, 4)).toBe("bcrt")
 })
 
 it('funds lnd1 and lndOutside1', async () => {
-	try {
-		let result = await bitcoindClient.generateToAddress(1, lnd1_wallet_addr)
-		expect(result[0].length).toEqual(64)
-		result = await bitcoindClient.generateToAddress(1, lndOutside1_wallet_addr)
-		expect(result[0].length).toEqual(64)
-		await bitcoindClient.generateToAddress(99, RANDOM_ADDRESS)
-	} catch (error) {
-		console.log(error)
-	}
+	let result = await bitcoindClient.generateToAddress(1, bank_address)
+	expect(result[0].length).toEqual(64)
+	result = await bitcoindClient.generateToAddress(1, lndOutside1_wallet_addr)
+	expect(result[0].length).toEqual(64)
+	await bitcoindClient.generateToAddress(99, RANDOM_ADDRESS)
 }, 10000)
 
 it('opens channel from lnd1 to lndOutside1', async () => {
-	let { public_key } = await lnService.getWalletInfo({ lnd: lndOutside1 })
+	const { public_key } = await lnService.getWalletInfo({ lnd: lndOutside1 })
 	
-	await lnService.addPeer({ lnd: lnd1, public_key, socket: `lnd-outside-1:9735` })
+	const { lnd } = lnService.authenticatedLndGrpc(getAuth())
+	
+	console.log({lnd})
 
-	await waitForNodeSync(lnd1)
+	// TODO: adminWallet should have an API for that
+	await lnService.addPeer({ lnd, public_key, socket: `lnd-outside-1:9735` })
+
+	await waitForNodeSync(lnd)
 	
-	let res = await lnService.openChannel({ lnd: lnd1, local_tokens: 100000, partner_public_key: public_key, partner_socket: `lnd-outside-1:9735`, give_tokens: 30000 })
+	const res = await lnService.openChannel({ lnd, local_tokens: 100000, partner_public_key: public_key, 
+		partner_socket: `lnd-outside-1:9735`, give_tokens: 30000 })
 	console.log("open channel res", res)
 }, 50000)
 
 it('opens channel from lndOutside1 to lndOutside2', async () => {
-	let { public_key } = await lnService.getWalletInfo({ lnd: lndOutside2 })
+	const { public_key } = await lnService.getWalletInfo({ lnd: lndOutside2 })
 	
 	await lnService.addPeer({ lnd: lndOutside1, public_key, socket: `lnd-outside-2:9735` })
 
 	await waitForNodeSync(lndOutside1)
 
-	let res = await lnService.openChannel({ lnd: lndOutside1, local_tokens: 100000, partner_public_key: public_key, partner_socket: `lnd-outside-2:9735`, give_tokens: 30000 })
+	const res = await lnService.openChannel({ lnd: lndOutside1, local_tokens: 100000, partner_public_key: public_key, partner_socket: `lnd-outside-2:9735`, give_tokens: 30000 })
 	await bitcoindClient.generateToAddress(5, RANDOM_ADDRESS)
 	console.log("open channel res", res)
 }, 50000)
 
 it('checks for channel existence', async () => {
-	await waitForNodeSync(lnd1)
 	await waitForNodeSync(lndOutside1)
-	await waitForNodeSync(lndOutside2)
-	let { channels } = await lnService.getChannels({ lnd: lnd1 })
-	expect(channels.length).toEqual(1)
-	channels = (await lnService.getChannels({ lnd: lndOutside2 })).channels
-	expect(channels.length).toEqual(1)
+	const { channels } = await lnService.getChannels({ lnd: lndOutside1 })
+	expect(channels.length).toEqual(2)
 }, 50000)
