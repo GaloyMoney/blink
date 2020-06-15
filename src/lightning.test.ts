00b33f3079e6b1835067cb5a99994c985fd3ed8f
krtk6160 <krtk.6160@gmail.com>
2020-06-16 03:51:31 +0530

    add funding tests

--- src/lightning.test.ts
+++ src/lightning.test.ts
@@ -4,25 +4,29 @@
 import { setupMongoConnection } from "./db"
 // this import needs to be before medici
 
-
 import moment from "moment"
 import { LightningUserWallet } from "./LightningUserWallet"
+import { btc2sat, waitForNodeSync, getAuth } from "./utils";
+import { login } from "./text";
+import { OnboardingEarn } from "./types"
 const lnService = require('ln-service')
-var lightningPayReq = require('bolt11')
+const lightningPayReq = require('bolt11')
 const mongoose = require("mongoose")
-
+const BitcoindClient = require('bitcoin-core')
 
 let lightningWallet
 let lightningWalletOutside1
 let lightningWalletOutside2
 
-const user1 = "user1"
+let user1
 const user2 = "user2"
 
 const lndOutside1Addr = process.env.LNDOUTSIDE1ADDR ?? 'lnd-outside-1'
 const lndOutside1Port = process.env.LNDOUTSIDE1RPCPORT ?? '10009'
 const lndOutside2Addr = process.env.LNDOUTSIDE2ADDR ?? 'lnd-outside-2'
 const lndOutside2Port = process.env.LNDOUTSIDE2RPCPORT ?? '10009'
+
+const testAccount = { phone: "+16505554321", code: 321321, network: "regtest" }
 
 beforeAll(async () => {
   await setupMongoConnection()
@@ -49,14 +53,12 @@ afterAll(async () => {
 });
 
 beforeEach(async () => {
-  lightningWallet = new LightningUserWallet({uid: user1})
-
-
-  // // example for @kartik
-
-  // lnService.createInvoice({lnd: lnd2, amoint... })
-  // lnService.pay({lnd: lnd2, amoint... })
-
+  const collections: any[] = await mongoose.connection.db.collections()
+  collections.forEach(async collection => await collection.deleteMany())
+  await login(testAccount)
+  let Users = mongoose.model("User")
+  user1 = (await Users.find({}))[0]._id
+  lightningWallet = new LightningUserWallet({ uid: user1 })
 })
 
 it('Lightning Wallet Get Info works', async () => {
@@ -79,8 +81,8 @@ it('add invoice', async () => {
 
   const InvoiceUser = mongoose.model("InvoiceUser")
   const {uid} = await InvoiceUser.findById(decodedHash)
-
-  expect(uid).toBe(user1)
+  //expect(uid).toBe(user1) does not work
+  expect(uid == user1).toBe(true)
 })
 
 
@@ -99,28 +101,52 @@ it('add invoice to different user', async () => {
 
 
 
-// it('list transactions', async () => {
+it('list transactions', async () => {
 
-//   const result = await lightningWallet.getTransactions()
-//   expect(result.length).toBe(0) 
+  const result = await lightningWallet.getTransactions()
+  expect(result.length).toBe(0) 
 
-//   // TODO validate a transaction to be and verify result == 1 afterwards.
-//   // TODO more testing with devnet
-// })
+  // TODO validate a transaction to be and verify result == 1 afterwards.
+  // TODO more testing with devnet
+})
 
-// it('get balance', async () => {
-//   const balance = await lightningWallet.getBalance()
-//   expect(balance).toBe(-0)
-// })
+it('get balance', async () => {
+  const balance = await lightningWallet.getBalance()
+  expect(balance).toBe(-0)
+})
 
+it('add earn adds balance correctly', async () => {
+  let currentBalance: number = await lightningWallet.getBalance()
+  let onBoardingEarnAmt: number = Object.values(OnboardingEarn).reduce((a, b) => a + b, 0)
+  await lightningWallet.addEarn(Object.keys(OnboardingEarn))
+  let finalBalance: number = await lightningWallet.getBalance()
+  expect(finalBalance).toBe(currentBalance + onBoardingEarnAmt)
+})
 
+it('receives external funding correctly', async () => {
+  const connection_obj = {
+    network: 'regtest', username: 'rpcuser', password: 'rpcpass',
+    host: process.env.BITCOINDADDR, port: process.env.BITCOINDPORT
+  }
+  const bitcoindClient = new BitcoindClient(connection_obj)
 
-// it('payInvoice', async () => {
-//   // TODO need a way to generate an invoice from another node
-//   var { request } = await lnService.createInvoice({ lnd: lightningWalletOutside1, tokens: 10000 })
-//   var result = await lightningWallet.payInvoice({ invoice: request })
-//   expect(result.result).toBe(true)
-// })
+  const { lnd } = lnService.authenticatedLndGrpc(getAuth())
+
+  let currentBalance: number = await lightningWallet.getBalance()
+  let onChainAddress: string = await lightningWallet.getOnChainAddress()
+  let result = await bitcoindClient.generateToAddress(1, onChainAddress)
+  await waitForNodeSync(lnd)
+  let finalBalance: number = await lightningWallet.getBalance()
+  expect(finalBalance).toBe(currentBalance + btc2sat(50))
+})
+
+it('payInvoice', async () => {
+  // TODO need a way to generate an invoice from another node
+  let { request } = await lnService.createInvoice({ lnd: lightningWalletOutside1, tokens: 10000 })
+  let currentBalance = await lightningWallet.getBalance()
+  let result = await lightningWallet.payInvoice({ invoice: request })
+  expect(result).toBe("success")
+})
 
 // it('payInvoiceToAnotherGaloyUser', async () => {
 //   // TODO Manage on us transaction from 2 users of our network
