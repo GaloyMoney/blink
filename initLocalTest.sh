2eac599184d5815ff68b3b5909ed28088faca5a8
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-06-24 14:26:25 -0700

    update package.json

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -1,8 +1,9 @@
-helm install mongodb --set mongodbUsername=testGaloy,mongodbPassword=testGaloy,mongodbDatabase=galoy,persistence.enabled=false,service.type=LoadBalancer bitnami/mongodb
 helm install bitcoind -f ../../bitcoind-chart/regtest-values.yaml ../../bitcoind-chart/
+helm install mongodb --set mongodbUsername=testGaloy,mongodbPassword=testGaloy,mongodbDatabase=galoy,persistence.enabled=false,service.type=LoadBalancer bitnami/mongodb
+
+kubectl wait --for=condition=ready pod -l app=bitcoind-container
+
 helm install lnd -f ../../lnd-chart/regtest-values.yaml ../../lnd-chart/
 
-kubectl wait --for=condition=ready pod -l app=mongodb
-kubectl wait --for=condition=ready pod -l app=bitcoind-container
 kubectl wait --for=condition=ready pod -l app=lnd-container
-
+kubectl wait --for=condition=ready pod -l app=mongodb
