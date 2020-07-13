5a892b6e542c783107f0ce5387b9037d451d8836
krtk6160 <krtk.6160@gmail.com>
2020-07-13 18:07:02 +0530

    add network to testnumber array

--- src/text.ts
+++ src/text.ts
@@ -7,7 +7,7 @@ import { randomIntFromInterval, createToken } from "./utils"
 const getTwilioClient = () => {
     const accountSID = "***REMOVED***"
     const authToken = "***REMOVED***"
-    
+
     const client = require('twilio')(
         accountSID,
         authToken
@@ -16,7 +16,7 @@ const getTwilioClient = () => {
     return client
 }
 
-const sendText = async ({body, to}) => {
+const sendText = async ({ body, to }) => {
     await getTwilioClient().messages.create({
         from: twilioPhoneNumber,
         to,
@@ -24,9 +24,9 @@ const sendText = async ({body, to}) => {
     })
 }
 
-export const TEST_NUMBER = [{phone: "+16505554321", code: 321321},{phone: "+16505554322", code: 321321}]
+export const TEST_NUMBER = [{ phone: "+16505554321", code: 321321, network: 'regtest' }, { phone: "+16505554322", code: 321321, network: 'regtest' }]
 
-export const requestPhoneCode = async ({phone}) => {
+export const requestPhoneCode = async ({ phone }) => {
 
     // make it possible to bypass the auth for testing purpose
     if (TEST_NUMBER.findIndex(item => item.phone === phone) !== -1) {
@@ -42,9 +42,9 @@ export const requestPhoneCode = async ({phone}) => {
         // in a row and ultimately randomly find a code
 
         const PhoneCode = mongoose.model("PhoneCode")
-        await PhoneCode.create({phone, code})
+        await PhoneCode.create({ phone, code })
 
-        await sendText({body, to: phone})
+        await sendText({ body, to: phone })
     } catch (err) {
         console.error(err)
         return false
@@ -53,7 +53,7 @@ export const requestPhoneCode = async ({phone}) => {
     return true
 }
 
-export const login = async ({phone, code, network}) => {
+export const login = async ({ phone, code, network }) => {
     // TODO assert network == process.env.network
 
     try {
@@ -71,8 +71,8 @@ export const login = async ({phone, code, network}) => {
         if (TEST_NUMBER.findIndex(item => item.phone === phone) !== -1 &&
             TEST_NUMBER.filter(item => item.phone === phone)[0].code === code) {
             test_account = true
-        // return null is the code is not correct
-        } else if(codes.findIndex(item => item.code === code) === -1) {
+            // return null is the code is not correct
+        } else if (codes.findIndex(item => item.code === code) === -1) {
             console.warn(`code is not correct: ${code} with ${phone}`)
             return null
         }
@@ -81,9 +81,9 @@ export const login = async ({phone, code, network}) => {
         const User = mongoose.model("User")
 
         // get User 
-        const user = await User.findOneAndUpdate({phone}, {level: 1}, {upsert: true, new: true})
+        const user = await User.findOneAndUpdate({ phone }, { level: 1 }, { upsert: true, new: true })
 
-        return createToken({uid: user._id, network})
+        return createToken({ uid: user._id, network })
     } catch (err) {
         console.error(err)
         throw err
