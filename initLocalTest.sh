47bb50b270fcdeb88b7a22103dfc4dcf502f0967
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-07-15 14:48:25 -0500

    fix selector?

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -6,4 +6,4 @@ kubectl wait --for=condition=ready pod -l app=bitcoind-container
 helm install lnd -f ../../lnd-chart/regtest-values.yaml ../../lnd-chart/
 
 kubectl wait --for=condition=ready pod -l app=lnd-container
-kubectl wait --for=condition=ready pod -l app=mongodb -timeout=-1
+kubectl wait --for=condition=ready pod -l=app.kubernetes.io/component=mongodb
