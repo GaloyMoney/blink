9fa0ff1c2709cb2a0736ba84d68c65eb5c53e57d
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-07-15 16:03:31 -0500

    missing namespace for wait

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -1,9 +1,9 @@
 helm install --namespace=$NAMESPACE bitcoind -f ../../bitcoind-chart/regtest-values.yaml ../../bitcoind-chart/
 helm install --namespace=$NAMESPACE mongodb --set auth.username=testGaloy,auth.password=testGaloy,auth.database=galoy,persistence.enabled=false bitnami/mongodb
 
-kubectl wait --for=condition=ready pod -l app=bitcoind-container
+kubectl wait --namespace=$NAMESPACE --for=condition=ready pod -l app=bitcoind-container
 
 helm install --namespace=$NAMESPACE lnd -f ../../lnd-chart/regtest-values.yaml ../../lnd-chart/
 
-kubectl wait --for=condition=ready pod -l app=lnd-container
+kubectl wait --namespace=$NAMESPACE --for=condition=ready pod -l app=lnd-container
 # kubectl wait --for=condition=ready pod -l app.kubernetes.io/component=mongodb
