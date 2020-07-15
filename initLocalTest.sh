cd72dff0bf889c4cef4f8f7da34ea51f530d2033
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-07-15 15:17:59 -0500

    missing namespace

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -1,9 +1,9 @@
-helm install bitcoind -f ../../bitcoind-chart/regtest-values.yaml ../../bitcoind-chart/
-helm install mongodb --set mongodbUsername=testGaloy,mongodbPassword=testGaloy,mongodbDatabase=galoy,persistence.enabled=false,service.type=LoadBalancer bitnami/mongodb
+helm install --namespace=$NAMESPACE bitcoind -f ../../bitcoind-chart/regtest-values.yaml ../../bitcoind-chart/
+helm install --namespace=$NAMESPACE mongodb --set mongodbUsername=testGaloy,mongodbPassword=testGaloy,mongodbDatabase=galoy,persistence.enabled=false,service.type=LoadBalancer bitnami/mongodb
 
 kubectl wait --for=condition=ready pod -l app=bitcoind-container
 
-helm install lnd -f ../../lnd-chart/regtest-values.yaml ../../lnd-chart/
+helm install --namespace=$NAMESPACE lnd -f ../../lnd-chart/regtest-values.yaml ../../lnd-chart/
 
 kubectl wait --for=condition=ready pod -l app=lnd-container
 kubectl wait --for=condition=ready pod -l=app.kubernetes.io/component=mongodb
