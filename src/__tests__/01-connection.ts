91838790ad4838b6401f0a17c965b4f938da16f1
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-07-26 18:37:59 -0500

    testing redis connection

--- src/__tests__/01-connection.ts
+++ src/__tests__/01-connection.ts
@@ -2,6 +2,8 @@
  * @jest-environment node
  */
 import { setupMongoConnection } from "../mongodb";
+const redis = require('redis')
+
 // this import needs to be before medici
 import {lndMain, lndOutside1, lndOutside2, bitcoindClient} from "../tests_utils/import"
 const mongoose = require("mongoose");
@@ -16,7 +18,7 @@ beforeAll(async () => {
 })
 
 afterAll(async () => {
-	return await mongoose.connection.close()
+	await mongoose.connection.close()
 })
 
 it('I can connect to bitcoind', async () => {
@@ -40,4 +42,20 @@ it('I can connect to outside lnds', async () => {
 it('I can connect to mongodb', async () => {
 	const users = await User.find()
 	expect(users).toEqual(expect.arrayContaining([]))
+})
+
+it('I can connect to redis', async () => {
+	const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_IP)
+	const value = "value"
+
+	client.on("error", function(error) {
+		throw new Error("redis issue");
+	})
+
+	client.set("key", value, (err, res) => {
+		client.get("key", (err, res) => {
+			expect(res).toBe(value)
+			client.quit()
+		});
+	});
 })
