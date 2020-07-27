09eaf4738f1169635ba319131eb233e801fd2eb8
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-07-27 11:27:15 -0500

    add 2 more gauges

--- src/prometheus.ts
+++ src/prometheus.ts
@@ -15,6 +15,8 @@ const mongoose = require("mongoose");
 const equity_g = new client.Gauge({ name: 'shareholder', help: 'value of shareholder' })
 const customers_g = new client.Gauge({ name: 'customers', help: 'how much money customers has' })
 const lightning_g = new client.Gauge({ name: 'lightning', help: 'how much money there is on lnd' })
+const assetsEqualLiabilities_g = new client.Gauge({ name: 'assetsEqLiabilities', help: 'do we have a balanced book' })
+const lndBalanceSheetAreSynced_g = new client.Gauge({ name: 'lndBalanceSync', help: 'are lnd in syncs with our books' })
 // const price_g = new client.Gauge({ name: 'price', help: 'BTC/USD price' })
 
 const main = async () => {
@@ -36,10 +38,13 @@ const main = async () => {
     await adminWallet.updateUsersPendingPayment()
     
     const {customers, equity, lightning} = await adminWallet.getBalanceSheet()
+    const { assetsEqualLiabilities, lndBalanceSheetAreSynced } = await adminWallet.balanceSheetIsBalanced()
 
     equity_g.set(equity)
     customers_g.set(customers)
     lightning_g.set(lightning)
+    assetsEqualLiabilities_g.set(assetsEqualLiabilities ? 1 : 0)
+    lndBalanceSheetAreSynced_g.set(lndBalanceSheetAreSynced ? 1 : 0)
     // price_g.set(price)
 
     res.set('Content-Type', register.contentType);
