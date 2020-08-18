a01fcd645625bbbffbeec97229eed493b9f59c5c
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-18 11:24:32 -0500

    new test credentials

--- src/notification.ts
+++ src/notification.ts
@@ -5,11 +5,9 @@ import { logger } from "./utils";
 // the key GOOGLE_APPLICATION_CREDENTIALS should be set in production to the path of the credentials
 // to use this class in a dev enviroment, set GOOGLE_APPLICATION_CREDENTIALS
 // more info at https://firebase.google.com/docs/admin/setup
-if(process.env.GOOGLE_APPLICATION_CREDENTIALS) { 
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-  });  
-}
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});  
 
 export const sendNotification = async ({uid, title, body}) => {
 
