855ac94f45fc44e74cb907fcb45a50d628a8974c
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-19 14:14:36 -0500

    persistance is not needed for redis when testing

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -4,9 +4,9 @@ if [ ${LOCAL} ]; then SERVICETYPE=LoadBalancer; else SERVICETYPE=ClusterIP; fi
 
 helm install --namespace=$NAMESPACE bitcoind -f ../../bitcoind-chart/values.yaml -f ../../bitcoind-chart/regtest-values.yaml --set serviceType=$SERVICETYPE ../../bitcoind-chart/
 helm install --namespace=$NAMESPACE mongodb --set auth.username=testGaloy,auth.password=testGaloy,auth.database=galoy,persistence.enabled=false,service.type=$SERVICETYPE bitnami/mongodb
-helm install --namespace=$NAMESPACE redis --set=cluster.enabled=false,usePassword=false,master.service.type=$SERVICETYPE bitnami/redis
+helm install --namespace=$NAMESPACE redis --set=cluster.enabled=false,usePassword=false,master.service.type=$SERVICETYPE,master.persistence.enabled=false	 bitnami/redis
 
-sleep 2
+sleep 5
 
 kubectl wait --namespace=$NAMESPACE --for=condition=ready pod -l app=bitcoind-container --timeout=1200s
 
@@ -15,6 +15,6 @@ helm install --namespace=$NAMESPACE lnd -f ../../lnd-chart/values.yaml -f ../../
 kubectl wait --namespace=$NAMESPACE --for=condition=ready pod -l app=redis --timeout=1200s
 kubectl wait --namespace=$NAMESPACE --for=condition=ready pod -l app.kubernetes.io/component=mongodb --timeout=1200s
 
-sleep 2
+sleep 5
 
 kubectl wait --namespace=$NAMESPACE --for=condition=ready pod -l app=lnd-container --timeout=1200s
