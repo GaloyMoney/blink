fd71041588f63a8c7284314f8dcde8dcc7698463
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-19 16:25:24 -0500

    going for notification instead of pulling from the server for receive payment

--- src/trigger.ts
+++ src/trigger.ts
@@ -1,30 +1,41 @@
-import { setupMongoConnection } from "./mongodb";
+import { InvoiceUser, setupMongoConnection } from "./mongodb";
 
 const lnService = require('ln-service');
 import express from 'express';
 import { subscribeToChannels, subscribeToInvoices, subscribeToTransactions } from 'ln-service';
 import { getAuth, logger, onchainTransactionEventHandler } from './utils';
+import { LightningUserWallet } from "./LightningUserWallet";
 
 const { lnd } = lnService.authenticatedLndGrpc(getAuth())
 
-const main = async () => {
+const main = async () => {	
 	lnService.getWalletInfo({ lnd }, (err, result) => {
 		logger.debug(err, result)
 	});
 
-	const result = await lnService.getChainTransactions({ lnd })
+	const subInvoices = subscribeToInvoices({ lnd });
+	subInvoices.on('invoice_updated', async invoice => {
+		logger.debug(invoice)
+
+		if (!invoice.is_confirmed) {
+			return
+		}
+
+		// FIXME: we're making 2x the request to Invoice User here. One in trigger, one in lighning.
+		const invoiceUser = await InvoiceUser.findOne({ _id: invoice.id, pending: true })
+		if (invoiceUser) {
+			const lightningAdminWallet = new LightningUserWallet({ uid: invoiceUser.uid })
+			await lightningAdminWallet.updatePendingInvoice({hash: invoice.id})
+		} else {
+			logger.warn({invoice}, "we received an invoice but had no user attached to it")
+		}
+	})
 
 	const subTransactions = subscribeToTransactions({ lnd });
 	subTransactions.on('chain_transaction', onchainTransactionEventHandler);
-
-	const subInvoices = subscribeToInvoices({ lnd });
-	subInvoices.on('invoice_updated', invoice => {
-		logger.debug(invoice)
-	})
-
 	const subChannels = subscribeToChannels({ lnd });
 	subChannels.on('channel_opened', channel => {
-		logger.debug(channel)
+		logger.info(channel)
 	})
 }
 
@@ -41,4 +52,4 @@ app.get('/health', (req, res) => {
 })
 app.listen(port, () => logger.debug(`Health check listening on port ${port}!`))
 
-setupMongoConnection().then(() => main()).catch((err) => logger.error(err))
+setupMongoConnection().then(main).catch((err) => logger.error(err))
