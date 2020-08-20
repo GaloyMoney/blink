a0787fb4a940a9d53bce4c9370629c5ce30fe334
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-19 19:33:43 -0500

    use space

--- src/trigger.ts
+++ src/trigger.ts
@@ -11,55 +11,55 @@ import { TransactionType } from "./types";
 const { lnd } = lnService.authenticatedLndGrpc(getAuth())
 
 const main = async () => {	
-	lnService.getWalletInfo({ lnd }, (err, result) => {
-		logger.debug(err, result)
-	});
+  lnService.getWalletInfo({ lnd }, (err, result) => {
+    logger.debug(err, result)
+  });
 
-	const subInvoices = subscribeToInvoices({ lnd });
-	subInvoices.on('invoice_updated', async invoice => {
-		logger.debug(invoice)
+  const subInvoices = subscribeToInvoices({ lnd });
+  subInvoices.on('invoice_updated', async invoice => {
+    logger.debug(invoice)
 
-		if (!invoice.is_confirmed) {
-			return
-		}
+    if (!invoice.is_confirmed) {
+      return
+    }
 
-		// FIXME: we're making 2x the request to Invoice User here. One in trigger, one in lighning.
-		const invoiceUser = await InvoiceUser.findOne({ _id: invoice.id, pending: true })
-		if (invoiceUser) {
+    // FIXME: we're making 2x the request to Invoice User here. One in trigger, one in lighning.
+    const invoiceUser = await InvoiceUser.findOne({ _id: invoice.id, pending: true })
+    if (invoiceUser) {
       const uid = invoiceUser.uid
       const hash = invoice.id as string
 
-			const lightningAdminWallet = new LightningUserWallet({ uid })
+      const lightningAdminWallet = new LightningUserWallet({ uid })
       await lightningAdminWallet.updatePendingInvoice({ hash })
       const data = {
         type: "paid-invoice" as TransactionType,
         hash
       }
       await sendNotification({uid, title: `You receive a payment of ${invoice.received} sats`, data})
-		} else {
-			logger.warn({invoice}, "we received an invoice but had no user attached to it")
-		}
-	})
+    } else {
+      logger.warn({invoice}, "we received an invoice but had no user attached to it")
+    }
+  })
 
-	const subTransactions = subscribeToTransactions({ lnd });
+  const subTransactions = subscribeToTransactions({ lnd });
   subTransactions.on('chain_transaction', onchainTransactionEventHandler);
   
-	const subChannels = subscribeToChannels({ lnd });
-	subChannels.on('channel_opened', channel => {
-		logger.info(channel)
-	})
+  const subChannels = subscribeToChannels({ lnd });
+  subChannels.on('channel_opened', channel => {
+    logger.info(channel)
+  })
 }
 
 const app = express()
 const port = 8888
 app.get('/health', (req, res) => {
-	lnService.getWalletInfo({ lnd }, (err, result) => {
-		if (err === null) {
-			return res.sendStatus(200)
-		} else {
-			return res.sendStatus(500)
-		}
-	});
+  lnService.getWalletInfo({ lnd }, (err, result) => {
+    if (err === null) {
+      return res.sendStatus(200)
+    } else {
+      return res.sendStatus(500)
+    }
+  });
 })
 app.listen(port, () => logger.debug(`Health check listening on port ${port}!`))
 
