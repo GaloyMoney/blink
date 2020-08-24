b4c027554f4cef4e50428c1a9a6ab9b5a2b6031f
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-08-24 18:42:16 -0500

    use the short term token strategy for getting lnd. for some reason, the other strategy stopped working

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -2,6 +2,7 @@ set -e
 
 if [ ${LOCAL} ]; then SERVICETYPE=LoadBalancer; else SERVICETYPE=ClusterIP; fi
 
+# bug with --wait: https://github.com/helm/helm/issues/7139 ?
 helm install --namespace=$NAMESPACE bitcoind -f ../../bitcoind-chart/values.yaml -f ../../bitcoind-chart/regtest-values.yaml --set serviceType=$SERVICETYPE ../../bitcoind-chart/
 helm install --namespace=$NAMESPACE mongodb --set auth.username=testGaloy,auth.password=testGaloy,auth.database=galoy,persistence.enabled=false,service.type=$SERVICETYPE bitnami/mongodb
 helm install --namespace=$NAMESPACE redis --set=cluster.enabled=false,usePassword=false,master.service.type=$SERVICETYPE,master.persistence.enabled=false	 bitnami/redis
