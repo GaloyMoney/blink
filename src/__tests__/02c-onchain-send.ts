e5810afce8a97d64e42f327b456c8441a2633b98
krtk6160 <krtk.6160@gmail.com>
2020-08-30 19:38:22 +0530

    add balance checks for onchain on-us test

--- src/__tests__/02c-onchain-send.ts
+++ src/__tests__/02c-onchain-send.ts
@@ -38,29 +38,29 @@ afterAll(async () => {
 	await quit()
 })
 
+const amount = 10000 // sats
 
 it('Sends onchain payment', async () => {
-  const amount = 10000 // sats
-  const {address} = await lnService.createChainAddress({ format: 'p2wpkh', lnd: lndOutside1 })
+  const { address } = await lnService.createChainAddress({ format: 'p2wpkh', lnd: lndOutside1 })
 
-	const payResult = await wallet.onChainPay({address, amount, description: "onchainpayment"})
+	const payResult = await wallet.onChainPay({ address, amount, description: "onchainpayment" })
   expect(payResult).toBeTruthy()
 
-  const [pendingTxn] = (await MainBook.ledger({account:wallet.accountPath, pending: true, memo: "onchainpayment"})).results
+  const [pendingTxn] = (await MainBook.ledger({ account: wallet.accountPath, pending: true, memo: "onchainpayment" })).results
 
 	const interimBalance = await wallet.getBalance()
 	expect(interimBalance).toBe(initialBalanceUser0 - amount - pendingTxn.fee)
 	// await checkIsBalanced()
-	
-  const sub = lnService.subscribeToTransactions({lnd:lndMain})
 
-	const subSpend = lnService.subscribeToChainSpend({lnd:lndMain, bech32_address: address, min_height: 1})
+  const sub = lnService.subscribeToTransactions({ lnd: lndMain })
+
+	const subSpend = lnService.subscribeToChainSpend({ lnd: lndMain, bech32_address: address, min_height: 1 })
 
   await Promise.all([
-    subSpend.once('confirmation', ({height}) => console.log({height})),
+    subSpend.once('confirmation', ({ height }) => console.log({ height })),
     sub.once('chain_transaction', onchainTransactionEventHandler),
     bitcoindClient.generateToAddress(6, RANDOM_ADDRESS),
-    waitUntilBlockHeight({lnd: lndMain, blockHeight: initBlockCount + 6}),
+    waitUntilBlockHeight({ lnd: lndMain, blockHeight: initBlockCount + 6 }),
   ])
 
   // FIXME why sleep is needed here?
@@ -69,7 +69,7 @@ it('Sends onchain payment', async () => {
   expect(notification.sendNotification.mock.calls.length).toBe(1)
   expect(notification.sendNotification.mock.calls[0][0].data.type).toBe("onchain_payment")
 
-  const [{pending, fee}] = (await MainBook.ledger({account:wallet.accountPath, hash: pendingTxn.hash, memo:"onchainpayment"})).results
+  const [{ pending, fee }] = (await MainBook.ledger({ account: wallet.accountPath, hash: pendingTxn.hash, memo: "onchainpayment" })).results
 	expect(pending).toBe(false)
 
 	const [txn] = (await wallet.getTransactions()).filter(tx => tx.hash === pendingTxn.hash)
@@ -81,3 +81,22 @@ it('Sends onchain payment', async () => {
 	// await checkIsBalanced()
 }, 100000)
 
+it('makes onchain on-us transaction', async () => {
+  //TODO: WIP
+  const userWallet2 = await getUserWallet(2)
+  const userWallet1 = await getUserWallet(1)
+
+  const user2Address = await userWallet2.getOnChainAddress()
+
+  const user1InitialBalance = await userWallet1.getBalance()
+  const user2InitialBalance = await userWallet2.getBalance()
+
+  const paymentResult = await userWallet1.onChainPay({ address: user2Address as string, amount })
+
+  const user1FinalBalance = await userWallet1.getBalance()
+  const user2FinalBalance = await userWallet2.getBalance()
+
+  expect(paymentResult).toBe(true)
+  expect(user1FinalBalance).toBe(user1InitialBalance - amount)
+  expect(user2FinalBalance).toBe(user2InitialBalance + amount)
+})
