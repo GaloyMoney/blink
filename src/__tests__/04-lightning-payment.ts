8bd2fd7bb065b403fca8f81b227fa776321dc818
krtk6160 <krtk.6160@gmail.com>
2020-08-30 19:59:05 +0530

    move onboarding earn to helper

--- src/__tests__/04-lightning-payment.ts
+++ src/__tests__/04-lightning-payment.ts
@@ -7,7 +7,6 @@ import { createHash, randomBytes } from 'crypto';
 import { LightningUserWallet } from "../LightningUserWallet";
 import { quit } from "../lock";
 import { checkIsBalanced, getUidFromToken, getUserWallet, lndOutside1, lndOutside2 } from "../tests/helper";
-import { OnboardingEarn } from "../types";
 const lnService = require('ln-service')
 const lightningPayReq = require('bolt11')
 const mongoose = require("mongoose")
@@ -16,13 +15,6 @@ let userWallet1, userWallet2
 let uidFromToken1, uidFromToken2
 
 const logger = require('pino')({ level: "debug" })
-
-
-//FIXME: Maybe switch to using single reward
-const onBoardingEarnAmt: number = Object.values(OnboardingEarn).reduce((a, b) => a + b, 0)
-const onBoardingEarnIds: string[] = Object.keys(OnboardingEarn)
-
-logger.info({onBoardingEarnAmt})
 
 beforeAll(async () => {
   await setupMongoConnection()
