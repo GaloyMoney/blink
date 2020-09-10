c19dda0cd01bc61f97cccf9af8200432ff0f7b0d
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-09-09 19:15:37 -0500

    make upgrade db more explicit

--- src/upgrade_schema.ts
+++ src/upgrade_schema.ts
@@ -16,16 +16,19 @@ export const upgrade = async () => {
     logger.info({dbVersion}, "entering upgrade db module version")
   
     if (dbVersion.version === 0) {
-      // all existing wallet should have "BTC" as currency
+      logger.info("all existing wallet should have BTC as currency")
+      // this is to enforce the index constraint
       await User.updateMany({}, {$set: {currency: "BTC"}})
       
-      // there needs to have a role: funder now
-      await User.findOneAndUpdate({phone: "+1***REMOVED***"}, {role: "funder"})
+      logger.info("there needs to have a role: funder")
+      await User.findOneAndUpdate({phone: "+1***REMOVED***", currency: "BTC"}, {role: "funder"})
   
-      // earn is no longer a particular type. replace with on_us
+      logger.info("earn is no longer a particular type. replace with on_us")
       await Transaction.updateMany({type: "earn"}, {$set: {type: "on_us"}})
-  
+      
+      logger.info("setting db version to 1")
       await DbVersion.findOneAndUpdate({}, {version: 1}, {upsert: true})
+
       logger.info("upgrade succesful to version 1")
 
     } else {
