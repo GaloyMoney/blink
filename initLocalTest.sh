5fc037315b56ca4d36e958a933eda2a1c3f10d92
krtk6160 <krtk.6160@gmail.com>
2020-09-29 20:57:32 +0530

    pass namespace explicitly to control scope

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -144,18 +144,17 @@ then
   kubectlWait app=test-chart
 elif [ "$NETWORK" == "testnet" ]
 then
-  NAMESPACE=monitoring
   SECRET=alertmanager-keys
 
-  helmUpgrade prometheus stable/prometheus -f ~/GaloyApp/backend/prometheus-server/values.yaml
+  NAMESPACE=monitoring helmUpgrade prometheus stable/prometheus -f ~/GaloyApp/backend/prometheus-server/values.yaml
 
   export SLACK_API_URL=$(kubectl get secret -n $NAMESPACE $SECRET -o jsonpath="{.data.SLACK_API_URL}" | base64 --decode)
   export SERVICE_KEY=$(kubectl get secret -n $NAMESPACE $SECRET -o jsonpath="{.data.SERVICE_KEY}" | base64 --decode)
 
   kubectl -n $NAMESPACE get configmaps prometheus-alertmanager -o yaml | sed -e "s|SLACK_API_URL|$SLACK_API_URL|; s|SERVICE_KEY|$SERVICE_KEY|" | kubectl -n $NAMESPACE apply -f -
 
-  helmUpgrade grafana stable/grafana -f ~/GaloyApp/backend/grafana/values.yaml
-  helmUpgrade mongo-exporter ~/GaloyApp/backend/mongo-exporter
+  NAMESPACE=monitoring helmUpgrade grafana stable/grafana -f ~/GaloyApp/backend/grafana/values.yaml
+  NAMESPACE=monitoring helmUpgrade mongo-exporter ~/GaloyApp/backend/mongo-exporter
 fi
 
 kubectlWait app=redis
