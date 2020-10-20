16e0e85497f2ffac57a4bf01957b642694a1b3a5
krtk6160 <krtk.6160@gmail.com>
2020-10-20 23:14:10 +0530

    add tests for setUsername

--- src/__tests__/01b-create-users.ts
+++ src/__tests__/01b-create-users.ts
@@ -5,13 +5,14 @@ const lnService = require('ln-service')
 import { setupMongoConnection, User } from "../mongodb";
 import { TEST_NUMBER } from "../text";
 import { createBrokerUid, getTestUserToken } from "../walletFactory";
+import { getUserWallet } from "../tests/helper"
 const mongoose = require("mongoose");
 
 
 // change role to admin
 // FIXME there should be an API for this
 export async function promoteToFunder(uid) {
-  await User.findOneAndUpdate({_id: uid}, {role: "funder"})
+  await User.findOneAndUpdate({ _id: uid }, { role: "funder" })
 }
 
 beforeAll(async () => {
@@ -25,24 +26,24 @@ afterAll(async () => {
 
 it('add user0 without currency (old account)', async () => {
   // TODO: query to add `currency` for exising users
-  await User.findOneAndUpdate({}, {phone: TEST_NUMBER[0].phone}, {upsert:true})
-  await User.updateMany({}, {$set: {currency: "BTC"}})
+  await User.findOneAndUpdate({}, { phone: TEST_NUMBER[0].phone }, { upsert: true })
+  await User.updateMany({}, { $set: { currency: "BTC" } })
 
   const token = await getTestUserToken(0)
   expect(token.currency).toBe("BTC")
 })
 
 
-it('add Funder', async () => {  
-  const {uid} = await getTestUserToken(4)
+it('add Funder', async () => {
+  const { uid } = await getTestUserToken(4)
   await promoteToFunder(uid)
 })
 
-it('add Broker', async () => {  
+it('add Broker', async () => {
   await createBrokerUid()
 })
 
-it('add user5 / usd user', async () => {  
+it('add user5 / usd user', async () => {
   const user5 = await getTestUserToken(5)
   expect(user5.currency).toBe("USD")
 
@@ -50,10 +51,20 @@ it('add user5 / usd user', async () => {
   expect(user6.currency).toBe("BTC")
 
   expect(user5.uid).not.toBe(user6.uid)
-  
-  const phoneUser5 = await User.findOne({_id: user5.uid}, {phone: 1, _id: 0})
-  const phoneUser6 = await User.findOne({_id: user6.uid}, {phone: 1, _id: 0})
+
+  const phoneUser5 = await User.findOne({ _id: user5.uid }, { phone: 1, _id: 0 })
+  const phoneUser6 = await User.findOne({ _id: user6.uid }, { phone: 1, _id: 0 })
   expect(phoneUser5).toStrictEqual(phoneUser6)
 })
 
+it('sets username for user', async () => {
+  const userWallet = await getUserWallet(1)
+  const result = await userWallet.setUsername({username: "xyz"})
+  expect(!!result).toBeTruthy()
+})
 
+it('does not set username when it already exists', async () => {
+  const userWallet = await getUserWallet(1)
+  const result = await userWallet.setUsername({username: "abc"})
+  expect(!!result).toBeFalsy()
+})
