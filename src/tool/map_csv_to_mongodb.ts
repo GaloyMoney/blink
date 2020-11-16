16bcef2fd9f241e09bbd04c672aea8f5605484a8
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-11-16 17:01:57 -0600

    missing commit

--- src/tool/map_csv_to_mongodb.ts
+++ src/tool/map_csv_to_mongodb.ts
@@ -1,13 +1,14 @@
-import { setupMongoConnection } from "../mongodb"
-
+import { MapDB, setupMongoConnection } from "../mongodb"
+import { sleep } from "../utils"
 const csv = require('csv-parser')
 const fs = require('fs')
+const util = require('util')  
 
 // source ../../exportLocal.sh && ts-node ./import_and_pay.ts
 
 export const baseLogger = require('pino')
 
-const insertMarkers = async () => {
+export const insertMarkers = async (executeScript = false) => {
 
   let results: any[] = [];
 
@@ -16,26 +17,35 @@ const insertMarkers = async () => {
   .on('data', (data) => results.push(data))
   .on('end', async () => {
     results = results.map(item => ({
-      title: item["name - en"],
+      title: item["name - es"],
       coordinate: {
         latitude: item.latitude,
         longitude: item.longitude,
-      }
+      },
+      username: item["***REMOVED*** username"]
     }))
-    // for (const id in results) {
-    //   results[id] = {...results[id], id}
-    // }
 
-    const util = require('util')
-    const data = util.inspect(results, {showHidden: false, depth: 3})
-
-    // TODO: execute the script here insteaad of showing instruction
-    const script = `run db.getCollection('maps').insertMany(${data})`
-     
+    if (executeScript) {
+      console.log(results)
+      console.log(typeof results)
+      await MapDB.insertMany(results) 
+    } else {
+      const script = `run db.getCollection('maps').insertMany(${util.inspect(results, {showHidden: false, depth: 3})})`
+      console.log(script)
+    }
   });
+
+  await sleep(1000)
+
 }
 
 // only execute if it is the main module
 if (require.main === module) {
-  setupMongoConnection().then(insertMarkers).catch((err) => baseLogger.error(err))
+  setupMongoConnection().then(() => insertMarkers()).catch((err) => baseLogger.error(err))
 }
+
+
+
+    // for (const id in results) {
+    //   results[id] = {...results[id], id}
+    // }
