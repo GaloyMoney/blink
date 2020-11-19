ca67a007e99c60be3e5310b5d205f457931f5579
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-11-19 15:26:59 -0600

    fix some bugs + some comments

--- src/Lightning.ts
+++ src/Lightning.ts
@@ -120,11 +120,12 @@ export const LightningMixin = (superclass) => class extends superclass {
     //
     // OR: add a lock
 
-
-
     // TODO: do a balance check, so that we don't probe needlessly if the user doesn't have the 
     // probably make sense to used a cached balance here. 
 
+    // TODO: if this is a node we are connected with, we may not even need a probe/round trip to redis
+    // we could handle this from the front end directly.
+    
     const lightningLogger = this.logger.child({ topic: "fee_estimation", protocol: "lightning" })
 
     const { mtokens, max_fee, destination, id, routeHint, messages, cltv_delta, features } = await this.validate(params, lightningLogger)
