e0b86456372205120d68b24daf8505b845d0e0fe
Nicolas Burtey <nicolas.burtey@gmail.com>
2020-12-03 09:36:00 -0600

    fix the bug properly

--- src/index.ts
+++ src/index.ts
@@ -106,10 +106,8 @@ const resolvers = {
         value = lastCached
       }
     
-      // TODO: there is probably a more efficient method than chunk)
-      // return chunk(value, length)[0]
       // console.log({value})
-      return [value[value.length - 1]]
+      return value.splice(-length)
     },
     earnList: async (_, __, { uid, user }) => {
       const response: Object[] = []
