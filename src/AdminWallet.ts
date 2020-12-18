fd05f734acc8ef58b16bc95c22e2769a70e8c4a0
krtk6160 <krtk.6160@gmail.com>
2020-12-18 19:50:04 +0530

    fix wrong import

--- src/AdminWallet.ts
+++ src/AdminWallet.ts
@@ -1,5 +1,5 @@
 import { filter, sumBy } from "lodash";
-import { accountingExpenses, escrowAccountingPath, lightningAccountingPath, openChannelFees } from "./ledger";
+import { accountingExpenses, escrowAccountingPath, lightningAccountingPath, lndFee } from "./ledger";
 import { InvoiceUser, MainBook, Transaction, User } from "./mongodb";
 import { baseLogger, getAuth } from "./utils";
 import { getBrokerWallet, getFunderWallet, WalletFactory } from "./walletFactory";
