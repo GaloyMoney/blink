b2341a4e7847a23e40469c8876cb499b1d24e179
krtk6160 <krtk.6160@gmail.com>
2021-01-02 21:23:37 +0530

    more debug

--- src/__tests__/01e-set-db-version.ts
+++ src/__tests__/01e-set-db-version.ts
@@ -50,7 +50,7 @@ it('applies version 9 upgrade correctly', async () => {
   const [journalId] = (await Transaction.find({ "accounts": lndFee }, { "_journal": 1 })).map(({ _journal }) => _journal)
 
   console.log(await Transaction.find({ "accounts": lndFee }))
-  await MainBook.void(journalId)
+  await MainBook.void(journalId, "test void")
   console.log(await Transaction.find({ "accounts": lndFee }))
 
   const { balance: expenseBalanceAfterVoid } = await MainBook.balance({
