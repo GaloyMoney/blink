dde8317a5c5f3fbd75b8f5d12bbe45cc6ebd5835
krtk6160 <krtk.6160@gmail.com>
2021-01-14 21:04:43 +0530

    improve test?

--- src/__tests__/02bb-notification.ts
+++ src/__tests__/02bb-notification.ts
@@ -1,7 +1,7 @@
 import { sendBalanceToUser } from "../dailyBalanceNotification";
 import { customerPath } from "../ledger";
 import { quit } from "../lock";
-import { MainBook, setupMongoConnection, User } from "../mongodb";
+import { MainBook, setupMongoConnection, User, Transaction } from "../mongodb";
 import { Price } from "../priceImpl";
 import { baseLogger } from "../utils";
 import { WalletFactory } from "../walletFactory";
@@ -20,17 +20,36 @@ afterAll(async () => {
   jest.restoreAllMocks();
 });
 
+it('tests isUserActive', async () => {
+  const initialActiveUsers = await getActiveUsers()
+  expect(initialActiveUsers.length).toBeGreaterThan(0)
+  
+  for (const activeUserAccountPath of initialActiveUsers) {
+    await Transaction.updateMany({ accounts: activeUserAccountPath }, { "$set": { "timestamp": new Date() } })
+  }
+  const finalNumActiveUsers = (await getActiveUsers()).length
+  expect(finalNumActiveUsers).toBe(0)
+})
+
 it('sends daily balance notification', async () => {
   await sendBalanceToUser()
-  const users = await User.find({})
-  let numActiveUsers = 0
-  for (const user of users) {
-    const userWallet = await WalletFactory({ user, uid: user._id, currency: user.currency, logger: baseLogger })
-    if (await userWallet.isUserActive()) numActiveUsers += 1
-  }
+  const numActiveUsers = (await getActiveUsers()).length
   expect(sendNotification.mock.calls.length).toBe(numActiveUsers)
   for (const [call] of sendNotification.mock.calls) {
     const { balance } = await MainBook.balance({ accounts: customerPath(call.uid) })
     expect(call.body).toBe(`Your balance is \$${price * -balance} (${-balance} sats)`)
   }
 })
+
+const getActiveUsers = async (): Promise<Array<string>> => {
+  const users = await User.find({})
+  const activeUsers = []
+  for (const user of users) {
+    const userWallet = await WalletFactory({ user, uid: user._id, currency: user.currency, logger: baseLogger })
+    if (await userWallet.isUserActive()) {
+      activeUsers.push(userWallet.accountPath)
+    }
+  }
+  return activeUsers
+}
+
