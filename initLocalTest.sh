d7da171834e548a67bca2f565d97c14afc1062d0
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-02-27 09:17:59 -0600

    this is not the root password graphql needs but a normal account

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -169,8 +169,7 @@ helmUpgrade galoy \
   --set tls=$TLS,macaroon=$MACAROON,mongodb.auth.rootPassword=$MONGODB_ROOT_PASSWORD,mongodb.auth.replicaSetKey=$MONGODB_REPLICA_SET_KEY,image.tag=$CIRCLE_SHA1 \
   $INFRADIR/galoy/
 
-kubectlWait app.kubernetes.io/component=mongodb
-kubectlWait app=redis
+kubectlWait app.kubernetes.io/instance=galoy
 
 if [ ${LOCAL} ]
 then
--- src/mongodb.ts
+++ src/mongodb.ts
@@ -15,7 +15,7 @@ export const MainBook = new book("MainBook")
 
 export const setupMongoConnection = async () => {
   const user = process.env.MONGODB_USER ?? "testGaloy"
-  const password = process.env.MONGODB_ROOT_PASSWORD ?? "testGaloy"
+  const password = process.env.MONGODB_PASSWORD ?? "testGaloy"
   const address = process.env.MONGODB_ADDRESS ?? "mongodb"
   const db = process.env.MONGODB_DATABASE ?? "galoy"
 
