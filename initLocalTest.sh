c87f9ed702783f47c743fcd89a5685e927f4993a
Kartik Shah <krtk.6160@gmail.com>
2021-03-21 19:06:12 +0530

    upgrade lnd to 1.1.3

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -8,7 +8,7 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo add galoy https://galoymoney.github.io/charts/
 helm repo update
 
-lndVersion="1.1.2"
+lndVersion="1.1.3"
 
 cd ./charts/galoy && helm dependency build && cd -
 cd ./charts/monitoring && helm dependency build && cd -
