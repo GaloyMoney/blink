dfe0ded5ea46a21905e2990fc8d5fb2c6383dee1
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-04-03 14:07:34 +0200

    this should make it clearer that getTokenFromPhoneIndex is only used for test

--- src/walletFactory.ts
+++ src/walletFactory.ts
@@ -42,29 +42,3 @@ export const getDealerWallet = async ({ logger }) => {
   return WalletFactory({ user: dealer, logger })
 }
 
-// utils function for test
-export const getTokenFromPhoneIndex = async (index) => {
-  const entry = yamlConfig.test_accounts[index]
-  const raw_token = await login({ ...entry, logger: baseLogger })
-  const token = jwt.verify(raw_token, process.env.JWT_SECRET);
-
-  const { uid } = token
-
-  if (entry.username) {
-    await User.findOneAndUpdate({ _id: uid }, { username: entry.username })
-  }
-
-  if (entry.currencies) {
-    await User.findOneAndUpdate({ _id: uid }, { currencies: entry.currencies })
-  }
-
-  if (entry.role) {
-    await User.findOneAndUpdate({ _id: uid }, { role: entry.role })
-  }
-
-  if (entry.title) {
-    await User.findOneAndUpdate({ _id: uid }, { title: entry.title })
-  }
-
-  return token
-}
