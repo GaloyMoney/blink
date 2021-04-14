a705fd8420cdc2853ad5f887d492b1976fd96d11
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-04-14 14:46:18 +0200

    fix yaml path?

--- src/schema.ts
+++ src/schema.ts
@@ -212,7 +212,7 @@ UserSchema.virtual('accountPath').get(function(this: typeof UserSchema) {
 })
 
 UserSchema.virtual('oldEnoughForWithdrawal').get(function(this: typeof UserSchema) {
-  return (Date.now() - this.created_at) > yamlConfig.oldEnoughForWithdrawal
+  return (Date.now() - this.created_at) > yamlConfig.limits.oldEnoughForWithdrawal
 })
 
 UserSchema.methods.limitHit = async function({on_us, amount}: {on_us: boolean, amount: number}) {
