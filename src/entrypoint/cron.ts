bdaa01e8534a0c2ffe92bed0b71d591312e3b84f
Kartik Shah <krtk.6160@gmail.com>
2021-04-18 17:08:32 +0530

    fix date calculation

--- src/entrypoint/cron.ts
+++ src/entrypoint/cron.ts
@@ -12,40 +12,43 @@ const MS_PER_DAY = 864e5
 const updateRoutingFees = async () => {
   const lastDay = await jobSchedule.findOne({})
 
-  const lastDate = new Date(lastDay ? lastDay.lastDay : 0)
+  const lastDate = new Date(lastDay?.lastDay ?? 0)
   lastDate.setUTCHours(0, 0, 0, 0)
   const after = lastDate.toISOString()
 
   const endDate = new Date(Date.now() - MS_PER_DAY);
   endDate.setUTCHours(0, 0, 0, 0)
-  const endDay = endDate.toDateString()
   const before = endDate.toISOString()
-
+  
   if((endDate.getTime() - lastDate.getTime()) / MS_PER_DAY < 1) {
     return
   }
-
+  
   const fees = await getRoutingFees({ lnd, before, after })
-
+  
   const type = "routing_fee"
   const metadata = { type, currency: "BTC", pending: false }
-
+  
   await MainBook.entry("routing fee")
-    .credit(revenueFeePath, fees, { ...metadata })
-    .debit(lndAccountingPath, fees, { ...metadata })
-    .commit()
+  .credit(revenueFeePath, fees, { ...metadata })
+  .debit(lndAccountingPath, fees, { ...metadata })
+  .commit()
 
+  endDate.setDate(endDate.getDate() + 1)
+  const endDay = endDate.toDateString()
   await jobSchedule.findOneAndUpdate({}, { lastDay: endDay }, { upsert: true })
 }
 
 const main = async () => {
   const mongoose = await setupMongoConnection()
-
+  
   await updateEscrows()
   await updateUsersPendingPayment()
-
+  
   const specterWallet = new SpecterWallet({ logger: baseLogger })
   await specterWallet.tentativelyRebalance()
+  
+  await updateRoutingFees()
 
   await mongoose.connection.close()
 
