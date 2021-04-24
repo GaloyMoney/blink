a6d0d9a5dd3aad60b5ff50e95cc484feaa3d1341
Kartik Shah <krtk.6160@gmail.com>
2021-04-25 00:42:41 +0530

    export ioredis client

--- src/redis.ts
+++ src/redis.ts
@@ -1,5 +1,11 @@
 import asyncRedis from "async-redis";
 import redis from "redis"
+import Redis from 'ioredis'
+
+export const ioredis = new Redis({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_IP
+})
 
 export const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_IP)
 let clientAsync 
