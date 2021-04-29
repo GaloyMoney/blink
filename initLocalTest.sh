0337871dcb8a7c00254d079c467fc41f6b5304a3
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-04-29 10:27:21 +0200

    new test

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -1,5 +1,9 @@
 set -e
 
+# for helm < 3.4
+# https://helm.sh/blog/new-location-stable-incubator-charts/
+helm repo add stable https://charts.helm.sh/stable --force-update
+
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
