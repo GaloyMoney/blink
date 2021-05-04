ed4165c1ead3b176433a9fe05125dfc75a119ba4
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-05-04 08:51:30 +0100

    fix test?

--- src/__tests__/08-static-specter.ts
+++ src/__tests__/08-static-specter.ts
@@ -6,21 +6,24 @@ import { btc2sat } from "../utils";
 
 it('deposit amount calculation', async () => {
   const lndBalance = btc2sat(2)
-  const result = SpecterWallet.isRebalanceNeeded({ lndBalance })
-
+  const onChain = btc2sat(1.5)
+  const result = SpecterWallet.isRebalanceNeeded({ lndBalance, onChain })
+  
   // expect(result).toStrictEqual({action: "deposit", sats: 100000000 })  
 })
 
 it('withdraw amount calculation', async () => {
   const lndBalance = btc2sat(.5)
-  const result = SpecterWallet.isRebalanceNeeded({ lndBalance })
-
-  // expect(result).toStrictEqual({action: "withdraw", sats: 50000000 })  
+  const onChain = btc2sat(.2)
+  const result = SpecterWallet.isRebalanceNeeded({ lndBalance, onChain })
+  
+  expect(result).toStrictEqual({action: "withdraw", sats: 50000000 })  
 })
 
 it('not doing anything', async () => {
   const lndBalance = btc2sat(1)
-  const result = SpecterWallet.isRebalanceNeeded({ lndBalance })
+  const onChain = btc2sat(.5)
+  const result = SpecterWallet.isRebalanceNeeded({ lndBalance, onChain })
 
-  // expect(result).toStrictEqual({action: undefined})  
+  expect(result).toStrictEqual({action: undefined})  
 })
