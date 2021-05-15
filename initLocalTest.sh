784823b08e8b6679efd733cd39a7c1c6f73a250e
Kartik Shah <krtk.6160@gmail.com>
2021-05-15 14:01:19 +0530

    upgrade to 0.1.13

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -12,8 +12,8 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo add galoy https://galoymoney.github.io/charts/
 helm repo update
 
-lndVersion="1.1.14"
-bitcoindVersion="0.1.12"
+lndVersion="1.1.16"
+bitcoindVersion="0.1.13"
 
 cd ./charts/galoy
 helm dependency build
