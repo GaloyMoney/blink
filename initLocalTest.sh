76ea0601d8db2206d3914c7abb1b71f4c9f45b16
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-05-20 10:06:43 +0100

    fixing merge

--- initLocalTest.sh
+++ initLocalTest.sh
@@ -12,8 +12,8 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo add galoy https://galoymoney.github.io/charts/
 helm repo update
 
-lndVersion="1.1.14"
-bitcoindVersion="0.1.11"
+lndVersion="1.1.18"
+bitcoindVersion="0.1.15"
 
 cd ./charts/galoy
 helm dependency build
