1bcca1963e679eecd63d05031ab492ac3b1f4c51
Kartik Shah <krtk.6160@gmail.com>
2021-05-30 18:22:23 +0530

    have separate branch for local dev

--- src/redis.ts
+++ src/redis.ts
@@ -1,16 +1,36 @@
 import Redis from "ioredis"
 
-// export const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_IP)
-// let clientAsync 
+let connectionObj = {}
 
-export const redis = new Redis({
-  sentinels: [
-    { host: process.env.REDIS_NODE, port: process.env.REDIS_PORT || 26379 },
-  ],
-  name: "mymaster",
-  natMap: {
-    "172.17.0.12:26379": { host: process.env.REDIS_NODE, port: 26379 },
-    "172.17.0.13:26379": { host: process.env.REDIS_NODE, port: 26379 },
-    "172.17.0.14:26379": { host: process.env.REDIS_NODE, port: 26379 }
+if(process.env.LOCAL === 'true') {
+  const REDIS_0_INTERNAL_IP = `${process.env.REDIS_0_INTERNAL_IP}:6379` 
+  const REDIS_1_INTERNAL_IP = `${process.env.REDIS_1_INTERNAL_IP}:6379`
+  const REDIS_2_INTERNAL_IP = `${process.env.REDIS_2_INTERNAL_IP}:6379`
+  
+  const natMap = {
+    [REDIS_0_INTERNAL_IP]: { host: process.env.REDIS_NODE, port: process.env.REDIS_0_PORT },
+    [REDIS_1_INTERNAL_IP]: { host: process.env.REDIS_NODE, port: process.env.REDIS_1_PORT },
+    [REDIS_2_INTERNAL_IP]: { host: process.env.REDIS_NODE, port: process.env.REDIS_2_PORT }
   }
-});
+  connectionObj = {
+    sentinels: [
+      { host: process.env.REDIS_NODE, port: process.env.REDIS_0_SENTINEL_PORT || 26379 },
+      { host: process.env.REDIS_NODE, port: process.env.REDIS_1_SENTINEL_PORT || 26379 },
+      { host: process.env.REDIS_NODE, port: process.env.REDIS_2_SENTINEL_PORT || 26379 },
+    ],
+    name: "mymaster",
+    natMap,
+  }
+} else {
+  connectionObj = {
+    sentinels: [
+      { host: `${process.env.REDIS_0_DNS}`, port: process.env.REDIS_0_PORT },
+      { host: `${process.env.REDIS_1_DNS}`, port: process.env.REDIS_1_PORT },
+      { host: `${process.env.REDIS_2_DNS}`, port: process.env.REDIS_2_PORT },
+    ],
+    name: "mymaster",
+  }
+}
+
+export const redis = new Redis(connectionObj);
+export const rateLimiterRedis = new Redis({ ...connectionObj, enable_offline_queue: false })
