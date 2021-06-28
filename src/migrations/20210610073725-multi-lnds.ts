0ea5c977691f686039e12bf11a92b363b177f115
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-06-28 12:28:47 +0100

    fix prettier/command unnecessary down path migration

--- src/migrations/20210610073725-multi-lnds.ts
+++ src/migrations/20210610073725-multi-lnds.ts
@@ -16,22 +16,27 @@ module.exports = {
       const onchain = onchain_addresses.map((address) => ({ address, pubkey }))
       await db.collection("users").updateOne({ _id: doc._id }, { $set: { onchain } })
     }
+
+    // TODO, eventually:
+    // await db.collection("invoiceuser").updateMany({}, { $unset: { onchain_addresses: "" } })
   },
 
-  async down(db) {
-    await db.collection("invoiceuser").updateMany({}, { $unset: { pubkey: "" } })
+  // not strictly needed even to roll back.
 
-    const cursor = db.collection("users").find()
-    while (await cursor.hasNext()) {
-      const doc = await cursor.next()
-      const onchain = doc.onchain
-      
-      if (!onchain) {
-        continue
-      }
+  // async down(db) {
+  //   await db.collection("invoiceuser").updateMany({}, { $unset: { pubkey: "" } })
 
-      const onchain_addresses = onchain.map(({address}) => address)
-      await db.collection("users").updateOne({ _id: doc._id }, { $set: { onchain_addresses } })
-    }
-  },
+  //   const cursor = db.collection("users").find()
+  //   while (await cursor.hasNext()) {
+  //     const doc = await cursor.next()
+  //     const onchain = doc.onchain
+
+  //     if (!onchain) {
+  //       continue
+  //     }
+
+  //     const onchain_addresses = onchain.map(({address}) => address)
+  //     await db.collection("users").updateOne({ _id: doc._id }, { $set: { onchain_addresses } })
+  //   }
+  // },
 }
