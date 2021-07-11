b0b3d92732a8e72ecb134c55e07b460ebeaa5be0
Kartik Shah <krtk.6160@gmail.com>
2021-07-11 16:03:16 +0530

    Enable redis password auth (#318)
    * Enable redis password auth
    
    * Fix prettier issue
    
    * Export redis_password in local script

--- exportLocal.sh
+++ exportLocal.sh
@@ -39,6 +39,7 @@ export LNDOUTSIDE2RPCPORT=$(kubectl get services | awk '/lnd-outside-2/ {print $
 
 export MONGODB_ADDRESS="$MINIKUBEIP:"$(kubectl get services | awk '/mongodb/ {print $5}' | grep -Po '27017:\K[0-9]+')
 export MONGODB_PASSWORD=$(kubectl get secret galoy-mongodb -o=go-template='{{index .data "mongodb-password" | base64decode}}')
+export REDIS_PASSWORD=$(kubectl get secret galoy-redis -o=go-template='{{index .data "redis-password" | base64decode}}')
 
 export REDIS_0_SENTINEL_PORT=$(kubectl get services galoy-redis | grep -Po '26379:\K[0-9]+')
 
