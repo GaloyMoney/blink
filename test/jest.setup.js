8645da393b0f4c6fc61be1ed4865b02fb98e3220
Juan P Lopez <dolcalmi@gmail.com>
2021-07-14 12:30:02 -0500

    Update jest config/setup files

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,1 +1,16 @@
+const { redis } = require("src/redis");
+const { setupMongoConnection } = require("src/mongodb")
+
+let mongoose
+
+beforeAll(async () => {
+  mongoose = await setupMongoConnection()
+})
+
+afterAll(async () => {
+  // avoid to use --forceExit
+  redis.disconnect()
+  await mongoose.connection.close()
+})
+
 jest.setTimeout(30000)
