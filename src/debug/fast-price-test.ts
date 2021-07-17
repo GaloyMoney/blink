4e211fc2abced09d1f809cb37914777034b2a473
Juan P Lopez <dolcalmi@gmail.com>
2021-07-17 11:48:11 -0500

    Move fast price test to debug

---
+++ src/debug/fast-price-test.ts
@@ -0,0 +1,17 @@
+/**
+ * how to run:
+ * yarn ts-node src/debug/fast-price-test.ts
+ */
+
+import { Price } from "../priceImpl"
+import { baseLogger } from "../logger"
+
+const main = async () => {
+  const price = new Price({logger: baseLogger})
+  // await price.fastUpdate()
+  return await price.update()
+}
+
+main()
+  .then((o) => console.log(o))
+  .catch((err) => console.log(err))
