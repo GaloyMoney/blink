628491178793c9160073acadebd47c4976eb407b
Juan P Lopez <dolcalmi@gmail.com>
2021-07-17 11:48:11 -0500

    Move fast price test to debug

--- src/debug/create-custom-invoices.ts
+++ src/debug/create-custom-invoices.ts
@@ -14,7 +14,6 @@ const { lnd } = getActiveLnd()
 
 const createCustomInvoice = async () => {
   const username = "abcdef"
-  console.log(username)
   const requestOrg = (await createInvoice({ lnd, description: "abc" })).request
   const decoded = parsePaymentRequest({ request: requestOrg })
   decoded["username"] = username
