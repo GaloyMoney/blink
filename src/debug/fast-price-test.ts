14627c1fb87c4450b8ffd5765cbee6e17a800fc6
Juan P Lopez <dolcalmi@gmail.com>
2021-07-18 14:29:48 -0500

    Move metadata setup to debug cmds, only required by mobile app

--- src/debug/fast-price-test.ts
+++ src/debug/fast-price-test.ts
@@ -5,11 +5,15 @@
 
 import { Price } from "../priceImpl"
 import { baseLogger } from "../logger"
+import { setupMongoConnectionSecondary } from "../mongodb"
 
 const main = async () => {
+  const mongoose = await setupMongoConnectionSecondary()
   const price = new Price({ logger: baseLogger })
   // await price.fastUpdate()
-  return await price.update()
+  const data = await price.update()
+  await mongoose.connection.close()
+  return data
 }
 
 main()
--- test/integration/01-setup/01-metadata.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DbMetadata } from "src/schema"
-
-describe("DbMetadata", () => {
-  it("sets min version for graphql", async () => {
-    await DbMetadata.create({ minBuildNumber: 200, lastBuildNumber: 200 })
-  })
-})
