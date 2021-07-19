9cebba9ab3f5580dce987e6707bd365809388444
Juan P Lopez <dolcalmi@gmail.com>
2021-07-18 23:27:14 -0500

    Move addInvoice and getOnchainFee to its own file

---
+++ test/integration/02-user-wallet/02-fees-onchain.spec.ts
@@ -0,0 +1,24 @@
+import { bitcoindClient, getUserWallet } from "test/helpers"
+
+jest.mock("src/realtimePrice", () => require("test/mocks/realtimePrice"))
+
+let userWallet0, userWallet1
+
+beforeAll(async () => {
+  userWallet0 = await getUserWallet(0)
+  userWallet1 = await getUserWallet(1)
+})
+
+describe("UserWallet - getOnchainFee", () => {
+  it("returns a fee greater than zero for an external address", async () => {
+    const address = await bitcoindClient.getNewAddress()
+    const fee = await userWallet0.getOnchainFee({ address })
+    expect(fee).toBeGreaterThan(0)
+  })
+
+  it("returns zero for an on us address", async () => {
+    const address = await userWallet1.getOnChainAddress()
+    const fee = await userWallet0.getOnchainFee({ address })
+    expect(fee).toBe(0)
+  })
+})
