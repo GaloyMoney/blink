5fd98e45cb48e905a1665b764f31b4918c843b26
Juan P Lopez <dolcalmi@gmail.com>
2021-07-18 23:25:01 -0500

    Add create default wallet

--- test/integration/01-setup/01-funds-bitcoind-lnds.spec.ts
+++ test/integration/01-setup/01-funds-bitcoind-lnds.spec.ts
@@ -1,28 +1,27 @@
-import { baseLogger } from "src/logger"
-import { btc2sat } from "src/utils"
-import { MainBook } from "src/mongodb"
 import { liabilitiesReserve, lndAccountingPath } from "src/ledger/ledger"
+import { baseLogger } from "src/logger"
+import { MainBook } from "src/mongodb"
+import { btc2sat } from "src/utils"
 import {
-  checkIsBalanced,
   lnd1,
   lndOutside1,
   bitcoindClient,
   getChainBalance,
   fundLnd,
   waitUntilSyncAll,
+  checkIsBalanced,
 } from "test/helpers"
 
 jest.mock("src/realtimePrice", () => require("test/mocks/realtimePrice"))
 
 const defaultWallet = ""
 
-beforeEach(async () => {
+afterEach(async () => {
   await waitUntilSyncAll()
 })
 
 describe("Bitcoind", () => {
-  it("should be funded mining 10 blocks", async () => {
-    const numOfBlock = 10
+  it("create default wallet", async () => {
     try {
       const { name } = await bitcoindClient.createWallet(defaultWallet)
       // depends of bitcoind version. needed in < 0.20 but failed in 0.21?
@@ -30,13 +29,15 @@ describe("Bitcoind", () => {
     } catch (error) {
       baseLogger.warn({ error }, "bitcoind wallet already exists")
     }
+    const wallets = await bitcoindClient.listWallets()
+    expect(wallets).toContain("")
+  })
 
+  it("should be funded mining 10 blocks", async () => {
+    const numOfBlock = 10
     const bitcoindAddress = await bitcoindClient.getNewAddress()
     await bitcoindClient.mineAndConfirm(numOfBlock, bitcoindAddress)
     const balance = await bitcoindClient.getBalance()
-
-    // invalid only when there are problems deleting channels and
-    // they get confirmed while mineAndConfirm (101 blocks)
     expect(balance).toBeGreaterThanOrEqual(50 * numOfBlock)
   })
 
@@ -55,7 +56,6 @@ describe("Bitcoind", () => {
     const sats = initialBalance + btc2sat(amount)
     await fundLnd(lnd1, amount)
     const { chain_balance: balance } = await getChainBalance({ lnd: lnd1 })
-
     expect(balance).toBe(sats)
 
     const metadata = { type: "onchain_receipt", currency: "BTC", pending: "false" }
@@ -64,7 +64,6 @@ describe("Bitcoind", () => {
       .credit(liabilitiesReserve, sats, metadata)
       .debit(lndAccountingPath, sats, metadata)
       .commit()
-
     await checkIsBalanced()
   })
 })
