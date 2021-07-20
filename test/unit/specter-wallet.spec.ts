ef2f92a772e62a909465a2d1b0b15026f67db588
vindard <17693119+vindard@users.noreply.github.com>
2021-07-20 15:32:21 -0400

    Convert isRebalanceNeeded to instance method
    This commit converts the method from being static and instantiates
    SpecterWallet as needed to call the method through the code.

--- test/unit/specter-wallet.spec.ts
+++ test/unit/specter-wallet.spec.ts
@@ -1,10 +1,20 @@
 import { redis } from "src/redis"
 import { btc2sat } from "src/utils"
 import { SpecterWallet } from "src/SpecterWallet"
+import { getSpecterWalletConfig } from "src/config"
+import { baseLogger } from "src/logger"
+
+let specterWallet
 
 beforeAll(() => {
   // avoids to use --forceExit and the need of a running redis
   redis.disconnect()
+
+  const specterWalletConfig = getSpecterWalletConfig()
+  specterWallet = new SpecterWallet({
+    logger: baseLogger,
+    config: specterWalletConfig,
+  })
 })
 
 describe("SpecterWallet", () => {
@@ -12,7 +22,7 @@ describe("SpecterWallet", () => {
     it("returns deposit amount calculation", () => {
       const lndBalance = btc2sat(1)
       const onChain = btc2sat(0.8)
-      const result = SpecterWallet.isRebalanceNeeded({ lndBalance, onChain })
+      const result = specterWallet.isRebalanceNeeded({ lndBalance, onChain })
 
       expect(result).toStrictEqual({
         action: "deposit",
@@ -24,7 +34,7 @@ describe("SpecterWallet", () => {
     it("returns withdraw amount calculation", () => {
       const lndBalance = btc2sat(0.2)
       const onChain = btc2sat(0.1)
-      const result = SpecterWallet.isRebalanceNeeded({ lndBalance, onChain })
+      const result = specterWallet.isRebalanceNeeded({ lndBalance, onChain })
 
       expect(result).toStrictEqual({ action: "withdraw", sats: 30000000 })
     })
@@ -32,7 +42,7 @@ describe("SpecterWallet", () => {
     it("returns undefined when no action needed", () => {
       const lndBalance = btc2sat(0.5)
       const onChain = btc2sat(0.5)
-      const result = SpecterWallet.isRebalanceNeeded({ lndBalance, onChain })
+      const result = specterWallet.isRebalanceNeeded({ lndBalance, onChain })
 
       expect(result).toStrictEqual({ action: undefined })
     })
