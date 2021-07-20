c383f9a62d5a98202246f81597aa69a7394e9373
vindard <17693119+vindard@users.noreply.github.com>
2021-07-20 10:21:51 -0400

    Move userWalletConfig into a getter function
    Implement getter function in 'config' module
    and call where needed.
    
    As part of this, a new 'ITransactionLimits'
    interface was created to avoid circular
    dependencies between 'types.ts' and
    'config.ts'.

--- src/walletFactory.ts
+++ src/walletFactory.ts
@@ -1,9 +1,10 @@
 import { TransactionLimits, yamlConfig } from "./config"
+import { getUserWalletConfig, yamlConfig } from "./config"
 import { NotFoundError } from "./error"
 import { LightningUserWallet } from "./LightningUserWallet"
 import { getCurrentPrice } from "./realtimePrice"
 import { User } from "./schema"
-import { Logger, UserWalletConfig } from "./types"
+import { Logger } from "./types"
 import { UserWallet } from "./userWallet"
 
 export const WalletFactory = async ({
@@ -17,6 +18,7 @@ export const WalletFactory = async ({
   const lastPrice = await getCurrentPrice()
   UserWallet.setCurrentPrice(lastPrice)
 
+<<<<<<< HEAD
   const transactionLimits = new TransactionLimits({
     level: user.level,
   })
@@ -26,6 +28,13 @@ export const WalletFactory = async ({
     dustThreshold: yamlConfig.onChainWallet.dustThreshold,
     limits: transactionLimits,
   }
+=======
+  if (user?.role === "dealer") {
+    return new FtxDealerWallet({ user, logger })
+  }
+
+  const userWalletConfig = getUserWalletConfig(user)
+>>>>>>> 47429139 (Move userWalletConfig into a getter function)
 
   return new LightningUserWallet({ user, logger, config: userWalletConfig })
 }
