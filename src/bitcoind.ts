aac416ca4b3d4b981ba15d2edb4dac381c8b5739
Juan <dev@juanaleman.com>
2021-07-27 18:37:55 -0400

    Add more values to the client result types

--- src/bitcoind.ts
+++ src/bitcoind.ts
@@ -27,14 +27,33 @@ type VOut = {
 
 type GetAddressInfoResult = {
   address: string
-  // TODO
+  scriptPubKey: string
+  ismine: boolean
+  iswatchonly: boolean
+  solvable: boolean
+  isscript: boolean
+  ischange: boolean
+  iswitness: boolean
+  // TODO? all avaialable: https://developer.bitcoin.org/reference/rpc/getaddressinfo.html#result
 }
 
 type InWalletTransaction = {
-  amount: number
-  fee: number
-  // TODO
-  txid: string
+  "amount": number
+  "fee": number
+  "confirmations": number
+  "generated": boolean
+  "trusted": boolean
+  "blockhash": string
+  "blockheight": number
+  "blockindex": number
+  "blocktime": number
+  "txid": string
+  "time": number
+  "timereceived": number
+  "comment": string
+  "bip125-replaceable": string
+  "hex": string
+  // TODO? all avaialable: https://developer.bitcoin.org/reference/rpc/gettransaction.html#result
 }
 
 export class BitcoindClient {
