75a095cbbee2f979fa066b59698ad27943553572
Kartik Shah <krtk.6160@gmail.com>
2021-08-03 14:24:24 +0530

    Upgrade lnd chart to v1.3.0

--- scripts/init-local-test.sh
+++ scripts/init-local-test.sh
@@ -12,7 +12,7 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo add galoy https://galoymoney.github.io/charts/
 helm repo update
 
-lndVersion="1.2.12"
+lndVersion="1.3.0"
 bitcoindVersion="0.2.2"
 
 if [ ${LOCAL} ]
