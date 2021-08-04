d975f0ba95a026f6057db7dbf8537be380766c6b
vindard <17693119+vindard@users.noreply.github.com>
2021-08-04 12:14:57 -0400

    Add test for memo spam filter

--- test/integration/02-user-wallet/02-receive-lightning.spec.ts
+++ test/integration/02-user-wallet/02-receive-lightning.spec.ts
@@ -1,6 +1,7 @@
 import { ledger } from "@services/mongodb"
 import { getHash } from "@core/utils"
 import { checkIsBalanced, getUserWallet, lndOutside1, pay } from "test/helpers"
+import { MEMO_SHARING_SATS_THRESHOLD } from "@config/app"
 
 jest.mock("@services/realtime-price", () => require("test/mocks/realtime-price"))
 jest.mock("@services/phone-provider", () => require("test/mocks/phone-provider"))
@@ -40,6 +41,11 @@ describe("UserWallet - Lightning", () => {
     expect(dbTx.memo).toBe(memo)
     expect(dbTx.pending).toBe(false)
 
+    // check that memo is not filtered by spam filter
+    const txns = await userWallet1.getTransactions()
+    const noSpamTxn = txns.filter((txn) => txn.hash === hash)[0]
+    expect(noSpamTxn.description).toBe(memo)
+
     const { BTC: finalBalance } = await userWallet1.getBalances()
     expect(finalBalance).toBe(initBalance1 + sats)
   })
@@ -61,6 +67,35 @@ describe("UserWallet - Lightning", () => {
     expect(dbTx.memo).toBe("")
     expect(dbTx.pending).toBe(false)
 
+    const { BTC: finalBalance } = await userWallet1.getBalances()
+    expect(finalBalance).toBe(initBalance1 + sats)
+  })
+
+  it("receives spam invoice", async () => {
+    // amount below MEMO_SPAM threshold
+    const sats = 100
+    const memo = "THIS MIGHT BE SPAM!!!"
+
+    // confirm that transaction should be filtered
+    expect(sats).toBeLessThan(MEMO_SHARING_SATS_THRESHOLD)
+
+    // process spam transaction
+    const invoice = await userWallet1.addInvoice({ value: sats, memo })
+    const hash = getHash(invoice)
+    await pay({ lnd: lndOutside1, request: invoice })
+    expect(await userWallet1.updatePendingInvoice({ hash })).toBeTruthy()
+
+    // check that spam memo is persisted to database
+    const dbTx = await ledger.getTransactionByHash(hash)
+    expect(dbTx.memo).toBe(memo)
+
+    // check that spam memo is filtered from transaction description
+    const txns = await userWallet1.getTransactions()
+    const spamTxn = txns.filter((txn) => txn.hash === hash)[0]
+    expect(dbTx.type).toBe("invoice")
+    expect(spamTxn.description).toBe(dbTx.type)
+
+    // confirm expected final balance
     const { BTC: finalBalance } = await userWallet1.getBalances()
     expect(finalBalance).toBe(initBalance1 + sats)
   })
