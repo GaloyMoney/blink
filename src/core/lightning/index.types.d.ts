670fa584ced884ecc1f91c8e399a8c51d0e99ff3
vindard <17693119+vindard@users.noreply.github.com>
2021-08-17 11:47:24 -0400

    Move wallet use-case types to 'app' folder

--- src/core/lightning/index.types.d.ts
+++ src/core/lightning/index.types.d.ts
@@ -19,31 +19,3 @@ interface IPaymentRequest {
   memo?: string
   isReward?: boolean
 }
-
-type AddInvoiceSelfArgs = {
-  walletId: WalletId
-  amount: Satoshis
-  memo?: string
-}
-
-type AddInvoiceNoAmountSelfArgs = {
-  walletId: WalletId
-  memo?: string
-}
-
-type AddInvoiceRecipientArgs = {
-  username: Username
-  amount: Satoshis
-  memo?: string
-}
-
-type AddInvoiceNoAmountRecipientArgs = {
-  username: Username
-  memo?: string
-}
-
-type BaseAddInvoiceArgs = {
-  amount: Satoshis
-  memo: string
-  walletInvoiceCreateFn: WalletInvoiceFactoryCreateMethod
-}
