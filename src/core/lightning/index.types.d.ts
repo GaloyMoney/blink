57d3d8a53232ad9d04f5145d0bde5db8290747f7
vindard <17693119+vindard@users.noreply.github.com>
2021-08-17 11:15:50 -0400

    Clean up types and validation for addInvoice... args

--- src/core/lightning/index.types.d.ts
+++ src/core/lightning/index.types.d.ts
@@ -22,12 +22,28 @@ interface IPaymentRequest {
 
 type AddInvoiceSelfArgs = {
   walletId: WalletId
+  amount: Satoshis
   memo?: string
-  amount?: Satoshis
+}
+
+type AddInvoiceNoAmountSelfArgs = {
+  walletId: WalletId
+  memo?: string
 }
 
 type AddInvoiceRecipientArgs = {
   username: Username
+  amount: Satoshis
   memo?: string
-  amount?: Satoshis
+}
+
+type AddInvoiceNoAmountRecipientArgs = {
+  username: Username
+  memo?: string
+}
+
+type BaseAddInvoiceArgs = {
+  amount: Satoshis
+  memo: string
+  walletInvoiceCreateFn: WalletInvoiceFactoryCreateMethod
 }
