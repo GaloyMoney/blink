297e0cf830f59ad03fdfa3300f03f9627799e5a9
vindard <17693119+vindard@users.noreply.github.com>
2021-08-18 17:07:44 -0400

    Swap 'findByUsername' in tests

--- test/integration/graphql/admin/mutations.spec.ts
+++ test/integration/graphql/admin/mutations.spec.ts
@@ -2,6 +2,7 @@ import { graphql } from "graphql"
 
 import { User } from "@services/mongoose/schema"
 import { gqlAdminSchema } from "@graphql/admin"
+import { AccountsRepository, UsersRepository } from "@services/mongoose"
 
 let user
 
@@ -33,9 +34,19 @@ describe("GraphQLMutationRoot", () => {
     `
     const result = await graphql(gqlAdminSchema, query, {})
     const { errors, data } = result
-    const updatedUser = await User.getUserByUsername("tester")
+
+    const usersRepo = UsersRepository()
+    const updatedUser = await usersRepo.findByUsername("tester" as Username)
     expect(errors).toBeNull
-    expect(updatedUser.level).toEqual(2)
+    expect(updatedUser).not.toBeInstanceOf(Error)
+    if (updatedUser instanceof Error) throw updatedUser
+
+    const accountsRepo = AccountsRepository()
+    const defaultAccount = await accountsRepo.findById(updatedUser.defaultAccountId)
+    expect(defaultAccount).not.toBeInstanceOf(Error)
+    if (defaultAccount instanceof Error) throw defaultAccount
+
+    expect(defaultAccount.level).toEqual(2)
     expect(data?.userUpdateLevel.userDetails.level).toEqual("TWO")
   })
 
@@ -59,10 +70,20 @@ describe("GraphQLMutationRoot", () => {
     `
     const result = await graphql(gqlAdminSchema, query, {})
     const { errors, data } = result
-    const updatedUser = await User.getUserByUsername("tester")
     expect(errors).toBeNull
+
+    const usersRepo = UsersRepository()
+    const updatedUser = await usersRepo.findByUsername("tester" as Username)
+    expect(updatedUser).not.toBeInstanceOf(Error)
+    if (updatedUser instanceof Error) throw updatedUser
+
+    const accountsRepo = AccountsRepository()
+    const defaultAccount = await accountsRepo.findById(updatedUser.defaultAccountId)
+    expect(defaultAccount).not.toBeInstanceOf(Error)
+    if (defaultAccount instanceof Error) throw defaultAccount
+
     expect(data?.userUpdateStatus.userDetails.id).toEqual(updatedUser.id)
-    expect(updatedUser.status).toEqual("locked")
+    expect(defaultAccount.status).toEqual("locked")
   })
 
   it("exposes merchantUpdateMapInfo", async () => {
@@ -85,8 +106,13 @@ describe("GraphQLMutationRoot", () => {
     `
     const result = await graphql(gqlAdminSchema, query, {})
     const { errors, data } = result
-    const updatedUser = await User.getUserByUsername("tester")
     expect(errors).toBeNull
+
+    const usersRepo = UsersRepository()
+    const updatedUser = await usersRepo.findByUsername("tester" as Username)
+    expect(updatedUser).not.toBeInstanceOf(Error)
+    if (updatedUser instanceof Error) throw updatedUser
+
     expect(data?.merchantUpdateMapInfo.userDetails.id).toEqual(updatedUser.id)
     expect(updatedUser.title).toEqual("MapTest")
     expect(updatedUser.coordinate).toEqual({ longitude: 1, latitude: -1 })
