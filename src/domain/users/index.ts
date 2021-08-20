2386bde5fb292f6cc543b7597f7a1a3e20156e98
vindard <17693119+vindard@users.noreply.github.com>
2021-08-20 08:03:46 -0400

    Change username regex from method to constant value

--- src/domain/users/index.ts
+++ src/domain/users/index.ts
@@ -5,11 +5,10 @@ export const UserLanguage = {
   ES_SV: "es",
 } as const
 
-export const getUsernameRegex = () => /(?!^(1|3|bc1|lnbc1))^[0-9a-z_]+$/i
+export const UsernameRegex = /(?!^(1|3|bc1|lnbc1))^[0-9a-z_]+$/i
 
 export const checkedToUsername = (username): Username | ValidationError => {
-  const regexUsername = getUsernameRegex()
-  if (!username.match(regexUsername)) {
+  if (!username.match(UsernameRegex)) {
     return new InvalidUsername(username)
   }
   return username as Username
