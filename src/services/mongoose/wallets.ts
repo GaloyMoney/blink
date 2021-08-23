d9b95127f0dfe930b1898d50f7279e01f06d5f00
vindard <17693119+vindard@users.noreply.github.com>
2021-08-23 09:27:51 -0400

    Implement 'findByWalletname' method on WalletsRepository

--- src/services/mongoose/wallets.ts
+++ src/services/mongoose/wallets.ts
@@ -4,6 +4,7 @@ import {
   RepositoryError,
 } from "@domain/errors"
 import { User } from "@services/mongoose/schema"
+import { caseInsensitiveRegex } from "./users"
 
 export const WalletsRepository = (): IWalletsRepository => {
   const findById = async (walletId: WalletId): Promise<Wallet | RepositoryError> => {
@@ -12,21 +13,22 @@ export const WalletsRepository = (): IWalletsRepository => {
       if (!result) {
         return new CouldNotFindError()
       }
-      const onChainAddressIdentifiers = result.onchain.map(({ pubkey, address }) => {
-        return {
-          pubkey: pubkey as Pubkey,
-          address: address as OnChainAddress,
-        }
-      })
-      const onChainAddresses = () =>
-        onChainAddressIdentifiers.map(({ address }) => address)
+      return resultToWallet(result)
+    } catch (err) {
+      return new UnknownRepositoryError(err)
+    }
+  }
 
-      return {
-        id: walletId,
-        depositFeeRatio: result.depositFeeRatio,
-        onChainAddresses,
-        onChainAddressIdentifiers,
+  const findByWalletname = async (
+    username: Walletname,
+  ): Promise<Wallet | RepositoryError> => {
+    try {
+      const result = await User.findOne({ username: caseInsensitiveRegex(username) })
+      if (!result) {
+        return new CouldNotFindError()
       }
+
+      return resultToWallet(result)
     } catch (err) {
       return new UnknownRepositoryError(err)
     }
@@ -34,5 +36,34 @@ export const WalletsRepository = (): IWalletsRepository => {
 
   return {
     findById,
+    findByWalletname,
+  }
+}
+
+const resultToWallet = (result: UserType): Wallet => {
+  const walletId = result.id as WalletId
+
+  const walletname: Walletname = result.username
+    ? (result.username as Walletname)
+    : ("" as Walletname)
+
+  const depositFeeRatio = result.depositFeeRatio as DepositFeeRatio
+
+  const onChainAddressIdentifiers = result.onchain
+    ? result.onchain.map(({ pubkey, address }) => {
+        return {
+          pubkey: pubkey as Pubkey,
+          address: address as OnChainAddress,
+        }
+      })
+    : []
+  const onChainAddresses = () => onChainAddressIdentifiers.map(({ address }) => address)
+
+  return {
+    id: walletId,
+    depositFeeRatio,
+    walletname,
+    onChainAddressIdentifiers,
+    onChainAddresses,
   }
 }
