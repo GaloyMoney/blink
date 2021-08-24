fa1105b3906a756256922e0df25c3e182ac6ad4d
vindard <17693119+vindard@users.noreply.github.com>
2021-08-24 09:42:32 -0400

    Switch checkedToUsername to checkedToWalletname

--- src/domain/users/index.ts
+++ src/domain/users/index.ts
@@ -1,15 +1,4 @@
-import { InvalidUsername } from "@domain/errors"
-
 export const UserLanguage = {
   EN_US: "en",
   ES_SV: "es",
 } as const
-
-export const UsernameRegex = /(?!^(1|3|bc1|lnbc1))^[0-9a-z_]+$/i
-
-export const checkedToUsername = (username): Username | ValidationError => {
-  if (!username.match(UsernameRegex)) {
-    return new InvalidUsername(username)
-  }
-  return username as Username
-}
