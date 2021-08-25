f4a2725235ea1e8f9303d98e5d66e69ad5921779
Juan P Lopez <dolcalmi@gmail.com>
2021-08-24 21:53:56 -0500

    Fix notification require path

--- test/integration/servers/trigger.spec.ts
+++ test/integration/servers/trigger.spec.ts
@@ -29,7 +29,7 @@ jest.mock("@services/realtime-price", () => require("test/mocks/realtime-price")
 jest.mock("@services/phone-provider", () => require("test/mocks/phone-provider"))
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { sendNotification } = require("@core/notifications/notification")
+const { sendNotification } = require("@services/notifications/notification")
 
 beforeAll(async () => {
   await bitcoindClient.loadWallet({ filename: "outside" })
