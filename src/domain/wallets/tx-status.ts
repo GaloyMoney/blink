82d90d960fdda196ce2e420d044d52f476473c1b
vindard <17693119+vindard@users.noreply.github.com>
2021-08-25 16:29:49 -0400

    Add TxStatus type/value to wallets domain

---
+++ src/domain/wallets/tx-status.ts
@@ -0,0 +1,5 @@
+export const TxStatus = {
+  Pending: "pending",
+  Success: "success",
+  Failure: "failure",
+} as const
--- src/graphql/types/scalar/tx-status.ts
+++ src/graphql/types/scalar/tx-status.ts
@@ -1,11 +1,12 @@
 import { GT } from "@graphql/index"
+import { TxStatus as DomainTxStatus } from "@domain/wallets"
 
 const TxStatus = new GT.Enum({
   name: "TxStatus",
   values: {
-    PENDING: {},
-    SUCCESS: {},
-    FAILURE: {},
+    PENDING: { value: DomainTxStatus.Pending },
+    SUCCESS: { value: DomainTxStatus.Success },
+    FAILURE: { value: DomainTxStatus.Failure },
   },
 })
 
