4468cdb820757fe204cf9eda5953413e3e252e4e
vindard <17693119+vindard@users.noreply.github.com>
2021-08-25 21:08:39 -0400

    Add 'lnInvoicePaymentSend' resolver to graphql

---
+++ src/graphql/root/mutation/ln-payment-send.ts
@@ -0,0 +1,40 @@
+import { GT } from "@graphql/index"
+import LnInvoicePaymentStatusPayload from "@graphql/types/payload/ln-invoice-payment-status"
+import LnInvoicePaymentRequest from "@graphql/types/scalar/ln-invoice-payment-request"
+import Memo from "@graphql/types/scalar/memo"
+
+const LnInvoicePaymentInput = new GT.Input({
+  name: "LnInvoicePaymentInput",
+  fields: () => ({
+    paymentRequest: { type: GT.NonNull(LnInvoicePaymentRequest) },
+    memo: { type: Memo },
+  }),
+})
+
+const lnInvoicePaymentSendMutation = GT.Field({
+  type: GT.NonNull(LnInvoicePaymentStatusPayload),
+  args: {
+    input: { type: GT.NonNull(LnInvoicePaymentInput) },
+  },
+  resolve: async (_, __, { wallet, paymentRequest, memo }) => {
+    for (const input of [memo, paymentRequest]) {
+      if (input instanceof Error) {
+        return { errors: [{ message: input.message }] }
+      }
+    }
+
+    try {
+      const status = await wallet.pay({ invoice: paymentRequest, memo })
+      return {
+        errors: [],
+        status,
+      }
+    } catch (err) {
+      return {
+        errors: [{ message: err.message }],
+      }
+    }
+  },
+})
+
+export default lnInvoicePaymentSendMutation
