f627db56e7a7da26b78a73b6c5db779a3cea481a
vindard <17693119+vindard@users.noreply.github.com>
2021-08-26 15:55:23 -0400

    Add receiveLnTx method to Ledger Service

--- src/domain/accounts/index.types.d.ts
+++ src/domain/accounts/index.types.d.ts
@@ -1,3 +1,6 @@
+declare const currencyRatioSymbol: unique symbol
+type CurrencyRatio = number & { [currencyRatioSymbol]: never }
+
 type AccountLevel =
   typeof import("./index").AccountLevel[keyof typeof import("./index").AccountLevel]
 
@@ -10,6 +13,11 @@ type Account = {
   status: AccountStatus
   walletIds: WalletId[]
 }
+
+type Currencies = {
+  id: Currency
+  ratio: CurrencyRatio
+}[]
 
 interface IAccountsRepository {
   findById(accountId: AccountId): Promise<Account | RepositoryError>
