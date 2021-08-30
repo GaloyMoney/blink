15fc9b831d55580e6883e7069d9426109960b9af
vindard <17693119+vindard@users.noreply.github.com>
2021-08-30 15:40:15 -0400

    Implement updatePendingPayments use-case

--- src/app/wallets/index.ts
+++ src/app/wallets/index.ts
@@ -1,6 +1,7 @@
 export * from "./get-transactions-for-wallet"
 export * from "./update-on-chain-receipt"
 export * from "./update-pending-invoices"
+export * from "./update-pending-payments"
 export * from "./add-invoice-for-wallet"
 
 import { WalletsRepository } from "@services/mongoose"
