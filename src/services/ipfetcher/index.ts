2252070982a4af3c5500aebbbde258bcb05afe32
bodymindarts <justin@galoy.io>
2021-09-01 14:58:12 +0200

    Add IpFetcherService

---
+++ src/services/ipfetcher/index.ts
@@ -0,0 +1,19 @@
+import axios from "axios"
+import { UnknownIpFetcherServiceError } from "@domain/ipfetcher"
+import { PROXY_CHECK_APIKEY } from "@config/app"
+
+export const IpFetcher = (): IIpFetcherService => {
+  const fetchIPInfo = async (ip: string): Promise<IPInfo | IpFetcherServiceError> => {
+    try {
+      const { data } = await axios.get(
+        `http://proxycheck.io/v2/${ip}?key=${PROXY_CHECK_APIKEY}&vpn=1&asn=1`,
+      )
+      return { ...data[ip], status: data.status }
+    } catch (err) {
+      return new UnknownIpFetcherServiceError(err)
+    }
+  }
+  return {
+    fetchIPInfo,
+  }
+}
