b434539d6ae80fe74294bb85879b33d51e1ef662
Samer Buna <samer@jscomplete.com>
2021-09-01 21:02:18 -0700

    Implement the walletNameAvailable query

---
+++ src/app/wallets/wallet-name-available.ts
@@ -0,0 +1,17 @@
+import { CouldNotFindError } from "@domain/errors"
+import { checkedToWalletName } from "@domain/wallets"
+import { WalletsRepository } from "@services/mongoose"
+
+export const walletNameAvailable = async (
+  walletName: string,
+): Promise<boolean | Error> => {
+  const checkedWalletName = checkedToWalletName(walletName)
+  if (checkedWalletName instanceof Error) return checkedWalletName
+
+  const walletsRepo = WalletsRepository()
+
+  const wallet = await walletsRepo.findByWalletName(checkedWalletName)
+  if (wallet instanceof CouldNotFindError) return true
+  if (wallet instanceof Error) return wallet
+  return false
+}
--- src/graphql/main/queries.ts
+++ src/graphql/main/queries.ts
@@ -1,12 +1,14 @@
 import MeQuery from "@graphql/root/query/me"
 import { GT } from "@graphql/index"
 import GlobalsQuery from "@graphql/root/query/globals"
+import WalletNameAvailableQuery from "@graphql/root/query/wallet-name-available"
 
 const QueryType = new GT.Object({
   name: "Query",
   fields: () => ({
     globals: GlobalsQuery,
     me: MeQuery,
+    walletNameAvailable: WalletNameAvailableQuery,
   }),
 })
 
