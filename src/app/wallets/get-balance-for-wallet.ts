29efc9f096b7a31a33f99a22d9e9d83e1a0cbe44
vindard <17693119+vindard@users.noreply.github.com>
2021-09-03 14:21:48 -0400

    Implement 'getBalanceForWallet' use-case

---
+++ src/app/wallets/get-balance-for-wallet.ts
@@ -0,0 +1,31 @@
+import { toLiabilitiesAccountId } from "@domain/ledger"
+import { LedgerService } from "@services/ledger"
+import * as Wallets from "@app/wallets"
+
+export const getBalanceForWallet = async ({
+  walletId,
+  lock,
+  logger,
+}: {
+  walletId: WalletId
+  lock?: DistributedLock
+  logger: Logger
+}): Promise<Balances | ApplicationError> => {
+  Wallets.updatePendingInvoices({
+    walletId: walletId,
+    lock,
+    logger,
+  })
+  const result = await Wallets.updatePendingPayments({
+    walletId: walletId,
+    lock,
+    logger,
+  })
+  if (result instanceof Error) throw result
+
+  const liabilitiesAccountId = toLiabilitiesAccountId(walletId)
+  const balance = await LedgerService().getAccountBalance(liabilitiesAccountId)
+  if (balance instanceof Error) return balance
+
+  return { BTC: balance, totalInBtc: balance }
+}
