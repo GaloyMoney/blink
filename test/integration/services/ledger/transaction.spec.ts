074ad142878b1d764d0d901b94dcfa44742ef83e
Juan P Lopez <dolcalmi@gmail.com>
2021-09-03 11:24:22 -0500

    Fix lock issues in ledger test

--- test/integration/services/ledger/transaction.spec.ts
+++ test/integration/services/ledger/transaction.spec.ts
@@ -3,6 +3,7 @@ import { baseLogger } from "@services/logger"
 import { UserWallet } from "@core/user-wallet"
 import { WalletFactory } from "@core/wallet-factory"
 import { ledger, setupMongoConnection } from "@services/mongodb"
+import { clearAccountLocks } from "test/helpers/redis"
 
 jest.mock("@services/realtime-price", () => require("test/mocks/realtime-price"))
 
@@ -12,6 +13,7 @@ let fullWalletBTC, fullWalletUSD
 let dealerPath, lndAccountingPath
 
 beforeAll(async () => {
+  await clearAccountLocks()
   mongoose = await setupMongoConnection()
   dealerPath = await ledger.dealerAccountPath()
   lndAccountingPath = ledger.lndAccountingPath
