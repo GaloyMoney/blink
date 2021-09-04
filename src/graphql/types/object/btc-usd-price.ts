bae77b42a34600b97887c76e56c8536d770485f0
Thomas Shelton <twshelton@hrtoolbox.com>
2021-09-03 22:31:58 -0400

    Draft - types and mutation for price list for graph

--- src/graphql/types/object/btc-usd-price.ts
+++ src/graphql/types/object/btc-usd-price.ts
@@ -1,12 +1,11 @@
 import { GT } from "@graphql/index"
-import Timestamp from "../scalar/timestamp"
 
 const BtcUsdPrice = new GT.Object({
   name: "BtcUsdPrice",
   fields: () => ({
-    id: { type: GT.NonNullID }, // Currently a date
-    timestamp: { type: GT.NonNull(Timestamp) },
-    price: { type: GT.NonNull(GT.Float) }, // Currently price of 1 sat
+    price: {
+      type: GT.NonNull(SatAmount),
+    },
   }),
 })
 
