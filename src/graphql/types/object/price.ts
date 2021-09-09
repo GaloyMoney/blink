9c26a632953184abc8645203569655629d18401b
Samer Buna <samer@jscomplete.com>
2021-09-09 12:15:37 -0700

    Rename BigInt => SafeInt

--- src/graphql/types/object/price.ts
+++ src/graphql/types/object/price.ts
@@ -1,12 +1,12 @@
 import { GT } from "@graphql/index"
-import BigInt from "../scalar/big-int"
+import SafeInt from "../scalar/safe-int"
 
 import ExchangeCurrencyUnit from "../scalar/exchange-currency-unit"
 
 const Price = new GT.Object({
   name: "Price",
   fields: () => ({
-    base: { type: GT.NonNull(BigInt) },
+    base: { type: GT.NonNull(SafeInt) },
     offset: { type: GT.NonNull(GT.Int) },
     currencyUnit: { type: GT.NonNull(ExchangeCurrencyUnit) },
     formattedAmount: { type: GT.NonNull(GT.String) },
--- src/graphql/types/scalar/safe-int.ts
+++ src/graphql/types/scalar/safe-int.ts
@@ -3,13 +3,13 @@ import { GT } from "@graphql/index"
 const MAX_INT = Number.MAX_SAFE_INTEGER
 const MIN_INT = Number.MIN_SAFE_INTEGER
 
-const BigInt = new GT.Scalar({
-  name: "BigInt",
+const SafeInt = new GT.Scalar({
+  name: "SafeInt",
   description:
-    "The `BigInt` scalar type represents non-fractional signed whole numeric values. " +
-    "BigInt can represent values between -(2^53) + 1 and 2^53 - 1",
-  serialize: coerceBigInt,
-  parseValue: coerceBigInt,
+    "The `SafeInt` scalar type represents non-fractional signed whole numeric values. " +
+    "SafeInt can represent values between -(2^53) + 1 and 2^53 - 1",
+  serialize: coerceSafeInt,
+  parseValue: coerceSafeInt,
   parseLiteral(ast) {
     if (ast.kind === GT.Kind.INT) {
       const num = parseInt(ast.value, 10)
@@ -21,23 +21,23 @@ const BigInt = new GT.Scalar({
   },
 })
 
-function coerceBigInt(value) {
+function coerceSafeInt(value) {
   if (value === "") {
     throw new Error(
-      "BigInt cannot represent non 53-bit signed integer value: (empty string)",
+      "SafeInt cannot represent non 53-bit signed integer value: (empty string)",
     )
   }
   const num = Number(value)
   if (num !== value || num > MAX_INT || num < MIN_INT) {
     throw new Error(
-      "BigInt cannot represent non 53-bit signed integer value: " + String(value),
+      "SafeInt cannot represent non 53-bit signed integer value: " + String(value),
     )
   }
   const int = Math.floor(num)
   if (int !== num) {
-    throw new Error("BigInt cannot represent non-integer value: " + String(value))
+    throw new Error("SafeInt cannot represent non-integer value: " + String(value))
   }
   return int
 }
 
-export default BigInt
+export default SafeInt
