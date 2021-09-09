72d25cc5818bab8741f1d5de75b4287547b81b31
Samer Buna <samer@jscomplete.com>
2021-09-09 07:29:31 -0700

    Implement the quizQuestions root query (#547)

---
+++ src/graphql/root/query/quiz-questions.ts
@@ -0,0 +1,16 @@
+import { GT } from "@graphql/index"
+
+import QuizQuestion from "@graphql/types/object/quiz-question"
+import { onboardingEarn } from "@config/app"
+
+const QuizQuestionsQuery = GT.Field({
+  type: GT.List(QuizQuestion),
+  resolve: async () => {
+    return Object.entries(onboardingEarn).map(([id, earnAmount]) => ({
+      id,
+      earnAmount,
+    }))
+  },
+})
+
+export default QuizQuestionsQuery
