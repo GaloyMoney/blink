82afb104611d053996e82c47804e6697764ab44a
vindard <17693119+vindard@users.noreply.github.com>
2021-09-23 17:12:20 -0400

    Refactor LnFeeCalculator constructor param types

--- src/domain/bitcoin/lightning/fee-calculator.ts
+++ src/domain/bitcoin/lightning/fee-calculator.ts
@@ -2,12 +2,9 @@ import { FEECAP, FEEMIN } from "@services/lnd/auth"
 import { toSats } from ".."
 
 export const LnFeeCalculator = (
-  { feeCap = FEECAP as Satoshis, feeMin = FEEMIN as Satoshis } = {
+  { feeCap = FEECAP, feeMin = FEEMIN } = {
     feeCap: FEECAP,
     feeMin: FEEMIN,
-  } as {
-    feeCap?: Satoshis
-    feeMin?: Satoshis
   },
 ): LnFeeCalculator => {
   return {
