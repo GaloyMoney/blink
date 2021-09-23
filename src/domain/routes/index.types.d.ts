ba5f2f97a481e6f1eb94f1f81751d83f6208dee1
vindard <17693119+vindard@users.noreply.github.com>
2021-09-23 08:08:04 -0400

    Refactor 'Route' type
    Import type from library and compose instead of defining explicitly.

--- src/domain/routes/index.types.d.ts
+++ src/domain/routes/index.types.d.ts
@@ -1,66 +1,15 @@
-type RawProbedHop = {
-  /** Standard Format Channel Id */
-  channel: string
-  /** Channel Capacity Tokens */
-  channel_capacity: number
-  /** Fee */
-  fee: number
-  /** Fee Millitokens */
-  fee_mtokens: string
-  /** Forward Tokens */
-  forward: number
-  /** Forward Millitokens */
-  forward_mtokens: string
-  /** Public Key Hex */
-  public_key?: string
-  /** Timeout Block Height */
-  timeout: number
-}
+type ProbeForRouteArgs = import("lightning").ProbeForRouteArgs
+type RawHopWithNumbers = NonNullable<ProbeForRouteArgs["routes"]>[number][number]
 
-type RawProbedRoute = {
-  /** Route Confidence Score Out Of One Million Number */
-  confidence?: number
-  /** Route Fee Tokens Rounded Down Number */
-  fee: number
-  /** Route Fee Millitokens String */
-  fee_mtokens: string
-  hops: RawProbedHop[]
-  messages?: {
-    /** Message Type Number String */
-    type: string
-    /** Message Raw Value Hex Encoded String */
-    value: string
-  }[]
-  /** Total Fee-Inclusive Millitokens String */
-  mtokens: string
-  /** Payment Identifier Hex String */
-  payment?: string
-  /** Payment Forwarding Fee Rounded Up Tokens Number */
-  safe_fee: number
-  /** Payment Tokens Rounded Up Number */
-  safe_tokens: number
-  /** Timeout Block Height Number */
-  timeout: number
-  /** Total Fee-Inclusive Tokens Rounded Down Number */
-  tokens: number
-  /** Total Millitokens String */
-  total_mtokens?: string
-}
+type PayViaPaymentDetailsArgs = import("lightning").PayViaPaymentDetailsArgs
+type RawHopWithStrings = NonNullable<PayViaPaymentDetailsArgs["routes"]>[number][number]
 
-type CachedRoute = RawProbedRoute & { pubkey: Pubkey }
+type PayViaRoutesArgs = import("lightning").PayViaRoutesArgs
+type RawRouteFromProbe = PayViaRoutesArgs["routes"][number]
+type RawRoute = { safe_fee: number } & RawRouteFromProbe
+type Route = { roundedUpFee: Satoshis; roundedDownFee: Satoshis }
 
-type RawInvoiceHop = {
-  /** Base Routing Fee In Millitokens */
-  base_fee_mtokens?: string
-  /** Standard Format Channel Id */
-  channel?: string
-  /** CLTV Blocks Delta */
-  cltv_delta?: number
-  /** Fee Rate In Millitokens Per Million */
-  fee_rate?: number
-  /** Forward Edge Public Key Hex */
-  public_key: string
-}
+type CachedRoute = { pubkey: Pubkey; route: RawRoute }
 
 interface IRoutesRepository {
   findByPaymentHash: ({
