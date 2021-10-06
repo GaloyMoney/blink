1d2b60df8e72f8a3bf4b163a8d23137cd0dac06a
vindard <17693119+vindard@users.noreply.github.com>
2021-10-06 19:50:11 -0400

    Fix 'walletPublicId' mapping in LedgerTx translation
    This property isn't being set on ledger transactions at this point in
    the codebase as yet. Will be done in a later change along with a schema
    change and migration.

--- src/services/ledger/index.ts
+++ src/services/ledger/index.ts
@@ -611,7 +611,7 @@ const translateToLedgerTx = (tx): LedgerTransaction => ({
   journalId: tx._journal.toString(),
   lnMemo: tx.memo,
   username: tx.username,
-  walletPublicId: tx.walletPublicId,
+  walletPublicId: tx.walletPublicId || null,
   memoFromPayer: tx.memoPayer,
   paymentHash: tx.hash,
   pubkey: tx.pubkey,
