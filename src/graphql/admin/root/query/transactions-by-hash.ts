ba66afdedc6af711573f695957e42887f2c89c7b
Samer Buna <samer@jscomplete.com>
2021-10-06 09:22:42 -0700

    Use PaymentHash type for the new admin query

--- src/graphql/admin/root/query/transactions-by-hash.ts
+++ src/graphql/admin/root/query/transactions-by-hash.ts
@@ -1,11 +1,13 @@
 import { GT } from "@graphql/index"
+
 import * as Wallets from "@app/wallets"
 import ITransaction from "@graphql/types/abstract/transaction"
+import PaymentHash from "@graphql/types/scalar/payment-hash"
 
 const TransactionsByHashQuery = GT.Field({
   type: GT.List(ITransaction),
   args: {
-    hash: { type: GT.NonNull(GT.String) },
+    hash: { type: GT.NonNull(PaymentHash) },
   },
   resolve: async (_, { hash }) => {
     if (hash instanceof Error) throw hash
