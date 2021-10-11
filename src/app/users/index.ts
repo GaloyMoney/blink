6d72b8d6167e1fdb6ec1788898fdd6ec5a46ff91
Samer Buna <samer@jscomplete.com>
2021-10-11 14:21:35 -0700

    Implement a contactByUsername field under user

--- src/app/users/index.ts
+++ src/app/users/index.ts
@@ -1,3 +1,4 @@
 export * from "./get-user"
 export * from "./username-available"
 export * from "./update-contact-alias"
+export * from "./get-contact-by-username"
--- src/domain/errors.ts
+++ src/domain/errors.ts
@@ -23,5 +23,6 @@ export class InsufficientBalanceError extends ValidationError {}
 export class InvalidTargetConfirmations extends ValidationError {}
 export class LimitsExceededError extends ValidationError {}
 export class NoUserForUsernameError extends ValidationError {}
+export class NoContactForUsernameError extends ValidationError {}
 
 export class TwoFALimitsExceededError extends LimitsExceededError {}
