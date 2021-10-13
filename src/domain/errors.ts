ff6ad9a1fbdf97e78ba6f5cadd78ed91a98c2cea
vindard <17693119+vindard@users.noreply.github.com>
2021-10-13 09:34:48 -0400

    Refactor error types for send payment amounts

--- src/domain/errors.ts
+++ src/domain/errors.ts
@@ -26,6 +26,8 @@ export class LimitsExceededError extends ValidationError {}
 export class NoUserForUsernameError extends ValidationError {}
 export class NoContactForUsernameError extends ValidationError {}
 export class IncorrectUseCaseError extends ValidationError {}
+export class LnPaymentRequestNonZeroAmountRequiredError extends ValidationError {}
+export class LnPaymentRequestZeroAmountRequiredError extends ValidationError {}
 export class NoWalletExistsForUserError extends ValidationError {}
 
 export class TwoFALimitsExceededError extends LimitsExceededError {}
