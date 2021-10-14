0a5abf6d66200426ae493866bc32ef49d775cd19
vindard <17693119+vindard@users.noreply.github.com>
2021-10-14 13:26:36 -0400

    Refactor error-map to include exhaustiveness check

--- src/app/error-map.ts
+++ src/app/error-map.ts
@@ -2,60 +2,64 @@ import {
   CustomError,
   TransactionRestrictedError,
   LightningPaymentError,
-  UnknownClientError,
   TwoFAError,
   NotFoundError,
   SelfPaymentError,
   InsufficientBalanceError,
   DbError,
 } from "@core/error"
-import { UnknownLightningServiceError } from "@domain/bitcoin/lightning"
-import {
-  AlreadyPaidError,
-  CouldNotFindUserError,
-  CouldNotFindWalletInvoiceError,
-  InsufficientBalanceError as DomainInsufficientBalanceError,
-  IntraledgerLimitsExceededError,
-  SelfPaymentError as DomainSelfPaymentError,
-  WithdrawalLimitsExceededError,
-} from "@domain/errors"
-import { UnknownLedgerError } from "@domain/ledger"
-import { TwoFANewCodeNeededError } from "@domain/twoFA"
+
+import * as DomainErrors from "@domain/errors"
+import * as DomainLightningErrors from "@domain/bitcoin/lightning/errors"
+import * as DomainLedgerErrors from "@domain/ledger/errors"
+import * as DomainTwoFAErrors from "@domain/twoFA/errors"
 import { baseLogger } from "@services/logger"
 
-export const mapError = (error: Error): CustomError => {
-  switch (error.constructor) {
-    case WithdrawalLimitsExceededError:
+const AllDomainErrors = {
+  ...DomainErrors,
+  ...DomainLightningErrors,
+  ...DomainLedgerErrors,
+  ...DomainTwoFAErrors,
+}
+
+function assertUnreachable(x: never): never {
+  throw new Error(`Didn't expect to get here with ${x}`)
+}
+
+export const mapError = (error: ApplicationError): CustomError => {
+  const errorName = error.constructor.name as keyof typeof AllDomainErrors
+  switch (errorName) {
+    case "WithdrawalLimitsExceededError":
       return new TransactionRestrictedError(error.message, { logger: baseLogger })
 
-    case IntraledgerLimitsExceededError:
+    case "IntraledgerLimitsExceededError":
       return new TransactionRestrictedError(error.message, { logger: baseLogger })
 
-    case TwoFANewCodeNeededError:
+    case "TwoFANewCodeNeededError":
       return new TwoFAError(error.message, { logger: baseLogger })
 
-    case AlreadyPaidError:
+    case "AlreadyPaidError":
       return new LightningPaymentError(error.message, { logger: baseLogger })
 
-    case CouldNotFindWalletInvoiceError:
+    case "CouldNotFindWalletInvoiceError":
       return new LightningPaymentError(error.message, { logger: baseLogger })
 
-    case CouldNotFindUserError:
+    case "CouldNotFindUserError":
       return new NotFoundError(error.message, { logger: baseLogger })
 
-    case DomainSelfPaymentError:
+    case "SelfPaymentError":
       return new SelfPaymentError(error.message, { logger: baseLogger })
 
-    case DomainInsufficientBalanceError:
+    case "InsufficientBalanceError":
       return new InsufficientBalanceError(error.message, { logger: baseLogger })
 
-    case UnknownLedgerError:
+    case "UnknownLedgerError":
       return new DbError(error.message, { logger: baseLogger, level: "fatal" })
 
-    case UnknownLightningServiceError:
+    case "UnknownLightningServiceError":
       return new LightningPaymentError(error.message, { logger: baseLogger })
 
     default:
-      return new UnknownClientError(error.message, { logger: baseLogger })
+      return assertUnreachable(errorName)
   }
 }
