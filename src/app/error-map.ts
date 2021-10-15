d37f1c0788448fbcc37b40ddbf627a6273f070ff
vindard <17693119+vindard@users.noreply.github.com>
2021-10-15 07:52:12 -0400

    Add user-friendly error messages to error map

--- src/app/error-map.ts
+++ src/app/error-map.ts
@@ -28,14 +28,16 @@ const assertUnreachable = (x: never): never => {
 }
 
 export const mapError = (error: ApplicationError): CustomError => {
-  let message: string
+  let message = ""
   const errorName = error.constructor.name as keyof typeof AllDomainErrors
   switch (errorName) {
     case "WithdrawalLimitsExceededError":
-      return new TransactionRestrictedError(error.message, { logger: baseLogger })
+      message = error.message
+      return new TransactionRestrictedError(message, { logger: baseLogger })
 
     case "IntraledgerLimitsExceededError":
-      return new TransactionRestrictedError(error.message, { logger: baseLogger })
+      message = error.message
+      return new TransactionRestrictedError(message, { logger: baseLogger })
 
     case "TwoFANewCodeNeededError":
       message = "Need a 2FA code to proceed with the payment"
@@ -44,155 +46,161 @@ export const mapError = (error: ApplicationError): CustomError => {
       })
 
     case "AlreadyPaidError":
-      return new LightningPaymentError(error.message, { logger: baseLogger })
+      message = "Invoice is already paid"
+      return new LightningPaymentError(message, { logger: baseLogger })
 
     case "CouldNotFindWalletInvoiceError":
-      return new LightningPaymentError(error.message, { logger: baseLogger })
+      message = `User tried to pay invoice with hash ${error.message}, but it does not exist`
+      return new LightningPaymentError(message, { logger: baseLogger })
 
-    case "CouldNotFindUserError":
-      return new NotFoundError(error.message, { logger: baseLogger })
+    case "CouldNotFindUserFromIdError":
+      message = `User does not exist for id ${error.message}`
+      return new NotFoundError(message, { logger: baseLogger })
+
+    case "CouldNotFindUserFromUsernameError":
+      message = `User does not exist for username ${error.message}`
+      return new NotFoundError(message, { logger: baseLogger })
+
+    case "CouldNotFindUserFromWalletIdError":
+      message = `User does not exist for wallet-id ${error.message}`
+      return new NotFoundError(message, { logger: baseLogger })
 
     case "SelfPaymentError":
-      return new SelfPaymentError(error.message, { logger: baseLogger })
+      message = "User tried to pay themselves"
+      return new SelfPaymentError(message, { logger: baseLogger })
 
     case "InsufficientBalanceError":
-      return new InsufficientBalanceError(error.message, { logger: baseLogger })
+      return new InsufficientBalanceError(message, { logger: baseLogger })
 
     case "UnknownLedgerError":
-      return new DbError(error.message, { logger: baseLogger, level: "fatal" })
+      return new DbError(message, { logger: baseLogger, level: "fatal" })
 
     case "UnknownLightningServiceError":
-      return new LightningPaymentError(error.message, { logger: baseLogger })
+      return new LightningPaymentError(message, { logger: baseLogger })
 
     // ----------
     // Unhandled below here
     // ----------
 
+    case "CouldNotFindUserError":
+      return new UnknownClientError(message)
+
     case "TwoFAError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "TwoFAValidationError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "UnknownTwoFAError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LedgerError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LedgerServiceError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LightningError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LnInvoiceDecodeError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LightningServiceError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "CouldNotDecodeReturnedPaymentRequest":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "InvoiceNotFoundError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LnPaymentPendingError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LnAlreadyPaidError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "NoValidNodeForPubkeyError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "PaymentNotFoundError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "InconsistentDataError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "AuthorizationError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "RepositoryError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "UnknownRepositoryError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "PersistError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "DuplicateError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "CouldNotFindError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "ValidationError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "InvalidSatoshiAmount":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "SatoshiAmountRequiredError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "InvalidUsername":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "InvalidPublicWalletId":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LessThanDustThresholdError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "InvalidTargetConfirmations":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "NoContactForUsernameError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LnPaymentRequestNonZeroAmountRequiredError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LnPaymentRequestZeroAmountRequiredError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "NoWalletExistsForUserError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "LimitsExceededError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "TwoFALimitsExceededError":
-      return new UnknownClientError(error.message)
-
-    case "CouldNotFindUserFromIdError":
-      return new UnknownClientError(error.message)
-
-    case "CouldNotFindUserFromUsernameError":
-      return new UnknownClientError(error.message)
-
-    case "CouldNotFindUserFromWalletIdError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "CouldNotFindWalletFromIdError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "CouldNotFindWalletFromUsernameError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "CouldNotFindWalletFromOnChainAddressError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "CouldNotFindWalletFromPublicIdError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     case "CouldNotFindWalletFromOnChainAddressesError":
-      return new UnknownClientError(error.message)
+      return new UnknownClientError(message)
 
     default:
       return assertUnreachable(errorName)
