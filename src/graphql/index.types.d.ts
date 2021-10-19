b0a644aa0318af609b927034c5b6648ba1e9be85
vindard <17693119+vindard@users.noreply.github.com>
2021-10-19 09:49:30 -0400

    Add new graphql-level errors

--- src/graphql/index.types.d.ts
+++ src/graphql/index.types.d.ts
@@ -15,3 +15,15 @@ type PricePointType = {
   timestamp: number
   price: PriceType
 }
+
+type LogFn = import("pino").LogFn
+
+type CustomErrorData = {
+  message?: string
+  logger: Logger
+  level?: "fatal" | "error" | "warn" | "info" | "debug" | "trace" | "silent"
+  forwardToClient?: boolean
+  [key: string]: unknown
+}
+
+type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
