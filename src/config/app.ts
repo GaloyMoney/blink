98917ad1ab2e46f238c95428c9beacee33eaedd4
Justin Carter <justin@galoy.io>
2021-10-21 20:53:50 +0200

    Add radix to parsing jaegerPort
    Co-authored-by: Juan P Lopez <dolcalmi@gmail.com>

--- src/config/app.ts
+++ src/config/app.ts
@@ -39,7 +39,7 @@ export const yamlConfig = _.merge(defaultConfig, customConfig)
 
 export const tracingConfig = {
   jaegerHost: process.env.JAEGER_HOST || "localhost",
-  jaegerPort: parseInt(process.env.JAEGER_PORT || "6832"),
+  jaegerPort: parseInt(process.env.JAEGER_PORT || "6832", 10),
   tracingServiceName: process.env.TRACING_SERVICE_NAME || "galoy-dev",
 }
 export const MEMO_SHARING_SATS_THRESHOLD = yamlConfig.limits.memoSharingSatsThreshold
