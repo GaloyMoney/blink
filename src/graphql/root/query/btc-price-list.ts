46436d3ebbc2e3062db6d9638be218d9ce2e5069
Juan P Lopez <dolcalmi@gmail.com>
2021-10-22 00:14:25 -0500

    Add candle size filter to btc price query

--- src/graphql/root/query/btc-price-list.ts
+++ src/graphql/root/query/btc-price-list.ts
@@ -28,6 +28,23 @@ const rangeStartDateMS: (string: typeof priceRangeValues[number]) => number = (r
   }
 }
 
+const getCandleSizeByRange: (string: typeof priceRangeValues[number]) => number = (
+  range,
+) => {
+  switch (range) {
+    case "ONE_DAY":
+      return 1
+    case "ONE_WEEK":
+      return 4
+    case "ONE_MONTH":
+      return 24
+    case "ONE_YEAR":
+      return 24 * 7
+    case "FIVE_YEARS":
+      return 24 * 30
+  }
+}
+
 const BtcPriceListQuery = GT.Field({
   type: GT.List(PricePoint),
   args: {
@@ -39,29 +56,34 @@ const BtcPriceListQuery = GT.Field({
     const { range } = args
 
     const rangeStart = rangeStartDateMS(range)
+    const candleSize = getCandleSizeByRange(range)
 
     if (!rangeStart) {
       throw new Error("Invalid range")
     }
 
     const hourlyPrices = await getHourlyPrice({ logger })
+    const hourlyReversePrices = hourlyPrices.reverse()
 
     const prices: PricePointType[] = []
 
-    for (const price of hourlyPrices.reverse()) {
+    for (let i = 0; i < hourlyReversePrices.length; i++) {
+      const price = hourlyReversePrices[i]
       if (1000 * price.id < rangeStart) {
         break
       }
-      const btcPriceInCents = price.o * 100 * 10 ** 8
-      prices.unshift({
-        timestamp: price.id,
-        price: {
-          formattedAmount: btcPriceInCents.toString(),
-          base: Math.round(btcPriceInCents * 10 ** 4),
-          offset: 4,
-          currencyUnit: "USDCENT",
-        },
-      })
+      if (i % candleSize === 0) {
+        const btcPriceInCents = price.o * 100 * 10 ** 8
+        prices.unshift({
+          timestamp: price.id,
+          price: {
+            formattedAmount: btcPriceInCents.toString(),
+            base: Math.round(btcPriceInCents * 10 ** 4),
+            offset: 4,
+            currencyUnit: "USDCENT",
+          },
+        })
+      }
     }
 
     // Add the current price as the last item in the array
