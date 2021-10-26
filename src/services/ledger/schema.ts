b8846a5544583566d55334c84dc8d3eb5a37a097
vindard <17693119+vindard@users.noreply.github.com>
2021-10-26 13:59:01 -0400

    Define schema type for transaction 'payment' property

--- src/services/ledger/schema.ts
+++ src/services/ledger/schema.ts
@@ -6,6 +6,77 @@ const Schema = mongoose.Schema
 const ledgerTransactionTypes = Object.keys(LedgerTransactionType).map(
   (txType) => LedgerTransactionType[txType],
 )
+
+const hopSchema = new Schema({
+  channel: String,
+  channel_capacity: {
+    type: Number,
+    min: 0,
+  },
+  fee: {
+    type: Number,
+    default: 0,
+  },
+  fee_mtokens: String,
+  forward: {
+    type: Number,
+    min: 0,
+  },
+  forward_mtokens: String,
+  public_key: String,
+  timeout: {
+    type: Number,
+    min: 0,
+  },
+})
+
+const pathSchema = new Schema({
+  fee: {
+    type: Number,
+    default: 0,
+  },
+  fee_mtokens: String,
+  hops: [hopSchema],
+  mtokens: String,
+  payment: String,
+  timeout: {
+    type: Number,
+    min: 0,
+  },
+  tokens: {
+    type: Number,
+    min: 0,
+  },
+  total_mtokens: String,
+})
+
+const paymentSchema = new Schema({
+  status: { type: String, enum: ["settled", "failed", "pending"], required: true },
+  confirmedAt: Date,
+  createdAt: Date,
+  destination: String,
+  milliSatsFee: {
+    type: Number,
+    default: 0,
+  },
+  paymentHash: String,
+  milliSatsAmount: {
+    type: Number,
+    min: 0,
+  },
+  paths: [pathSchema],
+  paymentRequest: String,
+  roundedUpFee: {
+    type: Number,
+    default: 0,
+  },
+  secret: String,
+  amount: {
+    type: Number,
+    min: 0,
+  },
+})
+
 const transactionSchema = new Schema({
   hash: {
     type: Schema.Types.String,
@@ -111,7 +182,7 @@ const transactionSchema = new Schema({
     min: 0,
   },
   meta: Schema.Types.Mixed,
-  payment: Schema.Types.Mixed,
+  payment: paymentSchema,
   datetime: Date,
   account_path: [String],
   accounts: String,
