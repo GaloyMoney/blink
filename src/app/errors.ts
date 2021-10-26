76d8b46b87e803b2c64415119b01310ec3e0d58a
vindard <17693119+vindard@users.noreply.github.com>
2021-10-26 00:11:20 -0400

    Refactor 'ApplicationError' type to derive from union of all app errors
    Refactor ApplicationError definition to couple it more closely to the
    full set of individual errors expected in the error-map switch.

---
+++ src/app/errors.ts
@@ -0,0 +1,23 @@
+import * as DomainErrors from "@domain/errors"
+import * as LedgerErrors from "@domain/ledger/errors"
+import * as OnChainErrors from "@domain/bitcoin/onchain/errors"
+import * as LightningErrors from "@domain/bitcoin/lightning/errors"
+import * as PriceServiceErrors from "@domain/price/errors"
+import * as TwoFAErrors from "@domain/twoFA/errors"
+import * as LockServiceErrors from "@domain/lock/errors"
+import * as IpFetcherErrors from "@domain/ipfetcher/errors"
+import * as AccountErrors from "@domain/accounts/errors"
+import * as NotificationsErrors from "@domain/notifications/errors"
+
+export const ApplicationErrors = {
+  ...DomainErrors,
+  ...LedgerErrors,
+  ...OnChainErrors,
+  ...LightningErrors,
+  ...PriceServiceErrors,
+  ...TwoFAErrors,
+  ...LockServiceErrors,
+  ...IpFetcherErrors,
+  ...AccountErrors,
+  ...NotificationsErrors,
+} as const
