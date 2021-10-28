0404cff1f1b1474bcc6877c64ac3df211fcdabad
bodymindarts <justin@galoy.io>
2021-10-28 11:10:15 +0200

    Another REPO_PATH fix

--- ci/tasks/helpers.sh
+++ ci/tasks/helpers.sh
@@ -4,7 +4,7 @@ function unpack_deps() {
 
   tar -zxvf bundled-deps/bundled-deps-*.tgz ./node_modules/ ./yarn.lock -C ${REPO_PATH:-repo}/ > /dev/null
 
-  pushd repo > /dev/null
+  pushd ${REPO_PATH:-repo} > /dev/null
 
   if [[ "$(git status -s -uno)" != "" ]]; then
     echo "Extracting deps has created a diff - deps are not in sync"
