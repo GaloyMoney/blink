018c4246e871f65150f76556a5839de9e15c7fa1
vindard <17693119+vindard@users.noreply.github.com>
2021-11-01 09:24:47 -0400

    Add onChainAddressCreateAttempt limiter to createOnChainAddress method

--- src/app/wallets/create-on-chain-address.ts
+++ src/app/wallets/create-on-chain-address.ts
@@ -1,11 +1,20 @@
 import { OnChainService } from "@services/lnd/onchain-service"
 import { TxDecoder } from "@domain/bitcoin/onchain"
-import { BTC_NETWORK } from "@config/app"
+import { BTC_NETWORK, getOnChainAddressCreateAttemptLimits } from "@config/app"
 import { WalletOnChainAddressesRepository, WalletsRepository } from "@services/mongoose"
+import { RedisRateLimitService } from "@services/rate-limit"
+import { RateLimitPrefix } from "@domain/rate-limit"
+import {
+  OnChainAddressCreateRateLimiterExceededError,
+  RateLimiterExceededError,
+} from "@domain/rate-limit/errors"
 
 export const createOnChainAddress = async (
   walletId: WalletId,
 ): Promise<OnChainAddress | ApplicationError> => {
+  const limitOk = await checkOnChainAddressWalletIdLimits(walletId)
+  if (limitOk instanceof Error) return limitOk
+
   const onChainService = OnChainService(TxDecoder(BTC_NETWORK))
   if (onChainService instanceof Error) return onChainService
 
@@ -29,4 +38,18 @@ export const createOnChainAddressByWalletPublicId = async (
   const wallet = await wallets.findByPublicId(walletPublicId)
   if (wallet instanceof Error) return wallet
   return createOnChainAddress(wallet.id)
+}
+
+const checkOnChainAddressWalletIdLimits = async (
+  walletId: WalletId,
+): Promise<true | RateLimiterExceededError> => {
+  const onChainAddressCreateAttempt = getOnChainAddressCreateAttemptLimits()
+  const limiter = RedisRateLimitService({
+    keyPrefix: RateLimitPrefix.onChainAddressCreate,
+    limitOptions: onChainAddressCreateAttempt,
+  })
+  const limitOk = await limiter.consume(walletId)
+  if (limitOk instanceof RateLimiterExceededError)
+    return new OnChainAddressCreateRateLimiterExceededError()
+  return limitOk
 }
