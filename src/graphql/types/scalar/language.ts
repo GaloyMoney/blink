5a89ce5a05ea4aaba4963eb484b1e2edbf95ed3a
Samer Buna <samer@jscomplete.com>
2021-10-31 19:28:55 -0700

    Change Language into a scalar with standard code values

--- src/graphql/types/scalar/language.ts
+++ src/graphql/types/scalar/language.ts
@@ -1,11 +1,35 @@
 import { GT } from "@graphql/index"
+import { UserInputError } from "apollo-server-errors"
 
-const Language = new GT.Enum({
+// TODO: Update database values and this map
+const languages = {
+  "": "DEFAULT",
+  "en": "en-US",
+  "es": "es-SV",
+}
+
+const Language = new GT.Scalar({
   name: "Language",
-  values: {
-    EN_US: { value: "en" },
-    ES_SV: { value: "es" },
+  serialize(value) {
+    return languages[value]
+  },
+  parseValue(value) {
+    return validLanguageValue(value)
+  },
+  parseLiteral(ast) {
+    if (ast.kind === GT.Kind.STRING) {
+      return validLanguageValue(ast.value)
+    }
+    return new UserInputError("Invalid type for Language")
   },
 })
+
+function validLanguageValue(value) {
+  const languageEntry = Object.entries(languages).find(([, v]) => v === value)
+  if (languageEntry) {
+    return languageEntry[0]
+  }
+  return new UserInputError("Invalid value for Language")
+}
 
 export default Language
