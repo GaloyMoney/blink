bf3e5faff046ffbed9c1aad6e2bf7fc4491e6527
vindard <17693119+vindard@users.noreply.github.com>
2021-11-03 15:05:37 -0400

    Add types to LnPaymentRepo 'update' method

--- src/services/mongoose/ln-payments.ts
+++ src/services/mongoose/ln-payments.ts
@@ -1,17 +1,18 @@
+import { toSats } from "@domain/bitcoin"
 import { UnknownRepositoryError } from "@domain/errors"
 import { LnPayment } from "@services/lnd/schema"
 
 export const LnPaymentsRepository = (): ILnPaymentsRepository => {
   const update = async (
     payment: LnPaymentLookup,
-  ): Promise<LnPaymentLookup | RepositoryError> => {
+  ): Promise<LnPayment | RepositoryError> => {
     try {
       const result = await LnPayment.findOneAndUpdate(
         { paymentHash: payment.paymentHash },
         payment,
-        { upsert: true },
+        { upsert: true, new: true },
       )
-      return result
+      return lnPaymentFromRaw(result)
     } catch (err) {
       return new UnknownRepositoryError(err)
     }
@@ -21,3 +22,19 @@ export const LnPaymentsRepository = (): ILnPaymentsRepository => {
     update,
   }
 }
+
+const lnPaymentFromRaw = (result: LnPaymentType): LnPayment => ({
+  id: result.id as PaymentId,
+  status: result.status as PaymentStatus,
+  confirmedAt: result.confirmedAt as Date | undefined,
+  createdAt: result.createdAt as Date,
+  destination: result.destination as Pubkey,
+  milliSatsFee: result.milliSatsFee as MilliSatoshis,
+  paymentHash: result.paymentHash as PaymentHash,
+  milliSatsAmount: result.milliSatsAmount as MilliSatoshis,
+  paths: result.paths as RawPaths,
+  paymentRequest: result.paymentRequest as EncodedPaymentRequest | undefined,
+  roundedUpFee: toSats(result.roundedUpFee),
+  secret: result.secret as PaymentSecret,
+  amount: toSats(result.amount),
+})
