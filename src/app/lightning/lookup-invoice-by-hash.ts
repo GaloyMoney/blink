ff8f35e08ca8488b80060cc8e554ab9f0668a2b7
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-11-06 21:48:15 +0000

    reorg import to the right layer

--- src/app/lightning/lookup-invoice-by-hash.ts
+++ src/app/lightning/lookup-invoice-by-hash.ts
@@ -1,13 +1,21 @@
 import { LndService } from "@services/lnd"
+import { WalletInvoicesRepository } from "@services/mongoose"
+import { RepositoryError } from "@domain/errors"
 
-export const lookupInvoiceByHashAndPubkey = async ({
+export const lookupInvoiceByHash = async ({
   paymentHash,
-  pubkey,
 }: {
   paymentHash: PaymentHash
-  pubkey: Pubkey
 }): Promise<LnInvoiceLookup | ApplicationError> => {
   const lndService = LndService()
   if (lndService instanceof Error) return lndService
+
+  const invoicesRepo = WalletInvoicesRepository()
+  const walletInvoice = await invoicesRepo.findByPaymentHash(paymentHash)
+
+  if (walletInvoice instanceof RepositoryError) return walletInvoice
+
+  const { pubkey } = walletInvoice
+
   return lndService.lookupInvoice({ paymentHash, pubkey })
 }
--- src/graphql/admin/root/query/lightning-invoice.ts
+++ src/graphql/admin/root/query/lightning-invoice.ts
@@ -1,9 +1,7 @@
 import { GT } from "@graphql/index"
-import { lookupInvoiceByHashAndPubkey } from "@app/lightning"
+import { lookupInvoiceByHash } from "@app/lightning"
 import PaymentHash from "@graphql/types/scalar/payment-hash"
 import LightningInvoice from "@graphql/admin/types/object/lightning-invoice"
-import { WalletInvoicesRepository } from "@services/mongoose"
-import { RepositoryError } from "@domain/errors"
 
 const LightningInvoiceQuery = GT.Field({
   type: GT.NonNull(LightningInvoice),
@@ -13,19 +11,10 @@ const LightningInvoiceQuery = GT.Field({
   resolve: async (_, { hash }) => {
     if (hash instanceof Error) throw hash
 
-    const invoicesRepo = WalletInvoicesRepository()
-    const walletInvoice = await invoicesRepo.findByPaymentHash(hash)
-
-    if (walletInvoice instanceof RepositoryError) return walletInvoice
-
-    const { pubkey } = walletInvoice
-
-    const paymentHash = hash as PaymentHash /* FIXME */
-
-    const lightningInvoice = await lookupInvoiceByHashAndPubkey({
-      paymentHash,
-      pubkey,
+    const lightningInvoice = await lookupInvoiceByHash({
+      paymentHash: hash,
     })
+
     if (lightningInvoice instanceof Error) throw lightningInvoice
 
     return lightningInvoice
