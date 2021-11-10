3bd1737d7f593d6fb6ab546c446aac0f3417ac6c
vindard <17693119+vindard@users.noreply.github.com>
2021-11-10 09:19:40 -0600

    Log payment metadata errors instead of returning in 'update-payment'

--- src/app/wallets/update-pending-payments.ts
+++ src/app/wallets/update-pending-payments.ts
@@ -100,7 +100,6 @@ const updatePendingPayment = async ({
           { error: lnPaymentLookup },
           "we couldn't update payment data to our database",
         )
-        return persistedPayment
       }
 
       if (status === PaymentStatus.Settled) {
