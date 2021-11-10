1766443975c79e85c7f09d6d6bfb6815afe8d4c5
daviroo <david.borthwick@gmail.com>
2021-11-10 09:57:27 +0000

    Removed TODOs

--- src/graphql/types/abstract/transaction.ts
+++ src/graphql/types/abstract/transaction.ts
@@ -25,11 +25,9 @@ export const transactionInterfaceFields = {
   },
   settlementAmount: {
     type: GT.NonNull(SatAmount),
-    //TODO: Does the amount include fees.
     description: "Amount of sats paid by the user.",
   },
   settlementFee: {
-    //TODO: Fees will be incorrect if probing fails and there is a fee refund.
     type: GT.NonNull(SatAmount),
   },
   settlementPrice: {
@@ -39,10 +37,7 @@ export const transactionInterfaceFields = {
       return {
         formattedAmount: settlementUsdPerSatInCents.toString(),
         base: Math.round(settlementUsdPerSatInCents * 10 ** 4),
-        //TODO: What is offset?  What is difference between offset + sats/usd?
         offset: 4,
-        //TODO: Is current offset 0.000001 to the dollar which would be 4 for offset and 2 for usd/cent?
-        // Why not use offset 6 and USD?
         currencyUnit: "USDCENT",
       }
     },
