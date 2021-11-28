ad05135d680eea6e40c972d9ee59c892d12e7407
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-11-28 20:35:09 +0000

    migrate admin use cases

--- src/app/accounts/index.ts
+++ src/app/accounts/index.ts
@@ -13,9 +13,19 @@ export * from "./add-earn"
 
 const accounts = AccountsRepository()
 
+const accounts = AccountsRepository()
+
 export const getAccount = async (accountId: AccountId) => {
-  const accounts = AccountsRepository()
   return accounts.findById(accountId)
+}
+
+export const getWalletPublicIdFromUsername = async (
+  username: Username,
+): Promise<WalletPublicId | Error> => {
+  const account = await accounts.findByUsername(username)
+  if (account instanceof Error) return account
+
+  return account.walletPublicId
 }
 
 export const getAccountByApiKey = async (
