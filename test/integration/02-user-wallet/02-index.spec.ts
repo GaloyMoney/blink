09d01e1116fe2024de1a158c351132bf5d37ec17
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-11-28 20:35:09 +0000

    migrate admin use cases

--- test/integration/02-user-wallet/02-index.spec.ts
+++ test/integration/02-user-wallet/02-index.spec.ts
@@ -152,6 +152,7 @@ describe("UserWallet", () => {
         id: userWallet2.user.id,
         status: "locked",
       })
+      console.log({ user }, "account3")
       if (user instanceof Error) {
         throw user
       }
