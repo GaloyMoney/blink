8c225112539521b31434a09beaa2109289819e81
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-12-04 14:24:26 +0000

    feat: make it easier to understand the diff of a .graphql
    by computing another generated file which is lexicographic based
    to remove the non deterministic aspect of the .graphql file creation
    
    still keeping the standard schema.graphql as this is the one
    that make most sense for an API user

--- src/graphql/admin/index.ts
+++ src/graphql/admin/index.ts
@@ -1,4 +1,4 @@
-import { GraphQLSchema, printSchema } from "graphql"
+import { GraphQLSchema, lexicographicSortSchema, printSchema } from "graphql"
 
 import { ALL_INTERFACE_TYPES } from "@graphql/types"
 
@@ -6,14 +6,17 @@ import QueryType from "./queries"
 import MutationType from "./mutations"
 import { isDev } from "@core/utils"
 
+if (isDev) {
+  import("@services/fs").then(({ writeSDLFile }) => {
+    writeSDLFile(
+      __dirname + "/schema.graphql",
+      printSchema(lexicographicSortSchema(gqlAdminSchema)),
+    )
+  })
+}
+
 export const gqlAdminSchema = new GraphQLSchema({
   query: QueryType,
   mutation: MutationType,
   types: ALL_INTERFACE_TYPES,
 })
-
-if (isDev) {
-  import("@services/fs").then(({ writeSDLFile }) => {
-    writeSDLFile(__dirname + "/schema.graphql", printSchema(gqlAdminSchema))
-  })
-}
