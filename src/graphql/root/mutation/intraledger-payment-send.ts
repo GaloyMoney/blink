btcpuertorico/galoy

complete addEarn more-efficient use case

src/graphql/root/mutation/intraledger-payment-send.ts
@@ -1,18 +1,18 @@
-import { getWalletByPublicId, intraledgerPaymentSend } from "@app/wallets"
 import { getUsernameFromWalletPublicId } from "@app/users"
+import { intraledgerPaymentSendUsername } from "@app/wallets"
 import { checkedToWalletPublicId } from "@domain/wallets"
+import { mapError } from "@graphql/error-map"
 import { GT } from "@graphql/index"
-
 import PaymentSendPayload from "@graphql/types/payload/payment-send"
 import Memo from "@graphql/types/scalar/memo"
 import SatAmount from "@graphql/types/scalar/sat-amount"
 import WalletId from "@graphql/types/scalar/wallet-id"
-import { mapError } from "@graphql/error-map"
+import { WalletsRepository } from "@services/mongoose"
 
 const IntraLedgerPaymentSendInput = new GT.Input({
   name: "IntraLedgerPaymentSendInput",
   fields: () => ({
-    walletId: { type: GT.NonNull(WalletId) },
+    walletId: { type: GT.NonNull(WalletId) }, // TODO: rename senderWalletId
     recipientWalletId: { type: GT.NonNull(WalletId) },
     amount: { type: GT.NonNull(SatAmount) },
     memo: { type: Memo },
@@ -32,9 +32,9 @@ const IntraLedgerPaymentSendMutation = GT.Field({
       }
     }
 
-    const wallet = await getWalletByPublicId(walletId)
-    if (wallet instanceof Error) {
-      const appErr = mapError(wallet)
+    const senderWalletPublicId = checkedToWalletPublicId(walletId)
+    if (senderWalletPublicId instanceof Error) {
+      const appErr = mapError(senderWalletPublicId)
       return { errors: [{ message: appErr.message }] }
     }
 
@@ -44,18 +44,26 @@ const IntraLedgerPaymentSendMutation = GT.Field({
       return { errors: [{ message: appErr.message }] }
     }
 
+    // FIXME: this logic below should be in app/ not graphql/
+    const senderWallet = await WalletsRepository().findByPublicId(senderWalletPublicId)
+    if (senderWallet instanceof Error) {
+      const appErr = mapError(senderWallet)
+      return { errors: [{ message: appErr.message }] }
+    }
+
+    // TODO: recipientUsername should become optional for IntraLedgerPaymentSendInput
     const recipientUsername = await getUsernameFromWalletPublicId(recipientWalletPublicId)
     if (recipientUsername instanceof Error) {
       const appErr = mapError(recipientUsername)
       return { errors: [{ message: appErr.message }] }
     }
 
-    const status = await intraledgerPaymentSend({
+    const status = await intraledgerPaymentSendUsername({
       recipientUsername,
       memo,
       amount,
-      walletId: wallet.id,
-      userId: user.id,
+      payerWalletId: senderWallet.id,
+      payerUserId: user.id,
       logger,
     })
     if (status instanceof Error) {

src/servers/graphql-old-server.ts
@@ -37,6 +37,7 @@ import { login } from "@app/users/login"
 import { requestPhoneCode } from "@app/users/request-phone-code"
 import { getLightningFee, getNoAmountLightningFee } from "@app/wallets/get-lightning-fee"
 import { LnPaymentRequestZeroAmountRequiredError } from "@domain/errors"
+import { addEarn } from "@app/accounts/add-earn"
 
 const graphqlLogger = baseLogger.child({ module: "graphql" })
 
@@ -347,8 +348,8 @@ const resolvers = {
               const status = await Wallets.lnInvoicePaymentSend({
                 paymentRequest: invoice,
                 memo,
-                walletId: wallet.user.id as WalletId,
-                userId: wallet.user.id as UserId,
+                payerWalletId: wallet.user.id as WalletId,
+                payerUserId: wallet.user.id as UserId,
                 logger,
               })
               if (status instanceof Error) throw mapError(status)
@@ -358,8 +359,8 @@ const resolvers = {
               paymentRequest: invoice,
               memo,
               amount,
-              walletId: wallet.user.id as WalletId,
-              userId: wallet.user.id as UserId,
+              payerWalletId: wallet.user.id as WalletId,
+              payerUserId: wallet.user.id as UserId,
               logger,
             })
             if (status instanceof Error) throw mapError(status)
@@ -367,12 +368,12 @@ const resolvers = {
           },
         ),
       payKeysendUsername: async ({ username, amount, memo }) => {
-        const status = await Wallets.intraledgerPaymentSend({
+        const status = await Wallets.intraledgerPaymentSendUsername({
           recipientUsername: username,
           memo,
           amount,
-          walletId: wallet.user.id,
-          userId: wallet.user.id,
+          payerWalletId: wallet.user.id,
+          payerUserId: wallet.user.id,
           logger,
         })
 
@@ -403,7 +404,7 @@ const resolvers = {
         return feeSatAmount
       },
     }),
-    earnCompleted: async (_, { ids }, { wallet }) => wallet.addEarn(ids),
+    earnCompleted: async (_, { ids }, { uid }) => addEarn({ id: ids[0], aid: uid }),
     onchain: (_, __, { wallet }) => ({
       getNewAddress: async () => {
         const address = await Wallets.createOnChainAddress(wallet.user.id)

src/services/ledger/accounts.ts
@@ -1,15 +1,14 @@
 // an accounting reminder:
 // https://en.wikipedia.org/wiki/Double-entry_bookkeeping
 
+import { liabilitiesMainAccount } from "@domain/ledger"
+
 // assets:
 export const assetsMainAccount = "Assets"
 export const bitcoindAccountingPath = `${assetsMainAccount}:Reserve:Bitcoind`
 export const lndAccountingPath = `${assetsMainAccount}:Reserve:Lightning` // TODO: rename to Assets:Lnd
 export const escrowAccountingPath = `${assetsMainAccount}:Reserve:Escrow` // TODO: rename to Assets:Lnd:Escrow
 
-// liabilities
-export const liabilitiesMainAccount = "Liabilities"
-export const accountPath = (uid) => `${liabilitiesMainAccount}:${uid}`
 export const resolveAccountId = (accountPath: string | string[]) => {
   let id: string | null = null
 
@@ -35,37 +34,53 @@ export const resolveAccountId = (accountPath: string | string[]) => {
   return id
 }
 
-let cacheDealerPath: string
-let cachebankOwnerPath: string
+let cacheDealerWalletId: WalletId
+let cacheBankOwnerWalletId: WalletId
+let cacheFunderWalletId: WalletId
 
 const throwError = (account) => Promise.reject(`Invalid ${account}AccountPath`)
-let bankOwnerResolver = (): Promise<string> => throwError("bankOwner")
-let dealerResolver = (): Promise<string> => throwError("dealer")
+let bankOwnerResolver = (): Promise<WalletId> => throwError("bankOwner")
+let dealerResolver = (): Promise<WalletId> => throwError("dealer")
+let funderResolver = (): Promise<WalletId> => throwError("funder")
 
-export function setBankOwnerAccountResolver(resolver: () => Promise<string>) {
+export function setBankOwnerAccountResolver(resolver: () => Promise<WalletId>) {
   bankOwnerResolver = resolver
 }
 
-export function setDealerAccountResolver(resolver: () => Promise<string>) {
+export function setDealerAccountResolver(resolver: () => Promise<WalletId>) {
   dealerResolver = resolver
 }
 
-export const dealerAccountPath = async () => {
-  if (cacheDealerPath) {
-    return cacheDealerPath
+export function setFunderAccountResolver(resolver: () => Promise<WalletId>) {
+  funderResolver = resolver
+}
+
+export const getDealerWalletId = async () => {
+  if (cacheDealerWalletId) {
+    return cacheDealerWalletId
   }
 
   const dealerId = await dealerResolver()
-  cacheDealerPath = accountPath(dealerId)
-  return cacheDealerPath
+  cacheDealerWalletId = dealerId
+  return cacheDealerWalletId
 }
 
-export const bankOwnerAccountPath = async () => {
-  if (cachebankOwnerPath) {
-    return cachebankOwnerPath
+export const getBankOwnerWalletId = async () => {
+  if (cacheBankOwnerWalletId) {
+    return cacheBankOwnerWalletId
   }
 
   const bankOwnerId = await bankOwnerResolver()
-  cachebankOwnerPath = accountPath(bankOwnerId)
-  return cachebankOwnerPath
+  cacheBankOwnerWalletId = bankOwnerId
+  return cacheBankOwnerWalletId
+}
+
+export const getFunderWalletId = async () => {
+  if (cacheFunderWalletId) {
+    return cacheFunderWalletId
+  }
+
+  const funderId = await funderResolver()
+  cacheFunderWalletId = funderId
+  return cacheFunderWalletId
 }

src/services/ledger/index.ts
@@ -18,20 +18,28 @@ import {
 } from "@domain/ledger/errors"
 import { MainBook } from "./books"
 import { toSats } from "@domain/bitcoin"
-import { LedgerTransactionType, liabilitiesMainAccount, toWalletId } from "@domain/ledger"
-import { lndAccountingPath, bankOwnerAccountPath } from "./accounts"
+import {
+  LedgerTransactionType,
+  liabilitiesMainAccount,
+  toLiabilitiesAccountId,
+  toWalletId,
+} from "@domain/ledger"
+import { lndAccountingPath, getBankOwnerWalletId } from "./accounts"
 
 type LoadLedgerParams = {
-  bankOwnerAccountResolver: () => Promise<string>
-  dealerAccountResolver: () => Promise<string>
+  bankOwnerAccountResolver: () => Promise<WalletId>
+  dealerAccountResolver: () => Promise<WalletId>
+  funderAccountResolver: () => Promise<WalletId>
 }
 
 export const loadLedger = ({
   bankOwnerAccountResolver,
   dealerAccountResolver,
+  funderAccountResolver,
 }: LoadLedgerParams) => {
   accounts.setBankOwnerAccountResolver(bankOwnerAccountResolver)
   accounts.setDealerAccountResolver(dealerAccountResolver)
+  accounts.setFunderAccountResolver(funderAccountResolver)
   return {
     ...accounts,
     ...queries,
@@ -122,7 +130,7 @@ export const LedgerService = (): ILedgerService => {
     })
   }
 
-  const getAccountBalance = async (
+  const getWalletBalance = async (
     liabilitiesAccountId: LiabilitiesAccountId,
   ): Promise<Satoshis | LedgerError> => {
     try {
@@ -280,7 +288,7 @@ export const LedgerService = (): ILedgerService => {
         .debit(lndAccountingPath, sats, metadata)
 
       if (fee > 0) {
-        const bankOwnerPath = await bankOwnerAccountPath()
+        const bankOwnerPath = toLiabilitiesAccountId(await getBankOwnerWalletId())
         entry.credit(bankOwnerPath, fee, metadata)
       }
 
@@ -319,7 +327,7 @@ export const LedgerService = (): ILedgerService => {
         .debit(lndAccountingPath, sats, metadata)
 
       if (fee > 0) {
-        const bankOwnerPath = await bankOwnerAccountPath()
+        const bankOwnerPath = toLiabilitiesAccountId(await getBankOwnerWalletId())
         entry.credit(bankOwnerPath, fee, metadata)
       }
 
@@ -481,7 +489,7 @@ export const LedgerService = (): ILedgerService => {
     })
   }
 
-  const addUsernameIntraledgerTxSend = async ({
+  const addPublicWalletIdIntraledgerTxSend = async ({
     liabilitiesAccountId,
     description,
     sats,
@@ -489,11 +497,11 @@ export const LedgerService = (): ILedgerService => {
     usd,
     usdFee,
     recipientLiabilitiesAccountId,
-    payerUsername,
-    recipientUsername,
     memoPayer,
-  }: AddUsernameIntraledgerTxSendArgs): Promise<LedgerJournal | LedgerError> => {
-    const metadata: AddUsernameIntraledgerTxSendMetadata = {
+    recipientUsername,
+    payerUsername,
+  }: AddPublicWalletIdIntraledgerTxSendArgs): Promise<LedgerJournal | LedgerError> => {
+    const metadata: AddPublicWalletIdIntraledgerTxSendMetadata = {
       type: LedgerTransactionType.IntraLedger,
       pending: false,
       fee,
@@ -582,7 +590,7 @@ export const LedgerService = (): ILedgerService => {
     getLiabilityTransactionsForContactUsername,
     listPendingPayments,
     getPendingPaymentsCount,
-    getAccountBalance,
+    getWalletBalance,
     twoFATxVolumeSince,
     withdrawalTxVolumeSince,
     intraledgerTxVolumeSince,
@@ -594,7 +602,7 @@ export const LedgerService = (): ILedgerService => {
     addLnTxSend,
     addLnIntraledgerTxSend,
     addOnChainIntraledgerTxSend,
-    addUsernameIntraledgerTxSend,
+    addPublicWalletIdIntraledgerTxSend,
     settlePendingLnPayments,
     voidLedgerTransactionsForJournal,
   }

src/services/mongodb/index.ts
@@ -8,17 +8,15 @@ import { loadLedger } from "@services/ledger"
 
 export const ledger = loadLedger({
   bankOwnerAccountResolver: async () => {
-    const { _id } = await User.findOne(
-      { role: "bankowner" },
-      { lastIPs: 0, lastConnection: 0 },
-    )
+    const { _id } = await User.findOne({ role: "bankowner" }, { id: 1 })
     return _id
   },
   dealerAccountResolver: async () => {
-    const { _id } = await User.findOne(
-      { role: "dealer" },
-      { lastIPs: 0, lastConnection: 0 },
-    )
+    const { _id } = await User.findOne({ role: "dealer" }, { id: 1 })
+    return _id
+  },
+  funderAccountResolver: async () => {
+    const { _id } = await User.findOne({ role: "funder" }, { id: 1 })
     return _id
   },
 })

test/integration/services/ledger/transaction.spec.ts
@@ -8,7 +8,7 @@ import { LedgerService } from "@services/ledger"
 import { toSats } from "@domain/bitcoin"
 import { DepositFeeCalculator } from "@domain/wallets"
 import { getCurrentPrice } from "@app/prices"
-import { LedgerTransactionType } from "@domain/ledger"
+import { LedgerTransactionType, toLiabilitiesAccountId } from "@domain/ledger"
 
 let mongoose
 
@@ -18,7 +18,7 @@ let dealerPath, lndAccountingPath
 beforeAll(async () => {
   await clearAccountLocks()
   mongoose = await setupMongoConnection()
-  dealerPath = await ledger.dealerAccountPath()
+  dealerPath = toLiabilitiesAccountId(await ledger.getDealerWalletId())
   lndAccountingPath = ledger.lndAccountingPath
 })
 
@@ -38,7 +38,7 @@ beforeEach(async () => {
 })
 
 const expectBalance = async ({ account, currency, balance }) => {
-  const balanceResult = await ledger.getAccountBalance(account, { currency })
+  const balanceResult = await ledger.getWalletBalance(account, { currency })
   expect(balanceResult).toBe(balance)
 }
 
@@ -262,7 +262,7 @@ describe("on us payment via Ledger Service", () => {
     const usd = sats * price
     const usdFee = fee * price
 
-    const result = await LedgerService().addUsernameIntraledgerTxSend({
+    const result = await LedgerService().addPublicWalletIdIntraledgerTxSend({
       liabilitiesAccountId: payer.accountPath,
       description: "desc",
       sats,

GaloyMoney/blink

src/graphql/root/mutation/intraledger-payment-send.ts
@@ -1,18 +1,17 @@
-import { getWallet, intraledgerPaymentSend } from "@app/wallets"
+import { getUsernameFromWalletId } from "@app/accounts"
+import { getWallet, intraledgerPaymentSendUsername } from "@app/wallets"
 import { checkedToWalletId } from "@domain/wallets"
+import { mapError } from "@graphql/error-map"
 import { GT } from "@graphql/index"
-
 import PaymentSendPayload from "@graphql/types/payload/payment-send"
 import Memo from "@graphql/types/scalar/memo"
 import SatAmount from "@graphql/types/scalar/sat-amount"
 import WalletId from "@graphql/types/scalar/wallet-id"
-import { mapError } from "@graphql/error-map"
-import { getUsernameFromWalletId } from "@app/accounts"
 
 const IntraLedgerPaymentSendInput = new GT.Input({
   name: "IntraLedgerPaymentSendInput",
   fields: () => ({
-    walletId: { type: GT.NonNull(WalletId) },
+    walletId: { type: GT.NonNull(WalletId) }, // TODO: rename senderWalletId
     recipientWalletId: { type: GT.NonNull(WalletId) },
     amount: { type: GT.NonNull(SatAmount) },
     memo: { type: Memo },
@@ -32,7 +31,13 @@ const IntraLedgerPaymentSendMutation = GT.Field({
       }
     }
 
-    const wallet = await getWallet(walletId)
+    const senderWalletId = checkedToWalletId(walletId)
+    if (senderWalletId instanceof Error) {
+      const appErr = mapError(senderWalletId)
+      return { errors: [{ message: appErr.message }] }
+    }
+
+    const wallet = await getWallet(senderWalletId)
     if (wallet instanceof Error) {
       const appErr = mapError(wallet)
       return { errors: [{ message: appErr.message }] }
@@ -50,12 +55,12 @@ const IntraLedgerPaymentSendMutation = GT.Field({
       return { errors: [{ message: appErr.message }] }
     }
 
-    const status = await intraledgerPaymentSend({
+    const status = await intraledgerPaymentSendUsername({
       recipientUsername,
       memo,
       amount,
-      walletId,
-      userId: user.id,
+      payerWalletId: walletId,
+      payerUserId: user.id,
       logger,
     })
     if (status instanceof Error) {

src/servers/graphql-old-server.ts
@@ -37,6 +37,7 @@ import { login } from "@app/users/login"
 import { requestPhoneCode } from "@app/users/request-phone-code"
 import { getLightningFee, getNoAmountLightningFee } from "@app/wallets/get-lightning-fee"
 import { LnPaymentRequestZeroAmountRequiredError } from "@domain/errors"
+import { addEarn } from "@app/accounts/add-earn"
 
 const graphqlLogger = baseLogger.child({ module: "graphql" })
 
@@ -324,8 +325,8 @@ const resolvers = {
               const status = await Wallets.lnInvoicePaymentSend({
                 paymentRequest: invoice,
                 memo,
-                walletId: wallet.user.walletId as WalletId,
-                userId: wallet.user.id as UserId,
+                payerWalletId: wallet.user.walletId as WalletId,
+                payerUserId: wallet.user.id as UserId,
                 logger,
               })
               if (status instanceof Error) throw mapError(status)
@@ -335,8 +336,8 @@ const resolvers = {
               paymentRequest: invoice,
               memo,
               amount,
-              walletId: wallet.user.walletId as WalletId,
-              userId: wallet.user.id as UserId,
+              payerWalletId: wallet.user.walletId as WalletId,
+              payerUserId: wallet.user.id as UserId,
               logger,
             })
             if (status instanceof Error) throw mapError(status)
@@ -344,12 +345,12 @@ const resolvers = {
           },
         ),
       payKeysendUsername: async ({ username, amount, memo }) => {
-        const status = await Wallets.intraledgerPaymentSend({
+        const status = await Wallets.intraledgerPaymentSendUsername({
           recipientUsername: username,
           memo,
           amount,
-          walletId: wallet.user.walletId,
-          userId: wallet.user.id,
+          payerWalletId: wallet.user.walletId,
+          payerUserId: wallet.user.id,
           logger,
         })
 
@@ -380,7 +381,7 @@ const resolvers = {
         return feeSatAmount
       },
     }),
-    earnCompleted: async (_, { ids }, { wallet }) => wallet.addEarn(ids),
+    earnCompleted: async (_, { ids }, { uid }) => addEarn({ id: ids[0], aid: uid }),
     onchain: (_, __, { wallet }) => ({
       getNewAddress: async () => {
         const address = await Wallets.createOnChainAddress(wallet.user.walletId)

src/services/ledger/accounts.ts
@@ -1,16 +1,15 @@
 // an accounting reminder:
 // https://en.wikipedia.org/wiki/Double-entry_bookkeeping
 
+import { liabilitiesMainAccount } from "@domain/ledger"
+
 // assets:
 export const assetsMainAccount = "Assets"
 export const bitcoindAccountingPath = `${assetsMainAccount}:Reserve:Bitcoind`
 export const lndAccountingPath = `${assetsMainAccount}:Reserve:Lightning` // TODO: rename to Assets:Lnd
 export const escrowAccountingPath = `${assetsMainAccount}:Reserve:Escrow` // TODO: rename to Assets:Lnd:Escrow
 
-// liabilities
-export const liabilitiesMainAccount = "Liabilities"
-export const walletPath = (walletId) => `${liabilitiesMainAccount}:${walletId}`
-export const resolveWalletId = (walletPath: string | string[]): WalletId | null => {
+export const resolveWalletId = (walletPath: string | string[]) => {
   let id: string | null = null
 
   if (!walletPath) {
@@ -36,37 +35,53 @@ export const resolveWalletId = (walletPath: string | string[]): WalletId | null 
   return id as WalletId
 }
 
-let cacheDealerPath: string
-let cachebankOwnerPath: string
+let cacheDealerWalletId: WalletId
+let cacheBankOwnerWalletId: WalletId
+let cacheFunderWalletId: WalletId
 
 const throwError = (wallet) => Promise.reject(`Invalid ${wallet}WalletPath`)
-let bankOwnerResolver = (): Promise<string> => throwError("bankOwner")
-let dealerResolver = (): Promise<string> => throwError("dealer")
+let bankOwnerResolver = (): Promise<WalletId> => throwError("bankOwner")
+let dealerResolver = (): Promise<WalletId> => throwError("dealer")
+let funderResolver = (): Promise<WalletId> => throwError("funder")
 
-export function setbankOwnerWalletResolver(resolver: () => Promise<string>) {
+export function setbankOwnerWalletResolver(resolver: () => Promise<WalletId>) {
   bankOwnerResolver = resolver
 }
 
-export function setdealerWalletResolver(resolver: () => Promise<string>) {
+export function setdealerWalletResolver(resolver: () => Promise<WalletId>) {
   dealerResolver = resolver
 }
 
-export const dealerAccountPath = async () => {
-  if (cacheDealerPath) {
-    return cacheDealerPath
+export function setFunderWalletResolver(resolver: () => Promise<WalletId>) {
+  funderResolver = resolver
+}
+
+export const getDealerWalletId = async () => {
+  if (cacheDealerWalletId) {
+    return cacheDealerWalletId
   }
 
   const dealerId = await dealerResolver()
-  cacheDealerPath = walletPath(dealerId)
-  return cacheDealerPath
+  cacheDealerWalletId = dealerId
+  return cacheDealerWalletId
 }
 
-export const bankOwnerAccountPath = async () => {
-  if (cachebankOwnerPath) {
-    return cachebankOwnerPath
+export const getBankOwnerWalletId = async () => {
+  if (cacheBankOwnerWalletId) {
+    return cacheBankOwnerWalletId
   }
 
   const bankOwnerId = await bankOwnerResolver()
-  cachebankOwnerPath = walletPath(bankOwnerId)
-  return cachebankOwnerPath
+  cacheBankOwnerWalletId = bankOwnerId
+  return cacheBankOwnerWalletId
+}
+
+export const getFunderWalletId = async () => {
+  if (cacheFunderWalletId) {
+    return cacheFunderWalletId
+  }
+
+  const funderId = await funderResolver()
+  cacheFunderWalletId = funderId
+  return cacheFunderWalletId
 }

src/services/ledger/index.ts
@@ -18,20 +18,28 @@ import {
 } from "@domain/ledger/errors"
 import { MainBook } from "./books"
 import { toSats } from "@domain/bitcoin"
-import { LedgerTransactionType, liabilitiesMainAccount, toWalletId } from "@domain/ledger"
-import { lndAccountingPath, bankOwnerAccountPath } from "./accounts"
+import {
+  LedgerTransactionType,
+  liabilitiesMainAccount,
+  toLiabilitiesWalletId,
+  toWalletId,
+} from "@domain/ledger"
+import { lndAccountingPath, getBankOwnerWalletId } from "./accounts"
 
 type LoadLedgerParams = {
-  bankOwnerWalletResolver: () => Promise<string>
-  dealerWalletResolver: () => Promise<string>
+  bankOwnerWalletResolver: () => Promise<WalletId>
+  dealerWalletResolver: () => Promise<WalletId>
+  funderWalletResolver: () => Promise<WalletId>
 }
 
 export const loadLedger = ({
   bankOwnerWalletResolver,
   dealerWalletResolver,
+  funderWalletResolver,
 }: LoadLedgerParams) => {
   accounts.setbankOwnerWalletResolver(bankOwnerWalletResolver)
   accounts.setdealerWalletResolver(dealerWalletResolver)
+  accounts.setFunderWalletResolver(funderWalletResolver)
   return {
     ...accounts,
     ...queries,
@@ -122,7 +130,7 @@ export const LedgerService = (): ILedgerService => {
     })
   }
 
-  const getAccountBalance = async (
+  const getWalletBalance = async (
     liabilitiesWalletId: LiabilitiesWalletId,
   ): Promise<Satoshis | LedgerError> => {
     try {
@@ -280,7 +288,7 @@ export const LedgerService = (): ILedgerService => {
         .debit(lndAccountingPath, sats, metadata)
 
       if (fee > 0) {
-        const bankOwnerPath = await bankOwnerAccountPath()
+        const bankOwnerPath = toLiabilitiesWalletId(await getBankOwnerWalletId())
         entry.credit(bankOwnerPath, fee, metadata)
       }
 
@@ -319,7 +327,7 @@ export const LedgerService = (): ILedgerService => {
         .debit(lndAccountingPath, sats, metadata)
 
       if (fee > 0) {
-        const bankOwnerPath = await bankOwnerAccountPath()
+        const bankOwnerPath = toLiabilitiesWalletId(await getBankOwnerWalletId())
         entry.credit(bankOwnerPath, fee, metadata)
       }
 
@@ -481,7 +489,7 @@ export const LedgerService = (): ILedgerService => {
     })
   }
 
-  const addUsernameIntraledgerTxSend = async ({
+  const addWalletIdIntraledgerTxSend = async ({
     liabilitiesWalletId,
     description,
     sats,
@@ -492,8 +500,8 @@ export const LedgerService = (): ILedgerService => {
     payerUsername,
     recipientUsername,
     memoPayer,
-  }: AddUsernameIntraledgerTxSendArgs): Promise<LedgerJournal | LedgerError> => {
-    const metadata: AddUsernameIntraledgerTxSendMetadata = {
+  }: addWalletIdIntraledgerTxSendArgs): Promise<LedgerJournal | LedgerError> => {
+    const metadata: addWalletIdIntraledgerTxSendMetadata = {
       type: LedgerTransactionType.IntraLedger,
       pending: false,
       fee,
@@ -582,7 +590,7 @@ export const LedgerService = (): ILedgerService => {
     getLiabilityTransactionsForContactUsername,
     listPendingPayments,
     getPendingPaymentsCount,
-    getAccountBalance,
+    getWalletBalance,
     twoFATxVolumeSince,
     withdrawalTxVolumeSince,
     intraledgerTxVolumeSince,
@@ -594,7 +602,7 @@ export const LedgerService = (): ILedgerService => {
     addLnTxSend,
     addLnIntraledgerTxSend,
     addOnChainIntraledgerTxSend,
-    addUsernameIntraledgerTxSend,
+    addWalletIdIntraledgerTxSend,
     settlePendingLnPayments,
     voidLedgerTransactionsForJournal,
   }

src/services/mongodb/index.ts
@@ -8,17 +8,15 @@ import { loadLedger } from "@services/ledger"
 
 export const ledger = loadLedger({
   bankOwnerWalletResolver: async () => {
-    const { walletId } = await User.findOne(
-      { role: "bankowner" },
-      { lastIPs: 0, lastConnection: 0 },
-    )
+    const { walletId } = await User.findOne({ role: "bankowner" }, { walletId: 1 })
     return walletId
   },
   dealerWalletResolver: async () => {
-    const { walletId } = await User.findOne(
-      { role: "dealer" },
-      { lastIPs: 0, lastConnection: 0 },
-    )
+    const { walletId } = await User.findOne({ role: "dealer" }, { walletId: 1 })
+    return walletId
+  },
+  funderWalletResolver: async () => {
+    const { walletId } = await User.findOne({ role: "funder" }, { walletId: 1 })
     return walletId
   },
 })

test/integration/services/ledger/transaction.spec.ts
@@ -8,7 +8,7 @@ import { LedgerService } from "@services/ledger"
 import { toSats } from "@domain/bitcoin"
 import { DepositFeeCalculator } from "@domain/wallets"
 import { getCurrentPrice } from "@app/prices"
-import { LedgerTransactionType } from "@domain/ledger"
+import { LedgerTransactionType, toLiabilitiesWalletId } from "@domain/ledger"
 
 let mongoose
 
@@ -18,7 +18,7 @@ let dealerPath, lndAccountingPath
 beforeAll(async () => {
   await clearAccountLocks()
   mongoose = await setupMongoConnection()
-  dealerPath = await ledger.dealerAccountPath()
+  dealerPath = toLiabilitiesWalletId(await ledger.getDealerWalletId())
   lndAccountingPath = ledger.lndAccountingPath
 })
 
@@ -38,7 +38,7 @@ beforeEach(async () => {
 })
 
 const expectBalance = async ({ account, currency, balance }) => {
-  const balanceResult = await ledger.getAccountBalance(account, { currency })
+  const balanceResult = await ledger.getWalletBalance(account, { currency })
   expect(balanceResult).toBe(balance)
 }
 
@@ -262,7 +262,7 @@ describe("on us payment via Ledger Service", () => {
     const usd = sats * price
     const usdFee = fee * price
 
-    const result = await LedgerService().addUsernameIntraledgerTxSend({
+    const result = await LedgerService().addWalletIdIntraledgerTxSend({
       liabilitiesWalletId: payer.walletPath,
       description: "desc",
       sats,