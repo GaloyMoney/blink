69792476859b6e9a72ac287017a1fe7d0775ab3d
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-12-15 18:46:12 +0000

    fix: remnant error

--- src/graphql/error-map.ts
+++ src/graphql/error-map.ts
@@ -193,7 +193,6 @@ export const mapError = (error: ApplicationError): CustomApolloError => {
     case "CouldNotFindWalletFromIdError":
     case "CouldNotFindWalletFromUsernameError":
     case "CouldNotFindWalletFromOnChainAddressError":
-    case "CouldNotFindWalletFromPublicIdError":
     case "CouldNotFindWalletFromOnChainAddressesError":
     case "LockError":
     case "LockServiceError":
