ea116c636af4e173de9eca1dc95260e185ce5628
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-12-16 12:31:37 +0000

    Revert "fix: regression publicWalletId -> walletId PR (#811)"
    This reverts commit 951b1a9aa643268ff2113a97f8268c5a47d9d12d.

--- src/graphql/types/object/fiat-wallet.ts
+++ src/graphql/types/object/fiat-wallet.ts
@@ -14,7 +14,7 @@ const FiatWallet = new GT.Object({
   fields: () => ({
     id: {
       type: GT.NonNullID,
-      resolve: (source) => source.id,
+      resolve: (source) => source.publicId,
     },
     walletCurrency: {
       type: GT.NonNull(WalletCurrency),
