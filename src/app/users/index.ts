3d056ed47dfe218dbbe88787ed6bdc17b730152e
Juan P Lopez <dolcalmi@gmail.com>
2021-12-16 10:00:56 -0500

    Add getUsernameFromWalletPublicId wrapper

--- src/app/users/index.ts
+++ src/app/users/index.ts
@@ -1,5 +1,8 @@
 import { wrapAsyncToRunInSpan } from "@services/tracing"
-import { getUserForLogin as getUserForLoginFn } from "./get-user"
+import {
+  getUserForLogin as getUserForLoginFn,
+  getUsernameFromWalletPublicId as getUsernameFromWalletPublicIdFn,
+} from "./get-user"
 
 export * from "./username-available"
 export * from "./update-contact-alias"
@@ -8,6 +11,9 @@ export * from "./add-new-contact"
 
 const telemetryWrappers = {
   getUserForLogin: wrapAsyncToRunInSpan({ fn: getUserForLoginFn }),
+  getUsernameFromWalletPublicId: wrapAsyncToRunInSpan({
+    fn: getUsernameFromWalletPublicIdFn,
+  }),
 }
 
-export const { getUserForLogin } = telemetryWrappers
+export const { getUserForLogin, getUsernameFromWalletPublicId } = telemetryWrappers
