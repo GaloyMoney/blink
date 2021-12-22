07077dff3d2dca3ef4dc75295ebf661b927da9a9
vindard <17693119+vindard@users.noreply.github.com>
2021-12-22 13:27:41 -0400

    feat(lnpayments): add new schema for LnPayment collection

---
+++ src/services/lnd/schema.ts
@@ -0,0 +1,55 @@
+import * as mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const confirmedDetailsSchema = new Schema<LnPaymentConfirmedDetails>({
+  confirmedAt: {
+    type: Date,
+    required: true,
+  },
+  destination: {
+    type: String,
+    required: true,
+  },
+  revealedPreImage: {
+    type: String,
+    required: true,
+  },
+  roundedUpFee: {
+    type: Number,
+    required: true,
+  },
+  milliSatsFee: {
+    type: Number,
+    required: true,
+  },
+  hopPubkeys: [String],
+})
+
+const paymentAttemptSchema = Schema.Types.Mixed
+
+const paymentSchema = new Schema<LnPaymentType>({
+  createdAt: Date,
+  status: {
+    type: String,
+    enum: ["settled", "failed", "pending"],
+    required: true,
+  },
+  paymentHash: {
+    type: String,
+    index: true,
+    unique: true,
+    required: true,
+  },
+  paymentRequest: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  milliSatsAmount: String,
+  roundedUpAmount: Number,
+  confirmedDetails: confirmedDetailsSchema,
+  attempts: [paymentAttemptSchema],
+})
+
+export const LnPayment = mongoose.model("LnPayment", paymentSchema)
---
+++ src/services/lnd/schema.types.d.ts
@@ -0,0 +1,11 @@
+interface LnPaymentType {
+  id: string
+  createdAt: Date
+  status: string
+  paymentHash: string
+  paymentRequest: string
+  milliSatsAmount: string
+  roundedUpAmount: number
+  confirmedDetails: LnPaymentConfirmedDetails | undefined
+  attempts: LnPaymentAttempt[]
+}
