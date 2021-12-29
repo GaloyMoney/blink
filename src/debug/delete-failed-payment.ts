1b2e1e60d5b3c8985a80a7010068af97a97b34a9
Nicolas Burtey <nicolas.burtey@gmail.com>
2021-12-29 09:17:47 +0000

    chore: update payment cleanup script

--- src/debug/delete-failed-payment.ts
+++ src/debug/delete-failed-payment.ts
@@ -1,7 +1,7 @@
-import { authenticatedLndGrpc, deleteFailedPayments } from "ln-service"
+import { authenticatedLndGrpc, deleteFailedPayAttempts } from "ln-service"
 
-// export LND1_TLS=$(kubectl exec lnd-0 -n mainnet  -- base64 /root/.lnd/tls.cert | tr -d '\n\r')
-// export LND1_MACAROON=$(kubectl exec lnd-0 -n mainnet -- base64 /root/.lnd/data/chain/bitcoin/mainnet/admin.macaroon | tr -d '\n\r')
+// export LND1_TLS=$(kubectl exec lnd1-0 -n galoy-bbw-bitcoin  -- base64 /root/.lnd/tls.cert | tr -d '\n\r')
+// export LND1_MACAROON=$(kubectl exec lnd1-0 -n galoy-bbw-bitcoin -- base64 /root/.lnd/data/chain/bitcoin/mainnet/admin.macaroon | tr -d '\n\r')
 
 const fn = async () => {
   try {
@@ -11,7 +11,8 @@ const fn = async () => {
       node: "localhost",
       port: 10009,
     })
-    console.log(await deleteFailedPayments({ lnd }))
+    console.log(await deleteFailedPayAttempts({ lnd }))
+    console.log("completed")
   } catch (err) {
     console.log({ err })
   }
