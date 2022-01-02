144cd5452664a74a5ae3dc5084aed6cc0b53d1a5
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-01-02 13:53:11 +0000

    chore: adding projection to wallet

--- src/services/mongoose/wallets.ts
+++ src/services/mongoose/wallets.ts
@@ -14,7 +14,7 @@ import { caseInsensitiveRegex } from "./users"
 export const WalletsRepository = (): IWalletsRepository => {
   const findById = async (walletId: WalletId): Promise<Wallet | RepositoryError> => {
     try {
-      const result = await User.findOne({ walletId })
+      const result = await User.findOne({ walletId }, projection)
       if (!result) {
         return new CouldNotFindWalletFromIdError()
       }
@@ -28,7 +28,10 @@ export const WalletsRepository = (): IWalletsRepository => {
     username: Username,
   ): Promise<Wallet | RepositoryError> => {
     try {
-      const result = await User.findOne({ username: caseInsensitiveRegex(username) })
+      const result = await User.findOne(
+        { username: caseInsensitiveRegex(username) },
+        projection,
+      )
       if (!result) {
         return new CouldNotFindWalletFromUsernameError()
       }
@@ -43,7 +46,7 @@ export const WalletsRepository = (): IWalletsRepository => {
     address: OnChainAddress,
   ): Promise<Wallet | RepositoryError> => {
     try {
-      const result = await User.findOne({ "onchain.address": address })
+      const result = await User.findOne({ "onchain.address": address }, projection)
       if (!result) {
         return new CouldNotFindWalletFromOnChainAddressError()
       }
@@ -57,7 +60,10 @@ export const WalletsRepository = (): IWalletsRepository => {
     addresses: string[],
   ): Promise<Wallet[] | RepositoryError> => {
     try {
-      const result = await User.find({ "onchain.address": { $in: addresses } })
+      const result = await User.find(
+        { "onchain.address": { $in: addresses } },
+        projection,
+      )
       if (!result) {
         return new CouldNotFindWalletFromOnChainAddressesError()
       }
@@ -95,4 +101,10 @@ const resultToWallet = (result: UserType): Wallet => {
     onChainAddressIdentifiers,
     onChainAddresses,
   }
+}
+const projection = {
+  walletId: 1,
+  depositFeeRatio: 1,
+  withdrawFee: 1,
+  onchain: 1,
 }
