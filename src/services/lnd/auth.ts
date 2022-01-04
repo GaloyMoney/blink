28f51f0886bdcbcb36cb9c13ee5de447764712f9
Juan P Lopez <dolcalmi@gmail.com>
2022-01-04 12:55:39 -0500

    Set lnd status inactive by default + add test mocks

--- src/services/lnd/auth.ts
+++ src/services/lnd/auth.ts
@@ -4,22 +4,14 @@ import sortBy from "lodash.sortby"
 
 const inputs: LndParams[] = getLndParams()
 
-// is this file being imported from trigger.ts?
-// FIXME: this is a hacky workaround to get active = true
-// for params to make the tests working.
-// otherwise the tests will have to probe before being able to call lnd
-// TODO: use a mock instead.
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const isTest = require.main!.filename.indexOf(".spec.") !== -1
-
-export const addProps = (array: LndParams[]) => {
+const addProps = (array: LndParams[]) => {
   const result: LndParamsAuthed[] = array.map((input) => {
     const socket = `${input.node}:${input.port}`
     return {
       ...input,
       socket,
       lnd: authenticatedLndGrpc({ ...input, socket }).lnd,
-      active: isTest,
+      active: false,
     }
   })
   return result
--- src/services/lnd/unauth.ts
+++ src/services/lnd/unauth.ts
@@ -4,22 +4,14 @@ import { getLndParams } from "@config/app"
 
 const inputs: LndParams[] = getLndParams()
 
-// is this file being imported from trigger.ts?
-// FIXME: this is a hacky workaround to get active = true
-// for params to make the tests working.
-// otherwise the tests will have to probe before being able to call lnd
-// TODO: use a mock instead.
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const isTest = require.main!.filename.indexOf(".spec.") !== -1
-
-export const addProps = (array) =>
+const addProps = (array) =>
   array.map((input) => {
     const socket = `${input.node}:${input.port}`
     return {
       ...input,
       socket,
       lnd: unauthenticatedLndGrpc({ ...input, socket }).lnd,
-      active: isTest,
+      active: false,
     }
   })
 
