6a18615f58a0e2620493a1387868f059da3b40db
vindard <17693119+vindard@users.noreply.github.com>
2022-01-04 09:21:55 -0400

    test: add test for reward txn memos below spam threshold

--- test/integration/02-user-wallet/02-receive-rewards.spec.ts
+++ test/integration/02-user-wallet/02-receive-rewards.spec.ts
@@ -1,10 +1,12 @@
-import { MS_PER_DAY, onboardingEarn } from "@config/app"
+import { MEMO_SHARING_SATS_THRESHOLD, MS_PER_DAY, onboardingEarn } from "@config/app"
 import find from "lodash.find"
 import difference from "lodash.difference"
 
 import { addEarn } from "@app/accounts/add-earn"
+import { getTransactionsForWalletId, intraledgerPaymentSendWalletId } from "@app/wallets"
 
 import { baseLogger } from "@services/logger"
+import { getFunderWalletId } from "@services/ledger/accounts"
 
 import { checkIsBalanced, getAndCreateUserWallet } from "test/helpers"
 import { resetSelfWalletIdLimits } from "test/helpers/rate-limit"
@@ -66,5 +68,41 @@ describe("UserWallet - addEarn", () => {
     // yet, if we do it another time, the balance should not increase,
     // because all the rewards has already been been consumed:
     await getAndVerifyRewards()
+  })
+
+  it("receives transaction with reward memo", async () => {
+    const onboardingEarnIds = Object.keys(onboardingEarn)
+    expect(onboardingEarnIds.length).toBeGreaterThanOrEqual(1)
+
+    const { result: transactionsBefore } = await getTransactionsForWalletId({
+      walletId: userWallet1.user.walletId,
+    })
+
+    let onboardingEarnId = ""
+    let txCheck: WalletTransaction | undefined
+    for (onboardingEarnId of onboardingEarnIds) {
+      txCheck = transactionsBefore?.find((tx) => tx.memo === onboardingEarnId)
+      if (!txCheck) break
+    }
+    expect(txCheck).toBeUndefined()
+
+    const amount = onboardingEarn[onboardingEarnId]
+    expect(amount).toBeLessThan(MEMO_SHARING_SATS_THRESHOLD)
+
+    const funderWalletId = await getFunderWalletId()
+    const payment = await intraledgerPaymentSendWalletId({
+      senderWalletId: funderWalletId,
+      recipientWalletId: userWallet1.user.walletId,
+      amount,
+      memo: onboardingEarnId,
+      logger: baseLogger,
+    })
+    if (payment instanceof Error) return payment
+
+    const { result: transactionsAfter } = await getTransactionsForWalletId({
+      walletId: userWallet1.user.walletId,
+    })
+    const rewardTx = transactionsAfter?.find((tx) => tx.memo === onboardingEarnId)
+    expect(rewardTx).not.toBeUndefined()
   })
 })
