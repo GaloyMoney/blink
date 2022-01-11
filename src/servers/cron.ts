0820c55bf088e660b3fedb705b783e28d1859279
bodymindarts <justin@galoy.io>
2022-01-11 14:41:47 +0100

    chore: improve tracing for cron

--- src/servers/cron.ts
+++ src/servers/cron.ts
@@ -1,4 +1,5 @@
 import { getSpecterWalletConfig } from "@config/app"
+import { asyncRunInSpan, SemanticAttributes } from "@services/tracing"
 
 import {
   deleteExpiredInvoiceUser,
@@ -17,43 +18,62 @@ import { SpecterWallet } from "@core/specter-wallet"
 import { activateLndHealthCheck } from "@services/lnd/health"
 
 const main = async () => {
-  const mongoose = await setupMongoConnection()
-
-  const rebalance = () => {
-    const specterWalletConfig = getSpecterWalletConfig()
-    const specterWallet = new SpecterWallet({
-      logger: baseLogger,
-      config: specterWalletConfig,
-    })
-    return specterWallet.tentativelyRebalance()
-  }
-
-  const updatePendingOnChainPayments = () =>
-    updateUsersPendingPayment({ onchainOnly: true })
-
-  const tasks = [
-    updateEscrows,
-    updatePendingLightningTransactions,
-    deleteExpiredInvoiceUser,
-    deleteFailedPaymentsAttemptAllLnds,
-    rebalance,
-    updateRoutingFees,
-    updatePendingOnChainPayments,
-  ]
-
   const results: Array<boolean> = []
-  for (const task of tasks) {
-    try {
-      baseLogger.info(`starting ${task.name}`)
-      await task()
-      results.push(true)
-    } catch (error) {
-      baseLogger.error({ error }, `issue with task ${task.name}`)
-      results.push(false)
-    }
-  }
+  await asyncRunInSpan(
+    "cron.main",
+    {
+      [SemanticAttributes.CODE_FUNCTION]: "main",
+      [SemanticAttributes.CODE_NAMESPACE]: "cron",
+    },
+    async () => {
+      const mongoose = await setupMongoConnection()
 
-  await mongoose.connection.close()
+      const rebalance = () => {
+        const specterWalletConfig = getSpecterWalletConfig()
+        const specterWallet = new SpecterWallet({
+          logger: baseLogger,
+          config: specterWalletConfig,
+        })
+        return specterWallet.tentativelyRebalance()
+      }
+
+      const updatePendingOnChainPayments = () =>
+        updateUsersPendingPayment({ onchainOnly: true })
+
+      const tasks = [
+        updateEscrows,
+        updatePendingLightningTransactions,
+        deleteExpiredInvoiceUser,
+        deleteFailedPaymentsAttemptAllLnds,
+        rebalance,
+        updateRoutingFees,
+        updatePendingOnChainPayments,
+      ]
+
+      for (const task of tasks) {
+        await asyncRunInSpan(
+          `cron.${task.name}`,
+          {
+            [SemanticAttributes.CODE_FUNCTION]: task.name,
+            [SemanticAttributes.CODE_NAMESPACE]: "cron",
+          },
+          async () => {
+            try {
+              baseLogger.info(`starting ${task.name}`)
+              await task()
+              results.push(true)
+            } catch (error) {
+              baseLogger.error({ error }, `issue with task ${task.name}`)
+              results.push(false)
+              return error
+            }
+          },
+        )
+      }
+
+      await mongoose.connection.close()
+    },
+  )
 
   // FIXME: we need to exit because we may have some pending promise
   process.exit(results.every((r) => r) ? 0 : 99)
