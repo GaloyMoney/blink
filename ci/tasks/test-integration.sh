8f7c0f0bb2f39ec955a6c7e37139f9c045857509
bodymindarts <justin@galoy.io>
2022-01-13 14:31:02 +0100

    ci: some fixes to ci related to migration

--- ci/tasks/test-integration.sh
+++ ci/tasks/test-integration.sh
@@ -34,7 +34,7 @@ rsync --delete -avr -e "ssh -l ${DOCKER_HOST_USER} ${ADDITIONAL_SSH_OPTS}" \
 echo "Done!"
 
 ssh ${ADDITIONAL_SSH_OPTS} ${DOCKER_HOST_USER}@${DOCKER_HOST_IP} \
-  "cd ${REPO_PATH}; DOCKER_HOST_IP=${DOCKER_HOST_IP} docker compose down --volumes --remove-orphans --timeout 1; DOCKER_HOST_IP=${DOCKER_HOST_IP} docker compose up -d"
+  "cd ${REPO_PATH}; DOCKER_HOST_IP=${DOCKER_HOST_IP} docker compose down --volumes --remove-orphans --timeout 1; DOCKER_HOST_IP=${DOCKER_HOST_IP} docker compose up integration-deps -d"
 
 export DOCKER_HOST=ssh://${DOCKER_HOST_USER}@${DOCKER_HOST_IP}
 
