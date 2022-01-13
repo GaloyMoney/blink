732b387189ab62b7b4882f5723e868fb396e3445
bodymindarts <justin@galoy.io>
2022-01-13 11:36:28 +0100

    Ensure migrations work locally in clean state

--- scripts/migrate-mongodb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-node_modules/.bin/migrate-mongodb status -f src/migrations/migrate-mongo-config.js
-node_modules/.bin/migrate-mongodb up -f src/migrations/migrate-mongo-config.js
-node_modules/.bin/migrate-mongodb status -f src/migrations/migrate-mongo-config.js
