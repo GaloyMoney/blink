ecb63a1e86941956f59e656a48ef085ed776a877
vindard <17693119+vindard@users.noreply.github.com>
2022-01-14 17:30:27 -0400

    refactor: re-assign admin gql secretPreImage type

--- src/graphql/admin/types/object/lightning-payment.ts
+++ src/graphql/admin/types/object/lightning-payment.ts
@@ -2,9 +2,9 @@ import { GT } from "@graphql/index"
 import LnPubkey from "@graphql/types/scalar/ln-pubkey"
 import Timestamp from "@graphql/types/scalar/timestamp"
 import SatAmount from "@graphql/types/scalar/sat-amount"
-import LnPaymentSecret from "@graphql/types/scalar/ln-payment-secret"
 import LnPaymentStatus from "@graphql/types/scalar/ln-payment-status"
 import LnPaymentRequest from "@graphql/types/scalar/ln-payment-request"
+import LnPaymentPreImage from "@graphql/types/scalar/ln-payment-preimage"
 
 const LightningPayment = new GT.Object({
   name: "LightningPayment",
@@ -14,7 +14,7 @@ const LightningPayment = new GT.Object({
     createdAt: { type: Timestamp },
     confirmedAt: { type: Timestamp },
     amount: { type: SatAmount },
-    revealedPreImage: { type: LnPaymentSecret },
+    revealedPreImage: { type: LnPaymentPreImage },
     request: { type: LnPaymentRequest },
     destination: { type: LnPubkey },
   }),
