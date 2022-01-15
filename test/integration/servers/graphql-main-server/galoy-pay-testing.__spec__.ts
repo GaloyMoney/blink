bac6143587756924008f65961f0a7f1c4c6998de
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-01-15 12:32:49 +0000

    fix: deactivating pay testing

--- test/integration/servers/graphql-main-server/galoy-pay-testing.__spec__.ts
+++ test/integration/servers/graphql-main-server/galoy-pay-testing.__spec__.ts
@@ -27,7 +27,7 @@ import {
 
 import { startServer, killServer } from "test/helpers/integration-server"
 
-jest.setTimeout(600000)
+jest.setTimeout(120000)
 
 let apolloClient: ApolloClient<NormalizedCacheObject>,
   disposeClient: () => void,
