a1dd11409386dd4bedab5865ef3c376b02f97b06
Kartik Shah <krtk.6160@gmail.com>
2022-01-17 22:26:30 +0530

    Add example

--- src/debug/reimburse.ts
+++ src/debug/reimburse.ts
@@ -8,36 +8,51 @@ import { getBankOwnerWalletId } from "@services/ledger/accounts"
 import { baseLogger } from "@services/logger"
 import { setupMongoConnection } from "@services/mongodb"
 
-type recipient = {
+type reimbursement = {
   recipientWalletId: WalletId
   amount: Satoshis
   memo: string
 }
 
-const reimburse = async (recipients: Array<recipient>) => {
-  const mongoose = await setupMongoConnection()
+const reimburse = async (reimbursements: Array<reimbursement>) => {
+  await setupMongoConnection()
   console.log("Mongoose connection ready")
   const bankOwnerWalletId = await getBankOwnerWalletId()
 
-  for (const recipient of recipients) {
-    console.log("attempting reimburse:", { recipient })
-    const res = await intraledgerPaymentSendWalletId({
-      recipientWalletId: recipient.recipientWalletId,
-      amount: recipient.amount,
+  for (const reimbursement of reimbursements) {
+    const reimburseResult = await intraledgerPaymentSendWalletId({
+      recipientWalletId: reimbursement.recipientWalletId,
+      amount: reimbursement.amount,
       logger: baseLogger,
       senderWalletId: bankOwnerWalletId,
-      memo: recipient.memo,
+      memo: reimbursement.memo,
     })
+    console.log({ ...reimbursement, reimbursementStatus: reimburseResult })
   }
 }
 
 // Populate the array below with the recipient wallet public ids, sats and memo to be shown in the reimbursement txn
-const recipients = [
+
+const reimbursements: Array<reimbursement> = [
   {
     recipientWalletId: "" as WalletId,
     amount: 0 as Satoshis,
-    memo: "refund",
+    memo: "",
   },
 ]
 
-reimburse(recipients).then(console.log).catch(console.error)
+// For example:
+// const reimbursements = [
+//   {
+//     recipientWalletId: "d7d03fea-7e8f-4d38-8f4f-169ae455341b" as WalletId,
+//     amount: 10 as Satoshis,
+//     memo: "test",
+//   },
+//   {
+//     recipientWalletId: "d7d03fea-7e8f-4d38-8f4f-169ae455341b" as WalletId,
+//     amount: 11 as Satoshis,
+//     memo: "test1",
+//   },
+// ]
+
+reimburse(reimbursements).then(console.log).catch(console.error)
