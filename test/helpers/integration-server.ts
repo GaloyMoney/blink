15b67e7e286408aeb990c27a2d8de96935862a65
Juan P Lopez <dolcalmi@gmail.com>
2022-01-17 13:25:57 -0500

    fix: local env integration test

--- test/helpers/integration-server.ts
+++ test/helpers/integration-server.ts
@@ -10,11 +10,14 @@ export const startServer = async (serverStartMessage = "Server ready"): Promise<
     const serverProcess = childProcess.spawn("make", ["start-api-ci"])
     const serverPid = serverProcess.pid as PID
     serverProcess.stdout.on("data", (data) => {
-      console.log(data.toString())
+      console.debug(`stdout: ${data.toString()}`)
       if (data.includes(serverStartMessage)) {
         resolve(serverPid)
       }
       serverProcess.removeAllListeners()
+    })
+    serverProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data.toString()}`)
     })
   })
 }
