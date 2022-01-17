c748aa470e32713ae6821fa03247da6e74348936
bodymindarts <justin@galoy.io>
2022-01-17 22:48:18 +0100

    fix: migration dir was bad

--- src/migrations/migrate-mongo-config.js
+++ src/migrations/migrate-mongo-config.js
@@ -17,7 +17,7 @@ const config = {
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-  migrationsDir: ".",
+  migrationsDir: "src/migrations",
 
   // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
   changelogCollectionName: "changelog",
