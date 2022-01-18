kelvinator07/galoy

chore: try opentelemetry event

src/services/ledger/index.ts
@@ -23,7 +23,7 @@ import {
   toWalletId,
 } from "@domain/ledger"
 
-import { wrapAsyncToRunInSpan } from "@services/tracing"
+import { addEventToCurrentSpan } from "@services/tracing"
 
 import * as accounts from "./accounts"
 import * as queries from "./query"
@@ -230,26 +230,24 @@ export const LedgerService = (): ILedgerService => {
     }))
 
     try {
-      const [result]: (TxVolume & { _id: null })[] = await wrapAsyncToRunInSpan({
-        namespace: `service.ledger`,
-        fn: async () =>
-          Transaction.aggregate([
-            {
-              $match: {
-                accounts: liabilitiesWalletId,
-                $or: txnTypesObj,
-                $and: [{ timestamp: { $gte: timestamp } }],
-              },
-            },
-            {
-              $group: {
-                _id: null,
-                outgoingSats: { $sum: "$debit" },
-                incomingSats: { $sum: "$credit" },
-              },
-            },
-          ]),
-      })()
+      addEventToCurrentSpan("volume aggregation stats")
+      const [result]: (TxVolume & { _id: null })[] = await Transaction.aggregate([
+        {
+          $match: {
+            accounts: liabilitiesWalletId,
+            $or: txnTypesObj,
+            $and: [{ timestamp: { $gte: timestamp } }],
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            outgoingSats: { $sum: "$debit" },
+            incomingSats: { $sum: "$credit" },
+          },
+        },
+      ])
+      addEventToCurrentSpan("volume aggregation ends")
 
       return {
         outgoingSats: toSats(result?.outgoingSats ?? 0),

GaloyMoney/blink

src/services/ledger/index.ts
@@ -23,7 +23,7 @@ import {
   toWalletId,
 } from "@domain/ledger"
 
-import { wrapAsyncToRunInSpan } from "@services/tracing"
+import { addEventToCurrentSpan } from "@services/tracing"
 
 import * as accounts from "./accounts"
 import * as queries from "./query"
@@ -230,26 +230,24 @@ export const LedgerService = (): ILedgerService => {
     }))
 
     try {
-      const [result]: (TxVolume & { _id: null })[] = await wrapAsyncToRunInSpan({
-        namespace: `service.ledger`,
-        fn: async () =>
-          Transaction.aggregate([
-            {
-              $match: {
-                accounts: liabilitiesWalletId,
-                $or: txnTypesObj,
-                $and: [{ timestamp: { $gte: timestamp } }],
-              },
-            },
-            {
-              $group: {
-                _id: null,
-                outgoingSats: { $sum: "$debit" },
-                incomingSats: { $sum: "$credit" },
-              },
-            },
-          ]),
-      })()
+      addEventToCurrentSpan("volume aggregation starts")
+      const [result]: (TxVolume & { _id: null })[] = await Transaction.aggregate([
+        {
+          $match: {
+            accounts: liabilitiesWalletId,
+            $or: txnTypesObj,
+            $and: [{ timestamp: { $gte: timestamp } }],
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            outgoingSats: { $sum: "$debit" },
+            incomingSats: { $sum: "$credit" },
+          },
+        },
+      ])
+      addEventToCurrentSpan("volume aggregation ends")
 
       return {
         outgoingSats: toSats(result?.outgoingSats ?? 0),