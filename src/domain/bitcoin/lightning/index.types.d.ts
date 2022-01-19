kelvinator07/galoy

refactor: change args/result of listPaymentsByPubkey to be an array

src/domain/bitcoin/lightning/index.types.d.ts
@@ -130,18 +130,16 @@ type ListLnPaymentsResult = {
 }
 
 type ListSettledAndFailedLnPaymentsByPubkeyPaginatedArgs = {
-  [key: Pubkey]: {
-    settledCursor: PagingToken | undefined | false
-    failedCursor: PagingToken | undefined | false
-  }
-}
+  pubkey: Pubkey
+  settledCursor: PagingToken | undefined | false
+  failedCursor: PagingToken | undefined | false
+}[]
 
 type ListSettledAndFailedLnPaymentsByPubkeyPaginatedResult = {
-  [key: Pubkey]: {
-    settled: ListLnPaymentsResult | LightningServiceError
-    failed: ListLnPaymentsResult | LightningServiceError
-  }
-}
+  pubkey: Pubkey
+  settled: ListLnPaymentsResult | LightningServiceError
+  failed: ListLnPaymentsResult | LightningServiceError
+}[]
 
 interface ILightningService {
   isLocal(pubkey: Pubkey): boolean | LightningServiceError

src/services/lnd/index.ts
@@ -36,8 +36,6 @@ import { LndOfflineError } from "@core/error"
 
 import { timeout } from "@utils"
 
-import { baseLogger } from "@services/logger"
-
 import { TIMEOUT_PAYMENT } from "./auth"
 import { getActiveLnd, getLndFromPubkey, getLnds } from "./utils"
 
@@ -304,19 +302,15 @@ export const LndService = (): ILightningService | LightningServiceError => {
 
   const listSettledAndFailedPaymentsByPubkeyPaginated = async (
     cursors: ListSettledAndFailedLnPaymentsByPubkeyPaginatedArgs,
-  ) => {
-    const result: ListSettledAndFailedLnPaymentsByPubkeyPaginatedResult = {}
+  ): Promise<ListSettledAndFailedLnPaymentsByPubkeyPaginatedResult> => {
+    const nullListLnPaymentsResult: ListLnPaymentsResult = {
+      lnPayments: [],
+      endCursor: false,
+    }
 
-    for (const key in cursors) {
-      const pubkey = key as Pubkey
-      const { settledCursor, failedCursor } = cursors[pubkey]
-
-      const nullListLnPaymentsResult: ListLnPaymentsResult = {
-        lnPayments: [],
-        endCursor: false,
-      }
-
-      result[pubkey] = {
+    return Promise.all(
+      cursors.map(async ({ pubkey, settledCursor, failedCursor }) => ({
+        pubkey,
         settled:
           settledCursor !== false
             ? await listSettledPayments({
@@ -331,10 +325,8 @@ export const LndService = (): ILightningService | LightningServiceError => {
                 pubkey,
               })
             : nullListLnPaymentsResult,
-      }
-    }
-
-    return result
+      })),
+    )
   }
 
   const cancelInvoice = async ({

GaloyMoney/blink

src/domain/bitcoin/lightning/index.types.d.ts
@@ -130,18 +130,16 @@ type ListLnPaymentsResult = {
 }
 
 type ListSettledAndFailedLnPaymentsByPubkeyArgs = {
-  [key: Pubkey]: {
-    settledAfter: PagingToken | undefined | false
-    failedAfter: PagingToken | undefined | false
-  }
-}
+  settledAfter: PagingToken | undefined | false
+  failedAfter: PagingToken | undefined | false
+  pubkey: Pubkey
+}[]
 
 type ListSettledAndFailedLnPaymentsByPubkeyResult = {
-  [key: Pubkey]: {
-    settled: ListLnPaymentsResult | LightningServiceError
-    failed: ListLnPaymentsResult | LightningServiceError
-  }
-}
+  settled: ListLnPaymentsResult | LightningServiceError
+  failed: ListLnPaymentsResult | LightningServiceError
+  pubkey: Pubkey
+}[]
 
 interface ILightningService {
   isLocal(pubkey: Pubkey): boolean | LightningServiceError

src/services/lnd/index.ts
@@ -36,8 +36,6 @@ import { LndOfflineError } from "@core/error"
 
 import { timeout } from "@utils"
 
-import { baseLogger } from "@services/logger"
-
 import { TIMEOUT_PAYMENT } from "./auth"
 import { getActiveLnd, getLndFromPubkey, getLnds } from "./utils"
 
@@ -304,19 +302,15 @@ export const LndService = (): ILightningService | LightningServiceError => {
 
   const listSettledAndFailedPaymentsByPubkey = async (
     cursors: ListSettledAndFailedLnPaymentsByPubkeyArgs,
-  ) => {
-    const result: ListSettledAndFailedLnPaymentsByPubkeyResult = {}
+  ): Promise<ListSettledAndFailedLnPaymentsByPubkeyResult> => {
+    const nullListLnPaymentsResult: ListLnPaymentsResult = {
+      lnPayments: [],
+      endCursor: false,
+    }
 
-    for (const key in cursors) {
-      const pubkey = key as Pubkey
-      const { settledAfter, failedAfter } = cursors[pubkey]
-
-      const nullListLnPaymentsResult: ListLnPaymentsResult = {
-        lnPayments: [],
-        endCursor: false,
-      }
-
-      result[pubkey] = {
+    return Promise.all(
+      cursors.map(async ({ pubkey, settledAfter, failedAfter }) => ({
+        pubkey,
         settled:
           settledAfter !== false
             ? await listSettledPayments({
@@ -331,10 +325,8 @@ export const LndService = (): ILightningService | LightningServiceError => {
                 pubkey,
               })
             : nullListLnPaymentsResult,
-      }
-    }
-
-    return result
+      })),
+    )
   }
 
   const cancelInvoice = async ({