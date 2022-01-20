997f85ad0ead7b387c43785f24462e719db5f995
Kartik Shah <krtk.6160@gmail.com>
2022-01-20 21:19:51 +0530

    Refactor script to read from json file

--- src/debug/reimburse.ts
+++ src/debug/reimburse.ts
@@ -1,16 +1,28 @@
 /**
  * how to run:
+ *
+ * Make sure there's a file named reimbursements.json in src/debug
+ * following the structure:
+ * {
+ *  "feeUpdateOperations" = [
+ *    { "walletId": "first-wallet-id", fee: 13 },
+ *    { "walletId": "second-wallet-id", fee: 10 },
+ *  ]
+ * }
  * . ./.envrc && yarn ts-node --files -r tsconfig-paths/register src/debug/reimburse.ts
  */
 
 import { intraledgerPaymentSendWalletId } from "@app/wallets"
+import { checkedToSats } from "@domain/bitcoin"
+import { checkedToWalletId } from "@domain/wallets"
 import { getBankOwnerWalletId } from "@services/ledger/accounts"
 import { baseLogger } from "@services/logger"
 import { setupMongoConnection } from "@services/mongodb"
+import { reimbursements } from "./reimbursements.json"
 
 type reimbursement = {
-  recipientWalletId: WalletId
-  amount: Satoshis
+  recipientWalletId: string
+  amount: number
   memo: string
 }
 
@@ -20,9 +32,22 @@ const reimburse = async (reimbursements: Array<reimbursement>) => {
   const bankOwnerWalletId = await getBankOwnerWalletId()
 
   for (const reimbursement of reimbursements) {
+    const recipientWalletId = checkedToWalletId(reimbursement.recipientWalletId)
+    if (recipientWalletId instanceof Error) {
+      console.error(`Invalid walletId: ${recipientWalletId}`)
+      continue
+    }
+
+    const amount = checkedToSats(reimbursement.amount)
+
+    if (amount instanceof Error) {
+      console.error(`Invalid amount: ${amount}`)
+      continue
+    }
+
     const reimburseResult = await intraledgerPaymentSendWalletId({
-      recipientWalletId: reimbursement.recipientWalletId,
-      amount: reimbursement.amount,
+      recipientWalletId,
+      amount,
       logger: baseLogger,
       senderWalletId: bankOwnerWalletId,
       memo: reimbursement.memo,
@@ -30,29 +55,5 @@ const reimburse = async (reimbursements: Array<reimbursement>) => {
     console.log({ ...reimbursement, reimbursementStatus: reimburseResult })
   }
 }
-
-// Populate the array below with the recipient wallet public ids, sats and memo to be shown in the reimbursement txn
-
-const reimbursements: Array<reimbursement> = [
-  {
-    recipientWalletId: "" as WalletId,
-    amount: 0 as Satoshis,
-    memo: "",
-  },
-]
-
-// For example:
-// const reimbursements = [
-//   {
-//     recipientWalletId: "d7d03fea-7e8f-4d38-8f4f-169ae455341b" as WalletId,
-//     amount: 10 as Satoshis,
-//     memo: "test",
-//   },
-//   {
-//     recipientWalletId: "d7d03fea-7e8f-4d38-8f4f-169ae455341b" as WalletId,
-//     amount: 11 as Satoshis,
-//     memo: "test1",
-//   },
-// ]
 
 reimburse(reimbursements).then(console.log).catch(console.error)
