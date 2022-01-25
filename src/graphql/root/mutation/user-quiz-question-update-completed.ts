kelvinator07/galoy

chore(tracing): add user attributes to quiz question resolver span

src/graphql/root/mutation/user-quiz-question-update-completed.ts
@@ -3,6 +3,11 @@ import { onboardingEarn } from "@config/app"
 import { GT } from "@graphql/index"
 
 import UserQuizQuestionUpdateCompletedPayload from "@graphql/types/payload/user-quiz-question-update-completed"
+import {
+  addAttributesToCurrentSpanAndPropagate,
+  SemanticAttributes,
+  ENDUSER_ALIAS,
+} from "@services/tracing"
 
 const UserQuizQuestionUpdateCompletedInput = new GT.Input({
   name: "UserQuizQuestionUpdateCompletedInput",
@@ -16,34 +21,41 @@ const UserQuizQuestionUpdateCompletedMutation = GT.Field({
   args: {
     input: { type: GT.NonNull(UserQuizQuestionUpdateCompletedInput) },
   },
-  resolve: async (_, args, { uid, logger }) => {
-    const { id } = args.input
+  resolve: async (_, args, { uid, domainUser, logger }) =>
+    addAttributesToCurrentSpanAndPropagate(
+      {
+        [SemanticAttributes.ENDUSER_ID]: domainUser?.id,
+        [ENDUSER_ALIAS]: domainUser?.username,
+      },
+      async () => {
+        const { id } = args.input
 
-    if (!onboardingEarn[id]) {
-      return { errors: [{ message: "Invalid input" }] }
-    }
+        if (!onboardingEarn[id]) {
+          return { errors: [{ message: "Invalid input" }] }
+        }
 
-    try {
-      const quizQuestions = await Accounts.addEarn({
-        quizQuestionId: id,
-        accountId: uid,
-        logger,
-      })
-      const question = quizQuestions[0]
+        try {
+          const quizQuestions = await Accounts.addEarn({
+            quizQuestionId: id,
+            accountId: uid,
+            logger,
+          })
+          const question = quizQuestions[0]
 
-      return {
-        errors: [],
-        userQuizQuestion: {
-          question: { id: question.id, earnAmount: question.value },
-          completed: question.completed,
-        },
-      }
-    } catch (err) {
-      return {
-        errors: [{ message: err.message || err.name }],
-      }
-    }
-  },
+          return {
+            errors: [],
+            userQuizQuestion: {
+              question: { id: question.id, earnAmount: question.value },
+              completed: question.completed,
+            },
+          }
+        } catch (err) {
+          return {
+            errors: [{ message: err.message || err.name }],
+          }
+        }
+      },
+    ),
 })
 
 export default UserQuizQuestionUpdateCompletedMutation

GaloyMoney/blink

src/graphql/root/mutation/user-quiz-question-update-completed.ts
@@ -3,6 +3,11 @@ import { mapError } from "@graphql/error-map"
 import { GT } from "@graphql/index"
 
 import UserQuizQuestionUpdateCompletedPayload from "@graphql/types/payload/user-quiz-question-update-completed"
+import {
+  addAttributesToCurrentSpanAndPropagate,
+  SemanticAttributes,
+  ENDUSER_ALIAS,
+} from "@services/tracing"
 
 const UserQuizQuestionUpdateCompletedInput = new GT.Input({
   name: "UserQuizQuestionUpdateCompletedInput",
@@ -19,28 +24,39 @@ const UserQuizQuestionUpdateCompletedMutation = GT.Field({
   resolve: async (
     _,
     args,
-    { domainAccount, logger }: { domainAccount: Account; logger: Logger },
-  ) => {
-    const { id } = args.input
-
-    const question = await Accounts.addEarn({
-      quizQuestionId: id,
-      accountId: domainAccount.id,
+    {
+      domainUser,
+      domainAccount,
       logger,
-    })
-    if (question instanceof Error) {
-      const appErr = mapError(question)
-      return { errors: [{ message: appErr.message }] }
-    }
-
-    return {
-      errors: [],
-      userQuizQuestion: {
-        question,
-        completed: true,
+    }: { domainUser: User; domainAccount: Account; logger: Logger },
+  ) =>
+    addAttributesToCurrentSpanAndPropagate(
+      {
+        [SemanticAttributes.ENDUSER_ID]: domainUser.id,
+        [ENDUSER_ALIAS]: domainAccount.username,
       },
-    }
-  },
+      async () => {
+        const { id } = args.input
+
+        const question = await Accounts.addEarn({
+          quizQuestionId: id,
+          accountId: domainAccount.id,
+          logger,
+        })
+        if (question instanceof Error) {
+          const appErr = mapError(question)
+          return { errors: [{ message: appErr.message }] }
+        }
+
+        return {
+          errors: [],
+          userQuizQuestion: {
+            question,
+            completed: true,
+          },
+        }
+      },
+    ),
 })
 
 export default UserQuizQuestionUpdateCompletedMutation
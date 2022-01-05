420aaeeaf8265f2ff0b8dbb78d5d8ec110ef9113
vindard <17693119+vindard@users.noreply.github.com>
2022-01-05 17:51:40 -0400

    chore(tracing): add user attributes to quiz question resolver span

--- src/graphql/root/mutation/user-quiz-question-update-completed.ts
+++ src/graphql/root/mutation/user-quiz-question-update-completed.ts
@@ -3,6 +3,11 @@ import { mapError } from "@graphql/error-map"
 import { GT } from "@graphql/index"
 
 import UserQuizQuestionUpdateCompletedPayload from "@graphql/types/payload/user-quiz-question-update-completed"
+import {
+  addAttributesToCurrentSpanAndPropagate,
+  SemanticAttributes,
+  ENDUSER_ALIAS,
+} from "@services/tracing"
 
 const UserQuizQuestionUpdateCompletedInput = new GT.Input({
   name: "UserQuizQuestionUpdateCompletedInput",
@@ -19,28 +24,39 @@ const UserQuizQuestionUpdateCompletedMutation = GT.Field({
   resolve: async (
     _,
     args,
-    { domainAccount, logger }: { domainAccount: Account; logger: Logger },
-  ) => {
-    const { id } = args.input
-
-    const question = await Accounts.addEarn({
-      quizQuestionId: id,
-      accountId: domainAccount.id,
+    {
+      domainUser,
+      domainAccount,
       logger,
-    })
-    if (question instanceof Error) {
-      const appErr = mapError(question)
-      return { errors: [{ message: appErr.message }] }
-    }
-
-    return {
-      errors: [],
-      userQuizQuestion: {
-        question,
-        completed: true,
+    }: { domainUser: User; domainAccount: Account; logger: Logger },
+  ) =>
+    addAttributesToCurrentSpanAndPropagate(
+      {
+        [SemanticAttributes.ENDUSER_ID]: domainUser.id,
+        [ENDUSER_ALIAS]: domainAccount.username,
       },
-    }
-  },
+      async () => {
+        const { id } = args.input
+
+        const question = await Accounts.addEarn({
+          quizQuestionId: id,
+          accountId: domainAccount.id,
+          logger,
+        })
+        if (question instanceof Error) {
+          const appErr = mapError(question)
+          return { errors: [{ message: appErr.message }] }
+        }
+
+        return {
+          errors: [],
+          userQuizQuestion: {
+            question,
+            completed: true,
+          },
+        }
+      },
+    ),
 })
 
 export default UserQuizQuestionUpdateCompletedMutation
