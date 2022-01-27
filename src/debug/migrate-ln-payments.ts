c222b65bcc05f866c4f086b0246803836c4b33e8
vindard <17693119+vindard@users.noreply.github.com>
2022-01-27 16:48:58 -0400

    refactor: move backfill-by-listpayments logic into separate function

--- src/debug/migrate-ln-payments.ts
+++ src/debug/migrate-ln-payments.ts
@@ -10,67 +10,71 @@ export const migrateLnPaymentsFromLnd = async (): Promise<true | ApplicationErro
 
   for (const key of pubkeys) {
     const pubkey = key as Pubkey
+    await migrateLnPaymentsByFunction({ pubkey, listFn: lndService.listPayments })
+  }
+  return true
+}
 
-    let after: PagingStartToken | PagingContinueToken | PagingStopToken = undefined
-    while (after !== false) {
-      const results: ListLnPaymentsResult | LightningServiceError =
-        await lndService.listPayments({
-          pubkey,
-          after,
-        })
-      if (results instanceof Error) {
-        baseLogger.error(
-          { error: results },
-          `Could not fetch payments for pubkey ${pubkey}`,
-        )
-        break
+const migrateLnPaymentsByFunction = async ({ pubkey, listFn }): Promise<true> => {
+  let after: PagingStartToken | PagingContinueToken | PagingStopToken = undefined
+  while (after !== false) {
+    const results: ListLnPaymentsResult | LightningServiceError = await listFn({
+      pubkey,
+      after,
+    })
+    if (results instanceof Error) {
+      baseLogger.error(
+        { error: results },
+        `Could not fetch payments for pubkey ${pubkey}`,
+      )
+      break
+    }
+    if (after == results.endCursor) break
+    after = results.endCursor
+
+    const hashes = results.lnPayments.map((p) => p.paymentHash)
+
+    // Fetch all hashes from lnPayments repo
+    const lnPaymentsRepo = LnPaymentsRepository()
+    const lnPaymentsPersisted = await lnPaymentsRepo.listByPaymentHashes(hashes)
+    if (lnPaymentsPersisted instanceof Error) continue
+
+    // Persist if lndPayment doesn't exist
+    for (const payment of results.lnPayments) {
+      const persistedPaymentLookup = lnPaymentsPersisted.find(
+        (elem) => elem.paymentHash === payment.paymentHash,
+      )
+      if (persistedPaymentLookup) continue
+
+      const partialLnPayment = {
+        paymentHash: payment.paymentHash,
+        paymentRequest: payment.paymentRequest,
+        sentFromPubkey: pubkey,
       }
-      if (after == results.endCursor) break
-      after = results.endCursor
+      const newLnPayment =
+        payment.status === PaymentStatus.Pending
+          ? partialLnPayment
+          : {
+              ...partialLnPayment,
+              createdAt: payment.createdAt,
+              status: payment.status,
+              milliSatsAmount: payment.milliSatsAmount,
+              roundedUpAmount: payment.roundedUpAmount,
+              confirmedDetails: payment.confirmedDetails,
+              attempts: payment.attempts,
+              isCompleteRecord: true,
+            }
 
-      const hashes = results.lnPayments.map((p) => p.paymentHash)
-
-      // Fetch all hashes from lnPayments repo
-      const lnPaymentsRepo = LnPaymentsRepository()
-      const lnPaymentsPersisted = await lnPaymentsRepo.listByPaymentHashes(hashes)
-      if (lnPaymentsPersisted instanceof Error) continue
-
-      // Persist if lndPayment doesn't exist
-      for (const payment of results.lnPayments) {
-        const persistedPaymentLookup = lnPaymentsPersisted.find(
-          (elem) => elem.paymentHash === payment.paymentHash,
+      const updatedPaymentLookup = await LnPaymentsRepository().persistNew(newLnPayment)
+      if (updatedPaymentLookup instanceof Error) {
+        baseLogger.error(
+          { error: updatedPaymentLookup },
+          "Could not update LnPayments repository",
         )
-        if (persistedPaymentLookup) continue
-
-        const partialLnPayment = {
-          paymentHash: payment.paymentHash,
-          paymentRequest: payment.paymentRequest,
-          sentFromPubkey: pubkey,
-        }
-        const newLnPayment =
-          payment.status === PaymentStatus.Pending
-            ? partialLnPayment
-            : {
-                ...partialLnPayment,
-                createdAt: payment.createdAt,
-                status: payment.status,
-                milliSatsAmount: payment.milliSatsAmount,
-                roundedUpAmount: payment.roundedUpAmount,
-                confirmedDetails: payment.confirmedDetails,
-                attempts: payment.attempts,
-                isCompleteRecord: true,
-              }
-
-        const updatedPaymentLookup = await LnPaymentsRepository().persistNew(newLnPayment)
-        if (updatedPaymentLookup instanceof Error) {
-          baseLogger.error(
-            { error: updatedPaymentLookup },
-            "Could not update LnPayments repository",
-          )
-          continue
-        }
+        continue
       }
     }
   }
+
   return true
 }
