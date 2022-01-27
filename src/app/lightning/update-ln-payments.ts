a3b7756fb4afb259282c4df70ea300484da839d7
vindard <17693119+vindard@users.noreply.github.com>
2022-01-27 17:04:54 -0400

    refactor: move updateLnPayments listpayments logic into separate function

--- src/app/lightning/update-ln-payments.ts
+++ src/app/lightning/update-ln-payments.ts
@@ -18,57 +18,71 @@ export const updateLnPayments = async (): Promise<true | ApplicationError> => {
 
   for (const key of pubkeys) {
     const pubkey = key as Pubkey
-
-    let after: PagingStartToken | PagingContinueToken | PagingStopToken = undefined
-    while (
-      processedLnPaymentsHashes.length < incompleteLnPayments.length &&
-      after !== false
-    ) {
-      // Fetch from Lightning service
-      const results: ListLnPaymentsResult | LightningServiceError =
-        await lndService.listSettledAndFailedPayments({
-          pubkey,
-          after,
-        })
-      if (results instanceof Error) {
-        baseLogger.error(
-          { error: results },
-          `Could not fetch payments for pubkey ${pubkey}`,
-        )
-        break
-      }
-      if (after === results.endCursor) break
-      after = results.endCursor
-
-      // Update LnPayments repository
-      for (const payment of results.lnPayments) {
-        const persistedPaymentLookup = incompleteLnPayments.find(
-          (elem) => elem.paymentHash === payment.paymentHash,
-        )
-        if (!persistedPaymentLookup) continue
-
-        persistedPaymentLookup.createdAt = payment.createdAt
-        persistedPaymentLookup.status = payment.status
-        persistedPaymentLookup.milliSatsAmount = payment.milliSatsAmount
-        persistedPaymentLookup.roundedUpAmount = payment.roundedUpAmount
-        persistedPaymentLookup.confirmedDetails = payment.confirmedDetails
-        persistedPaymentLookup.attempts = payment.attempts
-
-        persistedPaymentLookup.isCompleteRecord = true
-
-        const updatedPaymentLookup = await LnPaymentsRepository().update(
-          persistedPaymentLookup,
-        )
-        if (updatedPaymentLookup instanceof Error) {
-          baseLogger.error(
-            { error: updatedPaymentLookup },
-            "Could not update LnPayments repository",
-          )
-          continue
-        }
-        processedLnPaymentsHashes.push(payment.paymentHash)
-      }
-    }
+    await updateLnPaymentsByFunction({
+      processedLnPaymentsHashes,
+      incompleteLnPayments,
+      pubkey,
+      listFn: lndService.listSettledAndFailedPayments,
+    })
   }
   return true
+}
+
+const updateLnPaymentsByFunction = async ({
+  processedLnPaymentsHashes,
+  incompleteLnPayments,
+  pubkey,
+  listFn,
+}) => {
+  let after: PagingStartToken | PagingContinueToken | PagingStopToken = undefined
+  while (
+    processedLnPaymentsHashes.length < incompleteLnPayments.length &&
+    after !== false
+  ) {
+    // Fetch from Lightning service
+    const results: ListLnPaymentsResult | LightningServiceError = await listFn({
+      pubkey,
+      after,
+    })
+    if (results instanceof Error) {
+      baseLogger.error(
+        { error: results },
+        `Could not fetch payments for pubkey ${pubkey}`,
+      )
+      break
+    }
+    if (after === results.endCursor) break
+    after = results.endCursor
+
+    // Update LnPayments repository
+    for (const payment of results.lnPayments) {
+      const persistedPaymentLookup = incompleteLnPayments.find(
+        (elem) => elem.paymentHash === payment.paymentHash,
+      )
+      if (!persistedPaymentLookup) continue
+
+      persistedPaymentLookup.createdAt = payment.createdAt
+      persistedPaymentLookup.status = payment.status
+      persistedPaymentLookup.milliSatsAmount = payment.milliSatsAmount
+      persistedPaymentLookup.roundedUpAmount = payment.roundedUpAmount
+      persistedPaymentLookup.confirmedDetails = payment.confirmedDetails
+      persistedPaymentLookup.attempts = payment.attempts
+
+      persistedPaymentLookup.isCompleteRecord = true
+
+      const updatedPaymentLookup = await LnPaymentsRepository().update(
+        persistedPaymentLookup,
+      )
+      if (updatedPaymentLookup instanceof Error) {
+        baseLogger.error(
+          { error: updatedPaymentLookup },
+          "Could not update LnPayments repository",
+        )
+        continue
+      }
+      processedLnPaymentsHashes.push(payment.paymentHash)
+    }
+  }
+
+  return processedLnPaymentsHashes
 }
