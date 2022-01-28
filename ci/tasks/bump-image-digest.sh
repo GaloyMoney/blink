afc138b2a230c4a6a7b9f54892a4e92ed22805f2
bodymindarts <justin@galoy.io>
2022-01-28 14:56:11 +0100

    ci: fix commit message in charts PR

--- ci/tasks/bump-image-digest.sh
+++ ci/tasks/bump-image-digest.sh
@@ -24,5 +24,5 @@ fi
   git merge --no-edit ${BRANCH}
   git add -A
   git status
-  git commit -m "build: bump galoy image to '${digest}'"
+  git commit -m "chore(deps): bump galoy image to '${digest}'"
 )
