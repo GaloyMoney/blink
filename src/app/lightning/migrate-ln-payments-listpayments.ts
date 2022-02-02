974db111dcc6f0601744b4b9edcdb45e29ccd4bd
vindard <17693119+vindard@users.noreply.github.com>
2022-02-02 10:59:44 -0400

    wip: add temp changes for testing

---
+++ src/app/lightning/migrate-ln-payments-listpayments.ts
@@ -0,0 +1,242 @@
+import { getPayments } from "lightning"
+
+import {
+  CorruptLndDbError,
+  PaymentStatus,
+  UnknownLightningServiceError,
+} from "@domain/bitcoin/lightning"
+import { CouldNotFindError } from "@domain/errors"
+import { LndService } from "@services/lnd"
+import { getLndFromPubkey } from "@services/lnd/utils"
+import { baseLogger } from "@services/logger"
+import { LnPaymentsRepository } from "@services/mongoose"
+import {
+  addAttributesToCurrentSpan,
+  addEventToCurrentSpan,
+  asyncRunInSpan,
+  SemanticAttributes,
+} from "@services/tracing"
+
+const indexRegex = /{"offset":(\d+),"limit":\d+}/
+
+export const migrateLnPaymentsFromLnd = async () =>
+  asyncRunInSpan(
+    "debug.migrateLnPaymentsFromLnd",
+    { [SemanticAttributes.CODE_FUNCTION]: "debug.migrateLnPaymentsFromLnd" },
+    async (): Promise<true | ApplicationError> => {
+      const lndService = LndService()
+      if (lndService instanceof Error) return lndService
+
+      const listFns = [
+        lndService.listSettledAndPendingPayments,
+        lndService.listFailedPayments,
+      ]
+      const pubkeys = lndService.listActivePubkeys()
+
+      await listFns.map((listFn): Promise<true>[] =>
+        pubkeys.map(async (pubkey): Promise<true> => {
+          let count = await getFirstIndex(pubkey)
+          if (count instanceof Error) return true
+
+          while (count !== 0) {
+            count = await asyncRunInSpan(
+              "debug.migrateLnPaymentsByFunction",
+              {
+                [SemanticAttributes.CODE_FUNCTION]: "debug.migrateLnPaymentsByFunction",
+                "migrateLnPaymentsByFunction.iteration": `${listFn.name}:${count}`,
+                "migrateLnPaymentsByFunction.pubkey": pubkey,
+              },
+              async () => {
+                if (count instanceof Error) return count
+                count = await migrateLnPaymentsByFunction({
+                  offset: count,
+                  pubkey,
+                  listFn,
+                })
+                addAttributesToCurrentSpan({
+                  "migrateLnPaymentsByFunction.nextoffset":
+                    count instanceof Error ? count.name : count,
+                })
+                return count
+              },
+            )
+            if (count instanceof Error) break
+          }
+
+          return true
+        }),
+      )
+      return true
+    },
+  )
+
+const migrateLnPaymentsByFunction = async ({
+  offset,
+  pubkey,
+  listFn,
+}: {
+  offset: number
+  pubkey: Pubkey
+  listFn: (
+    args: ListLnPaymentsArgs,
+  ) => Promise<ListLnPaymentsResult | LightningServiceError>
+}): Promise<number | Error> => {
+  let count = offset
+  // Get next payment and unpack
+  const results: ListLnPaymentsResult | LightningServiceError = await listFn({
+    pubkey,
+    after: `{"offset":${offset},"limit":1}` as PagingContinueToken,
+  })
+  if (results instanceof CorruptLndDbError) {
+    const errMsg = `${results.name} at offset ${offset}`
+    baseLogger.error(errMsg)
+    addEventToCurrentSpan(errMsg)
+    count--
+    return count
+  }
+  if (results instanceof Error) {
+    const errMsg = `Could not fetch payments for pubkey ${pubkey}`
+    baseLogger.error({ error: results }, errMsg)
+    addEventToCurrentSpan(errMsg)
+    return results
+  }
+  const {
+    lnPayments: [payment],
+    endCursor,
+  } = results
+
+  // Set next cursor or exit
+  if (endCursor === false) {
+    count = 0
+  } else {
+    const indexMatch = endCursor.match(indexRegex)?.[1]
+    if (!indexMatch) {
+      count = 0
+    } else {
+      const index = parseInt(indexMatch)
+      if (isNaN(index)) {
+        baseLogger.error(`Invalid continue token: ${endCursor}`)
+        return new UnknownLightningServiceError()
+      }
+      count = index === count ? index - 1 : index
+    }
+  }
+
+  if (payment === undefined) {
+    const errMsg = `Skipping, 'payments' array empty for offset ${offset}`
+    baseLogger.error(errMsg)
+    addEventToCurrentSpan(errMsg)
+    return count
+  }
+  addAttributesToCurrentSpan({
+    "migrateLnPaymentsByFunction.paymentHash": payment.paymentHash,
+  })
+
+  // Fetch lnPayment from collection
+  const lnPaymentsRepo = LnPaymentsRepository()
+  const persistedPaymentLookup = await lnPaymentsRepo.findByPaymentHash(
+    payment.paymentHash,
+  )
+  if (
+    persistedPaymentLookup instanceof Error &&
+    !(persistedPaymentLookup instanceof CouldNotFindError)
+  ) {
+    const errMsg = `Skipping fetched payment at offset ${offset} for payment hash ${payment.paymentHash}`
+    baseLogger.error({ error: persistedPaymentLookup }, errMsg)
+    addEventToCurrentSpan(errMsg)
+    return count
+  }
+
+  // LnPayment: persist new
+  if (persistedPaymentLookup instanceof CouldNotFindError) {
+    const partialLnPayment = {
+      paymentHash: payment.paymentHash,
+      paymentRequest: payment.paymentRequest,
+      sentFromPubkey: pubkey,
+    }
+    const newLnPayment =
+      payment.status === PaymentStatus.Pending
+        ? partialLnPayment
+        : {
+            ...partialLnPayment,
+            createdAt: payment.createdAt,
+            status: payment.status,
+            milliSatsAmount: payment.milliSatsAmount,
+            roundedUpAmount: payment.roundedUpAmount,
+            confirmedDetails: payment.confirmedDetails,
+            attempts: payment.attempts,
+            isCompleteRecord: true,
+          }
+
+    const updatedPaymentLookup = await LnPaymentsRepository().persistNew(newLnPayment)
+    if (updatedPaymentLookup instanceof Error) {
+      const errMsg = `Could not persist new LnPayment at offset ${offset} for payment hash ${payment.paymentHash}`
+      baseLogger.error({ error: updatedPaymentLookup }, errMsg)
+      addEventToCurrentSpan(errMsg)
+    }
+    baseLogger.info(
+      `Success! Persisted new at offset ${offset} for payment hash ${payment.paymentHash}`,
+    )
+    addAttributesToCurrentSpan({
+      "migrateLnPaymentsByFunction.success": `persistedNew:${offset}:${payment.paymentHash}`,
+    })
+    return count
+  }
+
+  // LnPayment: already persisted
+  if (persistedPaymentLookup.isCompleteRecord) {
+    const infoMsg = `Skipping, record already exists at offset ${offset} for payment hash ${payment.paymentHash}`
+    baseLogger.info(infoMsg)
+    addEventToCurrentSpan(infoMsg)
+    return count
+  }
+
+  // LnPayment: exists but payment is still pending
+  if (payment.status === PaymentStatus.Pending) {
+    const infoMsg = `Skipping, payment still pending at offset ${offset} for payment hash ${payment.paymentHash}`
+    baseLogger.info(infoMsg)
+    addEventToCurrentSpan(infoMsg)
+    return count
+  }
+
+  // LnPayment: update completed payment
+  persistedPaymentLookup.createdAt = payment.createdAt
+  persistedPaymentLookup.status = payment.status
+  persistedPaymentLookup.milliSatsAmount = payment.milliSatsAmount
+  persistedPaymentLookup.roundedUpAmount = payment.roundedUpAmount
+  persistedPaymentLookup.confirmedDetails = payment.confirmedDetails
+  persistedPaymentLookup.attempts = payment.attempts
+
+  persistedPaymentLookup.isCompleteRecord = true
+
+  const updatedPaymentLookup = await LnPaymentsRepository().update(persistedPaymentLookup)
+  if (updatedPaymentLookup instanceof Error) {
+    const errMsg = `Could not update LnPayments repository at offset ${offset} for payment hash ${payment.paymentHash}`
+    baseLogger.error({ error: updatedPaymentLookup }, errMsg)
+    addEventToCurrentSpan(errMsg)
+  }
+  const infoMsg = `Success! Updated existing at offset ${offset} for payment hash ${payment.paymentHash}`
+  baseLogger.info(infoMsg)
+  addAttributesToCurrentSpan({
+    "migrateLnPaymentsByFunction.success": `updated:${offset}:${payment.paymentHash}`,
+  })
+  return count
+}
+
+const getFirstIndex = async (pubkey: Pubkey): Promise<number | ApplicationError> => {
+  const { lnd } = getLndFromPubkey({ pubkey })
+  let paymentsResults: GetPaymentsResults
+  try {
+    paymentsResults = await getPayments({ lnd, limit: 1 })
+  } catch (err) {
+    return new UnknownLightningServiceError(err)
+  }
+
+  const { next } = paymentsResults
+  if (next === undefined) return new UnknownLightningServiceError()
+
+  const indexMatch = next.match(indexRegex)?.[1]
+  return indexMatch === undefined
+    ? new UnknownLightningServiceError()
+    : parseInt(indexMatch) + 1
+}
--- test/integration/02-user-wallet/02-send-lightning.spec.ts
+++ test/integration/02-user-wallet/02-send-lightning.spec.ts
@@ -24,6 +24,9 @@ import { getActiveLnd, getInvoiceAttempt } from "@services/lnd/utils"
 import { baseLogger } from "@services/logger"
 import { LnPaymentsRepository, WalletInvoicesRepository } from "@services/mongoose"
 import { WalletInvoice } from "@services/mongoose/schema"
+
+import { migrateLnPaymentsFromLnd } from "@app/lightning/migrate-ln-payments-listpayments"
+
 import { sleep } from "@utils"
 
 import {
@@ -113,6 +116,11 @@ afterAll(() => {
 })
 
 describe("UserWallet - Lightning Pay", () => {
+  it.skip("custom test", async () => {
+    const res = await migrateLnPaymentsFromLnd()
+    console.log(res)
+  })
+
   it("sends to another Galoy user with memo", async () => {
     const memo = "invoiceMemo"
 
@@ -759,7 +767,7 @@ describe("UserWallet - Lightning Pay", () => {
         expect(finalBalance).toBe(initialBalance - amountInvoice - fee)
       })
 
-      it("pay hodl invoice & ln payments repo updates", async () => {
+      it.skip("pay hodl invoice & ln payments repo updates", async () => {
         const { id, secret } = createInvoiceHash()
 
         const { request } = await createHodlInvoice({
