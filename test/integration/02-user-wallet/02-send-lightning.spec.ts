25888cd1876ce53a29163ff4ce7737b9d18b6df5
vindard <17693119+vindard@users.noreply.github.com>
2022-02-02 11:12:32 -0400

    wip: remove temp changes for testing
    This reverts prior commit dccf3bf4c4b621424cc35e255155c967c0318b07.

--- test/integration/02-user-wallet/02-send-lightning.spec.ts
+++ test/integration/02-user-wallet/02-send-lightning.spec.ts
@@ -29,8 +29,6 @@ import {
 } from "@services/mongoose"
 import { WalletInvoice } from "@services/mongoose/schema"
 
-import { migrateLnPaymentsFromLndByHash } from "@app/lightning/migrate-ln-payments-trackpaymentsv2"
-
 import { sleep } from "@utils"
 
 import { getCurrentPrice } from "@app/prices"
@@ -162,11 +160,6 @@ afterAll(() => {
 })
 
 describe("UserWallet - Lightning Pay", () => {
-  it.skip("custom test", async () => {
-    const res = await migrateLnPaymentsFromLndByHash()
-    console.log(res)
-  })
-
   it("fails to pay when withdrawalLimit exceeded", async () => {
     const amountAboveThreshold = toCents(accountLimits.withdrawalLimit + 10)
 
@@ -1079,7 +1072,7 @@ describe("UserWallet - Lightning Pay", () => {
         expect(finalBalance).toBe(initialBalance - amountInvoice - fee)
       })
 
-      it.skip("pay hodl invoice & ln payments repo updates", async () => {
+      it("pay hodl invoice & ln payments repo updates", async () => {
         const { id, secret } = createInvoiceHash()
 
         const { request } = await createHodlInvoice({
