41d487a01e04a750c76cc460fd7f080d77a20649
Sandipan Dey <hey@sandipan.dev>
2022-02-04 20:32:26 +0530

    ci: updated pipeline to release source code with versions

--- ci/tasks/bump-image-digest.sh
+++ ci/tasks/bump-image-digest.sh
@@ -5,12 +5,14 @@ set -eu
 export digest=$(cat ./edge-image/digest)
 export migrate_digest=$(cat ./migrate-edge-image/digest)
 export ref=$(cat ./repo/.git/short_ref)
+export app_version=$(cat version/version)
 
 pushd charts-repo
 
 yq -i e '.image.digest = strenv(digest)' ./charts/galoy/values.yaml
 yq -i e '.image.git_ref = strenv(ref)' ./charts/galoy/values.yaml
 yq -i e '.mongodbMigrateImage.digest = strenv(migrate_digest)' ./charts/galoy/values.yaml
+yq -i e '.appVersion = strenv(app_version)' ./charts/galoy/Chart.yaml
 
 if [[ -z $(git config --global user.email) ]]; then
   git config --global user.email "bot@galoy.io"
