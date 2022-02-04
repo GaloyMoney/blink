4346a4db09795516fb5d43e22457671f47cb4c64
vindard <17693119+vindard@users.noreply.github.com>
2022-02-04 17:20:20 -0400

    refactor: abstract 'while' body into function

--- src/debug/migrate-ln-payments-listpayments.ts
+++ src/debug/migrate-ln-payments-listpayments.ts
@@ -1,3 +1,5 @@
+import { getPayments } from "lightning"
+
 import {
   CorruptLndDbError,
   PaymentStatus,
@@ -8,7 +10,6 @@ import { LndService } from "@services/lnd"
 import { getLndFromPubkey } from "@services/lnd/utils"
 import { baseLogger } from "@services/logger"
 import { LnPaymentsRepository } from "@services/mongoose"
-import { getPayments } from "lightning"
 
 const indexRegex = /{"offset":(\d+),"limit":\d+}/
 
@@ -22,127 +23,137 @@ export const migrateLnPaymentsFromLnd = async (): Promise<true | ApplicationErro
   ]
   const pubkeys = lndService.listActivePubkeys()
 
-  await listFns.map((listFn) =>
-    pubkeys.map(async (pubkey) => migrateLnPaymentsByFunction({ pubkey, listFn })),
+  await listFns.map((listFn): Promise<true>[] =>
+    pubkeys.map(async (pubkey): Promise<true> => {
+      let count = await getFirstIndex(pubkey)
+      if (count instanceof Error) return true
+
+      while (count !== 0) {
+        count = await migrateLnPaymentsByFunction({ offset: count, pubkey, listFn })
+        if (count instanceof Error) break
+      }
+
+      return true
+    }),
   )
   return true
 }
 
-const migrateLnPaymentsByFunction = async ({ pubkey, listFn }): Promise<true> => {
-  let count = await getFirstIndex(pubkey)
-  if (count instanceof Error) return true
+const migrateLnPaymentsByFunction = async ({
+  offset,
+  pubkey,
+  listFn,
+}: {
+  offset: number
+  pubkey: Pubkey
+  listFn: (
+    args: ListLnPaymentsArgs,
+  ) => Promise<ListLnPaymentsResult | LightningServiceError>
+}): Promise<number | Error> => {
+  let count = offset
+  // Get next payment and unpack
+  const results: ListLnPaymentsResult | LightningServiceError = await listFn({
+    pubkey,
+    after: `{"offset":${count},"limit":1}` as PagingContinueToken,
+  })
+  if (results instanceof CorruptLndDbError) {
+    count--
+    return count
+  }
+  if (results instanceof Error) {
+    baseLogger.error({ error: results }, `Could not fetch payments for pubkey ${pubkey}`)
+    return results
+  }
+  const {
+    lnPayments: [payment],
+    endCursor,
+  } = results
 
-  while (count !== 0) {
-    // Get next payment and unpack
-    const results: ListLnPaymentsResult | LightningServiceError = await listFn({
-      pubkey,
-      after: `{"offset":${count},"limit":1}` as PagingContinueToken,
-    })
-    if (results instanceof CorruptLndDbError) {
-      count--
-      continue
-    }
-    if (results instanceof Error) {
-      baseLogger.error(
-        { error: results },
-        `Could not fetch payments for pubkey ${pubkey}`,
-      )
-      break
-    }
-    const {
-      lnPayments: [payment],
-      endCursor,
-    } = results
-
-    // Set next cursor or exit
-    if (endCursor === false) {
+  // Set next cursor or exit
+  if (endCursor === false) {
+    count = 0
+  } else {
+    const indexMatch = endCursor.match(indexRegex)?.[1]
+    if (!indexMatch) {
       count = 0
     } else {
-      const indexMatch = endCursor.match(indexRegex)?.[1]
-      if (!indexMatch) {
-        count = 0
-      } else {
-        const index = parseInt(indexMatch)
-        if (isNaN(index)) {
-          baseLogger.error(`Invalid continue token: ${endCursor}`)
-          break
-        }
-        count = index === count ? index - 1 : index
+      const index = parseInt(indexMatch)
+      if (isNaN(index)) {
+        baseLogger.error(`Invalid continue token: ${endCursor}`)
+        return new UnknownLightningServiceError()
       }
+      count = index === count ? index - 1 : index
     }
+  }
 
-    if (payment === undefined) continue
+  if (payment === undefined) return count
 
-    // Fetch lnPayment from collection
-    const lnPaymentsRepo = LnPaymentsRepository()
-    const persistedPaymentLookup = await lnPaymentsRepo.findByPaymentHash(
-      payment.paymentHash,
-    )
-    if (
-      persistedPaymentLookup instanceof Error &&
-      !(persistedPaymentLookup instanceof CouldNotFindError)
-    )
-      continue
+  // Fetch lnPayment from collection
+  const lnPaymentsRepo = LnPaymentsRepository()
+  const persistedPaymentLookup = await lnPaymentsRepo.findByPaymentHash(
+    payment.paymentHash,
+  )
+  if (
+    persistedPaymentLookup instanceof Error &&
+    !(persistedPaymentLookup instanceof CouldNotFindError)
+  )
+    return count
 
-    // LnPayment: persist new
-    if (persistedPaymentLookup instanceof CouldNotFindError) {
-      const partialLnPayment = {
-        paymentHash: payment.paymentHash,
-        paymentRequest: payment.paymentRequest,
-        sentFromPubkey: pubkey,
-      }
-      const newLnPayment =
-        payment.status === PaymentStatus.Pending
-          ? partialLnPayment
-          : {
-              ...partialLnPayment,
-              createdAt: payment.createdAt,
-              status: payment.status,
-              milliSatsAmount: payment.milliSatsAmount,
-              roundedUpAmount: payment.roundedUpAmount,
-              confirmedDetails: payment.confirmedDetails,
-              attempts: payment.attempts,
-              isCompleteRecord: true,
-            }
-
-      const updatedPaymentLookup = await LnPaymentsRepository().persistNew(newLnPayment)
-      if (updatedPaymentLookup instanceof Error) {
-        baseLogger.error(
-          { error: updatedPaymentLookup },
-          "Could not update LnPayments repository",
-        )
-      }
-      continue
+  // LnPayment: persist new
+  if (persistedPaymentLookup instanceof CouldNotFindError) {
+    const partialLnPayment = {
+      paymentHash: payment.paymentHash,
+      paymentRequest: payment.paymentRequest,
+      sentFromPubkey: pubkey,
     }
+    const newLnPayment =
+      payment.status === PaymentStatus.Pending
+        ? partialLnPayment
+        : {
+            ...partialLnPayment,
+            createdAt: payment.createdAt,
+            status: payment.status,
+            milliSatsAmount: payment.milliSatsAmount,
+            roundedUpAmount: payment.roundedUpAmount,
+            confirmedDetails: payment.confirmedDetails,
+            attempts: payment.attempts,
+            isCompleteRecord: true,
+          }
 
-    // LnPayment: already persisted
-    if (persistedPaymentLookup.isCompleteRecord) continue
-
-    // LnPayment: exists but payment is still pending
-    if (payment.status === PaymentStatus.Pending) continue
-
-    // LnPayment: update completed payment
-    persistedPaymentLookup.createdAt = payment.createdAt
-    persistedPaymentLookup.status = payment.status
-    persistedPaymentLookup.milliSatsAmount = payment.milliSatsAmount
-    persistedPaymentLookup.roundedUpAmount = payment.roundedUpAmount
-    persistedPaymentLookup.confirmedDetails = payment.confirmedDetails
-    persistedPaymentLookup.attempts = payment.attempts
-
-    persistedPaymentLookup.isCompleteRecord = true
-
-    const updatedPaymentLookup = await LnPaymentsRepository().update(
-      persistedPaymentLookup,
-    )
+    const updatedPaymentLookup = await LnPaymentsRepository().persistNew(newLnPayment)
     if (updatedPaymentLookup instanceof Error) {
       baseLogger.error(
         { error: updatedPaymentLookup },
         "Could not update LnPayments repository",
       )
     }
+    return count
   }
 
-  return true
+  // LnPayment: already persisted
+  if (persistedPaymentLookup.isCompleteRecord) return count
+
+  // LnPayment: exists but payment is still pending
+  if (payment.status === PaymentStatus.Pending) return count
+
+  // LnPayment: update completed payment
+  persistedPaymentLookup.createdAt = payment.createdAt
+  persistedPaymentLookup.status = payment.status
+  persistedPaymentLookup.milliSatsAmount = payment.milliSatsAmount
+  persistedPaymentLookup.roundedUpAmount = payment.roundedUpAmount
+  persistedPaymentLookup.confirmedDetails = payment.confirmedDetails
+  persistedPaymentLookup.attempts = payment.attempts
+
+  persistedPaymentLookup.isCompleteRecord = true
+
+  const updatedPaymentLookup = await LnPaymentsRepository().update(persistedPaymentLookup)
+  if (updatedPaymentLookup instanceof Error) {
+    baseLogger.error(
+      { error: updatedPaymentLookup },
+      "Could not update LnPayments repository",
+    )
+  }
+  return count
 }
 
 const getFirstIndex = async (pubkey: Pubkey): Promise<number | ApplicationError> => {
