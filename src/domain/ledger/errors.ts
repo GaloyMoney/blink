217ed16dd0a7224ad365476c21cd8900880242a4
vindard <17693119+vindard@users.noreply.github.com>
2022-02-04 09:25:14 -0400

    feat: add getTransactionMetadataById method to Ledger service

--- src/domain/ledger/errors.ts
+++ src/domain/ledger/errors.ts
@@ -4,7 +4,9 @@ export class LedgerError extends Error {
 
 export class LedgerServiceError extends LedgerError {}
 export class FeeDifferenceError extends LedgerError {}
-export class CouldNotFindTransactionError extends LedgerError {}
 export class NoTransactionToSettleError extends LedgerServiceError {}
 export class NoTransactionToUpdateError extends LedgerServiceError {}
 export class UnknownLedgerError extends LedgerServiceError {}
+
+export class CouldNotFindTransactionError extends LedgerError {}
+export class CouldNotFindTransactionMetadataError extends CouldNotFindTransactionError {}
