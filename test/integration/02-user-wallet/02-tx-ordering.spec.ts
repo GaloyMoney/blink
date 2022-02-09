35ca27316005771bbb8f1b86f92a886654af8a7c
vindard <17693119+vindard@users.noreply.github.com>
2022-02-09 14:14:20 -0400

    test: generate some transactions for ordering test

--- test/integration/02-user-wallet/02-tx-ordering.spec.ts
+++ test/integration/02-user-wallet/02-tx-ordering.spec.ts
@@ -1,15 +1,35 @@
 import { Wallets } from "@app"
+import { MEMO_SHARING_SATS_THRESHOLD } from "@config"
+import { toSats } from "@domain/bitcoin"
+import { baseLogger } from "@services/logger"
 
 import {
   createUserWalletFromUserRef,
+  getAccountByTestUserRef,
   getDefaultWalletIdByTestUserRef,
+  getUserRecordByTestUserRef,
 } from "test/helpers"
 
+let userTypeA: UserRecord
+
+let usernameA: Username
+
+let accountB: Account
+
 let walletIdA: WalletId
+let walletIdB: WalletId
 
 beforeAll(async () => {
   await createUserWalletFromUserRef("A")
+  await createUserWalletFromUserRef("B")
+
+  accountB = await getAccountByTestUserRef("B")
+
   walletIdA = await getDefaultWalletIdByTestUserRef("A")
+  walletIdB = await getDefaultWalletIdByTestUserRef("B")
+
+  userTypeA = await getUserRecordByTestUserRef("A")
+  usernameA = userTypeA.username as Username
 })
 
 describe("UserWallet - Transactions ordering", () => {
@@ -17,6 +37,21 @@ describe("UserWallet - Transactions ordering", () => {
   // This is to explicitly capture this behaviour in our tests.
 
   it("transactions are in reverse chronological order", async () => {
+    const count = 3
+    const getMemo = (i) => `Test transaction ordering - ${i}`
+    for (let i = 1; i <= count; i++) {
+      const res = await Wallets.intraledgerPaymentSendUsername({
+        recipientUsername: usernameA,
+        memo: getMemo(i),
+        amount: toSats(MEMO_SHARING_SATS_THRESHOLD + 1),
+        senderWalletId: walletIdB,
+        senderAccount: accountB,
+        logger: baseLogger,
+      })
+      expect(res).not.toBeInstanceOf(Error)
+      if (res instanceof Error) throw res
+    }
+
     const txResult = await Wallets.getTransactionsForWalletId({
       walletId: walletIdA,
     })
@@ -24,8 +59,9 @@ describe("UserWallet - Transactions ordering", () => {
     expect(txns).not.toBeNull()
     if (!txns) return
 
-    expect(txns.length).toBeGreaterThan(1)
+    expect(txns.length).toBeGreaterThanOrEqual(count)
     expect(txns).toEqual([...txns].sort(walletTransactionsReverseByCreatedAt))
+    expect(txns[0].memo).toBe(getMemo(count))
   })
 })
 
