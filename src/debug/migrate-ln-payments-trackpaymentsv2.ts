378615e3d168f39a5043e52f34d08ac8ecfd7cb1
vindard <17693119+vindard@users.noreply.github.com>
2022-02-12 21:03:37 -0400

    feat: add function execute for 'migrate' from debug dir

--- src/debug/migrate-ln-payments-trackpaymentsv2.ts
+++ src/debug/migrate-ln-payments-trackpaymentsv2.ts
@@ -1,3 +1,14 @@
+/**
+ * how to run:
+ *	. ./.envrc && yarn ts-node \
+ *		--files \
+ *			-r tsconfig-paths/register \
+ *			-r src/services/tracing.ts \
+ *		src/debug/migrate-ln-payments-trackpaymentsv2.ts
+ */
+
+import { isUp } from "@services/lnd/health"
+import { params as unauthParams } from "@services/lnd/unauth"
 import { PaymentNotFoundError, PaymentStatus } from "@domain/bitcoin/lightning"
 import { CouldNotFindError } from "@domain/errors"
 import { LedgerService } from "@services/ledger"
@@ -10,10 +21,11 @@ import {
   asyncRunInSpan,
   SemanticAttributes,
 } from "@services/tracing"
+import { setupMongoConnection } from "@services/mongodb"
 
 let lndService
 
-export const migrateLnPaymentsFromLndByHash = async (): Promise<true> => {
+export const main = async (): Promise<true> => {
   lndService = LndService()
   if (lndService instanceof Error) return true
 
@@ -108,3 +120,11 @@ const migrateLnPayment = async (
       return true
     },
   )
+
+setupMongoConnection(false)
+  .then(async (mongoose) => {
+    await Promise.all(unauthParams.map((lndParams) => isUp(lndParams)))
+    await main()
+    if (mongoose) await mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
