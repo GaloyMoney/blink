1a6988a319abef143f1a20eb7d19442dd76b1a9e
Juan P Lopez <dolcalmi@gmail.com>
2022-02-15 13:31:03 -0500

    fix: tracing issue

--- src/services/tracing.ts
+++ src/services/tracing.ts
@@ -2,7 +2,6 @@ import {
   SemanticAttributes,
   SemanticResourceAttributes,
 } from "@opentelemetry/semantic-conventions"
-import type * as graphqlTypes from "graphql"
 import { W3CTraceContextPropagator } from "@opentelemetry/core"
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node"
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http"
@@ -30,7 +29,7 @@ propagation.setGlobalPropagator(new W3CTraceContextPropagator())
 
 // FYI this hook is executed BEFORE the `formatError` hook from apollo
 // The data.errors field here may still change before being returned to the client
-const gqlResponseHook = (span: Span, data: graphqlTypes.ExecutionResult) => {
+const gqlResponseHook = (span: Span, data) => {
   if (data.errors && data.errors.length > 0) {
     recordGqlErrors({ errors: data.errors, span, subPathName: "" })
   }
