c88dbedb2bd80d2c2828f6af85bcd8e6f50cc328
Arvin <17693119+vindard@users.noreply.github.com>
2022-02-22 11:24:10 -0400

    chore: add 'BadAmountForRouteError' details (#1114)

--- src/app/lightning/route-validator.ts
+++ src/app/lightning/route-validator.ts
@@ -2,7 +2,9 @@ import { BadAmountForRouteError } from "@domain/errors"
 
 export const RouteValidator = (rawRoute: RawRoute): RouteValidator => {
   const validate = (amount): true | ApplicationError => {
-    if (amount !== rawRoute.tokens) return new BadAmountForRouteError()
+    if (amount !== rawRoute.tokens) {
+      return new BadAmountForRouteError(`${amount} !== ${rawRoute.tokens}`)
+    }
 
     return true
   }
