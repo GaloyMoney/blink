b784dd914982610a7f67d59fc40302474922f5b6
Juan P Lopez <dolcalmi@gmail.com>
2022-02-22 20:57:23 -0500

    fix: restricted region error

--- src/domain/phone-provider/errors.ts
+++ src/domain/phone-provider/errors.ts
@@ -3,6 +3,7 @@ import { DomainError, ErrorLevel } from "@domain/errors"
 export class PhoneProviderServiceError extends DomainError {}
 
 export class InvalidPhoneNumberPhoneProviderError extends PhoneProviderServiceError {}
+export class RestrictedRegionPhoneProviderError extends PhoneProviderServiceError {}
 export class UnknownPhoneProviderServiceError extends PhoneProviderServiceError {
   level = ErrorLevel.Critical
 }
