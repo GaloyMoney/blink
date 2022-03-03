823331b3eb514a02a759bec3883170c79d9d668e
vindard <17693119+vindard@users.noreply.github.com>
2022-03-03 16:24:35 -0400

    chore: add tracing attributes to queries

--- src/graphql/root/query/me.ts
+++ src/graphql/root/query/me.ts
@@ -1,23 +1,10 @@
-import {
-  addAttributesToCurrentSpanAndPropagate,
-  SemanticAttributes,
-  ACCOUNT_USERNAME,
-} from "@services/tracing"
 import { GT } from "@graphql/index"
 
 import GraphQLUser from "@graphql/types/object/graphql-user"
 
 const MeQuery = GT.Field({
   type: GraphQLUser,
-  resolve: async (_, __, { ip, domainUser, domainAccount }) =>
-    addAttributesToCurrentSpanAndPropagate(
-      {
-        [SemanticAttributes.ENDUSER_ID]: domainUser?.id,
-        [ACCOUNT_USERNAME]: domainAccount?.username,
-        [SemanticAttributes.HTTP_CLIENT_IP]: ip,
-      },
-      () => domainUser,
-    ),
+  resolve: async (_, __, { domainUser }) => domainUser,
 })
 
 export default MeQuery
