817e1e1eef58d6681937bfeacba569708b73c2f5
vindard <17693119+vindard@users.noreply.github.com>
2022-03-09 19:26:21 -0400

    feat: implement PaymentBuilder

--- src/domain/payments/errors.ts
+++ src/domain/payments/errors.ts
@@ -1,6 +1,10 @@
-import { ValidationError } from "@domain/shared"
+import { DomainError, ValidationError } from "@domain/shared"
+
+export class PaymentError extends DomainError {}
 
 export class InvalidBtcPaymentAmountError extends ValidationError {}
 export class InvalidUsdPaymentAmountError extends ValidationError {}
 export class LnPaymentRequestNonZeroAmountRequiredError extends ValidationError {}
 export class LnPaymentRequestZeroAmountRequiredError extends ValidationError {}
+
+export class PaymentBuilderNotCompleteError extends PaymentError {}
--- src/domain/payments/index.types.d.ts
+++ src/domain/payments/index.types.d.ts
@@ -3,8 +3,25 @@ type Payment = {
   senderWalletCurrency: WalletCurrency
   btcPaymentAmount?: BtcPaymentAmount
   usdPaymentAmount?: UsdPaymentAmount
+  unknownCurrencyPaymentAmount?: number
+  hasInvalidBtcPaymentAmount?: boolean
+  hasInvalidUsdPaymentAmount?: boolean
+  paymentRequest?: EncodedPaymentRequest
+  isIntraledger: boolean
+  feeAmount: PaymentAmount<WalletCurrency>
+  balanceAmount: PaymentAmount<WalletCurrency>
+  hasEnoughBalance: boolean
 }
 
+type PaymentBuilder = {
+  withPaymentRequest(paymentRequest: EncodedPaymentRequest): PaymentBuilder
+  withBtcPaymentAmount(amount: BtcPaymentAmount): PaymentBuilder
+  withAmountFromUnknownCurrencyAmount(amount: number): PaymentBuilder
+  withSenderWallet(senderWallet: Wallet): PaymentBuilder
+  withCheckedIfLocal(isLocal: boolean): PaymentBuilder
+  withCheckedHasBalance(balance: CurrencyBaseAmount): PaymentBuilder
+  payment(): Payment | ApplicationError
+}
 interface PaymentsRepository {
   persistNew(Payment): Promise<Payment | RepositoryError>
 }
