cfae6b35176d5b62b80dc3be645718c76f06372c
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-03-14 16:25:08 +0000

    refactor: remove unneeded type

--- src/domain/bitcoin/index.types.d.ts
+++ src/domain/bitcoin/index.types.d.ts
@@ -4,8 +4,5 @@ type MilliSatoshis = number & { readonly brand: unique symbol }
 type BtcNetwork =
   typeof import("./index").BtcNetwork[keyof typeof import("./index").BtcNetwork]
 
-// inbound minus outbound
-type NetInboundFlow = number & { readonly brand: unique symbol }
-
 // positive means swap out imbalance
 type SwapOutImbalance = number & { readonly brand: unique symbol }
--- src/domain/ledger/imbalance-calculator.ts
+++ src/domain/ledger/imbalance-calculator.ts
@@ -1,3 +1,4 @@
+import { toSats } from "@domain/bitcoin"
 import { WithdrawalFeePriceMethod } from "@domain/wallets"
 
 const MS_PER_HOUR = (60 * 60 * 1000) as MilliSeconds
@@ -26,7 +27,7 @@ export const ImbalanceCalculator = ({
     })
     if (volume_ instanceof Error) return volume_
 
-    return (volume_.incomingBaseAmount - volume_.outgoingBaseAmount) as NetInboundFlow
+    return toSats(volume_.incomingBaseAmount - volume_.outgoingBaseAmount)
   }
 
   const getSwapOutImbalance = async (walletId: WalletId) => {
