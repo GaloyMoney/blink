66f1d4f5ef1b34f4eec63e40f07ec370b5dd4fe9
vindard <17693119+vindard@users.noreply.github.com>
2022-03-16 11:24:32 -0400

    wip: reimplement send-lightning use-cases

--- src/app/payments/send-lightning.ts
+++ src/app/payments/send-lightning.ts
@@ -3,13 +3,11 @@ import {
   LnPaymentRequestNonZeroAmountRequiredError,
   LnPaymentRequestZeroAmountRequiredError,
   CouldNotFindLightningPaymentFlowError,
-  LightningPaymentFlowBuilder,
 } from "@domain/payments"
 import { checkedToWalletId } from "@domain/wallets"
 import {
   decodeInvoice,
   LnAlreadyPaidError,
-  LnFeeCalculator,
   LnPaymentPendingError,
   PaymentSendStatus,
 } from "@domain/bitcoin/lightning"
@@ -18,7 +16,11 @@ import { LndService } from "@services/lnd"
 import { WalletsRepository } from "@services/mongoose"
 import { PaymentsRepository } from "@services/redis"
 
-import { constructPaymentFlowBuilder } from "./helpers"
+import { LockService } from "@services/lock"
+import { LedgerService } from "@services/ledger"
+import { InsufficientBalanceError } from "@domain/errors"
+
+import { constructPaymentFlowBuilder, newCheckWithdrawalLimits } from "./helpers"
 
 export const payInvoiceByWalletId = async ({
   paymentRequest,
@@ -38,6 +40,7 @@ export const payInvoiceByWalletId = async ({
 
   const decodedInvoice = decodeInvoice(paymentRequest)
   if (decodedInvoice instanceof Error) return decodedInvoice
+  const { paymentHash } = decodedInvoice
 
   const { paymentAmount: lnInvoiceAmount } = decodedInvoice
   if (!(lnInvoiceAmount && lnInvoiceAmount.amount > 0n)) {
@@ -66,7 +69,84 @@ export const payInvoiceByWalletId = async ({
   }
   if (paymentFlow instanceof Error) return paymentFlow
 
-  // look up if payment flow exists
-  // if not use builder
-  return PaymentSendStatus.Success
+  // Note: probe for USD fee not necessary since we can get the fee otherwise
+
+  // Validation checks
+  // - [x] checkedToAmount
+  // - [ ] sender account active
+  // - [x] checked to sender walletId
+  // - [x] check sender wallet exists
+  // - [ ] match sender wallet `accountId` to `account.id`
+  // - [x] optional, checkedToRecipientWalletId
+  // - [ ] optional, check recipient wallet exists
+  // - [ ] optional, check not self payment
+
+  // Get display currency price?
+
+  // ===
+  // LN
+  // ===
+
+  // - limits check
+  const limitCheck = await newCheckWithdrawalLimits({
+    amount: paymentFlow.paymentAmountInUsd(),
+    wallet: senderWallet,
+  })
+  if (limitCheck instanceof Error) return limitCheck
+
+  // - get cached route if exists
+  const { rawRoute, outgoingNodePubkey } = paymentFlow.routeFromCachedRoute()
+
+  // - validate route amount?
+
+  // - amount + fee in display currency?
+
+  // - ENTER LOCK
+  return LockService().lockWalletId(
+    { walletId: senderWallet.id, logger },
+    async (lock) => {
+      if (paymentFlow instanceof Error) return paymentFlow
+
+      // - get balance
+      const balance = await LedgerService().getWalletBalanceAmount({
+        walletId: senderWallet.id,
+        walletCurrency: senderWallet.currency,
+      })
+      if (balance instanceof Error) return balance
+
+      const paymentAmountInSenderWalletCurrency =
+        paymentFlow.paymentAmountInSenderWalletCurrency()
+
+      if (balance.amount < paymentAmountInSenderWalletCurrency.amount) {
+        const unitForMsg = senderWallet.currency === WalletCurrency.Btc ? "sats" : "cents"
+        return new InsufficientBalanceError(
+          `Payment amount '${paymentAmountInSenderWalletCurrency.amount}' ${unitForMsg} exceeds balance '${balance.amount}'`,
+        )
+      }
+
+      const journal = await LockService().extendLock({ logger, lock }, async () => {
+        if (paymentFlow instanceof Error) return paymentFlow
+
+        // - TODO: record initial transaction
+
+        const payResult = rawRoute
+          ? await lndService.payInvoiceViaRoutes({
+              paymentHash,
+              rawRoute,
+              pubkey: outgoingNodePubkey,
+            })
+          : await lndService.newPayInvoiceViaPaymentDetails({
+              decodedInvoice,
+              btcPaymentAmount: paymentFlow.paymentAmountInBtc(),
+              maxFeeAmount: paymentFlow.protocolFeeInBtc(),
+            })
+      })
+
+      return PaymentSendStatus.Success
+    },
+  )
+
+  // ===
+  // Intraledger
+  // ===
 }
