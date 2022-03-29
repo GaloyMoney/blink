078f136792985d6d365142c2be7ba0a7925afd2b
vindard <17693119+vindard@users.noreply.github.com>
2022-03-29 14:12:43 -0600

    chore: remove duplicate values declared

--- src/domain/shared/calculator.ts
+++ src/domain/shared/calculator.ts
@@ -1,15 +1,3 @@
-import { WalletCurrency } from "./primitives"
-
-export const ZERO_SATS = {
-  currency: WalletCurrency.Btc,
-  amount: 0n,
-} as const
-
-export const ZERO_CENTS = {
-  currency: WalletCurrency.Usd,
-  amount: 0n,
-} as const
-
 export const AmountCalculator = () => {
   const add = <T extends WalletCurrency>(a: PaymentAmount<T>, b: PaymentAmount<T>) => {
     return {
