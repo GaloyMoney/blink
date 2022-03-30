47926df9b15aff3411a35f54bbe8229b79f76185
Kartik Shah <krtk.6160@gmail.com>
2022-03-30 18:56:17 +0530

    refactor: take phone number as cmd arg

--- src/debug/create-txn.ts
+++ src/debug/create-txn.ts
@@ -1,7 +1,8 @@
 /**
  * how to run:
- * yarn ts-node --files -r tsconfig-paths/register src/debug/create-txn.ts 21000
- * The first and only argument is the amount of sats to assign to new funder
+ * yarn ts-node --files -r tsconfig-paths/register src/debug/create-txn.ts 21000 +786363
+ * The first argument is the amount of sats to assign to new funder
+ * The second argument is the phone number of the funder account
  */
 
 import { Wallets } from "@app"
@@ -9,6 +10,7 @@ import { getCurrentPrice } from "@app/prices/get-current-price"
 import { checkedToSats } from "@domain/bitcoin"
 import { DisplayCurrencyConverter } from "@domain/fiat/display-currency"
 import { WalletCurrency } from "@domain/shared"
+import { checkedToPhoneNumber } from "@domain/users"
 import { WalletType } from "@domain/wallets"
 import { LedgerService } from "@services/ledger"
 import { isUp } from "@services/lnd/health"
@@ -21,10 +23,8 @@ import {
   WalletsRepository,
 } from "@services/mongoose"
 
-const consolidateFunds = async (sats: Satoshis) => {
+const consolidateFunds = async (sats: Satoshis, phone: PhoneNumber) => {
   await setupMongoConnection()
-
-  const phone = "+1928282928" as PhoneNumber
 
   const user = await UsersRepository().persistNew({ phone, phoneMetadata: undefined })
   if (user instanceof Error) return user
@@ -77,15 +77,18 @@ const consolidateFunds = async (sats: Satoshis) => {
 
 const main = async () => {
   const args = process.argv
-  if (args.length === 3) {
+  if (args.length === 4) {
     const sats = parseInt(args[2])
     const checkedSats = checkedToSats(sats)
     if (checkedSats instanceof Error) return checkedSats
 
-    return consolidateFunds(checkedSats)
-  } else {
-    console.error("Invalid number of arguments")
+    const checkedPhone = checkedToPhoneNumber(args[3])
+    if (checkedPhone instanceof Error) return checkedPhone
+
+    return consolidateFunds(checkedSats, checkedPhone)
   }
+  console.error("Invalid number of arguments")
+  return
 }
 
 setupMongoConnection()
