df2c83215cbac3a3bfdd8e8e0ed96a03b732944d
vindard <17693119+vindard@users.noreply.github.com>
2022-04-12 10:23:17 -0400

    refactor: reimplement new reimburseFee helper

--- src/app/wallets/reimburse-fee.ts
+++ src/app/wallets/reimburse-fee.ts
@@ -4,10 +4,13 @@ import {
   DisplayCurrencyConverter,
   toDisplayCurrencyBaseAmount,
 } from "@domain/fiat/display-currency"
-import { FeeReimbursement } from "@domain/ledger/fee-reimbursement"
-import { WalletCurrency } from "@domain/shared"
+import { LedgerTransactionType } from "@domain/ledger"
+import { FeeReimbursement, NewFeeReimbursement } from "@domain/ledger/fee-reimbursement"
+import { paymentAmountFromSats, WalletCurrency } from "@domain/shared"
 import { DealerPriceService } from "@services/dealer-price"
 import { LedgerService } from "@services/ledger"
+
+import * as LedgerFacade from "@services/ledger/facade"
 
 export const reimburseFee = async ({
   walletId,
@@ -78,6 +81,84 @@ export const reimburseFee = async ({
     paymentFlow: {
       btcPaymentAmount: { amount: BigInt(paymentAmount), currency: WalletCurrency.Btc },
     },
+  })
+  if (result instanceof Error) return result
+
+  return true
+}
+
+export const newReimburseFee = async ({
+  paymentFlow,
+  journalId,
+  actualFee,
+  revealedPreImage,
+  logger,
+}: {
+  paymentFlow: PaymentFlow<WalletCurrency, WalletCurrency>
+  journalId: LedgerJournalId
+  actualFee: Satoshis
+  revealedPreImage?: RevealedPreImage
+  logger: Logger
+}): Promise<true | ApplicationError> => {
+  const actualFeeAmount = paymentAmountFromSats(actualFee)
+
+  const maxFeeAmounts = {
+    btc: paymentFlow.btcProtocolFee,
+    usd: paymentFlow.usdProtocolFee,
+  }
+
+  const feeDifference =
+    NewFeeReimbursement(maxFeeAmounts).getReimbursement(actualFeeAmount)
+  if (feeDifference instanceof Error) {
+    logger.warn(
+      { maxFee: maxFeeAmounts, actualFee: actualFeeAmount },
+      `Invalid reimbursement fee`,
+    )
+    return true
+  }
+
+  // TODO: only reimburse fees is this is above a (configurable) threshold
+  // ie: adding an entry for 1 sat fees may not be the best scalability wise for the db
+  if (feeDifference.btc.amount === 0n) {
+    return true
+  }
+
+  const amountDisplayCurrency = Number(
+    feeDifference.usd.amount,
+  ) as DisplayCurrencyBaseAmount
+
+  const metadata: FeeReimbursementLedgerMetadata = {
+    hash: paymentFlow.paymentHash,
+    type: LedgerTransactionType.LnFeeReimbursement,
+    pending: false,
+    usd: amountDisplayCurrency,
+    related_journal: journalId,
+  }
+
+  const txMetadata: LnLedgerTransactionMetadataUpdate = {
+    hash: paymentFlow.paymentHash,
+    revealedPreImage,
+  }
+
+  logger.info(
+    {
+      feeDifference,
+      maxFee: maxFeeAmounts,
+      actualFee,
+      paymentHash: paymentFlow.paymentHash,
+    },
+    "logging a fee difference",
+  )
+
+  const result = await LedgerFacade.recordReceive({
+    description: "fee reimbursement",
+    receiverWalletDescriptor: paymentFlow.senderWalletDescriptor(),
+    amountToCreditReceiver: {
+      usd: feeDifference.usd,
+      btc: feeDifference.btc,
+    },
+    metadata,
+    txMetadata,
   })
   if (result instanceof Error) return result
 
