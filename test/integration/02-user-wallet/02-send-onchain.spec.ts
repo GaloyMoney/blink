9186688e319b9b638cd2ad439e85a347e9bd2c84
vindard <17693119+vindard@users.noreply.github.com>
2022-04-12 11:12:18 -0400

    test: swap new use-case methods into integration tests

--- test/integration/02-user-wallet/02-send-onchain.spec.ts
+++ test/integration/02-user-wallet/02-send-onchain.spec.ts
@@ -711,7 +711,7 @@ describe("UserWallet - onChainPay", () => {
 
       const remainingLimit = await getRemainingTwoFALimit({
         walletId: walletIdA,
-        dCConverter,
+        satsToCents: dCConverter.fromSatsToCents,
       })
 
       const aboveThreshold = add(remainingLimit, toCents(10))
