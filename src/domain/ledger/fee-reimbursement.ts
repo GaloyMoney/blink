04c688a3e3ae8350980bc8268fe0d3088d241303
vindard <17693119+vindard@users.noreply.github.com>
2022-04-12 09:32:08 -0400

    feat: add new FeeReimbursement

--- src/domain/ledger/fee-reimbursement.ts
+++ src/domain/ledger/fee-reimbursement.ts
@@ -1,4 +1,6 @@
 import { toSats } from "@domain/bitcoin"
+import { PriceRatio } from "@domain/payments"
+import { WalletCurrency } from "@domain/shared"
 
 import { FeeDifferenceError } from "./errors"
 
@@ -7,6 +9,31 @@ export const FeeReimbursement = (prepaidFee: Satoshis): FeeReimbursement => {
     const feeDifference = toSats(prepaidFee - actualFee)
     if (feeDifference < 0) return new FeeDifferenceError()
     return feeDifference
+  }
+
+  return {
+    getReimbursement,
+  }
+}
+
+export const NewFeeReimbursement = (prepaidFeeAmount: {
+  btc: BtcPaymentAmount
+  usd: UsdPaymentAmount
+}): NewFeeReimbursement => {
+  const getReimbursement = (
+    actualFee: BtcPaymentAmount,
+  ): { btc: BtcPaymentAmount; usd: UsdPaymentAmount } | FeeDifferenceError => {
+    const feeDifferenceBtc = prepaidFeeAmount.btc.amount - actualFee.amount
+    if (feeDifferenceBtc < 0) return new FeeDifferenceError()
+    const feeDifferenceBtcAmount = {
+      amount: feeDifferenceBtc,
+      currency: WalletCurrency.Btc,
+    }
+
+    const priceRatio = PriceRatio(prepaidFeeAmount)
+    const feeDifferenceUsdAmount = priceRatio.convertFromBtc(feeDifferenceBtcAmount)
+
+    return { btc: feeDifferenceBtcAmount, usd: feeDifferenceUsdAmount }
   }
 
   return {
