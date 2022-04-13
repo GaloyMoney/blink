46ff297aeae7fc159ce0b4d84fc378aa9900cd6c
vindard <17693119+vindard@users.noreply.github.com>
2022-04-13 10:00:23 -0400

    fix: add an error return for PriceRatio

--- src/domain/payments/errors.ts
+++ src/domain/payments/errors.ts
@@ -3,6 +3,7 @@ import { CouldNotFindError } from "@domain/errors"
 
 export class InvalidBtcPaymentAmountError extends ValidationError {}
 export class InvalidUsdPaymentAmountError extends ValidationError {}
+export class InvalidZeroAmountPriceRatioInputError extends ValidationError {}
 export class LnPaymentRequestNonZeroAmountRequiredError extends ValidationError {}
 export class LnPaymentRequestZeroAmountRequiredError extends ValidationError {}
 
