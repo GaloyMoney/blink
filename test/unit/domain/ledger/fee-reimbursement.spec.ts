20960ec34147e63874e951323d5fd96db9be4f3a
vindard <17693119+vindard@users.noreply.github.com>
2022-04-13 08:22:42 -0400

    test: add unit tests for NewFeeReimbursement

--- test/unit/domain/ledger/fee-reimbursement.spec.ts
+++ test/unit/domain/ledger/fee-reimbursement.spec.ts
@@ -1,6 +1,7 @@
 import { toSats } from "@domain/bitcoin"
 import { FeeDifferenceError } from "@domain/ledger"
-import { FeeReimbursement } from "@domain/ledger/fee-reimbursement"
+import { FeeReimbursement, NewFeeReimbursement } from "@domain/ledger/fee-reimbursement"
+import { WalletCurrency } from "@domain/shared"
 
 describe("FeeReimbursement", () => {
   it("returns a fee difference for reimbursement", () => {
@@ -22,6 +23,44 @@ describe("FeeReimbursement", () => {
     const feeReimbursement = FeeReimbursement(prepaidFee)
     const actualFee = toSats(300)
     const feeDifference = feeReimbursement.getReimbursement(actualFee)
+    expect(feeDifference).toBeInstanceOf(FeeDifferenceError)
+  })
+})
+
+describe("NewFeeReimbursement", () => {
+  it("returns a fee difference for reimbursement", () => {
+    const prepaidFeeAmount = {
+      btc: { amount: 100n, currency: WalletCurrency.Btc },
+      usd: { amount: 5n, currency: WalletCurrency.Usd },
+    }
+    const feeReimbursement = NewFeeReimbursement(prepaidFeeAmount)
+    const actualFeeAmount = { amount: 20n, currency: WalletCurrency.Btc }
+    const feeDifferenceAmount = feeReimbursement.getReimbursement(actualFeeAmount)
+
+    const expectedFeeDifferenceAmount = {
+      btc: { amount: 80n, currency: WalletCurrency.Btc },
+      usd: { amount: 4n, currency: WalletCurrency.Usd },
+    }
+    expect(feeDifferenceAmount).toStrictEqual(expectedFeeDifferenceAmount)
+  })
+  it("returns FeeDifferenceError zero prepaid fee", () => {
+    const prepaidFeeAmount = {
+      btc: { amount: 0n, currency: WalletCurrency.Btc },
+      usd: { amount: 0n, currency: WalletCurrency.Usd },
+    }
+    const feeReimbursement = NewFeeReimbursement(prepaidFeeAmount)
+    const actualFeeAmount = { amount: 20n, currency: WalletCurrency.Btc }
+    const feeDifference = feeReimbursement.getReimbursement(actualFeeAmount)
+    expect(feeDifference).toBeInstanceOf(FeeDifferenceError)
+  })
+  it("returns FeeDifferenceError if actual fee is greater than prepaid fee", () => {
+    const prepaidFeeAmount = {
+      btc: { amount: 100n, currency: WalletCurrency.Btc },
+      usd: { amount: 5n, currency: WalletCurrency.Usd },
+    }
+    const feeReimbursement = NewFeeReimbursement(prepaidFeeAmount)
+    const actualFeeAmount = { amount: 300n, currency: WalletCurrency.Btc }
+    const feeDifference = feeReimbursement.getReimbursement(actualFeeAmount)
     expect(feeDifference).toBeInstanceOf(FeeDifferenceError)
   })
 })
