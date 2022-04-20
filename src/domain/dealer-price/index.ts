4ac2a2d416754e4f1e81418cda98993487defca9
vindard <17693119+vindard@users.noreply.github.com>
2022-04-20 01:35:47 -0400

    feat: add new 'DealerTypes' type

--- src/domain/dealer-price/index.ts
+++ src/domain/dealer-price/index.ts
@@ -1,1 +1,6 @@
 export * from "./errors"
+
+export const DealerTypes = {
+  Hedge: "HEDGE",
+  NoHedge: "NO_HEDGE",
+} as const
--- src/services/dealer-price/index.ts
+++ src/services/dealer-price/index.ts
@@ -1,21 +1,25 @@
 import { getDisplayCurrency, getDealerConfig, ConfigError } from "@config"
+import { DealerTypes } from "@domain/dealer-price"
 import { WalletCurrency } from "@domain/shared"
 
 import * as HedgingDealer from "./hedging-dealer"
 import * as NonHedgingDealer from "./non-hedging-dealer"
 
 let DealerPriceService: () => IDealerPriceService,
-  NewDealerPriceService: () => INewDealerPriceService
+  NewDealerPriceService: () => INewDealerPriceService,
+  dealerType: DealerTypes
 
 const usdHedgeEnabled = getDealerConfig().usd.hedgingEnabled
 const displayCurrency = getDisplayCurrency().code
 
 if (usdHedgeEnabled) {
   ;({ DealerPriceService, NewDealerPriceService } = HedgingDealer)
+  dealerType = DealerTypes.Hedge
 } else if (displayCurrency === WalletCurrency.Usd) {
   ;({ DealerPriceService, NewDealerPriceService } = NonHedgingDealer)
+  dealerType = DealerTypes.NoHedge
 } else {
   throw new ConfigError("No valid service available to use for Dealer service")
 }
 
-export { DealerPriceService, NewDealerPriceService }
+export { DealerPriceService, NewDealerPriceService, dealerType }
