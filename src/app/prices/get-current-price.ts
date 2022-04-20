aae3a9587b43bea180ff9c605e4ea90de51b876c
vindard <17693119+vindard@users.noreply.github.com>
2022-04-20 01:36:21 -0400

    feat: add midPrice fallback to PriceService in app layer

--- src/app/prices/get-current-price.ts
+++ src/app/prices/get-current-price.ts
@@ -18,10 +18,32 @@ export const getCurrentPrice = async (): Promise<
   return realtimePrice
 }
 
+export const newGetCurrentPrice = async (): Promise<
+  CentsPerSatsRatio | PriceServiceError
+> => {
+  const realtimePrice = await PriceService().getRealTimePrice()
+  if (realtimePrice instanceof Error) return newGetCachedPrice()
+  // keep price in cache for 10 mins in case the price pod is not online
+  await LocalCacheService().set<CentsPerSatsRatio>({
+    key: CacheKeys.CurrentPrice,
+    value: realtimePrice as unknown as CentsPerSatsRatio,
+    ttlSecs: toSeconds(600),
+  })
+  return realtimePrice as unknown as CentsPerSatsRatio
+}
+
 const getCachedPrice = async (): Promise<
   DisplayCurrencyPerSat | PriceNotAvailableError
 > => {
   const cachedPrice = await LocalCacheService().get<DisplayCurrencyPerSat>(
+    CacheKeys.CurrentPrice,
+  )
+  if (cachedPrice instanceof Error) return new PriceNotAvailableError()
+  return cachedPrice
+}
+
+const newGetCachedPrice = async (): Promise<CentsPerSatsRatio | PriceServiceError> => {
+  const cachedPrice = await LocalCacheService().get<CentsPerSatsRatio>(
     CacheKeys.CurrentPrice,
   )
   if (cachedPrice instanceof Error) return new PriceNotAvailableError()
