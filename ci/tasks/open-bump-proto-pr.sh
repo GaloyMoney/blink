cbc796e82253fab904b8aede712a6da107bb0b8f
Kartik Shah <krtk.6160@gmail.com>
2022-04-22 17:34:02 +0530

    ci: commit changes in sync proto task (#1249)

--- ci/tasks/open-bump-proto-pr.sh
+++ ci/tasks/open-bump-proto-pr.sh
@@ -4,7 +4,7 @@ set -eu
 
 gh pr close ${BOT_BRANCH} || true
 gh pr create \
-  --title "chore(deps): bump-dealer-proto" \
+  --title "chore(deps): bump dealer proto" \
   --base ${BRANCH} \
   --head ${BOT_BRANCH} \
   --label galoybot \
