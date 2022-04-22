3790256ef9d6ee0f50f0e8828b0bc5576142f895
Kartik Shah <krtk.6160@gmail.com>
2022-04-22 17:32:36 +0530

    ci: commit changes in sync proto task

--- ci/tasks/sync-proto.sh
+++ ci/tasks/sync-proto.sh
@@ -2,4 +2,22 @@
 
 set -eu
 
+export ref=$(cat ./src-repo/.git/short_ref)
 cp src-repo/${PROTO_FILE_SRC_PATH} repo/${PROTO_FILE_DEST_PATH}
+
+cd repo
+
+if [[ -z $(git config --global user.email) ]]; then
+  git config --global user.email "bot@galoy.io"
+fi
+if [[ -z $(git config --global user.name) ]]; then
+  git config --global user.name "CI Bot"
+fi
+
+(
+  cd $(git rev-parse --show-toplevel)
+  git merge --no-edit ${BRANCH}
+  git add -A
+  git status
+  git commit -m "chore(deps): bump dealer proto to '${ref}'"
+)
