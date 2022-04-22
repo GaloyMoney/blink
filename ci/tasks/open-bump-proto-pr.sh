1e971ce675d0c8eec3e3075da698e198b9440e34
Kartik Shah <krtk.6160@gmail.com>
2022-04-22 18:10:10 +0530

    ci: checkout main branch before opening pr

--- ci/tasks/open-bump-proto-pr.sh
+++ ci/tasks/open-bump-proto-pr.sh
@@ -5,9 +5,10 @@ set -eu
 cd repo
 
 gh pr close ${BOT_BRANCH} || true
+git checkout ${BRANCH}
 gh pr create \
   --title "chore(deps): bump dealer proto" \
-  --body ""
+  --body "" \
   --base ${BRANCH} \
   --head ${BOT_BRANCH} \
   --label galoybot
