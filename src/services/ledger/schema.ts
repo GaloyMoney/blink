803fe816050d9024b6ec612f28f637d0c85bd642
vindard <17693119+vindard@users.noreply.github.com>
2022-04-28 10:38:55 -0400

    chore: temporarily remove fee defaults from schema
    This can be added back after we've done all migrations to other
    transaction types.

--- src/services/ledger/schema.ts
+++ src/services/ledger/schema.ts
@@ -73,17 +73,14 @@ const transactionSchema = new Schema({
   centsAmount: Number,
   satsFee: {
     type: Number,
-    default: 0,
   },
   centsFee: {
     type: Number,
-    default: 0,
   },
 
   displayAmount: Number,
   displayFee: {
     type: Number,
-    default: 0,
   },
   displayCurrency: {
     type: String,
