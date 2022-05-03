atchrispinto/galoy

refactor: pass 'displayCurrency' values in from config

src/app/wallets/reimburse-fee.ts
@@ -1,4 +1,5 @@
 import { getCurrentPrice } from "@app/prices"
+import { getDisplayCurrency } from "@config"
 import { DealerPriceServiceError } from "@domain/dealer-price"
 import {
   DisplayCurrencyConverter,
@@ -82,7 +83,7 @@ export const reimburseFee = async ({
     },
     feeDisplayCurrency: usdFee,
     amountDisplayCurrency,
-    displayCurrency: WalletCurrency.Usd,
+    displayCurrency: getDisplayCurrency().code,
   })
   if (result instanceof Error) return result
 

src/services/ledger/send.ts
@@ -1,3 +1,5 @@
+import { getDisplayCurrency } from "@config"
+
 import { toSats } from "@domain/bitcoin"
 import { toCents } from "@domain/fiat"
 import { LedgerTransactionType } from "@domain/ledger"
@@ -59,7 +61,7 @@ export const send = {
       displayFee: centsFee as DisplayCurrencyBaseAmount,
       displayAmount: centsAmount as DisplayCurrencyBaseAmount,
 
-      displayCurrency: WalletCurrency.Usd,
+      displayCurrency: getDisplayCurrency().code,
       centsAmount: toCents(centsAmount),
       satsAmount: toSats(sats - feeRouting),
       centsFee: toCents(centsFee),

test/integration/services/ledger/facade.spec.ts
@@ -1,5 +1,6 @@
 import crypto from "crypto"
 
+import { getDisplayCurrency } from "@config"
 import { BtcWalletDescriptor, UsdWalletDescriptor, WalletCurrency } from "@domain/shared"
 import * as LedgerFacade from "@services/ledger/facade"
 
@@ -68,7 +69,7 @@ describe("Facade", () => {
         amountDisplayCurrency: Number(
           receiveAmount.usd.amount,
         ) as DisplayCurrencyBaseAmount,
-        displayCurrency: WalletCurrency.Usd,
+        displayCurrency: getDisplayCurrency().code,
         pubkey: crypto.randomUUID() as Pubkey,
         feeKnownInAdvance: true,
         paymentFlow: { btcPaymentAmount: receiveAmount.btc, btcProtocolFee: bankFee.btc },

GaloyMoney/blink

src/app/wallets/reimburse-fee.ts
@@ -1,4 +1,5 @@
 import { getCurrentPrice } from "@app/prices"
+import { getDisplayCurrencyConfig } from "@config"
 import { DealerPriceServiceError } from "@domain/dealer-price"
 import {
   DisplayCurrencyConverter,
@@ -82,7 +83,7 @@ export const reimburseFee = async ({
     },
     feeDisplayCurrency: usdFee,
     amountDisplayCurrency,
-    displayCurrency: WalletCurrency.Usd,
+    displayCurrency: getDisplayCurrencyConfig().code,
   })
   if (result instanceof Error) return result
 

src/services/ledger/send.ts
@@ -1,3 +1,5 @@
+import { getDisplayCurrencyConfig } from "@config"
+
 import { toSats } from "@domain/bitcoin"
 import { toCents } from "@domain/fiat"
 import { LedgerTransactionType } from "@domain/ledger"
@@ -59,7 +61,7 @@ export const send = {
       displayFee: centsFee as DisplayCurrencyBaseAmount,
       displayAmount: centsAmount as DisplayCurrencyBaseAmount,
 
-      displayCurrency: WalletCurrency.Usd,
+      displayCurrency: getDisplayCurrencyConfig().code,
       centsAmount: toCents(centsAmount),
       satsAmount: toSats(sats - feeRouting),
       centsFee: toCents(centsFee),

test/integration/services/ledger/facade.spec.ts
@@ -1,5 +1,6 @@
 import crypto from "crypto"
 
+import { getDisplayCurrencyConfig } from "@config"
 import { BtcWalletDescriptor, UsdWalletDescriptor, WalletCurrency } from "@domain/shared"
 import * as LedgerFacade from "@services/ledger/facade"
 
@@ -68,7 +69,7 @@ describe("Facade", () => {
         amountDisplayCurrency: Number(
           receiveAmount.usd.amount,
         ) as DisplayCurrencyBaseAmount,
-        displayCurrency: WalletCurrency.Usd,
+        displayCurrency: getDisplayCurrencyConfig().code,
         pubkey: crypto.randomUUID() as Pubkey,
         feeKnownInAdvance: true,
         paymentFlow: { btcPaymentAmount: receiveAmount.btc, btcProtocolFee: bankFee.btc },