940a10d7a93a9d88abb95968a622f7767228cd24
vindard <17693119+vindard@users.noreply.github.com>
2022-06-13 16:56:58 -0400

    refactor: move 'toPriceRatio' higher up in call stack

--- test/integration/app/wallets/invoice.spec.ts
+++ test/integration/app/wallets/invoice.spec.ts
@@ -139,9 +139,10 @@ describe("Wallet - addInvoice USD", () => {
   it("add a self generated USD invoice", async () => {
     const centsInput = 10000
 
-    const centsPerSat = await getCurrentPriceInCentsPerSat()
-    if (centsPerSat instanceof Error) return centsPerSat
-    const satsFallbackViaPriceService = (centsInput / centsPerSat) * 0.996 // 40 bps spread
+    const centsPerSatRatio = await getCurrentPriceInCentsPerSat()
+    if (centsPerSatRatio instanceof Error) return centsPerSatRatio
+    const satsFallbackViaPriceService =
+      (centsInput / centsPerSatRatio.usdPerSat()) * 0.996 // 40 bps spread
 
     const sats = await DealerPriceService().getSatsFromCentsForFutureBuy(
       toCents(centsInput),
