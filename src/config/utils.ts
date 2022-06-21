4e6764ca1dd883b45c069f5e85a9cc616c008676
vindard <17693119+vindard@users.noreply.github.com>
2022-06-21 14:01:33 -0400

    Revert "fix: merge arrays in custom yaml configs properly (#1389)"
    This reverts commit 49cd153596d6e381a81359d826976b3097a6877a.

--- src/config/utils.ts
+++
@@ -1,4 +0,0 @@
-import mergeWith from "lodash.mergewith"
-
-export const merge = (defaultConfig, customConfig) =>
-  mergeWith(defaultConfig, customConfig, (a, b) => (Array.isArray(b) ? b : undefined))
