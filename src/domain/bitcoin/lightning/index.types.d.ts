0a0102d68e446d470867285f5a3dc3cde7e99468
vindard <17693119+vindard@users.noreply.github.com>
2022-06-24 12:17:44 -0400

    refactor: swap 'registerInvoice' method to work with hodl invoices

--- src/domain/bitcoin/lightning/index.types.d.ts
+++ src/domain/bitcoin/lightning/index.types.d.ts
@@ -50,6 +50,7 @@ type LnInvoiceLookup = {
   readonly createdAt: Date
   readonly confirmedAt: Date | undefined
   readonly isSettled: boolean
+  readonly isHeld: boolean
   readonly roundedDownReceived: Satoshis
   readonly milliSatsReceived: MilliSatoshis
   readonly secretPreImage: SecretPreImage
@@ -109,6 +110,7 @@ type LnInvoice = {
 }
 
 type RegisterInvoiceArgs = {
+  paymentHash: PaymentHash
   description: string
   descriptionHash?: string
   sats: Satoshis
@@ -116,6 +118,7 @@ type RegisterInvoiceArgs = {
 }
 
 type NewRegisterInvoiceArgs = {
+  paymentHash: PaymentHash
   description: string
   descriptionHash?: string
   btcPaymentAmount: BtcPaymentAmount
@@ -217,6 +220,14 @@ interface ILightningService {
   }: {
     paymentHash: PaymentHash
     pubkey?: Pubkey
+  }): Promise<true | LightningServiceError>
+
+  settleInvoice({
+    pubkey,
+    secret,
+  }: {
+    pubkey: Pubkey
+    secret: SecretPreImage
   }): Promise<true | LightningServiceError>
 
   cancelInvoice({
