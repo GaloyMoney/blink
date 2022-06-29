07b74cf5383a66847df52455387667ee11301981
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-06-29 18:10:48 +0100

    chore: deactivate snapshot

--- src/services/ledger/books.ts
+++ src/services/ledger/books.ts
@@ -2,6 +2,6 @@ import { Book, Entry as MediciEntryFromPackage } from "medici"
 
 import { Transaction, TransactionMetadata } from "./schema"
 
-export const MainBook = new Book("MainBook")
+export const MainBook = new Book("MainBook", { balanceSnapshotSec: 0 })
 
 export { Transaction, TransactionMetadata, MediciEntryFromPackage }
