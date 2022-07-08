902f872a207708226bc95f2a01cf73ca87e6a297
Juan P Lopez <dolcalmi@gmail.com>
2022-07-07 22:22:31 -0500

    feat: enable medici balance cache

--- src/services/ledger/books.ts
+++ src/services/ledger/books.ts
@@ -2,6 +2,6 @@ import { Book, Entry as MediciEntryFromPackage } from "medici"
 
 import { Transaction, TransactionMetadata } from "./schema"
 
-export const MainBook = new Book("MainBook", { balanceSnapshotSec: 0 })
+export const MainBook = new Book("MainBook")
 
 export { Transaction, TransactionMetadata, MediciEntryFromPackage }
