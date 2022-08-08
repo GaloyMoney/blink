cec1678523db557af96e5479d4919d1c0a4f001f
galoybot <86534088+galoybot@users.noreply.github.com>
2022-08-08 20:57:10 +0200

    ci(shared): bump vendored ci files (#1509)

--- ci/vendor/tasks/helpers.sh
+++ ci/vendor/tasks/helpers.sh
@@ -2,10 +2,12 @@ export CARGO_HOME="$(pwd)/cargo-home"
 export CARGO_TARGET_DIR="$(pwd)/cargo-target-dir"
 
 function unpack_deps() {
+  REPO_PATH=${REPO_PATH:-repo}
+
   if [[ -f ${REPO_PATH}/yarn.lock ]]; then
     echo "Unpacking nodejs deps... "
 
-    pushd ${REPO_PATH:-repo} > /dev/null
+    pushd ${REPO_PATH} > /dev/null
 
     tar -zxvf ../bundled-deps/bundled-deps-*.tgz ./node_modules/ ./yarn.lock > /dev/null
 
