ebffe7b031b481269804ebe7aafe92e3cc25f852
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-08-09 23:48:45 +0100

    chore: revert: "chore: reconnect to nodes on every cronjob (#1517)"
    This reverts commit c6f651180a7ac2c3636aa9f54832160b31de0a03.

--- src/servers/cron.ts
+++ src/servers/cron.ts
@@ -4,7 +4,6 @@ import {
   deleteExpiredLightningPaymentFlows,
   deleteExpiredWalletInvoice,
   deleteFailedPaymentsAttemptAllLnds,
-  getLnds,
   updateEscrows,
   updateRoutingRevenues,
 } from "@services/lnd/utils"
@@ -14,9 +13,6 @@ import { setupMongoConnection } from "@services/mongodb"
 import { activateLndHealthCheck } from "@services/lnd/health"
 import { ColdStorage, Lightning, Wallets, Payments } from "@app"
 import { getCronConfig, TWO_MONTHS_IN_MS } from "@config"
-
-import { reconnect } from "balanceofsatoshis/network"
-import { LndService } from "@services/lnd"
 
 const logger = baseLogger.child({ module: "cron" })
 
@@ -58,18 +54,7 @@ const main = async () => {
     if (result instanceof Error) throw result
   }
 
-  const reconnectNodes = async () => {
-    const lndService = LndService()
-    if (lndService instanceof Error) throw lndService
-
-    const lndsParamsAuth = getLnds({ type: "offchain" })
-    for (const { lnd } of lndsParamsAuth) {
-      await reconnect({ lnd })
-    }
-  }
-
   const tasks = [
-    reconnectNodes,
     updateEscrows,
     updatePendingLightningInvoices,
     updatePendingLightningPayments,
