b689dcc28e175f64da9f26dc9e912124ae196a1e
Juan P Lopez <dolcalmi@gmail.com>
2022-08-17 22:23:26 -0500

    fix: remove type as required from yaml schema

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -243,7 +243,7 @@ export const configSchema = {
               editable: { type: "boolean", default: false },
               index: { type: "boolean", default: false },
             },
-            required: ["name", "type"],
+            required: ["name"],
             additionalProperties: false,
           },
           uniqueItems: true,
