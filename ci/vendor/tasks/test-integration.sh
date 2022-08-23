d4b8bcda71aa565364188662bc4bd74cef753f94
openoms <43343391+openoms@users.noreply.github.com>
2022-08-23 16:24:35 +0100

    Revert "fix: rsync to exclude local-regtest-lnd-data (#1560)"
    This reverts commit d39690997b11144d5cb250cc641486b8d1dc23a9.

--- ci/vendor/tasks/test-integration.sh
+++ ci/vendor/tasks/test-integration.sh
@@ -32,7 +32,7 @@ export ADDITIONAL_SSH_OPTS="-o StrictHostKeyChecking=no -i ${CI_ROOT}/login.ssh"
 pushd ${REPO_PATH}
 
 echo "Syncing repo to docker-host... "
-rsync --delete --exclude target --exclude dev/lnd/local-regtest-lnd-data -avr -e "ssh -l ${DOCKER_HOST_USER} ${ADDITIONAL_SSH_OPTS}" \
+rsync --delete --exclude target -avr -e "ssh -l ${DOCKER_HOST_USER} ${ADDITIONAL_SSH_OPTS}" \
   ./ ${DOCKER_HOST_IP}:${REPO_PATH} > /dev/null
 echo "Done!"
 
