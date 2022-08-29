f8e666e8ece83f3488337a2ff91a6a4a8e50f06a
vindard <17693119+vindard@users.noreply.github.com>
2022-08-29 12:36:43 -0400

    refactor: abstract default onchain service error handling

--- src/services/lnd/onchain-service.ts
+++ src/services/lnd/onchain-service.ts
@@ -92,8 +92,7 @@ export const OnChainService = (
         after,
       })
     } catch (err) {
-      const errDetails = parseLndErrorDetails(err)
-      return new UnknownOnChainServiceError(errDetails)
+      return handleCommonOnChainServiceErrors(err)
     }
   }
 
@@ -126,8 +125,7 @@ export const OnChainService = (
 
       return { address: address as OnChainAddress, pubkey }
     } catch (err) {
-      const errDetails = parseLndErrorDetails(err)
-      return new UnknownOnChainServiceError(errDetails)
+      return handleCommonOnChainServiceErrors(err)
     }
   }
 
@@ -162,7 +160,7 @@ export const OnChainService = (
         case KnownLndErrorDetails.InsufficientFunds:
           return new InsufficientOnChainFundsError()
         default:
-          return new UnknownOnChainServiceError(err)
+          return handleCommonOnChainServiceErrors(err)
       }
     }
   }
@@ -183,8 +181,7 @@ export const OnChainService = (
 
       return id as OnChainTxHash
     } catch (err) {
-      const errDetails = parseLndErrorDetails(err)
-      return new UnknownOnChainServiceError(errDetails)
+      return handleCommonOnChainServiceErrors(err)
     }
   }
 
@@ -252,3 +249,17 @@ const getCachedHeight = async (): Promise<number> => {
   if (cachedHeight instanceof Error) return 0
   return cachedHeight
 }
+
+const handleCommonOnChainServiceErrors = (err: Error) => {
+  const errDetails = parseLndErrorDetails(err)
+  switch (errDetails) {
+    default:
+      return new UnknownOnChainServiceError(msgForUnknown(err))
+  }
+}
+
+const msgForUnknown = (err: Error) =>
+  JSON.stringify({
+    parsedLndErrorDetails: parseLndErrorDetails(err),
+    detailsFromLnd: err,
+  })
