3a3fb882009f7a09628548856c0547208075e07b
ntheile <ntheile@gmail.com>
2022-09-14 16:37:55 -0500

    feat: add loopserver regtest pregenerated macaroons

--- dev/bin/save-loop-data.sh
+++ dev/bin/save-loop-data.sh
@@ -3,6 +3,7 @@
 # script to generate the loop wallets for testing
 NETWORK=regtest
 mkdir ../lnd/loop/$NETWORK 2>/dev/null
+mkdir ../lnd/loop/server/$NETWORK 2>/dev/null
 
 fetch_loop_data() {
   local container_id=$(docker ps -q -f status=running -f name="galoy-$1")
@@ -24,7 +25,21 @@ fetch_loop_data() {
   fi
 }
 
+fetch_loopserver_data() {
+  local outside=$(docker ps -q -f status=running -f name="galoy-lnd-outside-1-1")
+  docker cp $outside:/root/.lnd/data/chain/bitcoin/$NETWORK/admin.macaroon ../lnd/loop/server/$NETWORK
+  docker cp $outside:/root/.lnd/data/chain/bitcoin/$NETWORK/signer.macaroon ../lnd/loop/server/$NETWORK
+  docker cp $outside:/root/.lnd/data/chain/bitcoin/$NETWORK/walletkit.macaroon ../lnd/loop/server/$NETWORK
+  docker cp $outside:/root/.lnd/data/chain/bitcoin/$NETWORK/chainnotifier.macaroon ../lnd/loop/server/$NETWORK
+  docker cp $outside:/root/.lnd/data/chain/bitcoin/$NETWORK/invoices.macaroon ../lnd/loop/server/$NETWORK
+  docker cp $outside:/root/.lnd/data/chain/bitcoin/$NETWORK/router.macaroon ../lnd/loop/server/$NETWORK
+  docker cp $outside:/root/.lnd/data/chain/bitcoin/$NETWORK/admin.macaroon ../lnd/loop/server/$NETWORK/readonly.macaroon
+
+}
+
 for i in loopd1-1 loopd2-1; do
   echo "Saving data for $i"
   fetch_loop_data "$i"
 done
+
+fetch_loopserver_data
