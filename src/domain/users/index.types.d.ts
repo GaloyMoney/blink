68ec4ca5f52af54b2a6d7bd66ed9dbfabf49feb1
nicolasburtey <nicolasburtey@users.noreply.github.com>
2022-09-23 16:43:43 -0500

    chore: move readonlyArray in right file (#1699)

--- src/domain/users/index.types.d.ts
+++ src/domain/users/index.types.d.ts
@@ -61,12 +61,3 @@ interface IUsersRepository {
   persistNew({ phone, phoneMetadata }: NewUserInfo): Promise<User | RepositoryError>
   update(user: User): Promise<User | RepositoryError>
 }
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface ReadonlyArray<T> {
-  includes<S, R extends `${Extract<S, string>}`>(
-    this: ReadonlyArray<R>,
-    searchElement: S,
-    fromIndex?: number,
-  ): searchElement is R & S
-}
