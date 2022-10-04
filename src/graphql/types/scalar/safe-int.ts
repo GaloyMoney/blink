9d2ddeb97b865857729e80f9008688a701d8305a
vindard <17693119+vindard@users.noreply.github.com>
2022-10-04 12:52:16 -0400

    fix: map safe-int errors to apollo error

--- src/graphql/types/scalar/safe-int.ts
+++ src/graphql/types/scalar/safe-int.ts
@@ -1,4 +1,6 @@
+import { ValidationInternalError } from "@graphql/error"
 import { GT } from "@graphql/index"
+import { baseLogger } from "@services/logger"
 
 const MAX_INT = Number.MAX_SAFE_INTEGER
 const MIN_INT = Number.MIN_SAFE_INTEGER
@@ -22,19 +24,25 @@ const SafeInt = GT.Scalar({
 
 function coerceSafeInt(value: unknown) {
   if (value === "") {
-    throw new Error(
-      "SafeInt cannot represent non 53-bit signed integer value: (empty string)",
-    )
+    throw new ValidationInternalError({
+      message: "SafeInt cannot represent non 53-bit signed integer value: (empty string)",
+      logger: baseLogger,
+    })
   }
   const num = Number(value)
   if (num !== value || num > MAX_INT || num < MIN_INT) {
-    throw new Error(
-      "SafeInt cannot represent non 53-bit signed integer value: " + String(value),
-    )
+    throw new ValidationInternalError({
+      message:
+        "SafeInt cannot represent non 53-bit signed integer value: " + String(value),
+      logger: baseLogger,
+    })
   }
   const int = Math.floor(num)
   if (int !== num) {
-    throw new Error("SafeInt cannot represent non-integer value: " + String(value))
+    throw new ValidationInternalError({
+      message: "SafeInt cannot represent non-integer value: " + String(value),
+      logger: baseLogger,
+    })
   }
   return int
 }
