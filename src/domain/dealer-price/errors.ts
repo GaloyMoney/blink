543b6f99dbaa2f6d5356c714cc46be8f45e698b2
Arvin <17693119+vindard@users.noreply.github.com>
2022-10-04 17:03:58 -0400

    chore: map dealer stale price error (#1740)

--- src/domain/dealer-price/errors.ts
+++ src/domain/dealer-price/errors.ts
@@ -5,6 +5,9 @@ export class DealerPriceError extends DomainError {}
 export class DealerPriceServiceError extends DealerPriceError {}
 export class DealerPriceNotAvailableError extends DealerPriceServiceError {}
 export class NoConnectionToDealerError extends DealerPriceServiceError {}
+export class DealerStalePriceError extends DealerPriceServiceError {
+  level = ErrorLevel.Critical
+}
 export class UnknownDealerPriceServiceError extends DealerPriceServiceError {
   level = ErrorLevel.Critical
 }
