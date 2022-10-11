96e32da3ff80bc786a438fd08babf44200c5463e
vindard <17693119+vindard@users.noreply.github.com>
2022-10-10 20:08:41 -0400

    feat: implement migration for intraledger to self-trade

---
+++ src/migrations/20221010162913-add-self-trade-type.ts
@@ -0,0 +1,156 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const { Types } = require("mongoose")
+
+const toWalletId = (walletIdPath) => {
+  const path = walletIdPath.split(":")
+  return Array.isArray(path) && path.length === 2 && path[0] === "Liabilities" && path[1]
+    ? path[1]
+    : undefined
+}
+
+const getNonUserWalletIds = async ({ usersCollection, walletsCollection }) => {
+  const accountIds = []
+  const usersCursor = usersCollection.find({
+    role: { $in: ["funder", "dealer", "bankowner"] },
+  })
+  for await (const user of usersCursor) {
+    const accountId = user._id.toString()
+    accountIds.push(accountId)
+  }
+  const accountObjectIds = accountIds.map((id) => new Types.ObjectId(id))
+
+  const nonUserWalletIds = []
+  const walletsCursor = walletsCollection.find({
+    _accountId: { $in: accountObjectIds },
+  })
+  for await (const wallet of walletsCursor) {
+    nonUserWalletIds.push(wallet.id)
+  }
+  console.log("HERE 0:", nonUserWalletIds)
+  return nonUserWalletIds
+}
+
+module.exports = {
+  async up(db) {
+    const usersCollection = db.collection("users")
+    const walletsCollection = db.collection("wallets")
+    const txnsCollection = db.collection("medici_transactions")
+
+    const nonUserWalletIds = await getNonUserWalletIds({
+      usersCollection,
+      walletsCollection,
+    })
+
+    const accountIdsByWalletId = {}
+    const getSelfTradeType = async (txnAgg) => {
+      // BUILD 'txns' FOR JOURNALID
+      // =====
+      const txns = []
+      const txnsCursor = txnsCollection.find({ _journal: txnAgg._id })
+      for await (const txn of txnsCursor) {
+        // Filter internal wallet IDs
+        if (nonUserWalletIds.includes(toWalletId(txn.accounts))) continue
+
+        // Filter non-user walletIds
+        const walletId = toWalletId(txn.accounts)
+        if (!walletId) continue
+
+        // Fetch accountId locally
+        let accountId = accountIdsByWalletId[walletId]
+        if (accountId) {
+          txns.push({ walletId, currency: txn.currency, accountId })
+          continue
+        }
+
+        // Fetch accountId from db if not cached
+        const wallet = await walletsCollection.findOne({ id: walletId })
+        if (!wallet) continue
+        accountId = wallet._accountId.toString()
+        if (!accountId) continue
+
+        accountIdsByWalletId[walletId] = accountId
+        txns.push({ walletId, currency: txn.currency, accountId })
+      }
+
+      // CHECK 'txns' TO DETERMINE IF SELF-TRADE
+      // =====
+
+      // Check that we have txns with valid walletIds
+      if (txns && txns.length === 0) return false
+      console.log("\nHERE 1:", { [txnAgg._id]: txns })
+
+      // Check if we have exactly 2 txns
+      if (txns.length !== 2) return false
+
+      // Check if txns have different currencies
+      if (txns[0].currency === txns[1].currency) return false
+
+      // Compare accountIds to see if self-trade
+      const accountIdsSet = new Set(txns.map((txn) => txn.accountId))
+      console.log("HERE 2:", { accountIdsSet })
+      if (accountIdsSet.size > 1) return false
+
+      // RETURN SELF-TRADE TYPE
+      // =====
+      console.log("HERE 3:", txnAgg.type)
+      switch (txnAgg.type) {
+        case "on_us":
+          return "self_trade"
+        case "ln_on_us":
+          return "ln_self_trade"
+        case "onchain_on_us":
+          return "onchain_self_trade"
+        default:
+          return false
+      }
+    }
+
+    // FETCH INTRALEDGER TXNS AND UPDATE TYPE FOR SELF-TRADES
+    // =====
+    const journalCursor = txnsCollection.aggregate([
+      { $match: { type: { $regex: "on_us" }, currency: "USD" } },
+      {
+        $group: {
+          _id: "$_journal",
+          createdAt: { $first: "$timestamp" },
+          type: { $first: "$type" },
+        },
+      },
+      { $sort: { createdAt: -1 } },
+    ])
+    for await (const txn of journalCursor) {
+      const { _id: _journal, type } = txn
+      const selfTradeType = await getSelfTradeType(txn)
+      if (!selfTradeType) continue
+
+      console.log("HERE 4:", _journal)
+      const result = await txnsCollection.updateMany(
+        { _journal },
+        [
+          {
+            $set: {
+              type: selfTradeType,
+              type_old: "$type",
+            },
+          },
+        ],
+        {
+          multi: true,
+        },
+      )
+      console.log("HERE 5:", result)
+      const { modifiedCount, matchedCount } = result
+      console.log(
+        `changed txn type from ${type} to ${selfTradeType} for ${modifiedCount} of ${matchedCount} transactions for journalId: ${_journal.toString()}`,
+      )
+    }
+  },
+
+  async down(db) {
+    console.log(db)
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  },
+}
