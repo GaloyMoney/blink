2f88f9eceb32382e759678f630003f2e2f6c2dfc
nicolasburtey <nicolasburtey@users.noreply.github.com>
2022-10-25 05:37:16 -0500

    chore: adding more opentelemetry for cron/extendSession (#1878)
    also, throw if we are not able to get the list of users, so that it get logged correctly on opentelemetry

--- src/app/auth/index.ts
+++ src/app/auth/index.ts
@@ -1,18 +1,34 @@
+import { ErrorLevel } from "@domain/shared"
 import { extendSession, listIdentities } from "@services/kratos"
 
-// TODO: interface/type should be reworled so that it doesn't have to come from private
+// TODO: interface/type should be reworked so that it doesn't have to come from private
 import { listSessionsInternal } from "@services/kratos/private"
+import { recordExceptionInCurrentSpan } from "@services/tracing"
 
 export const extendSessions = async (): Promise<void | KratosError> => {
   const users = await listIdentities()
-  if (users instanceof Error) return users
+  if (users instanceof Error) throw users
 
   for (const user of users) {
     const sessions = await listSessionsInternal(user.id)
-    if (sessions instanceof Error) return sessions
+    if (sessions instanceof Error) {
+      recordExceptionInCurrentSpan({
+        error: "impossible to get session",
+        level: ErrorLevel.Info,
+        attributes: { user: user.id, phone: user.phone },
+      })
+      continue
+    }
 
     for (const session of sessions) {
-      await extendSession({ session })
+      const res = await extendSession({ session })
+      if (res instanceof Error) {
+        recordExceptionInCurrentSpan({
+          error: "impossible to extend session",
+          level: ErrorLevel.Info,
+          attributes: { user: user.id, phone: user.phone },
+        })
+      }
     }
   }
 }
