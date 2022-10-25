b0bf0cd27dcd5528ac091360f5f98f64f940942f
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-10-25 12:14:16 +0100

    fix: listIdentities was in indefinite loop if there was no entry

--- src/services/kratos/identity.ts
+++ src/services/kratos/identity.ts
@@ -19,7 +19,7 @@ export const getNextPage = (link: string): number | undefined => {
 // FIXME: there is a bug where page = 0 and page = 1 return the same result
 // related bug: https://github.com/ory/kratos/issues/2834
 // with 1, only 1 entry will be missing in the result
-const perPage = isDev ? 3 : 1
+const perPage = isDev ? 3 : 50
 
 export const listIdentities = async (): Promise<IdentityPhone[] | KratosError> => {
   try {
@@ -28,27 +28,23 @@ export const listIdentities = async (): Promise<IdentityPhone[] | KratosError> =
 
     let hasNext = true
 
-    // const res = await kratosAdmin.adminListIdentities(perPage, undefined)
     let page: number | undefined = 0
 
     while (hasNext) {
-      // Note: this call is paginated, only return 250 records by default
+      // Note: this call is paginated, return 250 records `perPage` by default
       const res = await kratosAdmin.adminListIdentities(perPage, page)
       identities.push(...res.data)
 
-      // const prevPage = page
-      totalCount = res.headers["x-total-count"]
+      totalCount = Number(res.headers["x-total-count"])
 
       page = getNextPage(res.headers.link)
-      // console.log({ prevPage, page, link: res.headers.link })
-      hasNext = page !== undefined
+      hasNext = page !== undefined && totalCount !== 0
     }
 
     // FIXME(nb) function above return duplicated query for the first 2 calls, so removing them here
     const uniqueIdentities = identities.filter(
       (value, index, self) => index === self.findIndex((t) => t.id === value.id),
     )
-    console.log({ uniqueIdentities: uniqueIdentities.length, totalCount })
     assert(totalCount == uniqueIdentities.length)
 
     return uniqueIdentities.map(toDomainIdentityPhone)
