4a289586f401800e3bf685b944dc4e225e5dd95e
vindard <17693119+vindard@users.noreply.github.com>
2022-10-31 17:04:47 -0400

    fix: add error handling to gql 'OnChainTxFeeQuery'

--- src/graphql/types/object/onchain-tx-fee.ts
+++ src/graphql/types/object/onchain-tx-fee.ts
@@ -1,13 +1,18 @@
 import { GT } from "@graphql/index"
 
+import IError from "../abstract/error"
+
 import SatAmount from "../scalar/sat-amount"
 import TargetConfirmations from "../scalar/target-confirmations"
 
 const OnChainTxFee = GT.Object({
   name: "OnChainTxFee",
   fields: () => ({
-    amount: { type: GT.NonNull(SatAmount) },
-    targetConfirmations: { type: GT.NonNull(TargetConfirmations) },
+    errors: {
+      type: GT.NonNullList(IError),
+    },
+    amount: { type: SatAmount },
+    targetConfirmations: { type: TargetConfirmations },
   }),
 })
 
