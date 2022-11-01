a37ddc39b0ef13e3b4f5ed2e3b1730f8bd451853
vindard <17693119+vindard@users.noreply.github.com>
2022-11-01 17:21:07 -0400

    test(refactor): unify on/off-chain PaymentFlow tests

--- test/unit/payments/payment-flow.spec.ts
+++ test/unit/payments/payment-flow.spec.ts
@@ -8,14 +8,85 @@ import { PaymentInitiationMethod, SettlementMethod } from "@domain/wallets"
 
 const calc = AmountCalculator()
 
-describe("PaymentFlowFromLedgerTransaction", () => {
-  const satsAmount = toSats(20_000)
-  const centsAmount = toCents(1_000)
-  const satsFee = toSats(400)
-  const centsFee = toCents(20)
-  const timestamp = new Date()
+const btcPaymentAmount = { amount: BigInt(20_000), currency: WalletCurrency.Btc }
+const usdPaymentAmount = { amount: BigInt(1_000), currency: WalletCurrency.Usd }
+const btcProtocolFee = { amount: BigInt(400), currency: WalletCurrency.Btc }
+const usdProtocolFee = { amount: BigInt(20), currency: WalletCurrency.Usd }
+const btcBankFee = { amount: BigInt(40), currency: WalletCurrency.Btc }
+const usdBankFee = { amount: BigInt(2), currency: WalletCurrency.Usd }
+const timestamp = new Date()
 
-  const paymentFlowState: PaymentFlowState<WalletCurrency, WalletCurrency> = {
+const walletsToTest = [
+  {
+    name: "btc",
+    sendAmount: calc.add(btcPaymentAmount, btcProtocolFee),
+    inputAmount: btcPaymentAmount.amount,
+  },
+  {
+    name: "usd",
+    sendAmount: calc.add(usdPaymentAmount, usdProtocolFee),
+    inputAmount: usdPaymentAmount.amount,
+  },
+]
+
+const runCheckBalanceTests = <S extends WalletCurrency, R extends WalletCurrency>({
+  name,
+  paymentFlow,
+}: {
+  name: string
+  paymentFlow: PaymentFlow<S, R> | OnChainPaymentFlow<S, R>
+}) => {
+  const sendAmount =
+    paymentFlow.senderWalletCurrency === WalletCurrency.Btc
+      ? paymentFlow.totalAmountsForPayment().btc
+      : paymentFlow.totalAmountsForPayment().usd
+
+  describe("checkBalanceForSend", () => {
+    describe(`${name} sending wallet`, () => {
+      it("passes for send amount under balance", () => {
+        const balanceForSend = {
+          amount: sendAmount.amount + 1n,
+          currency: sendAmount.currency as S,
+        }
+        const check = paymentFlow.checkBalanceForSend(balanceForSend)
+        console.log(check)
+        expect(check).not.toBeInstanceOf(Error)
+        expect(check).toBe(true)
+      })
+
+      it("passes for send amount equal to balance", () => {
+        const balanceForSend = sendAmount as BalanceAmount<S>
+        const check = paymentFlow.checkBalanceForSend(balanceForSend)
+        expect(check).not.toBeInstanceOf(Error)
+        expect(check).toBe(true)
+      })
+
+      it("fails for send amount above to balance", () => {
+        const balanceForSend = {
+          amount: sendAmount.amount - 1n,
+          currency: sendAmount.currency as S,
+        }
+        const check = paymentFlow.checkBalanceForSend(balanceForSend)
+        expect(check).toBeInstanceOf(InsufficientBalanceError)
+      })
+
+      it("fails for wrong balance currency", () => {
+        const balanceForSend = {
+          amount: sendAmount.amount + 1n,
+          currency:
+            sendAmount.currency === WalletCurrency.Btc
+              ? (WalletCurrency.Usd as S)
+              : (WalletCurrency.Btc as S),
+        }
+        const check = paymentFlow.checkBalanceForSend(balanceForSend)
+        expect(check).toBeInstanceOf(InvalidCurrencyForWalletError)
+      })
+    })
+  })
+}
+
+describe("LightningPaymentFlowFromLedgerTransaction", <S extends WalletCurrency, R extends WalletCurrency>() => {
+  const paymentFlowState: PaymentFlowState<S, R> = {
     senderWalletId: "walletId" as WalletId,
     senderAccountId: "accountId" as AccountId,
     settlementMethod: SettlementMethod.Lightning,
@@ -27,156 +98,29 @@ describe("PaymentFlowFromLedgerTransaction", () => {
     createdAt: timestamp,
     paymentSentAndPending: true,
 
-    btcPaymentAmount: {
-      amount: BigInt(satsAmount),
-      currency: WalletCurrency.Btc,
-    },
-    usdPaymentAmount: {
-      amount: BigInt(centsAmount),
-      currency: WalletCurrency.Usd,
-    },
+    btcPaymentAmount,
+    usdPaymentAmount,
 
     inputAmount: undefined as unknown as bigint,
-    senderWalletCurrency: undefined as unknown as WalletCurrency,
+    senderWalletCurrency: undefined as unknown as S,
 
-    btcProtocolFee: {
-      amount: BigInt(satsFee),
-      currency: WalletCurrency.Btc,
-    },
-    usdProtocolFee: {
-      amount: BigInt(centsFee),
-      currency: WalletCurrency.Usd,
-    },
+    btcProtocolFee,
+    usdProtocolFee,
   }
 
-  const walletsToTest = [
-    {
-      name: "btc",
-      senderCurrency: WalletCurrency.Btc,
-      sendAmount: BigInt(satsAmount + satsFee),
-      inputAmount: BigInt(satsAmount),
-    },
-    {
-      name: "usd",
-      senderCurrency: WalletCurrency.Usd,
-      sendAmount: BigInt(centsAmount + centsFee),
-      inputAmount: BigInt(centsAmount),
-    },
-  ]
-  describe("checkBalanceForSend", () => {
-    for (const { name, senderCurrency, sendAmount, inputAmount } of walletsToTest) {
-      describe(`${name} sending wallet`, () => {
-        const paymentFlow = PaymentFlow({
-          ...paymentFlowState,
-          inputAmount,
-          senderWalletCurrency: senderCurrency,
-        })
-        if (paymentFlow instanceof Error) throw paymentFlow
-
-        it("passes for send amount under balance", () => {
-          const balanceForSend = {
-            amount: sendAmount + 1n,
-            currency: senderCurrency,
-          }
-          const check = paymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).not.toBeInstanceOf(Error)
-          expect(check).toBe(true)
-        })
-
-        it("passes for send amount equal to balance", () => {
-          const balanceForSend = {
-            amount: sendAmount,
-            currency: senderCurrency,
-          }
-          const check = paymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).not.toBeInstanceOf(Error)
-          expect(check).toBe(true)
-        })
-
-        it("fails for send amount above to balance", () => {
-          const balanceForSend = {
-            amount: sendAmount - 1n,
-            currency: senderCurrency,
-          }
-          const check = paymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).toBeInstanceOf(InsufficientBalanceError)
-        })
-
-        it("fails for wrong balance currency", () => {
-          const balanceForSend = {
-            amount: sendAmount + 1n,
-            currency:
-              senderCurrency === WalletCurrency.Btc
-                ? WalletCurrency.Usd
-                : WalletCurrency.Btc,
-          }
-          const check = paymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).toBeInstanceOf(InvalidCurrencyForWalletError)
-        })
-      })
-    }
-  })
-
-  describe("inputAmountFromLedgerTransaction", () => {
-    const baseLedgerTransaction = {
-      fee: toSats(1),
-      usd: 0.2,
-      feeUsd: 0.01,
-
-      satsAmount: toSats(1000),
-      centsAmount: toCents(20),
-      satsFee: toSats(1),
-      centsFee: toCents(1),
-      displayAmount: 20 as DisplayCurrencyBaseAmount,
-      displayFee: 1 as DisplayCurrencyBaseAmount,
-      displayCurrency: "USD",
-    }
-
-    const btcLedgerTransaction = {
-      debit: toSats(1001),
-      credit: toSats(0),
-      currency: "BTC",
-      ...baseLedgerTransaction,
-    } as LedgerTransaction<"BTC">
-
-    const usdLedgerTransaction = {
-      debit: toCents(21),
-      credit: toCents(0),
-      currency: "USD",
-      ...baseLedgerTransaction,
-    } as LedgerTransaction<"USD">
-
-    it("calculates the correct input amount given a BTC LedgerTransaction", () => {
-      const inputAmount = inputAmountFromLedgerTransaction(btcLedgerTransaction)
-      expect(inputAmount).not.toBeInstanceOf(Error)
-
-      const satsAmount = safeBigInt(baseLedgerTransaction.satsAmount)
-      expect(satsAmount).not.toBeInstanceOf(Error)
-
-      expect(inputAmount).toEqual(satsAmount)
+  for (const { name, sendAmount, inputAmount } of walletsToTest) {
+    const paymentFlow = PaymentFlow<S, R>({
+      ...paymentFlowState,
+      inputAmount,
+      senderWalletCurrency: sendAmount.currency as S,
     })
+    if (paymentFlow instanceof Error) throw paymentFlow
 
-    it("calculates the correct input amount given a USD LedgerTransaction", () => {
-      const inputAmount = inputAmountFromLedgerTransaction(usdLedgerTransaction)
-      expect(inputAmount).not.toBeInstanceOf(Error)
-
-      const centsAmount = safeBigInt(baseLedgerTransaction.centsAmount)
-      expect(centsAmount).not.toBeInstanceOf(Error)
-
-      expect(inputAmount).toEqual(centsAmount)
-    })
-  })
+    runCheckBalanceTests({ name, paymentFlow })
+  }
 })
 
 describe("OnChainPaymentFlowFromLedgerTransaction", <S extends WalletCurrency, R extends WalletCurrency>() => {
-  const btcPaymentAmount = { amount: BigInt(20_000), currency: WalletCurrency.Btc }
-  const usdPaymentAmount = { amount: BigInt(1_000), currency: WalletCurrency.Usd }
-  const btcProtocolFee = { amount: BigInt(400), currency: WalletCurrency.Btc }
-  const usdProtocolFee = { amount: BigInt(20), currency: WalletCurrency.Usd }
-  const btcBankFee = { amount: BigInt(40), currency: WalletCurrency.Btc }
-  const usdBankFee = { amount: BigInt(2), currency: WalletCurrency.Usd }
-  const timestamp = new Date()
-
   const onChainPaymentFlowState: OnChainPaymentFlowState<S, R> = {
     senderWalletId: "walletId" as WalletId,
     senderAccountId: "accountId" as AccountId,
@@ -199,116 +143,64 @@ describe("OnChainPaymentFlowFromLedgerTransaction", <S extends WalletCurrency, R
     usdBankFee,
   }
 
-  const walletsToTest = [
-    {
-      name: "btc",
-      sendAmount: calc.add(btcPaymentAmount, btcProtocolFee),
-      inputAmount: btcPaymentAmount.amount,
-    },
-    {
-      name: "usd",
-      sendAmount: calc.add(usdPaymentAmount, usdProtocolFee),
-      inputAmount: usdPaymentAmount.amount,
-    },
-  ]
-  describe("checkBalanceForSend", () => {
-    for (const { name, sendAmount, inputAmount } of walletsToTest) {
-      describe(`${name} sending wallet`, () => {
-        const onChainPaymentFlow = OnChainPaymentFlow({
-          ...onChainPaymentFlowState,
-          inputAmount,
-          senderWalletCurrency: sendAmount.currency,
-        })
-        if (onChainPaymentFlow instanceof Error) throw onChainPaymentFlow
+  for (const { name, sendAmount, inputAmount } of walletsToTest) {
+    const onChainPaymentFlow = OnChainPaymentFlow({
+      ...onChainPaymentFlowState,
+      inputAmount,
+      senderWalletCurrency: sendAmount.currency,
+    })
+    if (onChainPaymentFlow instanceof Error) throw onChainPaymentFlow
 
-        it("passes for send amount under balance", () => {
-          const balanceForSend = {
-            amount: sendAmount.amount + 1n,
-            currency: sendAmount.currency,
-          }
-          const check = onChainPaymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).not.toBeInstanceOf(Error)
-          expect(check).toBe(true)
-        })
+    runCheckBalanceTests({ name, paymentFlow: onChainPaymentFlow })
+  }
+})
 
-        it("passes for send amount equal to balance", () => {
-          const balanceForSend = sendAmount as BalanceAmount<S>
-          const check = onChainPaymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).not.toBeInstanceOf(Error)
-          expect(check).toBe(true)
-        })
+describe("inputAmountFromLedgerTransaction", () => {
+  const baseLedgerTransaction = {
+    fee: toSats(1),
+    usd: 0.2,
+    feeUsd: 0.01,
 
-        it("fails for send amount above to balance", () => {
-          const balanceForSend = {
-            amount: sendAmount.amount - 1n,
-            currency: sendAmount.currency,
-          }
-          const check = onChainPaymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).toBeInstanceOf(InsufficientBalanceError)
-        })
+    satsAmount: toSats(1000),
+    centsAmount: toCents(20),
+    satsFee: toSats(1),
+    centsFee: toCents(1),
+    displayAmount: 20 as DisplayCurrencyBaseAmount,
+    displayFee: 1 as DisplayCurrencyBaseAmount,
+    displayCurrency: "USD",
+  }
 
-        it("fails for wrong balance currency", () => {
-          const balanceForSend = {
-            amount: sendAmount.amount + 1n,
-            currency:
-              sendAmount.currency === WalletCurrency.Btc
-                ? WalletCurrency.Usd
-                : WalletCurrency.Btc,
-          }
-          const check = onChainPaymentFlow.checkBalanceForSend(balanceForSend)
-          expect(check).toBeInstanceOf(InvalidCurrencyForWalletError)
-        })
-      })
-    }
+  const btcLedgerTransaction = {
+    debit: toSats(1001),
+    credit: toSats(0),
+    currency: "BTC",
+    ...baseLedgerTransaction,
+  } as LedgerTransaction<"BTC">
+
+  const usdLedgerTransaction = {
+    debit: toCents(21),
+    credit: toCents(0),
+    currency: "USD",
+    ...baseLedgerTransaction,
+  } as LedgerTransaction<"USD">
+
+  it("calculates the correct input amount given a BTC LedgerTransaction", () => {
+    const inputAmount = inputAmountFromLedgerTransaction(btcLedgerTransaction)
+    expect(inputAmount).not.toBeInstanceOf(Error)
+
+    const satsAmount = safeBigInt(baseLedgerTransaction.satsAmount)
+    expect(satsAmount).not.toBeInstanceOf(Error)
+
+    expect(inputAmount).toEqual(satsAmount)
   })
 
-  describe("inputAmountFromLedgerTransaction", () => {
-    const baseLedgerTransaction = {
-      fee: toSats(1),
-      usd: 0.2,
-      feeUsd: 0.01,
+  it("calculates the correct input amount given a USD LedgerTransaction", () => {
+    const inputAmount = inputAmountFromLedgerTransaction(usdLedgerTransaction)
+    expect(inputAmount).not.toBeInstanceOf(Error)
 
-      satsAmount: toSats(1000),
-      centsAmount: toCents(20),
-      satsFee: toSats(1),
-      centsFee: toCents(1),
-      displayAmount: 20 as DisplayCurrencyBaseAmount,
-      displayFee: 1 as DisplayCurrencyBaseAmount,
-      displayCurrency: "USD",
-    }
+    const centsAmount = safeBigInt(baseLedgerTransaction.centsAmount)
+    expect(centsAmount).not.toBeInstanceOf(Error)
 
-    const btcLedgerTransaction = {
-      debit: toSats(1001),
-      credit: toSats(0),
-      currency: "BTC",
-      ...baseLedgerTransaction,
-    } as LedgerTransaction<"BTC">
-
-    const usdLedgerTransaction = {
-      debit: toCents(21),
-      credit: toCents(0),
-      currency: "USD",
-      ...baseLedgerTransaction,
-    } as LedgerTransaction<"USD">
-
-    it("calculates the correct input amount given a BTC LedgerTransaction", () => {
-      const inputAmount = inputAmountFromLedgerTransaction(btcLedgerTransaction)
-      expect(inputAmount).not.toBeInstanceOf(Error)
-
-      const satsAmount = safeBigInt(baseLedgerTransaction.satsAmount)
-      expect(satsAmount).not.toBeInstanceOf(Error)
-
-      expect(inputAmount).toEqual(satsAmount)
-    })
-
-    it("calculates the correct input amount given a USD LedgerTransaction", () => {
-      const inputAmount = inputAmountFromLedgerTransaction(usdLedgerTransaction)
-      expect(inputAmount).not.toBeInstanceOf(Error)
-
-      const centsAmount = safeBigInt(baseLedgerTransaction.centsAmount)
-      expect(centsAmount).not.toBeInstanceOf(Error)
-
-      expect(inputAmount).toEqual(centsAmount)
-    })
+    expect(inputAmount).toEqual(centsAmount)
   })
 })
