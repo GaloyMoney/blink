6caaac1ca042212e710b0b26d4d33bb700053d13
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-11-29 21:12:16 +0000

    chore: fix compilation

--- src/services/kratos/auth-phone-no-password.ts
+++ src/services/kratos/auth-phone-no-password.ts
@@ -12,7 +12,6 @@ import {
   AdminCreateIdentityBody,
   AdminUpdateIdentityBody,
   Identity,
-  IdentityState,
   SuccessfulSelfServiceLoginWithoutBrowser,
   SuccessfulSelfServiceRegistrationWithoutBrowser,
 } from "@ory/client"
