da72cbc3450d5251f1ab33103bdc5909aa16b141
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-12-02 16:56:07 +0000

    chore: fix compilation

--- src/services/kratos/index.types.d.ts
+++ src/services/kratos/index.types.d.ts
@@ -1,7 +1,5 @@
 type KratosIdentity = import("@ory/client").Identity
 
-type KratosError = import("./errors").KratosError
-
 // Kratos SDK is implying that identity always exist for Session
 //
 // TODO: verify identity is always defined for session
