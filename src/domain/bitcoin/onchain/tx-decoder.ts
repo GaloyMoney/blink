a783f5ad56f80509d76002a61fddeeb32692eb5a
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-12-03 10:06:03 +0000

    chore: remove hacky workaround.
    will no longer be neeeded with update of libbitcoinjs

--- src/domain/bitcoin/onchain/tx-decoder.ts
+++ src/domain/bitcoin/onchain/tx-decoder.ts
@@ -24,22 +24,11 @@ export const TxDecoder = (networkName: BtcNetwork): TxDecoder => {
   }
 }
 
-// FIXME: https://github.com/bitcoinjs/bitcoinjs-lib/issues/1800#issuecomment-1126629937
-const wrappedFromOutputScript = (script: Buffer, network: networks.Network) => {
-  const oldWarn = console.warn
-  console.warn = () => {
-    //
-  }
-  const result = address.fromOutputScript(script, network)
-  console.warn = oldWarn
-  return result
-}
-
-function decodeOutput(tx: Transaction, network: Network): TxOut[] {
+const decodeOutput = (tx: Transaction, network: Network): TxOut[] => {
   const format = (out: TxOutput, network: Network) => {
     let decodedAddress: OnChainAddress | null = null
     try {
-      decodedAddress = wrappedFromOutputScript(out.script, network) as OnChainAddress
+      decodedAddress = address.fromOutputScript(out.script, network) as OnChainAddress
     } catch (_) {
       // OP_RETURN outputs don't have a valid address associated with them
     }
