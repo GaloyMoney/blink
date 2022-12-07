30c7040c88ae119a0e1fd8665550eba810511aa7
Juan P Lopez <dolcalmi@gmail.com>
2022-12-06 21:41:12 -0500

    fix: rename id to userId and remove users without phone

--- src/migrations/2022120510370802-fix-migration.ts
+++ src/migrations/2022120510370802-fix-migration.ts
@@ -1,41 +1,14 @@
 module.exports = {
   async up(db) {
-    console.log("Begin account to users property migration")
+    const removed = await db.collection("users").deleteMany({ phone: { $exists: false } })
+    console.log({ removed }, "removed users")
 
-    const users = db.collection("users").find({ id: { $exists: true } })
-
-    let progress = 0
-    for await (const user of users) {
-      progress++
-
-      const userId = user.id
-      const phoneMetadata = user.phoneMetadata
-      const language = user.language ?? ""
-      const deviceTokens = user.deviceTokens ?? []
-      const phone = user.phone
-      const createdAt = user.createdAt
-
-      await db.collection("users").updateOne(
-        {
-          userId,
-        },
-        {
-          $set: {
-            phoneMetadata,
-            language,
-            deviceTokens,
-            phone,
-            createdAt,
-          },
-        },
-        { upsert: true },
+    const updated = await db
+      .collection("users")
+      .updateMany(
+        { id: { $exists: true }, userId: { $exists: false } },
+        { $rename: { id: "userId" } },
       )
-
-      if (progress % 1000 === 0) {
-        console.log(`${progress} users updated`)
-      }
-    }
-
-    console.log("Finish account to users property migration")
+    console.log({ updated }, "updated users")
   },
 }
