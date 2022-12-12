088a517de7219b57e960638eee04ff4338cd9b54
vindard <17693119+vindard@users.noreply.github.com>
2022-12-12 08:33:32 -0500

    feat(graphql): add weekly limits range as sample

--- src/graphql/types/index.ts
+++ src/graphql/types/index.ts
@@ -4,6 +4,7 @@ import ConsumerAccount from "./object/consumer-account"
 import UsdWallet from "./object/usd-wallet"
 import GraphQLApplicationError from "./object/graphql-application-error"
 import DailyAccountLimit from "./object/daily-account-limit"
+import WeeklyAccountLimit from "./object/weekly-account-limit"
 
 // The following types are not directly included
 // in the GraphQL schema. They only implement interfaces.
@@ -15,4 +16,5 @@ export const ALL_INTERFACE_TYPES = [
   BtcWallet,
   UsdWallet,
   DailyAccountLimit,
+  WeeklyAccountLimit,
 ]
