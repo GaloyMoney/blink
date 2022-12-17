afbe1d33138ae53da8cb8b79960d7dd78075b2ca
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-12-17 21:22:45 +0000

    chore: add tracing for notifications

--- src/services/notifications/push-notifications.ts
+++ src/services/notifications/push-notifications.ts
@@ -7,6 +7,12 @@ import {
 import { baseLogger } from "@services/logger"
 import { googleApplicationCredentialsIsSet } from "@config"
 import { Messaging } from "firebase-admin/lib/messaging/messaging"
+import {
+  addAttributesToCurrentSpan,
+  recordExceptionInCurrentSpan,
+  wrapAsyncToRunInSpan,
+} from "@services/tracing"
+import { ErrorLevel } from "@domain/shared"
 
 const logger = baseLogger.child({ module: "notifications" })
 
@@ -23,6 +29,59 @@ if (googleApplicationCredentialsIsSet()) {
   messaging = admin.messaging()
 }
 
+const sendToDevice = async (
+  tokens: DeviceToken[],
+  message: MessagingPayload & {
+    notification: NotificationMessagePayload
+  },
+) => {
+  try {
+    const response = await messaging.sendToDevice(tokens, message)
+    logger.info({ response, tokens, message }, "notification was sent successfully")
+
+    response.results.forEach((item) => {
+      if (item?.error?.message) {
+        recordExceptionInCurrentSpan({
+          error: item.error.message,
+          level: ErrorLevel.Info,
+        })
+      }
+    })
+
+    addAttributesToCurrentSpan({
+      failureCount: response.failureCount,
+      successCount: response.successCount,
+      canonicalRegistrationTokenCount: response.canonicalRegistrationTokenCount,
+    })
+
+    // TODO token clean up
+    // ie, when the message below is received
+
+    //   "response": {
+    //   "results": [
+    //     {
+    //       "error": {
+    //         "code": "messaging/registration-token-not-registered",
+    //         "message": "The provided registration token is not registered. A previously valid registration token can be unregistered for a variety of reasons. See the error documentation for more details. Remove this registration token and stop using it to send messages."
+    //       }
+    //     },
+    //     {
+    //       "messageId": "0:1671300966132147%7c88793f7c88793f"
+    //     }
+    //   ],
+    //   "canonicalRegistrationTokenCount": 0,
+    //   "failureCount": 1,
+    //   "successCount": 1,
+    //   "multicastId": 2601374049640558600
+    // },
+
+    return true
+  } catch (err) {
+    logger.error({ err, tokens, message }, "impossible to send notification")
+    return new UnknownNotificationsServiceError(err?.message)
+  }
+}
+
 export const PushNotificationsService = (): IPushNotificationsService => {
   const sendNotification = async ({
     deviceTokens,
@@ -35,12 +94,8 @@ export const PushNotificationsService = (): IPushNotificationsService => {
       // if we don't set notification, this will appear for background but not quit stage
       // we may be able to use data only, but this should be implemented first:
       // https://rnfirebase.io/messaging/usage#background-application-state
-      notification: { title },
+      notification: { title, body },
       data: data || {},
-    }
-
-    if (body) {
-      message.notification.body = body
     }
 
     const tokens = deviceTokens.filter((token) => token.length === 163)
@@ -49,36 +104,11 @@ export const PushNotificationsService = (): IPushNotificationsService => {
       return new InvalidDeviceNotificationsServiceError()
     }
 
-    try {
-      const response = await messaging.sendToDevice(tokens, message)
-      logger.info({ response, tokens, message }, "notification was sent successfully")
-
-      // TODO token clean up
-      // ie, when the message below is received
-
-      //   "response": {
-      //   "results": [
-      //     {
-      //       "error": {
-      //         "code": "messaging/registration-token-not-registered",
-      //         "message": "The provided registration token is not registered. A previously valid registration token can be unregistered for a variety of reasons. See the error documentation for more details. Remove this registration token and stop using it to send messages."
-      //       }
-      //     },
-      //     {
-      //       "messageId": "0:1671300966132147%7c88793f7c88793f"
-      //     }
-      //   ],
-      //   "canonicalRegistrationTokenCount": 0,
-      //   "failureCount": 1,
-      //   "successCount": 1,
-      //   "multicastId": 2601374049640558600
-      // },
-
-      return true
-    } catch (err) {
-      logger.error({ err, tokens, message }, "impossible to send notification")
-      return new UnknownNotificationsServiceError(err?.message)
-    }
+    return wrapAsyncToRunInSpan({
+      namespace: "app.notifications",
+      fnName: "sendToDevice",
+      fn: () => sendToDevice(tokens, message),
+    })()
   }
 
   return { sendNotification }
