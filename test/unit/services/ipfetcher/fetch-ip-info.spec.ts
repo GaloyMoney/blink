ec96c43bcaee40e9d380a8ec76d59795f3e96c3a
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-12-22 20:40:33 +0000

    test: move isPrivateIp into domain

--- test/unit/services/ipfetcher/fetch-ip-info.spec.ts
+++ test/unit/services/ipfetcher/fetch-ip-info.spec.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 import MockAdapter from "axios-mock-adapter"
 import { IpFetcher } from "@services/ipfetcher"
-import { isPrivateIp } from "@domain/accounts-ips"
 
 let mock
 
@@ -57,28 +56,6 @@ describe("IpFetcher - fetchIPInfo", () => {
         status: "ok",
       }),
     )
-  })
-})
-
-describe("ipaddr.js", () => {
-  it("check ipv6 private address validation", () => {
-    const localhost = "::ffff:127.0.0.1"
-    expect(isPrivateIp(localhost)).toBeTruthy()
-  })
-
-  it("check ipv4 localhost address validation", () => {
-    const localhost = "127.0.0.1"
-    expect(isPrivateIp(localhost)).toBeTruthy()
-  })
-
-  it("check ipv4 private address validation", () => {
-    const localhost = "192.168.1.1"
-    expect(isPrivateIp(localhost)).toBeTruthy()
-  })
-
-  it("check non private IP", () => {
-    const localhost = "152.231.190.229"
-    expect(isPrivateIp(localhost)).toBeFalsy()
   })
 })
 
