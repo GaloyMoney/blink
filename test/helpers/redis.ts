67d5de0b1e2072958e62a53c165ca02b3d26daef
vindard <17693119+vindard@users.noreply.github.com>
2022-12-23 22:53:47 -0400

    test: delete unused okex redis publish functions

--- test/helpers/redis.ts
+++ test/helpers/redis.ts
@@ -1,7 +1,3 @@
-import crypto from "crypto"
-
-import Redis from "ioredis"
-
 import { RateLimitPrefix } from "@domain/rate-limit"
 import { redis } from "@services/redis"
 
@@ -23,47 +19,3 @@ export const clearLimitersWithExclusions = async (exclusions: string[]) => {
 }
 
 export const clearLimiters = () => clearLimitersWithExclusions([])
-
-let ioRedis
-let pricePublishInterval
-
-const executePublish = async (ioRedis) => {
-  const timestamp = Math.floor(new Date().getTime() / 1000)
-  const message = {
-    meta: {
-      publishedAt: timestamp,
-      correlationId: crypto.randomUUID(),
-    },
-    payloadType: "OkexBtcUsdSwapPricePayload",
-    payload: {
-      timestamp,
-      exchange: "okex",
-      instrumentId: "BTC-USD-SWAP",
-      askPrice: {
-        numeratorUnit: "USD_CENT",
-        denominatorUnit: "SATOSHI",
-        offset: 12,
-        base: "20000000000",
-      },
-      bidPrice: {
-        numeratorUnit: "USD_CENT",
-        denominatorUnit: "SATOSHI",
-        offset: 12,
-        base: "20000000000",
-      },
-    },
-  }
-  const channel = "galoy.stablesats.price.okex.btc-usd-swap"
-  await ioRedis.publish(channel, JSON.stringify(message))
-}
-
-export const publishOkexPrice = async () => {
-  ioRedis = new Redis(6379, process.env.REDIS_0_INTERNAL_IP || "localhost")
-  await executePublish(ioRedis)
-  pricePublishInterval = setInterval(() => executePublish(ioRedis), 1000)
-}
-
-export const cancelOkexPricePublish = () => {
-  clearInterval(pricePublishInterval)
-  ioRedis.quit()
-}
