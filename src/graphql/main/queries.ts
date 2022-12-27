26df9f29ec149dee53de764435faa3aa06b1eddf
vindard <17693119+vindard@users.noreply.github.com>
2022-12-27 00:54:15 -0400

    feat(gql): add new gql query for USD onchain fee probe

--- src/graphql/main/queries.ts
+++ src/graphql/main/queries.ts
@@ -7,6 +7,7 @@ import BtcPriceListQuery from "@graphql/root/query/btc-price-list"
 import QuizQuestionsQuery from "@graphql/root/query/quiz-questions"
 import MobileVersionsQuery from "@graphql/root/query/mobile-versions"
 import OnChainTxFeeQuery from "@graphql/root/query/on-chain-tx-fee-query"
+import OnChainUsdTxFeeQuery from "@graphql/root/query/on-chain-usd-tx-fee-query"
 import UsernameAvailableQuery from "@graphql/root/query/username-available"
 import BusinessMapMarkersQuery from "@graphql/root/query/business-map-markers"
 import AccountDefaultWalletQuery from "@graphql/root/query/account-default-wallet"
@@ -25,6 +26,7 @@ const fields = {
   btcPrice: BtcPriceQuery,
   btcPriceList: BtcPriceListQuery,
   onChainTxFee: OnChainTxFeeQuery,
+  onChainUsdTxFee: OnChainUsdTxFeeQuery,
   lnInvoicePaymentStatus: LnInvoicePaymentStatusQuery,
 } as const
 
