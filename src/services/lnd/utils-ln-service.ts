5f74fa335f4b14d72609c513976a2bb585e0550c
openoms <oms@tuta.io>
2022-12-27 16:44:55 +0100

    fix getTotalPendingHtlcCount function

--- src/services/lnd/utils-ln-service.ts
+++ src/services/lnd/utils-ln-service.ts
@@ -7,29 +7,44 @@ const lndsParamsAuth = getLnds({ type: "offchain", active: true })
 const lnd1 = lndsParamsAuth[0].lnd
 const lnd2 = lndsParamsAuth[1].lnd
 
-interface Channel {
-  pending_payments: number[]
-}
-
-async function getPendingPaymentCountLnd1(): Promise<number> {
+async function getPendingHtlcCountLnd1(): Promise<{ [channelId: string]: number }> {
   const channels = await lnService.getChannels({ lnd1 })
-  const pendingPayments = channels.filter(
-    (channel: Channel) => channel.pending_payments.length > 0,
-  )
-  return pendingPayments.length
+  const pendingHtlcCounts: { [channelId: string]: number } = {}
+  channels.forEach((channel: { id: string; pending_payments: any[] }) => {
+    pendingHtlcCounts[channel.id] = channel.pending_payments.length
+  })
+  return pendingHtlcCounts
 }
 
-async function getPendingPaymentCountLnd2(): Promise<number> {
+async function getPendingHtlcCountLnd2(): Promise<{ [channelId: string]: number }> {
   const channels = await lnService.getChannels({ lnd2 })
-  const pendingPayments = channels.filter(
-    (channel: Channel) => channel.pending_payments.length > 0,
-  )
-  return pendingPayments.length
+  const pendingHtlcCounts: { [channelId: string]: number } = {}
+  channels.forEach((channel: { id: string; pending_payments: any[] }) => {
+    pendingHtlcCounts[channel.id] = channel.pending_payments.length
+  })
+  return pendingHtlcCounts
 }
 
-export async function getPendingPaymentCount(): Promise<number> {
-  const pendingPaymentCountLnd1 = await getPendingPaymentCountLnd1()
-  const pendingPaymentCountLnd2 = await getPendingPaymentCountLnd2()
-  const pendingPaymentCount = pendingPaymentCountLnd1 + pendingPaymentCountLnd2
-  return pendingPaymentCount
+async function getTotalPendingHtlcCountLnd1(): Promise<number> {
+  const pendingHtlcCounts = await getPendingHtlcCountLnd1()
+  const totalPendingHtlcCount = Object.values(pendingHtlcCounts).reduce(
+    (a, b) => a + b,
+    0,
+  )
+  return totalPendingHtlcCount
+}
+
+async function getTotalPendingHtlcCountLnd2(): Promise<number> {
+  const pendingHtlcCounts = await getPendingHtlcCountLnd2()
+  const totalPendingHtlcCount = Object.values(pendingHtlcCounts).reduce(
+    (a, b) => a + b,
+    0,
+  )
+  return totalPendingHtlcCount
+}
+
+export async function getTotalPendingHtlcCount(): Promise<number> {
+  const totalPendingHtlcCountLnd1 = await getTotalPendingHtlcCountLnd1()
+  const totalPendingHtlcCountLnd2 = await getTotalPendingHtlcCountLnd2()
+  return totalPendingHtlcCountLnd1 + totalPendingHtlcCountLnd2
 }
