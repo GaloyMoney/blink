a031fbf993aa0e358ed59cecfe2e29741e61b8df
vindard <17693119+vindard@users.noreply.github.com>
2022-12-27 00:54:15 -0400

    feat(gql): add new gql query for USD onchain fee probe

---
+++ src/graphql/root/query/on-chain-usd-tx-fee-query.ts
@@ -0,0 +1,48 @@
+import { Wallets } from "@app"
+
+import { GT } from "@graphql/index"
+import { mapError } from "@graphql/error-map"
+import { validateIsUsdWalletForQuery } from "@graphql/helpers"
+
+import WalletId from "@graphql/types/scalar/wallet-id"
+import CentAmount from "@graphql/types/scalar/cent-amount"
+import OnChainAddress from "@graphql/types/scalar/on-chain-address"
+import TargetConfirmations from "@graphql/types/scalar/target-confirmations"
+
+import OnChainTxFee from "@graphql/types/object/onchain-tx-fee"
+
+const OnChainUsdTxFeeQuery = GT.Field({
+  type: GT.NonNull(OnChainTxFee),
+  args: {
+    walletId: { type: GT.NonNull(WalletId) },
+    address: { type: GT.NonNull(OnChainAddress) },
+    amount: { type: GT.NonNull(CentAmount) },
+    targetConfirmations: { type: TargetConfirmations, defaultValue: 1 },
+  },
+  resolve: async (_, args, { domainAccount }) => {
+    const { walletId, address, amount, targetConfirmations } = args
+
+    for (const input of [walletId, address, amount, targetConfirmations]) {
+      if (input instanceof Error) throw input
+    }
+
+    const usdWalletValidated = await validateIsUsdWalletForQuery(walletId)
+    if (usdWalletValidated instanceof Error) throw usdWalletValidated
+
+    const fee = await Wallets.getOnChainFee({
+      walletId,
+      account: domainAccount as Account,
+      amount,
+      address,
+      targetConfirmations,
+    })
+    if (fee instanceof Error) throw mapError(fee)
+
+    return {
+      amount: fee,
+      targetConfirmations,
+    }
+  },
+})
+
+export default OnChainUsdTxFeeQuery
