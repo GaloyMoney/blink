111f7a4d9fa0988fea57d1a41eff075a4a0abbb3
vindard <17693119+vindard@users.noreply.github.com>
2023-01-04 18:35:23 -0400

    refactor: de-deduplicate code block in WalletAddressReceiver

--- src/domain/wallet-on-chain-addresses/wallet-address-receiver.ts
+++ src/domain/wallet-on-chain-addresses/wallet-address-receiver.ts
@@ -12,30 +12,12 @@ export const WalletAddressReceiver = async <S extends WalletCurrency>({
 }: WalletAddressReceiverArgs<S>): Promise<
   WalletAddressReceiver | DealerPriceServiceError | ValidationError
 > => {
-  if (walletAddress.recipientWalletDescriptor.currency === WalletCurrency.Btc) {
-    const receivedUsd = await usdFromBtcMidPrice(receivedBtc)
-    if (receivedUsd instanceof Error) return receivedUsd
+  const usdFromBtcFn =
+    walletAddress.recipientWalletDescriptor.currency === WalletCurrency.Btc
+      ? usdFromBtcMidPrice
+      : usdFromBtc
 
-    const priceRatio = PriceRatio({ usd: receivedUsd, btc: receivedBtc })
-    if (priceRatio instanceof Error) return priceRatio
-
-    const bankFee = {
-      usdBankFee: priceRatio.convertFromBtc(feeBtc),
-      btcBankFee: feeBtc,
-    }
-
-    return {
-      btcToCreditReceiver: calc.sub(receivedBtc, feeBtc),
-      usdToCreditReceiver: calc.sub(receivedUsd, bankFee.usdBankFee),
-      ...bankFee,
-      receivedAmount: () =>
-        walletAddress.recipientWalletDescriptor.currency === WalletCurrency.Btc
-          ? receivedBtc
-          : receivedUsd,
-    }
-  }
-
-  const receivedUsd = await usdFromBtc(receivedBtc)
+  const receivedUsd = await usdFromBtcFn(receivedBtc)
   if (receivedUsd instanceof Error) return receivedUsd
 
   const priceRatio = PriceRatio({ usd: receivedUsd, btc: receivedBtc })
