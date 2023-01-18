e593bd2ccb1c010e0f0131023b02454490fde52d
nick theile <ntheile@gmail.com>
2023-01-18 13:08:31 -0600

    fix: kratos extend session cron (#2185)
    * fix: kratos extend session cron
    
    * chore: add tracing for session counts

--- src/app/auth/index.ts
+++ src/app/auth/index.ts
@@ -7,11 +7,20 @@ import { extendSession, IdentityRepository } from "@services/kratos"
 
 // TODO: interface/type should be reworked so that it doesn't have to come from private
 import { listSessionsInternal } from "@services/kratos/private"
-import { recordExceptionInCurrentSpan } from "@services/tracing"
+import {
+  recordExceptionInCurrentSpan,
+  addAttributesToCurrentSpan,
+} from "@services/tracing"
 
 export const extendSessions = async (): Promise<void | KratosError> => {
   const users = await IdentityRepository().listIdentities()
   if (users instanceof Error) return users
+
+  const countOfTotalUsers = users.length
+  let countOfTotalSessions = 0
+  let countOfExtendedSessions = 0
+  let countOfExtendedSessionErrors = 0
+  let countOfInactiveSessions = 0
 
   for (const user of users) {
     const sessions = await listSessionsInternal(user.id)
@@ -25,14 +34,27 @@ export const extendSessions = async (): Promise<void | KratosError> => {
     }
 
     for (const session of sessions) {
-      const res = await extendSession({ session })
-      if (res instanceof Error) {
+      countOfTotalSessions++
+      const hasExtended = await extendSession({ session })
+      if (hasExtended instanceof Error) {
+        countOfExtendedSessionErrors++
         recordExceptionInCurrentSpan({
           error: "impossible to extend session",
           level: ErrorLevel.Info,
           attributes: { user: user.id, phone: user.phone },
         })
       }
+      hasExtended ? countOfExtendedSessions++ : countOfInactiveSessions++
     }
   }
+  addAttributesToCurrentSpan({
+    countOfTotalUsers: countOfTotalUsers === 0 ? "none" : countOfTotalUsers, // *0 does not log in honeycomb properly sometimes, so set to none
+    countOfTotalSessions: countOfTotalSessions === 0 ? "none" : countOfTotalSessions,
+    countOfExtendedSessions:
+      countOfExtendedSessions === 0 ? "none" : countOfExtendedSessions,
+    countOfExtendedSessionErrors:
+      countOfExtendedSessionErrors === 0 ? "none" : countOfExtendedSessionErrors,
+    countOfInactiveSessions:
+      countOfInactiveSessions === 0 ? "none" : countOfInactiveSessions,
+  })
 }
