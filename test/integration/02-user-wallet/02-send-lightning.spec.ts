65269e1a25073e40b7112a876df01d5b761e4074
Juan P Lopez <dolcalmi@gmail.com>
2023-01-23 21:52:22 -0500

    fix: update fee calc rounding in tests

--- test/integration/02-user-wallet/02-send-lightning.spec.ts
+++ test/integration/02-user-wallet/02-send-lightning.spec.ts
@@ -1194,9 +1194,9 @@ describe("UserWallet - Lightning Pay", () => {
         if (baseMilliSats === undefined || feeRatePpm === undefined) {
           throw new Error("Undefined baseMilliSats or feeRatePpm")
         }
-        const baseFee = parseInt(baseMilliSats, 10) / 1000
-        const feeRate = (amountInvoice * feeRatePpm) / 1_000_000
-        const fee = Math.ceil(baseFee + feeRate)
+        const baseFee = BigInt(baseMilliSats) / BigInt(1000)
+        const feeRate = (BigInt(amountInvoice) * BigInt(feeRatePpm)) / BigInt(1_000_000)
+        const fee = Number(baseFee + feeRate)
 
         expect(finalBalance).toBe(initialBalance - amountInvoice - fee)
       })
