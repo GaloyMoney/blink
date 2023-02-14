1d1b561c6b8ee95b228d8e417089bc3ee61d88b9
vindard <17693119+vindard@users.noreply.github.com>
2023-02-14 10:01:04 -0400

    test: add findForRoute case for high proportional fee

--- test/integration/services/lnd/lnd-service.spec.ts
+++ test/integration/services/lnd/lnd-service.spec.ts
@@ -1,15 +1,18 @@
-import { createInvoice } from "lightning"
+import { createInvoice, getChannel, getChannels, pay } from "lightning"
 
 import { LndService } from "@services/lnd"
 
 import {
   decodeInvoice,
   MaxFeeTooLargeForRoutelessPaymentError,
+  RouteNotFoundError,
 } from "@domain/bitcoin/lightning"
 import { LnFees } from "@domain/payments"
 import { AmountCalculator, WalletCurrency } from "@domain/shared"
 
-import { lndOutside1 } from "test/helpers"
+import { sleep } from "@utils"
+
+import { lndOutside1, lndOutside3, setChannelFees } from "test/helpers"
 
 const calc = AmountCalculator()
 
@@ -47,6 +50,67 @@ describe("LndService", () => {
         maxFeeAmount: calc.add(feeAmount, ONE_SAT),
       })
       expect(paid).toBeInstanceOf(MaxFeeTooLargeForRoutelessPaymentError)
+    })
+  })
+
+  describe("findRouteForInvoice", () => {
+    const lndService = LndService()
+    if (lndService instanceof Error) throw lndService
+
+    it("probes across route with fee higher than payment amount", async () => {
+      const amountInvoice = 1
+
+      // Get routing channel details
+      const { channels } = await getChannels({ lnd: lndOutside3 })
+      const {
+        id: chanId,
+        remote_balance: remoteBalance,
+        remote_reserve: remoteReserve,
+      } = channels[0]
+
+      // Rebalance lndOutside3 route
+      const { request: invoice } = await createInvoice({
+        lnd: lndOutside1,
+        tokens:
+          remoteBalance < remoteReserve ? amountInvoice + remoteReserve : amountInvoice,
+      })
+      const rebalance = await pay({ lnd: lndOutside3, request: invoice })
+      expect(rebalance.is_confirmed).toBe(true)
+      await sleep(500) // rebalance can take some time to settle even after promise is resolved
+
+      // Bump base fee on routing node for disproportionate probe fee
+      const channel = await getChannel({ id: chanId, lnd: lndOutside3 })
+      let count = 0
+      const countMax = 3
+      let setOnLndOutside1
+      while (count < countMax && setOnLndOutside1 !== true) {
+        if (count > 0) await sleep(500)
+        count++
+
+        setOnLndOutside1 = await setChannelFees({
+          lnd: lndOutside1,
+          channel,
+          base: 10,
+          rate: 5000,
+        })
+      }
+      expect(count).toBeGreaterThan(0)
+      expect(count).toBeLessThan(countMax)
+      expect(setOnLndOutside1).toBe(true)
+
+      // Execute fee probe
+      const { request } = await createInvoice({
+        lnd: lndOutside3,
+        tokens: amountInvoice,
+        is_including_private_channels: true,
+      })
+      const decodedInvoice = decodeInvoice(request)
+      if (decodedInvoice instanceof Error) throw decodedInvoice
+
+      const probed = await lndService.findRouteForInvoice({
+        invoice: decodedInvoice,
+      })
+      expect(probed).toBeInstanceOf(RouteNotFoundError)
     })
   })
 })
