5b969b17a2f43235cdeabdbd9cb25b96ab69285b
Juan P Lopez <dolcalmi@gmail.com>
2023-02-24 07:09:39 -0500

    fix: add id to currencyList (#2363)
    * fix: add id to currencyList
    
    * Update currency.ts
    
    ---------
    
    Co-authored-by: nicolasburtey <nicolasburtey@users.noreply.github.com>

--- src/graphql/types/object/currency.ts
+++ src/graphql/types/object/currency.ts
@@ -3,7 +3,10 @@ import { GT } from "@graphql/index"
 const Currency = GT.Object({
   name: "Currency",
   fields: () => ({
-    code: { type: GT.NonNull(GT.String) },
+    id: {
+      type: GT.NonNullID,
+      resolve: (source) => source.code,
+    },
     symbol: { type: GT.NonNull(GT.String) },
     name: { type: GT.NonNull(GT.String) },
     flag: { type: GT.NonNull(GT.String) },
