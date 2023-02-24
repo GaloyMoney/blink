92beff44bc1fe1fd8c8448a0c9fbcab07908f149
Arvin <17693119+vindard@users.noreply.github.com>
2023-02-24 12:07:58 -0400

    fix: revert payment on verifyMaxFee error (#2364)
    * test: add integration case to check verifyMaxFee behaviour
    
    * fix: revert payment on verifyMaxFee error
    
    * test(refactor): clean up LnFees mock

--- test/integration/02-user-wallet/02-send-lightning.spec.ts
+++ test/integration/02-user-wallet/02-send-lightning.spec.ts
@@ -11,6 +11,7 @@ import {
   InvalidFeeProbeStateError,
   InvoiceExpiredOrBadPaymentHashError,
   LightningServiceError,
+  MaxFeeTooLargeForRoutelessPaymentError,
   PaymentNotFoundError,
   PaymentSendStatus,
   PaymentStatus,
@@ -31,6 +32,7 @@ import {
   PriceRatio,
   ZeroAmountForUsdRecipientError,
 } from "@domain/payments"
+import * as LnFeesImpl from "@domain/payments/ln-fees"
 import {
   AmountCalculator,
   paymentAmountFromNumber,
@@ -622,6 +624,55 @@ describe("UserWallet - Lightning Pay", () => {
     // imbalance is reduced with lightning payment
     expect(Number(imbalanceFinal.amount)).toBe(
       Number(imbalanceInit.amount) - amountInvoice,
+    )
+  })
+
+  it("pay zero amount invoice & revert txn when verifyMaxFee fails", async () => {
+    const { LnFees: LnFeesOrig } = jest.requireActual("@domain/payments/ln-fees")
+    jest
+      .spyOn(LnFeesImpl, "LnFees")
+      // 1st call is in PaymentFlow
+      .mockReturnValueOnce({
+        ...LnFeesOrig(),
+      })
+      // 2nd call is in use-case at verifyMaxFee
+      .mockReturnValueOnce({
+        ...LnFeesOrig(),
+        verifyMaxFee: () => new MaxFeeTooLargeForRoutelessPaymentError(),
+      })
+
+    const { request, id } = await createInvoice({ lnd: lndOutside1 })
+    const paymentHash = id as PaymentHash
+
+    const paymentResult = await Payments.payNoAmountInvoiceByWalletIdForBtcWallet({
+      uncheckedPaymentRequest: request,
+      memo: null,
+      amount: amountInvoice,
+      senderWalletId: walletIdB,
+      senderAccount: accountB,
+    })
+    expect(paymentResult).toBeInstanceOf(MaxFeeTooLargeForRoutelessPaymentError)
+
+    const txns = await LedgerService().getTransactionsByHash(paymentHash)
+    if (txns instanceof Error) throw txns
+
+    const { satsAmount, satsFee } = txns[0]
+    expect(txns.length).toEqual(2)
+    expect(txns).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          lnMemo: "Payment canceled",
+          credit: (satsAmount || 0) + (satsFee || 0),
+          debit: 0,
+          pendingConfirmation: false,
+        }),
+        expect.objectContaining({
+          lnMemo: "",
+          debit: (satsAmount || 0) + (satsFee || 0),
+          credit: 0,
+          pendingConfirmation: false,
+        }),
+      ]),
     )
   })
 
