1909f666faa1a5eb9666bda880f8913a5cfb8139
vindard <17693119+vindard@users.noreply.github.com>
2023-03-23 10:48:51 +0700

    refactor: remove unused 'displayAmountFromNumber' method

--- src/domain/shared/primitives.ts
+++ src/domain/shared/primitives.ts
@@ -92,19 +92,6 @@ export const paymentAmountFromNumber = <T extends WalletCurrency>({
   }
 }
 
-export const displayAmountFromNumber = <T extends DisplayCurrency>({
-  amount,
-  currency,
-}: {
-  amount: number
-  currency: T
-}): DisplayAmount<T> | ValidationError => {
-  return {
-    amount,
-    currency,
-  }
-}
-
 export const balanceAmountFromNumber = <T extends WalletCurrency>({
   amount,
   currency,
