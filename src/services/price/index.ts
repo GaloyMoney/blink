25f4bcc9a66fbf42b5175e2e944c382662f38086
Kartik Shah <krtk.6160@gmail.com>
2023-04-04 23:28:20 +0530

    refactor: make realtime price env var name explicit

--- src/services/price/index.ts
+++ src/services/price/index.ts
@@ -18,8 +18,8 @@ import { baseLogger } from "../logger"
 
 import { PriceHistoryProtoDescriptor, PriceProtoDescriptor } from "./grpc"
 
-const priceUrl = process.env.PRICE_HOST ?? "galoy-price"
-const pricePort = process.env.PRICE_PORT ?? "50051"
+const priceUrl = process.env.PRICE_REALTIME_HOST ?? "galoy-price"
+const pricePort = process.env.PRICE_REALTIME_PORT ?? "50051"
 const fullUrl = `${priceUrl}:${pricePort}`
 const priceClient = new PriceProtoDescriptor.PriceFeed(
   fullUrl,
