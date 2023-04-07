bcb5be323dbb708c45c7296fec687ffe56c30bd3
vindard <17693119+vindard@users.noreply.github.com>
2023-04-07 16:37:56 +0700

    chore: add cron span attribute to track job completion
    This is needed to determine if a job timed out silently

--- src/servers/cron.ts
+++ src/servers/cron.ts
@@ -1,4 +1,4 @@
-import { wrapAsyncToRunInSpan } from "@services/tracing"
+import { addAttributesToCurrentSpan, wrapAsyncToRunInSpan } from "@services/tracing"
 
 import {
   deleteExpiredLightningPaymentFlows,
@@ -89,7 +89,15 @@ const main = async () => {
   for (const task of tasks) {
     try {
       logger.info(`starting ${task.name}`)
-      const wrappedTask = wrapAsyncToRunInSpan({ namespace: "cron", fn: task })
+      const wrappedTask = wrapAsyncToRunInSpan({
+        namespace: "cron",
+        spanAttributes: { jobCompleted: "false" },
+        fn: async () => {
+          const result = await task()
+          addAttributesToCurrentSpan({ jobCompleted: "true" })
+          return result
+        },
+      })
       await wrappedTask()
       results.push(true)
     } catch (error) {
