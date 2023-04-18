533aa2d2be9070dcf44947424166a4e50d20c449
galoybot <86534088+galoybot@users.noreply.github.com>
2023-04-18 08:51:36 +0200

    ci(shared): bump vendored ci files (#2543)

--- ci/vendor/tasks/bump-image-digest.sh
+++ ci/vendor/tasks/bump-image-digest.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
+#! Auto synced from Shared CI Resources repository
+#! Don't change this file, instead change it in github.com/GaloyMoney/concourse-shared
+
 set -eu
 
-export digest=$(cat ./edge-image/digest)
-export ref=$(cat ./repo/.git/short_ref)
-export app_version=$(cat version/version)
+. pipeline-tasks/ci/vendor/tasks/helpers.sh
 
-pushd charts-repo
+CI_ROOT=$(pwd)
 
-yq -i e '.image.digest = strenv(digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
-yq -i e '.image.git_ref = strenv(ref)' ./charts/${CHARTS_SUBDIR}/values.yaml
-yq -i e '.appVersion = strenv(app_version)' ./charts/${CHARTS_SUBDIR}/Chart.yaml
+unpack_deps
 
-if [[ -z $(git config --global user.email) ]]; then
-  git config --global user.email "bot@galoy.io"
-fi
-if [[ -z $(git config --global user.name) ]]; then
-  git config --global user.name "CI Bot"
-fi
+cat <<EOF > ${CI_ROOT}/gcloud-creds.json
+${GOOGLE_CREDENTIALS}
+EOF
+cat <<EOF > ${CI_ROOT}/login.ssh
+${SSH_PRIVATE_KEY}
+EOF
+chmod 600 ${CI_ROOT}/login.ssh
+cat <<EOF > ${CI_ROOT}/login.ssh.pub
+${SSH_PUB_KEY}
+EOF
+gcloud auth activate-service-account --key-file ${CI_ROOT}/gcloud-creds.json
+gcloud compute os-login ssh-keys add --key-file=${CI_ROOT}/login.ssh.pub > /dev/null
 
-(
-  cd $(git rev-parse --show-toplevel)
-  git merge --no-edit ${BRANCH}
-  git add -A
-  git status
-  git commit -m "chore(${CHARTS_SUBDIR}): bump ${CHARTS_SUBDIR} image to '${digest}'"
-)
+mkdir ~/.ssh
+cp ${CI_ROOT}/login.ssh ~/.ssh/id_rsa
+cp ${CI_ROOT}/login.ssh.pub ~/.ssh/id_rsa.pub
+
+export DOCKER_HOST_USER="sa_$(cat ${CI_ROOT}/gcloud-creds.json  | jq -r '.client_id')"
+export DOCKER_HOST=ssh://${DOCKER_HOST_USER}@${DOCKER_HOST_IP}
+export ADDITIONAL_SSH_OPTS="-o StrictHostKeyChecking=no -i ${CI_ROOT}/login.ssh"
+
+pushd ${REPO_PATH}
+
+echo "Syncing repo to docker-host... "
+rsync --delete --exclude target -avr -e "ssh -l ${DOCKER_HOST_USER} ${ADDITIONAL_SSH_OPTS}" \
+  ./ ${DOCKER_HOST_IP}:${REPO_PATH} > /dev/null
+echo "Done!"
+
+docker compose down --remove-orphans --timeout 1
+
+ssh ${ADDITIONAL_SSH_OPTS} ${DOCKER_HOST_USER}@${DOCKER_HOST_IP} \
+  "cd ${REPO_PATH}; docker compose -f docker-compose.yml up integration-tests"
+
+container_id=$(docker ps -q -f status=exited -f name="${PWD##*/}-integration-tests-")
+test_status=$(docker inspect $container_id --format='{{.State.ExitCode}}')
+
+docker compose down --remove-orphans --timeout 1
+
+exit $test_status
--- ci/vendor/tasks/helpers.sh
+++ ci/vendor/tasks/helpers.sh
@@ -1,5 +1,14 @@
-export CARGO_HOME="$(pwd)/cargo-home"
-export CARGO_TARGET_DIR="$(pwd)/cargo-target-dir"
+#!/bin/bash
+
+#! Auto synced from Shared CI Resources repository
+#! Don't change this file, instead change it in github.com/GaloyMoney/concourse-shared
+
+if [[ -z $(git config --global user.email) ]]; then
+  git config --global user.email "bot@galoy.io"
+fi
+if [[ -z $(git config --global user.name) ]]; then
+  git config --global user.name "CI Bot"
+fi
 
 function unpack_deps() {
   REPO_PATH=${REPO_PATH:-repo}
