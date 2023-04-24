a1b40dcb87c39efee25a76d8739e823cf31e2e2d
nicolasburtey <nicolasburtey@users.noreply.github.com>
2023-04-24 11:16:39 +0100

    chore: upgrade to kratos 13 (#2563)
    Co-authored-by: Nicolas Burtey <nb@galoy.io>

--- src/servers/middlewares/kratos-router.ts
+++ src/servers/middlewares/kratos-router.ts
@@ -11,6 +11,7 @@ import {
 } from "@app/accounts"
 import { checkedToPhoneNumber } from "@domain/users"
 import { checkedToUserId } from "@domain/accounts"
+import { UsersRepository } from "@services/mongoose"
 
 const kratosRouter = express.Router({ caseSensitive: true })
 
@@ -18,6 +19,61 @@ const { callbackApiKey } = getKratosPasswords()
 
 kratosRouter.use(cors({ origin: true, credentials: true }))
 kratosRouter.use(express.json())
+
+kratosRouter.post(
+  "/preregistration",
+  wrapAsyncToRunInSpan({
+    namespace: "registration",
+    fn: async (req: express.Request, res: express.Response) => {
+      const key = req.headers.authorization
+
+      if (!key) {
+        console.log("missing authorization header")
+        res.status(401).send("missing authorization header")
+        return
+      }
+
+      if (key !== callbackApiKey) {
+        console.log("incorrect authorization header")
+        res.status(401).send("incorrect authorization header")
+        return
+      }
+
+      const body = req.body
+      const { phone: phoneRaw, schema_id } = body
+
+      assert(schema_id === "phone_no_password_v0", "unsupported schema")
+
+      // phone+code flow
+      if (phoneRaw) {
+        const phone = checkedToPhoneNumber(phoneRaw)
+        if (phone instanceof Error) {
+          console.log("invalid phone")
+          res.status(400).send("invalid phone")
+          return
+        }
+
+        const user = await UsersRepository().findByPhone(phone)
+
+        // we expect the phone number to not exist
+        if (user instanceof Error) {
+          console.log("phone doesn't already exist")
+          res.sendStatus(200)
+          return
+        }
+
+        console.error("phone already exist")
+        res.status(500).send(`phone already exist`)
+        return
+      }
+
+      // TODO
+      // email+password flow
+      res.status(500).send(`unsupported flow`)
+      return
+    },
+  }),
+)
 
 kratosRouter.post(
   "/registration",
@@ -51,16 +107,18 @@ kratosRouter.post(
 
       const userIdChecked = checkedToUserId(userId)
       if (userIdChecked instanceof Error) {
+        // TODO: log this error as critical to honeycomb
         console.log("invalid userId")
         res.status(400).send("invalid userId")
         return
       }
 
-      let account
+      let account: Account | RepositoryError
       // phone+code flow
       if (phoneRaw) {
         const phone = checkedToPhoneNumber(phoneRaw)
         if (phone instanceof Error) {
+          // TODO: log this error as critical to honeycomb
           console.log("invalid phone")
           res.status(400).send("invalid phone")
           return
@@ -83,6 +141,7 @@ kratosRouter.post(
       }
 
       if (account instanceof Error) {
+        // TODO: log this error as critical to honeycomb
         console.log(`error createAccountWithPhoneIdentifier: ${account}`)
         res.status(500).send(`error createAccountWithPhoneIdentifier: ${account}`)
         return
