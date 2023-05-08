11824615938a259b311f02204d59a5c53b3ef0d2
vindard <17693119+vindard@users.noreply.github.com>
2023-05-08 18:15:34 +0700

    feat: connect bria daemon to app via grpc interface

---
+++ src/domain/bitcoin/bria/index.types.d.ts
@@ -0,0 +1,39 @@
+type ClientReadableStream<T> = import("@grpc/grpc-js").ClientReadableStream<T>
+
+type UtxoDetectedEvent = {
+  payload: "utxo_detected"
+  sequence: string
+  recorded_at: number
+  utxo_detected: {
+    wallet_id: WalletId
+    tx_id: OnChainTxHash
+    vout: number
+    satoshis: string
+    address: OnChainAddress
+  }
+}
+
+type UtxoSettledEvent = {
+  payload: "utxo_settled"
+  sequence: string
+  recorded_at: number
+  utxo_settled: {
+    wallet_id: WalletId
+    tx_id: OnChainTxHash
+    vout: number
+    satoshis: string
+    address: OnChainAddress
+    block_height: number
+    block_time: string
+  }
+}
+
+type UtxoEvent = UtxoDetectedEvent | UtxoSettledEvent
+
+type UtxoEventHandler = (event: UtxoEvent) => true | ApplicationError
+
+interface INewOnChainService {
+  subscribeToAll(
+    callback: UtxoEventHandler,
+  ): ClientReadableStream<UtxoEvent> | OnChainServiceError
+}
