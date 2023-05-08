5a7aaa005a0127211047f8e95db9bc62b06d70a7
Kartik Shah <krtk.6160@gmail.com>
2023-05-09 00:35:53 +0530

    refactor: sync proto task

--- ci/tasks/sync-proto.sh
+++ ci/tasks/sync-proto.sh
@@ -4,19 +4,15 @@ set -eu
 
 export ref=$(cat ./src-repo/.git/short_ref)
 
-if [ "$RUN_BUF_GENERATE" == "true" ]; then
-  pushd src-repo
-  yarn install
-  export PATH=$PATH:$(pwd)/node_modules/.bin
-  pushd ${PROTO_FILES_SRC_PATH}/
-  buf generate
-  popd
-  popd
-fi
-
 cp -R src-repo/${PROTO_FILES_SRC_PATH}/* repo/${PROTO_FILES_DEST_PATH}
 
-cd repo/${PROTO_FILES_DEST_PATH}
+if [[ -n "$BUF_CONFIG_PATH" ]]; then
+  pushd repo
+  yarn install
+  export PATH=$PATH:$(pwd)/node_modules/.bin
+  pushd ${BUF_CONFIG_PATH}/
+  buf generate
+fi
 
 if [[ -z $(git config --global user.email) ]]; then
   git config --global user.email "bot@galoy.io"
