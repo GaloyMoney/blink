3acdb87c80db7d33d9c914b2cef8ac0cdd81b2a4
vindard <17693119+vindard@users.noreply.github.com>
2023-05-10 19:00:12 +0700

    chore: complete boilerplate for 'utxoDetected' handler

--- src/app/wallets/add-pending-transaction.ts
+++ src/app/wallets/add-pending-transaction.ts
@@ -1,33 +1,19 @@
 import { PendingOnChainTransactionsRepository } from "@services/mongoose/pending-onchain-transactions"
 
 export const addPendingTransaction = async ({
-  rawUserId,
+  address,
   txHash,
   vout,
   amount,
 }: {
-  rawUserId: UserId | undefined
+  address: OnChainAddress
   txHash: OnChainTxHash
   vout: number
   amount: BtcPaymentAmount
 }) => {
-  // SECTION: Do checks
-  let userId: UserId | undefined = undefined
-  if (rawUserId === undefined) {
-    // const userId = Wallets.lookupUserIdForAddress(addressInfo.address)
-    userId = "result" as UserId
-    // BriaService.updateAddressMetadata({address, userId})
-    // BriaService.getNewAddress({address, userId})
-  } else {
-    userId = rawUserId
-  }
-  if (!userId) {
-    return
-  }
-
   // SECTION: Repo add transaction
   const pendingTxn = PendingOnChainTransactionsRepository().persistNew({
-    userId,
+    address,
     txHash,
     vout,
     amount,
