40b35de5e158c6ec38e2994885bf0e0dda120370
ntheile <ntheile@gmail.com>
2023-06-01 14:23:02 -0500

    refactor: verify jwt

--- src/services/kratos/auth-device-account.ts
+++ src/services/kratos/auth-device-account.ts
@@ -1,10 +1,3 @@
-import * as jose from "node-jose"
-
-import { getAppCheckConfig, getJwksArgs, isCI, isDev } from "@config"
-
-import jwksRsa from "jwks-rsa"
-import jsonwebtoken from "jsonwebtoken"
-
 import { KratosError } from "./errors"
 
 import { AuthWithPhonePasswordlessService } from "./auth-phone-no-password"
@@ -26,54 +19,7 @@ export const AuthWithDeviceAccountService = () => {
     return kratosResult.sessionToken
   }
 
-  const verifyJwt = async (token: string) => {
-    const audience = getAppCheckConfig().audience
-    const issuer = getAppCheckConfig().issuer
-    const jwksUri = getAppCheckConfig().jwksUri
-
-    // Get the JSON web keys (JWKS)
-    let client = await jwksRsa({
-      jwksUri,
-    })
-    let jwkJson
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    jwkJson = (await client.getKeys()) as Array<any>
-
-    // Decode the token
-    const decodedToken = jsonwebtoken.decode(token, { complete: true })
-    if (!decodedToken) return Error("problem with jwt")
-
-    // Find the kid (Key Id) used to sign the token
-    const kid = decodedToken.header.kid
-    let kidJwk = jwkJson.find((j) => j.kid === kid)
-
-    // if in dev environment, use local jwks.json if you dont have access to firebase appcheck debug
-    if (!kidJwk && (isDev || isCI)) {
-      client = await jwksRsa({
-        jwksUri: getJwksArgs().jwksUri,
-      })
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      jwkJson = (await client.getKeys()) as Array<any>
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      kidJwk = jwkJson.find((j: any) => j.kid === kid)
-    }
-
-    // Create a Key object from the JWK and to PEM format
-    const jwtAskey = await jose.JWK.asKey(kidJwk)
-    const pem = jwtAskey.toPEM(false)
-
-    // Verify the token
-    const verifiedToken = jsonwebtoken.verify(token, pem, {
-      algorithms: ["RS256"],
-      audience,
-      issuer,
-    })
-
-    return verifiedToken
-  }
-
   return {
     upgradeToPhoneSchema,
-    verifyJwt,
   }
 }
