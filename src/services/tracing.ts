cc25f28035d9e884f2397081cc3033832b4d2f14
Nicolas Burtey <nb@galoy.io>
2023-06-19 20:20:04 +0100

    chore!: upgrade otlp-agent dependencies

--- src/services/tracing.ts
+++ src/services/tracing.ts
@@ -1,5 +1,3 @@
-/* eslint @typescript-eslint/ban-ts-comment: "off" */
-
 import {
   SemanticAttributes,
   SemanticResourceAttributes,
@@ -13,8 +11,9 @@ import { IORedisInstrumentation } from "@opentelemetry/instrumentation-ioredis"
 import { GrpcInstrumentation } from "@opentelemetry/instrumentation-grpc"
 import { registerInstrumentations } from "@opentelemetry/instrumentation"
 import { SimpleSpanProcessor, Span as SdkSpan } from "@opentelemetry/sdk-trace-base"
-import { JaegerExporter } from "@opentelemetry/exporter-jaeger"
 import { Resource } from "@opentelemetry/resources"
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto"
+
 import {
   trace,
   context,
@@ -143,6 +142,7 @@ const recordGqlErrors = ({
     })
   }
 
+  /* eslint @typescript-eslint/ban-ts-comment: "off" */
   // @ts-ignore-next-line no-implicit-any error
   errors.forEach((err, idx) => {
     setErrorAttribute({
@@ -229,11 +229,15 @@ class SpanProcessorWrapper extends SimpleSpanProcessor {
     super.onStart(span, parentContext)
   }
 }
+
+const otlpHost = process.env.OTLP_HOST || "localhost"
+const otlpPort = parseInt(process.env.OTLP_PORT || "4318", 10)
+const hostname = `http://${otlpHost}:${otlpPort}`
+
 provider.addSpanProcessor(
   new SpanProcessorWrapper(
-    new JaegerExporter({
-      host: tracingConfig?.jaegerHost,
-      port: tracingConfig?.jaegerPort,
+    new OTLPTraceExporter({
+      hostname,
     }),
   ),
 )
