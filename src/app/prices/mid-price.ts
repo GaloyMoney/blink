a0e4fadbd55a2b54ad3dd435a9ee19f8d5566d4c
ntheile <ntheile@gmail.com>
2023-06-20 16:30:52 -0500

    chore: fix circular deps

--- src/app/prices/mid-price.ts
+++ src/app/prices/mid-price.ts
@@ -1,7 +1,5 @@
 import { getDealerConfig } from "@config"
 
-import { getCurrentPriceAsWalletPriceRatio } from "@app/prices"
-
 import { DisplayCurrency } from "@domain/fiat"
 import { ErrorLevel, ExchangeCurrencyUnit, WalletCurrency } from "@domain/shared"
 
@@ -12,6 +10,8 @@ import {
   SemanticAttributes,
 } from "@services/tracing"
 import { DealerPriceService } from "@services/dealer-price"
+
+import { getCurrentPriceAsWalletPriceRatio } from "./get-current-price"
 
 const usdHedgeEnabled = getDealerConfig().usd.hedgingEnabled
 const dealer = DealerPriceService()
