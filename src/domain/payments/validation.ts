765ee2ea90bcf4af6d012a711cc5c02b701e83cd
bodymindarts <justin@galoy.io>
2023-06-26 09:58:11 +0200

    refactor move validation fns out of index in domain/payments

---
+++ src/domain/payments/validation.ts
@@ -0,0 +1,46 @@
+import {
+  MAX_CENTS,
+  MAX_SATS,
+  paymentAmountFromNumber,
+  WalletCurrency,
+  BtcAmountTooLargeError,
+  UsdAmountTooLargeError,
+} from "@domain/shared"
+
+import { InvalidBtcPaymentAmountError, InvalidUsdPaymentAmountError } from "./errors"
+
+export const checkedToBtcPaymentAmount = (
+  amount: number | null,
+): BtcPaymentAmount | ValidationError => {
+  if (amount === null) {
+    return new InvalidBtcPaymentAmountError()
+  }
+
+  if (amount > MAX_SATS.amount) {
+    return new BtcAmountTooLargeError()
+  }
+
+  if (Math.floor(amount) != amount) {
+    return new InvalidBtcPaymentAmountError()
+  }
+  if (!(amount && amount > 0)) return new InvalidBtcPaymentAmountError()
+  return paymentAmountFromNumber({ amount, currency: WalletCurrency.Btc })
+}
+
+export const checkedToUsdPaymentAmount = (
+  amount: number | null,
+): UsdPaymentAmount | ValidationError => {
+  if (amount === null) {
+    return new InvalidUsdPaymentAmountError()
+  }
+
+  if (amount > MAX_CENTS.amount) {
+    return new UsdAmountTooLargeError()
+  }
+
+  if (Math.floor(amount) != amount) {
+    return new InvalidUsdPaymentAmountError()
+  }
+  if (!(amount && amount > 0)) return new InvalidUsdPaymentAmountError()
+  return paymentAmountFromNumber({ amount, currency: WalletCurrency.Usd })
+}
