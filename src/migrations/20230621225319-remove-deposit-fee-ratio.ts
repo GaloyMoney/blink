46fb6a47b517d825f767780498a97ef4647dee75
Juan P Lopez <dolcalmi@gmail.com>
2023-06-26 11:04:22 -0500

    fix: lint issues

--- src/migrations/20230621225319-remove-deposit-fee-ratio.ts
+++ src/migrations/20230621225319-remove-deposit-fee-ratio.ts
@@ -1,17 +1,13 @@
 module.exports = {
-  async up(db, client) {
+  async up(db) {
     console.log("Begin depositFeeRatio removal")
     const res = await db
       .collection("accounts")
-      .updateMany(
-        {},
-        { $unset: { depositFeeRatio: 1 } },
-        { multi: true },
-      )
+      .updateMany({}, { $unset: { depositFeeRatio: 1 } }, { multi: true })
     console.log({ res }, `removing depositFeeRatio fields`)
   },
 
   async down() {
     console.log(`rollback depositFeeRatio removal not needed`)
-  }
-};
+  },
+}
