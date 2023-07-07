e7705c2e36db6cf12753210fd94edbbb55c97e01
vindard <17693119+vindard@users.noreply.github.com>
2023-07-06 22:40:52 -0400

    test: add onchain-address tests

---
+++ test/integration/app/wallets/get-last-on-chain-address.spec.ts
@@ -0,0 +1,29 @@
+import { Accounts, Wallets } from "@app"
+
+import { AccountStatus } from "@domain/accounts"
+import { InactiveAccountError } from "@domain/errors"
+
+import { AccountsRepository } from "@services/mongoose"
+
+import { createRandomUserAndWallet } from "test/helpers"
+
+describe("onChainAddress", () => {
+  it("fails if account is locked", async () => {
+    const newWalletDescriptor = await createRandomUserAndWallet()
+    const newAccount = await AccountsRepository().findById(newWalletDescriptor.accountId)
+    if (newAccount instanceof Error) throw newAccount
+
+    // Lock account
+    const updatedAccount = await Accounts.updateAccountStatus({
+      id: newAccount.id,
+      status: AccountStatus.Locked,
+      updatedByUserId: newAccount.kratosUserId,
+    })
+    if (updatedAccount instanceof Error) throw updatedAccount
+    expect(updatedAccount.status).toEqual(AccountStatus.Locked)
+
+    // Create address attempt
+    const res = await Wallets.getLastOnChainAddress(newWalletDescriptor.id)
+    expect(res).toBeInstanceOf(InactiveAccountError)
+  })
+})
