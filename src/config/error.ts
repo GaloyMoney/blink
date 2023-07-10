b92f1aa67d690a29b4f7aea893334ff3b3f03958
vindard <17693119+vindard@users.noreply.github.com>
2023-07-10 13:27:20 -0400

    chore: improve error granularity for static account resolvers

--- src/config/error.ts
+++ src/config/error.ts
@@ -6,3 +6,18 @@ export class ConfigError<T> extends Error {
     this.data = data
   }
 }
+
+export class MissingBankOwnerConfigError<T> extends ConfigError<T> {}
+export class MissingBankOwnerAccountConfigError<
+  T,
+> extends MissingBankOwnerConfigError<T> {}
+
+export class MissingDealerConfigError<T> extends ConfigError<T> {}
+export class MissingDealerAccountConfigError<T> extends MissingDealerConfigError<T> {}
+export class MissingBtcDealerWalletConfigError<T> extends MissingDealerConfigError<T> {}
+export class MissingUsdDealerWalletConfigError<T> extends MissingDealerConfigError<T> {}
+
+export class MissingFunderConfigError<T> extends ConfigError<T> {}
+export class MissingFunderAccountConfigError<T> extends MissingFunderConfigError<T> {}
+
+export class UnknownConfigError<T> extends ConfigError<T> {}
