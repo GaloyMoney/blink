8fb98ac77ae0bc5f5fa0523413c7721506cb2319
Nicolas Burtey <nb@galoy.io>
2023-07-28 21:51:54 +0100

    chore: another fix

--- test/legacy-integration/app/users/user-role.spec.ts
+++ test/legacy-integration/app/users/user-role.spec.ts
@@ -1,10 +1,10 @@
 import { AccountsRepository } from "@services/mongoose"
 
-import { createUserAndWalletFromPhone, getUserIdByPhone, randomPhone } from "test/helpers"
+import { createUserAndWalletFromPhone, getUserIdByPhone } from "test/helpers"
 
 describe("Users - role", () => {
   it("persists a role set in test accounts", async () => {
-    const phone = randomPhone()
+    const phone = "+16505554336" as PhoneNumber
     await createUserAndWalletFromPhone(phone)
     const userId = await getUserIdByPhone(phone)
     const account = await AccountsRepository().findByUserId(userId)
