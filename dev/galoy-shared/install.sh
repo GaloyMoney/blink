886ecd1cc5fbc3d423ac928a86ee9684f62a1a23
ntheile <ntheile@gmail.com>
2023-08-04 13:44:12 -0500

    fix: link

--- dev/galoy-shared/install.sh
+++ dev/galoy-shared/install.sh
@@ -1,8 +1,8 @@
 read -p "Make sure you have vendir and docker installed...[Press enter to continue]"
 mkdir -p dev/galoy
 mkdir -p dev/galoy/vendor
-curl -o dev/galoy/vendir.yml https://raw.githubusercontent.com/GaloyMoney/galoy/1015418d59a6d5daae071a2b60324632aa7b4799/dev/galoy-shared/vendir.yml
 cd dev/galoy
+curl -o vendir.yml https://raw.githubusercontent.com/GaloyMoney/galoy/45c3153d717a5b80501f2d6ab7747a6319ae0605/dev/galoy-shared/vendir.yml
 # todo check if vendir exists
 vendir sync
 sleep 1 # it takes a second to copy from tmp vendir to vendor
@@ -11,5 +11,4 @@ chmod +x ./docker/run.sh
 ./docker/run.sh
 sleep 5 # it takes a few seconds for the server to boot
 open http://localhost:4002/graphql
-echo "install completed successfully! please read docs to create a supergraph here: https://github.com/GaloyMoney/galoy/blob/1015418d59a6d5daae071a2b60324632aa7b4799/dev/galoy-shared/README.md#to-run-a-supergraph-in-your-project "
-open https://github.com/GaloyMoney/galoy/blob/1015418d59a6d5daae071a2b60324632aa7b4799/dev/galoy-shared/README.md#to-run-a-supergraph-in-your-project
+echo "install completed successfully! please read docs to create a supergraph here: https://github.com/GaloyMoney/galoy/blob/45c3153d717a5b80501f2d6ab7747a6319ae0605/dev/galoy-shared/README.md#to-run-a-supergraph-in-your-project"
