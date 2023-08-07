bdb6cc800a20f9d3f8e5309a8a478436429c3553
bodymindarts <justin@galoy.io>
2023-08-07 09:03:43 +0200

    chore: initial quickstart boilerplate

---
+++ quickstart/bin/re-render.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+GALOY_DEV_DIR=${REPO_ROOT}/quickstart/dev
+GALOY_ROOT_DIR=${REPO_ROOT}/quickstart/galoy
+
+pushd ${REPO_ROOT}/quickstart
+
+vendir sync
+
+ytt -f ./docker-compose.tmpl.yml -f ${GALOY_ROOT_DIR}/docker-compose.yml -f ${GALOY_ROOT_DIR}/docker-compose.override.yml > docker-compose.yml
+
+pushd ${GALOY_ROOT_DIR}
+source .envrc
+envsubst < .env.ci > ${GALOY_ROOT_DIR}/../.env.galoy
