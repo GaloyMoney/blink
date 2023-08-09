58ba3e24336652b3f34bd1ab5ba3c6543881f61c
bodymindarts <justin@galoy.io>
2023-08-09 16:21:17 +0200

    ci: debug git ref

--- quickstart/bin/bump-galoy-git-ref.sh
+++ quickstart/bin/bump-galoy-git-ref.sh
@@ -6,6 +6,6 @@ TMPDIR=""
 TMPDIR=$(mktemp -d -t repipe.XXXXXX)
 trap "rm -rf ${TMPDIR}" INT TERM QUIT EXIT
 
-sed "s/^galoy_git_ref:.*/galoy_git_ref: ${2}/" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
+sed "s/^galoy_git_ref:.*/galoy_git_ref: ${1}/" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
 
 mv ${TMPDIR}/new_values.yml ${DIR}/../vendir/values.yml
