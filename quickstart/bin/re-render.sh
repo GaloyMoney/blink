122968f4859ba14885da80ca2f5ea8148c68a703
bodymindarts <justin@galoy.io>
2023-09-19 14:59:50 +0200

    chore: some formatting

--- quickstart/bin/re-render.sh
+++ quickstart/bin/re-render.sh
@@ -3,19 +3,18 @@
 set -e
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-GALOY_DEV_DIR=${REPO_ROOT}/quickstart/dev
 GALOY_ROOT_DIR=${REPO_ROOT}/quickstart/galoy
 
-pushd ${REPO_ROOT}/quickstart
+pushd "${REPO_ROOT}/quickstart"
 
 ytt -f vendir > vendir.yml
 vendir sync
 
-ytt -f ./docker-compose.tmpl.yml -f ${GALOY_ROOT_DIR}/docker-compose.yml -f ${GALOY_ROOT_DIR}/docker-compose.override.yml > docker-compose.yml
+ytt -f ./docker-compose.tmpl.yml -f "${GALOY_ROOT_DIR}/docker-compose.yml" -f "${GALOY_ROOT_DIR}/docker-compose.override.yml" > docker-compose.yml
 
-pushd ${GALOY_ROOT_DIR}
+pushd "${GALOY_ROOT_DIR}"
 source .envrc
 mkdir -p "${GALOY_ROOT_DIR}/../vendor/galoy-quickstart/env"
 
 export OTEL_EXPORTER_OTLP_ENDPOINT=http://otel-agent:4318
-envsubst < .env.ci | grep -v '^LND2' > ${GALOY_ROOT_DIR}/../.env.galoy
+envsubst < .env.ci | grep -v '^LND2' > "${GALOY_ROOT_DIR}/../.env.galoy"
