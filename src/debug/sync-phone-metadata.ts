bc73c7a86a3d5bd4c33f4c41d0e4deed7f6fc353
Nicolas Burtey <nb@galoy.io>
2023-09-21 13:15:20 +0100

    chore: add sync metadata script

---
+++ src/debug/sync-phone-metadata.ts
@@ -0,0 +1,54 @@
+/**
+ * how to run:
+ * yarn ts-node --files -r tsconfig-paths/register src/debug/sync-phone-metadata.ts
+ */
+
+import { setupMongoConnection } from "@services/mongodb"
+import { User } from "@services/mongoose/schema"
+import { TwilioClient } from "@services/twilio"
+
+const main = async () => {
+  const users = await User.find({ phoneMetadata: { $exists: false } })
+  if (users instanceof Error) return users
+
+  console.log("usersCount:", users.length)
+
+  let progress = 0
+  for await (const user of users) {
+    const phone = user.phone as PhoneNumber
+    if (!phone) {
+      console.log(`user ${user.id} has no phone`)
+      continue
+    }
+
+    const newPhoneMetadata = await TwilioClient().getCarrier(phone)
+    if (newPhoneMetadata instanceof Error) {
+      console.log(`error getting phone metadata for ${phone}: ${newPhoneMetadata}`)
+      continue
+    }
+
+    const res = await User.updateOne(
+      { id: user.id },
+      { $set: { phoneMetadata: newPhoneMetadata } },
+    )
+    console.log(
+      `updated user ${user.id} with phoneMetadata: ${JSON.stringify(
+        newPhoneMetadata,
+      )}, res: ${JSON.stringify(res)}`,
+    )
+
+    progress++
+    if (progress % 1000 === 0) {
+      console.log(`${progress} accounts iterated`)
+    }
+  }
+
+  console.log("completed")
+}
+
+setupMongoConnection()
+  .then(async (mongoose) => {
+    await main()
+    return mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
