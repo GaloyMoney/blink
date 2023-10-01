a078617f0c296268bd7ea791b5445b012c15847e
bodymindarts <justin@galoy.io>
2023-10-01 15:19:13 +0200

    chore: re-render

--- quickstart/bin/re-render.sh
+++ quickstart/bin/re-render.sh
@@ -3,14 +3,14 @@
 set -e
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-GALOY_ROOT_DIR=${REPO_ROOT}/quickstart/galoy/core/api
+GALOY_ROOT_DIR=${REPO_ROOT}/quickstart/galoy
 
-pushd "${REPO_ROOT}/quickstart"
+pushd ${REPO_ROOT}/quickstart
 
 ytt -f vendir > vendir.yml
 vendir sync
 
-ytt -f ./docker-compose.tmpl.yml -f "${GALOY_ROOT_DIR}/docker-compose.yml" -f "${GALOY_ROOT_DIR}/docker-compose.override.yml" > docker-compose.yml
+ytt -f ./docker-compose.tmpl.yml -f ${GALOY_ROOT_DIR}/docker-compose.yml -f ${GALOY_ROOT_DIR}/docker-compose.override.yml > docker-compose.yml
 
 pushd ${GALOY_ROOT_DIR}
 source .env
