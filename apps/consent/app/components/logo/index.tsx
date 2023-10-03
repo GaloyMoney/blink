a88c989ba1574d519f363f55611c9f6ccdbe16e5
Siddharth <siddharth@debian.siddharth>
2023-10-04 02:14:04 +0530

    feat: added blink logo

--- apps/consent/app/components/logo/index.tsx
+++ apps/consent/app/components/logo/index.tsx
@@ -1,13 +1,12 @@
-import React from "react"
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
 const Logo: React.FC = () => {
   return (
     <div className="flex justify-center mb-4">
-      {/* TODO use Blink logo ?  */}
-      <Image src="/favicon.ico" alt="Galoy" width={50} height={50} />
+      <Image src="/blink_logo.svg" alt="Galoy" width={120} height={120} />
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
