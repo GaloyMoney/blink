141dc1fbca570d3ccdcf5a184a06382b18ed2f3b
Justin Carter <justin@galoy.io>
2023-10-11 14:56:44 +0200

    ci: explicit trigger image (#3331)
    * ci: explicit trigger image
    
    * chore: add result to .gitignore

--- ci/tasks/prep-quickstart.sh
+++ ci/tasks/prep-quickstart.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-export digest=$(cat ./edge-image/digest | sed 's/:/@/')
+export api_digest=$(cat ./edge-image/digest | sed 's/:/@/')
+export trigger_digest=$(cat ./trigger-edge-image/digest | sed 's/:/@/')
 export migrate_digest=$(cat ./migrate-edge-image/digest | sed 's/:/@/')
 
-pushd repo/quickstart
+pushd repo/quickstart || exit 1
 
 ./bin/bump-galoy-git-ref.sh "$(git rev-parse --verify HEAD)"
 
-./bin/bump-galoy-image-digest.sh "$digest"
+./bin/bump-galoy-image-digest.sh "api" "$api_digest"
+./bin/bump-galoy-image-digest.sh "trigger" "$trigger_digest"
 ./bin/bump-mongodb-migrate-image-digest.sh "$migrate_digest"
 make re-render
 
 pushd ../
 
 git add -A
-git commit -m "chore(release): [ci skip] bump quickstart image to ${digest}"
+git commit -m "chore(release): [ci skip] bump quickstart image to ${api_digest}"
