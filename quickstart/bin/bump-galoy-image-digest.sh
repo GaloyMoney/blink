c862700714f9f4981c71679a56c13c95caa4a49a
bodymindarts <justin@galoy.io>
2023-10-11 14:39:11 +0200

    ci: explicit trigger image

--- quickstart/bin/bump-galoy-image-digest.sh
+++ quickstart/bin/bump-galoy-image-digest.sh
@@ -6,6 +6,6 @@ TMPDIR=""
 TMPDIR=$(mktemp -d -t repipe.XXXXXX)
 trap "rm -rf ${TMPDIR}" INT TERM QUIT EXIT
 
-sed "s/^#@ galoy_image_digest = \".*\"/#@ galoy_image_digest = \"${1}\"/" ${DIR}/../docker-compose.tmpl.yml > ${TMPDIR}/docker-compose.tmpl.yml
+sed "s/^#@ galoy_${1}_image_digest = \".*\"/#@ galoy_${1}_image_digest = \"${2}\"/" ${DIR}/../docker-compose.tmpl.yml > ${TMPDIR}/docker-compose.tmpl.yml
 
 mv ${TMPDIR}/docker-compose.tmpl.yml ${DIR}/../docker-compose.tmpl.yml
