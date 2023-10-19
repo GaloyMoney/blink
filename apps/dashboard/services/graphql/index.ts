79a8a6d925a66c3c958ef0a398669f1f0e9208b9
Siddharth <siddharth@debian.siddharth>
2023-10-19 18:55:21 +0530

    fix: dashboard build

--- apps/dashboard/services/graphql/index.ts
+++ apps/dashboard/services/graphql/index.ts
@@ -1,36 +1,17 @@
-import { env } from "@/env";
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  ApolloLink,
-} from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+import { env } from "@/env";
 
-const headerSettingLink = new ApolloLink((operation, forward) => {
-  const request = operation.getContext().request;
-  operation.setContext({
-    headers: {
-      "x-real-ip": request?.headers.get("x-real-ip") || "",
-      "x-forwarded-for": request?.headers.get("x-forwarded-for") || "",
-    },
-  });
-  return forward(operation);
-});
-
-export const apollo = (token: string, request: Request) =>
+export const apollo = (token: string) =>
   registerApolloClient(() => {
-    const httpLink = new HttpLink({
-      headers: {
-        ["Oauth2-Token"]: token,
-      },
-      uri: env.CORE_URL,
-      fetchOptions: { cache: "no-store" },
-    });
-
-    const link = ApolloLink.from([headerSettingLink, httpLink]);
     return new ApolloClient({
       cache: new InMemoryCache(),
-      link,
+      link: new HttpLink({
+        headers: {
+          ["Oauth2-Token"]: token,
+        },
+        uri: env.CORE_URL,
+        fetchOptions: { cache: "no-store" },
+      }),
     });
   });
