50fc3343814c5d6f58af4498777bad98bb01a3bb
Siddharth <siddharth@debian.siddharth>
2023-10-19 14:33:23 +0530

    fix: rate limit headers

--- apps/consent/app/graphql/apollo-config.ts
+++ apps/consent/app/graphql/apollo-config.ts
@@ -1,11 +1,15 @@
 import { env } from "../../env";
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-export const graphQlClient = (authToken?: string) => {
+export const graphQlClient = (authToken?: string, request?: Request) => {
   const httpLink = new HttpLink({
     uri: env.GRAPHQL_ENDPOINT,
     fetchOptions: { cache: "no-store" },
-    headers: authToken ? { authorization: `Bearer ${authToken}` } : undefined,
+    headers: {
+      ...(authToken ? { authorization: `Bearer ${authToken}` } : undefined),
+      "x-real-ip": request?.headers.get("x-real-ip") || "",
+      "x-forwarded-for": request?.headers.get("x-forwarded-for") || "",
+    },
   });
 
   return new ApolloClient({
--- apps/dashboard/services/graphql/index.ts
+++ apps/dashboard/services/graphql/index.ts
@@ -1,14 +1,16 @@
+import { env } from "@/env";
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
-import { env } from "@/env";
 
-export const apollo = (token: string) =>
+export const apollo = (token: string, request: Request) =>
   registerApolloClient(() => {
     return new ApolloClient({
       cache: new InMemoryCache(),
       link: new HttpLink({
         headers: {
           ["Oauth2-Token"]: token,
+          "x-real-ip": request.headers.get("x-real-ip") || "",
+          "x-forwarded-for": request.headers.get("x-forwarded-for") || "",
         },
         uri: env.CORE_URL,
         fetchOptions: { cache: "no-store" },
