b0acbd83addbb0b8e6169a5d7adeb22444123dfd
Arvin <17693119+vindard@users.noreply.github.com>
2023-10-26 11:03:36 -0400

    build: switch nix docker image to 'buildImage' (#3432)
    * build: switch nix docker image to 'buildImage'
    
    * chore: remove comments from flake.nix
    
    ---------
    
    Co-authored-by: bodymindarts <justin@galoy.io>

--- flake.nix
+++ flake.nix
@@ -175,11 +175,26 @@
           consent = nextDerivation {pkgName = "consent";};
           dashboard = nextDerivation {pkgName = "dashboard";};
 
-          dockerImage = pkgs.dockerTools.buildNixShellImage {
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "galoy-dev";
             tag = "latest";
-            drv = pkgs.stdenv.mkDerivation {
-              name = "galoy-dev";
-              inherit nativeBuildInputs;
+
+            fromImage = pkgs.dockerTools.pullImage {
+              imageName = "nixos/nix";
+              imageDigest = "sha256:85299d86263a3059cf19f419f9d286cc9f06d3c13146a8ebbb21b3437f598357";
+              sha256 = "19fw0n3wmddahzr20mhdqv6jkjn1kanh6n2mrr08ai53dr8ph5n7";
+              finalImageTag = "2.2.1";
+              finalImageName = "nix";
+            };
+
+            config = {
+              Cmd = ["bash"];
+            };
+
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = nativeBuildInputs ++ [ pkgs.bash ];
+              pathsToLink = [ "/bin" ];
             };
           };
         };
