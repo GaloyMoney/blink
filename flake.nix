9a09d9ded3dcbef6cd41c3f674bce875dab6e275
vindard <17693119+vindard@users.noreply.github.com>
2023-10-26 10:37:03 -0400

    build: remove base image

--- flake.nix
+++ flake.nix
@@ -179,14 +179,6 @@
             name = "galoy-dev";
             tag = "latest";
 
-            fromImage = pkgs.dockerTools.pullImage {
-              imageName = "nixos/nix";
-              imageDigest = "sha256:85299d86263a3059cf19f419f9d286cc9f06d3c13146a8ebbb21b3437f598357";
-              sha256 = "19fw0n3wmddahzr20mhdqv6jkjn1kanh6n2mrr08ai53dr8ph5n7";
-              finalImageTag = "2.2.1";
-              finalImageName = "nix";
-            };
-
             config = {
               Cmd = ["bash"];
             };
