9da2427dc775946c33c7014cfd89e828253643d5
Siddharth <siddharth@debian.siddharth>
2023-10-29 00:23:50 +0530

    feat: added auto submit on verification page in consent

--- apps/consent/app/components/varification-components/verification-code-form.tsx
+++ apps/consent/app/components/varification-components/verification-code-form.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+"use client"
+import React, { useState, useEffect } from "react"
 
 import InputComponent from "@/app/components/input-component"
 import PrimaryButtonComponent from "@/app/components/button/primary-button-component"
@@ -21,35 +22,53 @@ const VerificationCodeForm: React.FC<VerificationCodeFormProps> = ({
   remember,
   loginType,
   value,
-}) => (
-  <>
-    <h1 id="verification-title" className="text-center mb-4 text-xl font-semibold">
-      Enter Verification Code
-    </h1>
-    <form action={formAction} className="flex flex-col">
-      <input type="hidden" name="login_challenge" value={login_challenge} />
-      <input type="hidden" name="loginId" value={loginId} />
-      <input type="hidden" name="remember" value={remember} />
-      <input type="hidden" name="loginType" value={loginType} />
-      <input type="hidden" name="value" value={value} />
-      <div className="flex flex-col items-center mb-4">
-        <p className="text-center text-sm w-60 mb-1">
-          The code was sent to your {loginType}{" "}
-        </p>
-        <span className="font-semibold">{value}</span>
-      </div>
-      <InputComponent
-        type="text"
-        id="code"
-        name="code"
-        placeholder="Enter code here"
-        data-testid="verification_code_input"
-      />
-      <PrimaryButtonComponent data-testid="verification_code_submit_btn" type="submit">
-        Submit
-      </PrimaryButtonComponent>
-    </form>
-  </>
-)
+}) => {
+  const [code, setCode] = useState("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length <= 6) {
+      setCode(e.target.value)
+    }
+  }
+
+  useEffect(() => {
+    if (code.length === 6 && !isNaN(Number(code))) {
+      document.forms[0].requestSubmit()
+    }
+  }, [code])
+
+  return (
+    <>
+      <h1 id="verification-title" className="text-center mb-4 text-xl font-semibold">
+        Enter Verification Code
+      </h1>
+      <form action={formAction} className="flex flex-col">
+        <input type="hidden" name="login_challenge" value={login_challenge} />
+        <input type="hidden" name="loginId" value={loginId} />
+        <input type="hidden" name="remember" value={remember} />
+        <input type="hidden" name="loginType" value={loginType} />
+        <input type="hidden" name="value" value={value} />
+        <div className="flex flex-col items-center mb-4">
+          <p className="text-center text-sm w-60 mb-1">
+            The code was sent to your {loginType}{" "}
+          </p>
+          <span className="font-semibold">{value}</span>
+        </div>
+        <InputComponent
+          type="text"
+          id="code"
+          name="code"
+          placeholder="Enter code here"
+          data-testid="verification_code_input"
+          onChange={handleChange}
+          value={code}
+        />
+        <PrimaryButtonComponent data-testid="verification_code_submit_btn" type="submit">
+          Submit
+        </PrimaryButtonComponent>
+      </form>
+    </>
+  )
+}
 
 export default VerificationCodeForm
