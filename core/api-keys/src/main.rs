80839bcb484c5922255a7bec43c0a38a70b94ca9
vindard <17693119+vindard@users.noreply.github.com>
2023-10-31 11:30:45 -0400

    feat: add hello-world mutation

--- core/api-keys/src/main.rs
+++ core/api-keys/src/main.rs
@@ -1,12 +1,12 @@
-use api_keys::graphql::QueryRoot;
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use api_keys::graphql::{MutationRoot, QueryRoot};
+use async_graphql::{EmptySubscription, Schema};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::{routing::get, Extension, Router};
 
 #[tokio::main]
 async fn main() {
     // Create a GraphQL schema with a simple query
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription).finish();
 
     // Create an axum router
     let app = Router::new()
@@ -25,7 +25,7 @@ async fn main() {
 
 // GraphQL handler
 async fn graphql_handler(
-    schema: Extension<Schema<QueryRoot, EmptyMutation, EmptySubscription>>,
+    schema: Extension<Schema<QueryRoot, MutationRoot, EmptySubscription>>,
     req: GraphQLRequest,
 ) -> GraphQLResponse {
     schema.execute(req.into_inner()).await.into()
