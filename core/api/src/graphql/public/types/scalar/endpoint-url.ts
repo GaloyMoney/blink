826921e9c8df36043738d56a8f0877101136201f
Juan P Lopez <dolcalmi@gmail.com>
2023-11-07 21:34:16 -0500

    fix: add url(protocol) validation to add callback mutation

--- core/api/src/graphql/public/types/scalar/endpoint-url.ts
+++ core/api/src/graphql/public/types/scalar/endpoint-url.ts
@@ -1,14 +1,32 @@
+import { URL } from "url"
+
 import { GT } from "@/graphql/index"
+import { InputValidationError } from "@/graphql/error"
 
 const EndpointUrl = GT.Scalar({
   name: "EndpointUrl",
   description: "Url that will be fetched on events for the account",
-  serialize(value) {
-    if (typeof value !== "string") {
-      return "Invalid value for EndpointUrl"
+  parseValue(value) {
+    if (typeof value === "string") {
+      return validUrlValue(value)
     }
-    return value
+    return new InputValidationError({ message: "Invalid type for EndpointUrl" })
+  },
+  parseLiteral(ast) {
+    if (ast.kind === GT.Kind.STRING) {
+      return validUrlValue(ast.value)
+    }
+    return new InputValidationError({ message: "Invalid type for EndpointUrl" })
   },
 })
+
+function validUrlValue(value: string) {
+  try {
+    new URL(value)
+    return value
+  } catch (error) {
+    return new InputValidationError({ message: "Invalid value for EndpointUrl" })
+  }
+}
 
 export default EndpointUrl
