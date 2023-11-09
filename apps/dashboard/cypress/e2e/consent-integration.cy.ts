805cf26852e73c7e023a131a9355c4816f69ccde
Siddharth <siddharth@debian.siddharth>
2023-11-09 16:50:53 +0530

    test: using email test for consent integration

--- apps/dashboard/cypress/e2e/consent-integration.cy.ts
+++ apps/dashboard/cypress/e2e/consent-integration.cy.ts
@@ -1,7 +1,6 @@
 describe("Consent integration Test", () => {
   const signInData = {
-    PHONE_NUMBER: "+16505554350",
-    VERIFICATION_CODE: "000000",
+    EMAIL: "test@galoy.io",
   }
 
   before(() => {
@@ -10,29 +9,31 @@ describe("Consent integration Test", () => {
 
   it("Consent integration", () => {
     cy.contains("button", "Sign in with Blink").click()
+    const email = signInData.EMAIL
     cy.wait(2000)
-    cy.get("[data-testid=sign_in_with_phone_text]").click()
-    cy.get("[data-testid=phone_number_input]").type(signInData.PHONE_NUMBER)
-    cy.get("[data-testid=phone_login_next_btn]").click()
-    cy.wait(2000)
-    cy.get("[data-testid=verification_code_input]").type(signInData.VERIFICATION_CODE)
-    cy.wait(2000)
+    cy.get("[data-testid=email_id_input]").type(email)
+    cy.get("[data-testid=email_login_next_btn]").click()
+    cy.getOTP(email).then((otp) => {
+      const code = otp
+      cy.wait(2000)
+      cy.get("[data-testid=verification_code_input]").type(code)
 
-    cy.contains("label", "offline").click()
-    cy.contains("label", "transactions:read").click()
-    cy.contains("label", "payments:send").click()
+      cy.contains("label", "offline").click()
+      cy.contains("label", "transactions:read").click()
+      cy.contains("label", "payments:send").click()
 
-    cy.get("[data-testid=submit_consent_btn]").click()
-    cy.wait(5000)
-    cy.getCookie("next-auth.session-token").then((cookie) => {
-      if (cookie && cookie.value) {
-        cy.writeFile(".env.test", `NEXT_AUTH_SESSION_TOKEN=${cookie.value}\n`, {
-          flag: "w",
-        })
-        cy.log("Session token saved to .env.test")
-      } else {
-        cy.log("Session token not found")
-      }
+      cy.get("[data-testid=submit_consent_btn]").click()
+      cy.wait(5000)
+      cy.getCookie("next-auth.session-token").then((cookie) => {
+        if (cookie && cookie.value) {
+          cy.writeFile(".env.test", `NEXT_AUTH_SESSION_TOKEN=${cookie.value}\n`, {
+            flag: "w",
+          })
+          cy.log("Session token saved to .env.test")
+        } else {
+          cy.log("Session token not found")
+        }
+      })
     })
   })
 })
