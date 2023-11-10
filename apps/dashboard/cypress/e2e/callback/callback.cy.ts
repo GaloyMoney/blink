9bba8e3c7311b659aed65b2d88b3ce9f1c0d8c6e
Siddharth <siddharth@debian.siddharth>
2023-11-10 11:56:28 +0530

    test: changes in test
    rebase

--- apps/dashboard/cypress/e2e/callback/callback.cy.ts
+++ apps/dashboard/cypress/e2e/callback/callback.cy.ts
@@ -2,16 +2,31 @@ import { testData } from "../../support/test-data"
 
 describe("Callback Test", () => {
   beforeEach(() => {
-    cy.viewport(3000, 2000)
+    cy.viewport(1920, 1080)
     cy.setCookie("next-auth.session-token", testData.NEXT_AUTH_SESSION_TOKEN, {
       secure: true,
     })
     cy.visit("/")
-    cy.wait(2000)
-    cy.get("[data-testid=sidebar-callback-link]").click()
-    cy.get("[data-testid=add-callback-btn]").click()
-    cy.get("[data-testid=add-callback-input]").type(testData.CALLBACK_URL)
-    cy.get("[data-testid=add-callback-create-btn]").click()
+    cy.get("[data-testid=sidebar-callback-link]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
+    cy.get("[data-testid=add-callback-btn]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
+    cy.get("[data-testid=add-callback-input]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .type(testData.CALLBACK_URL)
+    cy.get("[data-testid=add-callback-create-btn]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
 
     cy.contains("td", testData.CALLBACK_URL)
       .should("exist")
@@ -21,7 +36,6 @@ describe("Callback Test", () => {
   })
 
   it("Callback Addition Test", () => {
-    cy.wait(2000)
     cy.get("@callbackId")
       .should("exist")
       .then((id) => {
@@ -30,15 +44,21 @@ describe("Callback Test", () => {
   })
 
   it("Callback Deletion Test", () => {
-    cy.wait(2000)
     cy.get("@callbackId")
       .should("exist")
       .then((id) => {
-        cy.get(`[data-testid="delete-callback-btn-${id}"]`).filter(":visible").click()
+        cy.get(`[data-testid="delete-callback-btn-${id}"]`)
+          .filter(":visible")
+          .should("exist")
+          .and("be.visible")
+          .and("not.be.disabled")
+          .click()
         cy.get(`[data-testid="delete-callback-confirm-btn-${id}"]`)
           .filter(":visible")
+          .should("exist")
+          .and("be.visible")
+          .and("not.be.disabled")
           .click()
-        cy.wait(500)
         cy.get(`[data-testid="${id}"]`).should("not.exist")
       })
   })
--- apps/dashboard/cypress/e2e/consent-integration.cy.ts
+++ apps/dashboard/cypress/e2e/consent-integration.cy.ts
@@ -8,21 +8,56 @@ describe("Consent integration Test", () => {
   })
 
   it("Consent integration", () => {
-    cy.contains("button", "Sign in with Blink").click()
+    cy.contains("button", "Sign in with Blink")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
+
     const email = signInData.EMAIL
-    cy.wait(2000)
-    cy.get("[data-testid=email_id_input]").type(email)
-    cy.get("[data-testid=email_login_next_btn]").click()
+    cy.wait(1000)
+    cy.get("[data-testid=email_id_input]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .type(email)
+
+    cy.get("[data-testid=email_login_next_btn]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
+
     cy.getOTP(email).then((otp) => {
       const code = otp
-      cy.wait(2000)
-      cy.get("[data-testid=verification_code_input]").type(code)
+      cy.get("[data-testid=verification_code_input]")
+        .should("exist")
+        .and("be.visible")
+        .and("not.be.disabled")
+        .type(code)
 
-      cy.contains("label", "offline").click()
-      cy.contains("label", "transactions:read").click()
-      cy.contains("label", "payments:send").click()
+      cy.contains("label", "offline")
+        .should("exist")
+        .and("be.visible")
+        .and("not.be.disabled")
+        .click()
+      cy.contains("label", "transactions:read")
+        .should("exist")
+        .and("be.visible")
+        .and("not.be.disabled")
+        .click()
+      cy.contains("label", "payments:send")
+        .should("exist")
+        .and("be.visible")
+        .and("not.be.disabled")
+        .click()
 
-      cy.get("[data-testid=submit_consent_btn]").click()
+      cy.get("[data-testid=submit_consent_btn]")
+        .should("exist")
+        .and("be.visible")
+        .and("not.be.disabled")
+        .click()
+
       cy.wait(5000)
       cy.getCookie("next-auth.session-token").then((cookie) => {
         if (cookie && cookie.value) {
