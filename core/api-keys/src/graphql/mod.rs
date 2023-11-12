2bc8e91d9972082f8d8993c388421f4a11344335
Sandipan Dey <hey@sandipan.dev>
2023-11-12 19:01:34 +0530

    chore(api-keys): add tracing to graphql schema

--- core/api-keys/src/graphql/mod.rs
+++ core/api-keys/src/graphql/mod.rs
@@ -1,7 +1,7 @@
 mod convert;
 mod schema;
 
-use async_graphql::*;
+use async_graphql::{extensions::Tracing, *};
 
 pub use schema::*;
 
@@ -9,6 +9,7 @@ use crate::app::ApiKeysApp;
 
 pub fn schema(app: Option<ApiKeysApp>) -> Schema<Query, Mutation, EmptySubscription> {
     let schema = Schema::build(Query, Mutation, EmptySubscription);
+    let schema = schema.extension(Tracing);
     if let Some(app) = app {
         schema.data(app).finish()
     } else {
