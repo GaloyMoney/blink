ce56d34f22f9f7b6faf6c6a5f6e7533e83ade184
Siddharth <siddharth@debian.siddharth>
2023-11-15 22:20:57 +0530

    test: test refactor

--- apps/dashboard/cypress/e2e/callback/callback.cy.ts
+++ apps/dashboard/cypress/e2e/callback/callback.cy.ts
@@ -1,6 +1,8 @@
 import { testData } from "../../support/test-data"
 
 describe("Callback Test", () => {
+  let callbackId: string | undefined
+
   beforeEach(() => {
     cy.viewport(1920, 1080)
     cy.setCookie("next-auth.session-token", testData.NEXT_AUTH_SESSION_TOKEN, {
@@ -8,70 +10,58 @@ describe("Callback Test", () => {
     })
     cy.visit("/")
 
-    cy.get("[data-testid=sidebar-callback-link]").should("exist")
-    cy.get("[data-testid=sidebar-callback-link]").should("be.visible")
-    cy.get("[data-testid=sidebar-callback-link]").should("not.be.disabled")
-    cy.get("[data-testid=sidebar-callback-link]").click()
+    cy.get("[data-testid=sidebar-callback-link]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
+  })
 
-    cy.get("[data-testid=add-callback-btn]").should("exist")
-    cy.get("[data-testid=add-callback-btn]").should("be.visible")
-    cy.get("[data-testid=add-callback-btn]").should("not.be.disabled")
-    cy.get("[data-testid=add-callback-btn]").click()
+  it("Callback Addition Test", () => {
+    cy.get("[data-testid=add-callback-btn]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
 
-    cy.get("[data-testid=add-callback-input]").should("exist")
-    cy.get("[data-testid=add-callback-input]").should("be.visible")
-    cy.get("[data-testid=add-callback-input]").should("not.be.disabled")
-    cy.get("[data-testid=add-callback-input]").type(testData.CALLBACK_URL)
+    cy.get("[data-testid=add-callback-input]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .type(testData.CALLBACK_URL)
 
-    cy.get("[data-testid=add-callback-create-btn]").should("exist")
-    cy.get("[data-testid=add-callback-create-btn]").should("be.visible")
-    cy.get("[data-testid=add-callback-create-btn]").should("not.be.disabled")
-    cy.get("[data-testid=add-callback-create-btn]").click()
+    cy.get("[data-testid=add-callback-create-btn]")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
 
     cy.contains("td", testData.CALLBACK_URL)
       .should("exist")
       .parent("tr")
       .invoke("attr", "data-testid")
-      .as("callbackId")
-  })
-
-  it("Callback Addition Test", () => {
-    cy.get("@callbackId")
-      .should("exist")
       .then((id) => {
-        cy.log("Callback ID:", id)
+        callbackId = id // Store the callback ID
+        cy.log("Callback ID:", callbackId)
       })
   })
 
   it("Callback Deletion Test", () => {
-    cy.get("@callbackId")
+    expect(callbackId).to.exist // Ensure the callback ID is available
+    cy.get(`[data-testid="delete-callback-btn-${callbackId}"]`)
+      .filter(":visible")
       .should("exist")
-      .then((id) => {
-        cy.get(`[data-testid="delete-callback-btn-${id}"]`)
-          .filter(":visible")
-          .should("exist")
-        cy.get(`[data-testid="delete-callback-btn-${id}"]`)
-          .filter(":visible")
-          .should("be.visible")
-        cy.get(`[data-testid="delete-callback-btn-${id}"]`)
-          .filter(":visible")
-          .should("not.be.disabled")
-        cy.get(`[data-testid="delete-callback-btn-${id}"]`).filter(":visible").click()
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
 
-        cy.get(`[data-testid="delete-callback-confirm-btn-${id}"]`)
-          .filter(":visible")
-          .should("exist")
-        cy.get(`[data-testid="delete-callback-confirm-btn-${id}"]`)
-          .filter(":visible")
-          .should("be.visible")
-        cy.get(`[data-testid="delete-callback-confirm-btn-${id}"]`)
-          .filter(":visible")
-          .should("not.be.disabled")
-        cy.get(`[data-testid="delete-callback-confirm-btn-${id}"]`)
-          .filter(":visible")
-          .click()
+    cy.get(`[data-testid="delete-callback-confirm-btn-${callbackId}"]`)
+      .filter(":visible")
+      .should("exist")
+      .and("be.visible")
+      .and("not.be.disabled")
+      .click()
 
-        cy.get(`[data-testid="${id}"]`).should("not.exist")
-      })
+    cy.get(`[data-testid="${callbackId}"]`).should("not.exist")
   })
 })
