1216a4a5416bbc87cdb075c99490f143dfa20034
Siddharth <siddharth@debian.siddharth>
2023-11-20 18:12:52 +0530

    test: split commands in test

--- apps/dashboard/cypress/e2e/consent-integration.cy.ts
+++ apps/dashboard/cypress/e2e/consent-integration.cy.ts
@@ -11,30 +11,37 @@ describe("Consent integration Test", () => {
   it("Consent integration", () => {
     cy.contains("button", "Sign in with Blink").should("exist")
     cy.contains("button", "Sign in with Blink").should("be.visible")
-    cy.contains("button", "Sign in with Blink").should("not.be.disabled").click()
+    cy.contains("button", "Sign in with Blink").should("not.be.disabled")
+    cy.contains("button", "Sign in with Blink").click()
 
     const email = signInData.EMAIL
     cy.get("[data-testid=email_id_input]").should("exist")
     cy.get("[data-testid=email_id_input]").should("be.visible")
-    cy.get("[data-testid=email_id_input]").should("not.be.disabled").type(email)
+    cy.get("[data-testid=email_id_input]").should("not.be.disabled")
+    cy.get("[data-testid=email_id_input]").type(email)
 
     cy.get("[data-testid=email_login_next_btn]").should("exist")
     cy.get("[data-testid=email_login_next_btn]").should("be.visible")
-    cy.get("[data-testid=email_login_next_btn]").should("not.be.disabled").click()
+    cy.get("[data-testid=email_login_next_btn]").should("not.be.disabled")
+    cy.get("[data-testid=email_login_next_btn]").click()
 
     cy.getOTP(email).then((otp) => {
       const code = otp
       cy.get("[data-testid=verification_code_input]").should("exist")
       cy.get("[data-testid=verification_code_input]").should("be.visible")
-      cy.get("[data-testid=verification_code_input]").should("not.be.disabled").type(code)
+      cy.get("[data-testid=verification_code_input]").should("not.be.disabled")
+      cy.get("[data-testid=verification_code_input]").type(code)
 
       cy.contains("label", "read").should("exist")
       cy.contains("label", "read").should("be.visible")
-      cy.contains("label", "read").should("not.be.disabled").click()
+      cy.contains("label", "read").should("not.be.disabled")
+      cy.contains("label", "read").should("not.be.disabled")
+      cy.contains("label", "read").click()
 
       cy.contains("label", "write").should("exist")
       cy.contains("label", "write").should("be.visible")
-      cy.contains("label", "write").should("not.be.disabled").click()
+      cy.contains("label", "write").should("not.be.disabled")
+      cy.contains("label", "write").click()
 
       cy.get("[data-testid=submit_consent_btn]").should("exist")
       cy.get("[data-testid=submit_consent_btn]").should("be.visible")
