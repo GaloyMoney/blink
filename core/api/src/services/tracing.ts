cd080f5534d798f39d27f4ca5654ecd9df41dce8
nicolasburtey <nicolasburtey@users.noreply.github.com>
2023-11-22 15:17:44 +0000

    chore: removing express instrumentation (#3588)
    Co-authored-by: Nicolas Burtey <nb@galoy.io>

--- core/api/src/services/tracing.ts
+++ core/api/src/services/tracing.ts
@@ -4,7 +4,6 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation"
 import { GraphQLInstrumentation } from "@opentelemetry/instrumentation-graphql"
 import { GrpcInstrumentation } from "@opentelemetry/instrumentation-grpc"
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http"
-import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express"
 import { IORedisInstrumentation } from "@opentelemetry/instrumentation-ioredis"
 import { MongoDBInstrumentation } from "@opentelemetry/instrumentation-mongodb"
 import { Span as SdkSpan, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base"
@@ -207,14 +206,6 @@ registerInstrumentations({
     new MongoDBInstrumentation(),
     new GrpcInstrumentation(),
     new IORedisInstrumentation(),
-    new ExpressInstrumentation({
-      requestHook: (span, request) => {
-        console.log("requestHook", request)
-        console.log("span", span)
-        // span NonRecordingSpan {}
-      },
-      enabled: true,
-    }),
   ],
 })
 
