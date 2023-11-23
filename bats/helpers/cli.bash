cc886fb86ca3ee15016ce8119e3e37ee939fa87a
Sandipan Dey <hey@sandipan.dev>
2023-11-23 14:29:57 +0530

    test: move ln tests to top level bats

--- bats/helpers/cli.bash
+++ bats/helpers/cli.bash
@@ -1,3 +1,15 @@
+tilt_cli() {
+  tilt $@
+}
+
+bria_cli() {
+ docker exec "${COMPOSE_PROJECT_NAME}-bria-1" bria $@ 
+}
+
+mongo_cli() {
+  docker exec "${COMPOSE_PROJECT_NAME}-mongodb-1" mongosh --quiet mongodb://localhost:27017/galoy --eval $@
+}
+
 bitcoin_cli() {
   docker exec "${COMPOSE_PROJECT_NAME}-bitcoind-1" bitcoin-cli $@
 }
@@ -10,14 +22,43 @@ lnd_cli() {
       $@
 }
 
-bria_cli() {
- docker exec "${COMPOSE_PROJECT_NAME}-bria-1" bria $@ 
+lnd2_cli() {
+  docker exec "${COMPOSE_PROJECT_NAME}-lnd2-1" \
+    lncli \
+      --macaroonpath /root/.lnd/admin.macaroon \
+      --tlscertpath /root/.lnd/tls.cert \
+      $@
 }
 
-tilt_cli() {
-  tilt $@
+lnd_outside_cli() {
+  docker exec "${COMPOSE_PROJECT_NAME}-lnd-outside-1-1" \
+    lncli \
+      --macaroonpath /root/.lnd/admin.macaroon \
+      --tlscertpath /root/.lnd/tls.cert \
+      $@
 }
 
-mongo_cli() {
-  docker exec "${COMPOSE_PROJECT_NAME}-mongodb-1" mongosh --quiet mongodb://localhost:27017/galoy --eval $@
+lnd_outside_2_cli() {
+  docker exec "${COMPOSE_PROJECT_NAME}-lnd-outside-2-1" \
+    lncli \
+      --macaroonpath /root/.lnd/admin.macaroon \
+      --tlscertpath /root/.lnd/tls.cert \
+      $@
+}
+
+run_with_lnd() {
+  local lnd_name="$1"
+  shift  # This will shift away the function name, so $1 becomes the next argument
+
+  if [[ "$lnd_name" == "lnd_cli" ]]; then
+    lnd_cli "$@"
+  elif [[ "$lnd_name" == "lnd2_cli" ]]; then
+    lnd2_cli "$@"
+  elif [[ "$lnd_name" == "lnd_outside_cli" ]]; then
+    lnd_outside_cli "$@"
+  elif [[ "$lnd_name" == "lnd_outside_2_cli" ]]; then
+    lnd_outside_2_cli "$@"
+  else
+    echo "Invalid function name passed!" && return 1
+  fi
 }
--- bats/helpers/ln.bash
+++ bats/helpers/ln.bash
@@ -2,6 +2,119 @@ CURRENT_FILE=${BASH_SOURCE:-bats/helpers/.}
 source "$(dirname "$CURRENT_FILE")/_common.bash"
 source "$(dirname "$CURRENT_FILE")/cli.bash"
 
+fund_user_lightning() {
+  local token_name=$1
+  local wallet_id_name="$token_name.${2}_id" # btc_wallet or usd_wallet
+  local amount=$3
+
+  variables=$(
+    jq -n \
+    --arg wallet_id "$(read_value $wallet_id_name)" \
+    '{input: {walletId: $wallet_id}}'
+  )
+  exec_graphql "$token_name" 'ln-no-amount-invoice-create' "$variables"
+  invoice="$(graphql_output '.data.lnNoAmountInvoiceCreate.invoice')"
+
+  payment_request="$(echo $invoice | jq -r '.paymentRequest')"
+  [[ "${payment_request}" != "null" ]]
+  payment_hash="$(echo $invoice | jq -r '.paymentHash')"
+  [[ "${payment_hash}" != "null" ]]
+
+  lnd_outside_cli payinvoice -f \
+    --pay_req "$payment_request" \
+    --amt "$amount"
+
+  retry 15 1 check_for_ln_initiated_settled "$token_name" "$payment_hash"
+}
+
+lnds_init() {
+  # Clean up any existing channels
+  close_partner_initiated_channels_with_external || true
+
+  # Mine onchain balance
+  local amount="1"
+  local address="$(lnd_outside_cli newaddress p2wkh | jq -r '.address')"
+  local local_amount="10000000"
+  local push_amount="5000000"
+  bitcoin_cli sendtoaddress "$address" "$amount"
+  bitcoin_cli -generate 3
+
+  no_pending_channels() {
+    pending_channel="$(lnd_outside_cli pendingchannels | jq -r '.pending_open_channels[0]')"
+    if [[ "$pending_channel" != "null" ]]; then
+      bitcoin_cli -generate 3
+      exit 1
+    fi
+  }
+
+  synced_to_graph() {
+    is_synced="$(lnd_outside_cli getinfo | jq -r '.synced_to_graph')"
+    [[ "$is_synced" == "true" ]] || exit 1
+  }
+
+  # Open channel from lndoutside1 -> lnd1
+  pubkey="$(lnd_cli getinfo | jq -r '.identity_pubkey')"
+  endpoint="${COMPOSE_PROJECT_NAME}-lnd1-1:9735"
+
+  lnd_outside_cli connect "${pubkey}@${endpoint}" || true
+  retry 10 1 synced_to_graph
+  lnd_outside_cli openchannel \
+    --node_key "$pubkey" \
+    --local_amt "$local_amount"
+
+  retry 10 1 mempool_not_empty
+  retry 10 1 no_pending_channels
+
+  # Open channel with push from lndoutside1 -> lndoutside2
+  pubkey="$(lnd_outside_2_cli getinfo | jq -r '.identity_pubkey')"
+  endpoint="${COMPOSE_PROJECT_NAME}-lnd-outside-2-1:9735"
+
+  lnd_outside_cli connect "${pubkey}@${endpoint}" || true
+  retry 10 1 synced_to_graph
+  lnd_outside_cli openchannel \
+    --node_key "$pubkey" \
+    --local_amt "$local_amount" \
+    --push_amt "$push_amount"
+
+  retry 10 1 mempool_not_empty
+  retry 10 1 no_pending_channels
+
+  # FIXME: we may need some check on the graph or something else
+  # NB: I get randomly a "no route" error otherwise
+  sleep 10
+}
+
+close_partner_initiated_channels_with_external() {
+  close_channels_with_external() {
+    lnd_cli_value="$1"
+    lnd1_pubkey=$(lnd_cli getinfo | jq -r '.identity_pubkey')
+    lnd2_pubkey=$(lnd2_cli getinfo | jq -r '.identity_pubkey')
+
+    partner_initiated_external_channel_filter='
+    .channels[]?
+      | select(.initiator != true)
+      | select(.remote_pubkey != $lnd1_pubkey)
+      | select(.remote_pubkey != $lnd2_pubkey)
+      | .channel_point
+    '
+
+    run_with_lnd "$lnd_cli_value" listchannels \
+      | jq -r \
+        --arg lnd1_pubkey "$lnd1_pubkey" \
+        --arg lnd2_pubkey "$lnd2_pubkey" \
+        "$partner_initiated_external_channel_filter" \
+      | while read -r channel_point; do
+          funding_txid="${channel_point%%:*}"
+          run_with_lnd "$lnd_cli_value" closechannel "$funding_txid"
+        done
+  }
+
+  close_channels_with_external lnd_cli
+  close_channels_with_external lnd2_cli
+  close_channels_with_external lnd_outside_cli
+  close_channels_with_external lnd_outside_2_cli
+}
+
 create_new_lnd_onchain_address() {
   local wallet_name=$1
   local wallet_id=$(read_value $wallet_name)
@@ -30,4 +143,50 @@ create_new_lnd_onchain_address() {
   insert_lnd1_address "$wallet_id" "$address" > /dev/null
 
   echo $address
+}
+
+# Status of LN Payment
+check_for_ln_initiated_settled() {
+  check_for_ln_initiated_status "SUCCESS" "$@"
+}
+
+check_for_ln_initiated_pending() {
+  check_for_ln_initiated_status "PENDING" "$@"
+}
+
+check_for_ln_initiated_status() {
+  local expected_status=$1
+  local token_name=$2
+  local payment_hash=$3
+  local first=${4:-"2"}
+
+  variables=$(
+  jq -n \
+  --argjson first "$first" \
+  '{"first": $first}'
+  )
+  exec_graphql "$token_name" 'transactions' "$variables"
+
+  status="$(get_from_transaction_by_ln_hash_and_status $payment_hash $expected_status '.status')"
+  [[ "${status}" == "${expected_status}" ]] || return 1
+}
+
+get_from_transaction_by_ln_hash_and_status() {
+  payment_hash="$1"
+  expected_status="$2"
+  property_query="$3"
+
+  jq_query='
+    .data.me.defaultAccount.transactions.edges[]
+    | select(.node.initiationVia.paymentHash == $payment_hash)
+    | select(.node.status == $expected_status)
+    .node'
+
+  echo $output \
+    | jq -r \
+      --arg payment_hash "$payment_hash" \
+      --arg expected_status "$expected_status" \
+      "$jq_query" \
+    | jq -r "$property_query" \
+    | head -n 1
 }
