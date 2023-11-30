787f83405791251cbbee15847d00d14a01d5cef3
Sam Peters <unclesamtoshi@protonmail.com>
2023-11-30 14:02:36 -0700

    chore: address pr feedback

--- apps/pay/next.config.js
+++ apps/pay/next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   rewrites() {
     return [{ source: "/.well-known/lnurlp/:username", destination: "/lnurlp/:username" }]
   },
+  experimental: {
+    outputFileTracingRoot: require("path").join(__dirname, "../../"),
+    instrumentationHook: true,
+  },
+  output: "standalone",
 }
