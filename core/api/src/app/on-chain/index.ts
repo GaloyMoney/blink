5eccde200dccc35edacbf2cd252fc85b957a610f
bodymindarts <justin@galoy.io>
2023-12-01 16:31:40 +0100

    chore: wip expose arrivalInMempoolEstimatedAt

--- core/api/src/app/on-chain/index.ts
+++ core/api/src/app/on-chain/index.ts
@@ -2,6 +2,7 @@ export * from "./rebalance-to-cold-wallet"
 export * from "./record-hot-to-cold-transfer"
 
 import { OnChainService } from "@/services/bria"
+import { LedgerService } from "@/services/ledger"
 
 export const getHotBalance = async (): Promise<BtcPaymentAmount | ApplicationError> => {
   const service = OnChainService()
@@ -11,4 +12,20 @@ export const getHotBalance = async (): Promise<BtcPaymentAmount | ApplicationErr
 export const getColdBalance = async (): Promise<BtcPaymentAmount | ApplicationError> => {
   const service = OnChainService()
   return service.getColdBalance()
+}
+
+export const getBatchInclusionEstimatedAt = async (
+  ledgerTransactionId: LedgerTransactionId,
+): Promise<number | undefined | ApplicationError> => {
+  const ledgerTransaction = await LedgerService().getTransactionById(ledgerTransactionId)
+  if (ledgerTransaction instanceof Error) {
+    return ledgerTransaction
+  }
+  const payout = await OnChainService().findPayoutByLedgerJournalId(
+    ledgerTransaction.journalId,
+  )
+  if (payout instanceof Error) {
+    return payout
+  }
+  return payout.batchInclusionEstimatedAt
 }
