4f24e16112ba8419b24bab785519c85b0e8917da
Arvin <17693119+vindard@users.noreply.github.com>
2023-12-02 15:25:08 -0400

    build(ci): optionally add SSL_CERT_FILE if present (#3649)

--- ci/apps/tasks/buck-task.sh
+++ ci/apps/tasks/buck-task.sh
@@ -4,4 +4,8 @@ set -e
 
 cd repo
 
-buck2 ${BUCK_CMD} ${BUCK_TARGET}
+if [[ -z "${SSL_CERT_FILE}" ]]; then
+  buck2 ${BUCK_CMD} ${BUCK_TARGET}
+else
+  buck2 ${BUCK_CMD} ${BUCK_TARGET} -- --env SSL_CERT_FILE=${SSL_CERT_FILE}
+fi
