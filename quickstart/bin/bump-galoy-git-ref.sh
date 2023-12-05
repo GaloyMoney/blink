26a39bf2836d4f4e0dcea9e6f96eb169ebe4bb5b
vindard <17693119+vindard@users.noreply.github.com>
2023-12-05 16:51:07 -0400

    build(ci): bring in git url from pull_request event for vendir sync
    This brings in the git repo url as well in the case where a PR is opened
    from a fork outside our own repos.
    
    Github events API sample values from:
    https://github.com/orgs/community/discussions/73499#discussioncomment-7483499

--- quickstart/bin/bump-galoy-git-ref.sh
+++ quickstart/bin/bump-galoy-git-ref.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git_ref_sha="${1}"
+git_url="${2}"
 
 DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -6,6 +8,8 @@ TMPDIR=""
 TMPDIR=$(mktemp -d -t repipe.XXXXXX)
 trap "rm -rf ${TMPDIR}" INT TERM QUIT EXIT
 
-sed "s/^galoy_git_ref:.*/galoy_git_ref: ${1}/" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
+sed "s/^galoy_git_ref:.*/galoy_git_ref: ${git_ref_sha}/" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
+mv ${TMPDIR}/new_values.yml ${DIR}/../vendir/values.yml
 
+sed "s/^galoy_repo_url:.*/galoy_repo_url: ${git_url}/" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
 mv ${TMPDIR}/new_values.yml ${DIR}/../vendir/values.yml
