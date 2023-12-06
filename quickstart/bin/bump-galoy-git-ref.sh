a35d7b8cf2b9875f860f868bc67c86c17fe2b8b1
vindard <17693119+vindard@users.noreply.github.com>
2023-12-06 00:52:19 -0400

    refactor: change sed delimiters
    Change delimiters to avoid collision with url string that also use the
    '/' character.

--- quickstart/bin/bump-galoy-git-ref.sh
+++ quickstart/bin/bump-galoy-git-ref.sh
@@ -8,8 +8,8 @@ TMPDIR=""
 TMPDIR=$(mktemp -d -t repipe.XXXXXX)
 trap "rm -rf ${TMPDIR}" INT TERM QUIT EXIT
 
-sed "s/^galoy_git_ref:.*/galoy_git_ref: ${git_ref_sha}/" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
+sed "s|^galoy_git_ref:.*|galoy_git_ref: ${git_ref_sha}|" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
 mv ${TMPDIR}/new_values.yml ${DIR}/../vendir/values.yml
 
-sed "s/^galoy_repo_url:.*/galoy_repo_url: ${git_url}/" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
+sed "s|^galoy_repo_url:.*|galoy_repo_url: ${git_url}|" ${DIR}/../vendir/values.yml > ${TMPDIR}/new_values.yml
 mv ${TMPDIR}/new_values.yml ${DIR}/../vendir/values.yml
