6321874a7b4f441e10afca1c91b5afef8728a887
vindard <17693119+vindard@users.noreply.github.com>
2023-12-07 16:42:22 -0400

    test(core): move 'updateAccountStatus' tests to new integration format

--- core/api/test/legacy-integration/02-user-wallet/02-a-user-setup.spec.ts
+++ core/api/test/legacy-integration/02-user-wallet/02-a-user-setup.spec.ts
@@ -1,6 +1,3 @@
-import { randomUUID } from "crypto"
-
-import { Accounts } from "@/app"
 import { CsvWalletsExport } from "@/services/ledger/csv-wallet-export"
 
 import {
@@ -8,25 +5,19 @@ import {
   randomPhone,
   createUserAndWalletFromPhone,
   getDefaultWalletIdByPhone,
-  getAccountIdByPhone,
 } from "test/helpers"
 
 let walletIdA: WalletId
-let accountIdC: AccountId
 
 const phoneA = randomPhone()
-const phoneC = randomPhone()
 
 describe("UserWallet", () => {
   beforeAll(async () => {
     await createMandatoryUsers()
 
     await createUserAndWalletFromPhone(phoneA)
-    await createUserAndWalletFromPhone(phoneC)
 
     walletIdA = await getDefaultWalletIdByPhone(phoneA)
-
-    accountIdC = await getAccountIdByPhone(phoneC)
   })
 
   describe("getStringCsv", () => {
@@ -39,51 +30,6 @@ describe("UserWallet", () => {
       expect(typeof base64Data).toBe("string")
       const data = Buffer.from(base64Data, "base64")
       expect(data.includes(csvHeader)).toBeTruthy()
-    })
-  })
-
-  describe("updateAccountStatus", () => {
-    it("sets account status (with history) for given user id", async () => {
-      let account
-
-      const updatedByPrivilegedClientId = randomUUID() as PrivilegedClientId
-
-      account = await Accounts.updateAccountStatus({
-        accountId: accountIdC,
-        status: "pending",
-        updatedByPrivilegedClientId,
-      })
-      if (account instanceof Error) {
-        throw account
-      }
-      expect(account.status).toEqual("pending")
-
-      account = await Accounts.updateAccountStatus({
-        accountId: account.id,
-        status: "locked",
-        updatedByPrivilegedClientId,
-        comment: "Looks spammy",
-      })
-      if (account instanceof Error) {
-        throw account
-      }
-      expect(account.statusHistory.slice(-1)[0]).toMatchObject({
-        status: "locked",
-        updatedByPrivilegedClientId,
-        comment: "Looks spammy",
-      })
-      expect(account.status).toEqual("locked")
-
-      account = await Accounts.updateAccountStatus({
-        accountId: account.id,
-        status: "active",
-        updatedByPrivilegedClientId,
-      })
-      if (account instanceof Error) {
-        throw account
-      }
-      expect(account.statusHistory.length).toBe(4)
-      expect(account.status).toEqual("active")
     })
   })
 })
