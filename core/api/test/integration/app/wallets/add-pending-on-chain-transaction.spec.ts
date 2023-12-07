e8aa7a8b0fd1aefb4fc35859ada02d2a26264a94
Arvin <17693119+vindard@users.noreply.github.com>
2023-12-07 05:23:45 -0400

    test(core): move legacy tx-display integration tests (#3636)
    * test: move LnSendOffChain metadata check
    
    * test: move LnFeeReimbursementReceive metadata check
    
    * test: move LnTradeIntraAccountLedger metadata check
    
    * test: move LnIntraledgerLedger metadata check
    
    * test: move WalletIdTradeIntraAccountLedger metadata check
    
    * test: move WalletIdIntraledgerLedger metadata check
    
    * test: move LnReceiveLedger metadata check
    
    * test: move OnChainSendLedger metadata check
    
    * test: move OnChainReceiveLedger metadata check
    
    * test: move OnChainTradeIntraAccountLedger metadata check
    
    * test: move OnChainIntraledgerLedger metadata check
    
    * test: move LnFailedPaymentReceiveLedger metadata check
    
    * test: move add-pending-onchain display-converter check
    
    * refactor: restore all mocks after each test

--- core/api/test/integration/app/wallets/add-pending-on-chain-transaction.spec.ts
+++ core/api/test/integration/app/wallets/add-pending-on-chain-transaction.spec.ts
@@ -1,6 +1,7 @@
 import { addPendingTransaction } from "@/app/wallets"
 
 import { WalletCurrency } from "@/domain/shared"
+import * as DisplayAmountsConverterImpl from "@/domain/fiat"
 
 import { WalletOnChainAddressesRepository } from "@/services/mongoose"
 import { Wallet, WalletOnChainPendingReceive } from "@/services/mongoose/schema"
@@ -19,13 +20,15 @@ afterEach(async () => {
     { $pull: { onchain: { address } } },
     { multi: true }, // This option updates all matching documents
   )
+
+  jest.restoreAllMocks()
 })
 
 describe("addPendingTransaction", () => {
   it("calls sendFilteredNotification on pending onchain receive", async () => {
     // Setup mocks
     const sendFilteredNotification = jest.fn()
-    const pushNotificationsServiceSpy = jest
+    jest
       .spyOn(PushNotificationsServiceImpl, "PushNotificationsService")
       .mockImplementationOnce(() => ({
         sendFilteredNotification,
@@ -52,8 +55,33 @@ describe("addPendingTransaction", () => {
     // Expect sent notification
     expect(sendFilteredNotification.mock.calls.length).toBe(1)
     expect(sendFilteredNotification.mock.calls[0][0].title).toBeTruthy()
+  })
 
-    // Restore system state
-    pushNotificationsServiceSpy.mockRestore()
+  it("calls DisplayConverter on pending onchain receive", async () => {
+    // Setup mocks
+    const displayAmountsConverterSpy = jest.spyOn(
+      DisplayAmountsConverterImpl,
+      "DisplayAmountsConverter",
+    )
+
+    // Create user
+    const { btcWalletDescriptor } = await createRandomUserAndWallets()
+
+    // Add address to user wallet
+    await WalletOnChainAddressesRepository().persistNew({
+      walletId: btcWalletDescriptor.id,
+      onChainAddress: { address },
+    })
+
+    // Add pending transaction
+    await addPendingTransaction({
+      txId: "txId" as OnChainTxHash,
+      vout: 0 as OnChainTxVout,
+      satoshis: btcAmount,
+      address,
+    })
+
+    // Expect sent notification
+    expect(displayAmountsConverterSpy).toHaveBeenCalledTimes(1)
   })
 })
