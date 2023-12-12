232173ca4b3fe162da511641c9ef12c67626e9f1
vindard <17693119+vindard@users.noreply.github.com>
2023-12-11 21:18:46 -0400

    test(core): remove redundant add-invoice use-case tests

--- core/api/test/legacy-integration/app/wallets/invoice.spec.ts
+++ core/api/test/legacy-integration/app/wallets/invoice.spec.ts
@@ -4,22 +4,17 @@ import {
   getInvoiceCreateAttemptLimits,
   getInvoiceCreateForRecipientAttemptLimits,
 } from "@/config"
-import { decodeInvoice } from "@/domain/bitcoin/lightning"
-import { toCents } from "@/domain/fiat"
 import {
   InvoiceCreateForRecipientRateLimiterExceededError,
   InvoiceCreateRateLimiterExceededError,
 } from "@/domain/rate-limit/errors"
 import { WalletCurrency } from "@/domain/shared"
 import { WalletType } from "@/domain/wallets"
-import { DealerPriceService } from "@/services/dealer-price"
-import { WalletInvoicesRepository } from "@/services/mongoose"
 
 import {
   createUserAndWalletFromPhone,
   getAccountIdByPhone,
   getDefaultWalletIdByPhone,
-  getHash,
   randomPhone,
 } from "test/helpers"
 import {
@@ -28,12 +23,7 @@ import {
 } from "test/helpers/rate-limit"
 
 let walletIdBtc: WalletId
-let walletIdUsd: WalletId
 let accountIdB: AccountId
-
-const walletInvoices = WalletInvoicesRepository()
-
-const dealerFns = DealerPriceService()
 
 beforeAll(async () => {
   const phoneB = randomPhone()
@@ -48,218 +38,10 @@ beforeAll(async () => {
     currency: WalletCurrency.Usd,
   })
   if (wallet instanceof Error) return wallet
-  walletIdUsd = wallet.id
 })
 
 afterAll(() => {
   jest.restoreAllMocks()
-})
-
-describe("Wallet - addInvoice BTC", () => {
-  it("add a self generated invoice", async () => {
-    const amountInput = 1000
-
-    const invoice = await Wallets.addInvoiceForSelfForBtcWallet({
-      walletId: walletIdBtc,
-      amount: amountInput,
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-
-    expect(request.startsWith("lnbcrt10")).toBeTruthy()
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-
-    const {
-      recipientWalletDescriptor: { id: walletId, currency },
-    } = result
-
-    expect(walletId).toBe(walletIdBtc)
-    expect(currency).toBe(WalletCurrency.Btc)
-
-    const decodedInvoice = decodeInvoice(request)
-    if (decodedInvoice instanceof Error) throw decodedInvoice
-
-    const { amount } = decodedInvoice
-    expect(amount).toBe(amountInput)
-  })
-
-  it("add a self generated invoice without amount", async () => {
-    const invoice = await Wallets.addInvoiceNoAmountForSelf({
-      walletId: walletIdBtc,
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-
-    const {
-      recipientWalletDescriptor: { id: walletId, currency },
-    } = result
-
-    expect(walletId).toBe(walletIdBtc)
-    expect(currency).toBe(WalletCurrency.Btc)
-  })
-
-  it("adds a public with amount invoice", async () => {
-    const amountInput = 10
-
-    const invoice = await Wallets.addInvoiceForRecipientForBtcWallet({
-      recipientWalletId: walletIdBtc,
-      amount: amountInput,
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-    expect(request.startsWith("lnbcrt1")).toBeTruthy()
-
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-
-    const {
-      recipientWalletDescriptor: { id: walletId },
-      selfGenerated,
-    } = result
-
-    expect(String(walletId)).toBe(String(walletIdBtc))
-    expect(selfGenerated).toBe(false)
-
-    const decodedInvoice = decodeInvoice(request)
-    if (decodedInvoice instanceof Error) throw decodedInvoice
-
-    const { amount } = decodedInvoice
-    expect(amount).toBe(amountInput)
-  })
-
-  it("adds a public no amount invoice", async () => {
-    const invoice = await Wallets.addInvoiceNoAmountForRecipient({
-      recipientWalletId: walletIdBtc,
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-    expect(request.startsWith("lnbcrt1")).toBeTruthy()
-
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-    const {
-      recipientWalletDescriptor: { id: walletId },
-      selfGenerated,
-    } = result
-    expect(String(walletId)).toBe(String(walletIdBtc))
-    expect(selfGenerated).toBe(false)
-  })
-})
-
-describe("Wallet - addInvoice USD", () => {
-  it("add a self generated USD invoice", async () => {
-    const centsInput = 10000
-
-    const btcAmount = await dealerFns.getSatsFromCentsForFutureBuy({
-      amount: BigInt(centsInput),
-      currency: WalletCurrency.Usd,
-    })
-    if (btcAmount instanceof Error) throw btcAmount
-    const sats = Number(btcAmount.amount)
-
-    const invoice = await Wallets.addInvoiceForSelfForUsdWallet({
-      walletId: walletIdUsd,
-      amount: toCents(centsInput),
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-
-    expect(request.startsWith("lnbcrt")).toBeTruthy()
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-    const {
-      recipientWalletDescriptor: { id: walletId, currency },
-      usdAmount,
-    } = result
-
-    expect(String(walletId)).toBe(String(walletIdUsd))
-    expect(usdAmount?.amount).toBe(BigInt(centsInput))
-    expect(currency).toBe(WalletCurrency.Usd)
-
-    const decodedInvoice = decodeInvoice(request)
-    if (decodedInvoice instanceof Error) throw decodedInvoice
-
-    const { amount } = decodedInvoice
-    expect(amount).toBe(sats)
-  })
-
-  it("add a self generated invoice without amount", async () => {
-    const invoice = await Wallets.addInvoiceNoAmountForSelf({
-      walletId: walletIdUsd,
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-    const {
-      recipientWalletDescriptor: { id: walletId },
-      usdAmount,
-    } = result
-    expect(String(walletId)).toBe(String(walletIdUsd))
-    expect(usdAmount?.amount).toBe(undefined)
-  })
-
-  it("adds a public with amount invoice", async () => {
-    const centsInput = 10000
-
-    const btcAmount = await dealerFns.getSatsFromCentsForFutureBuy({
-      amount: BigInt(centsInput),
-      currency: WalletCurrency.Usd,
-    })
-    if (btcAmount instanceof Error) throw btcAmount
-    const sats = Number(btcAmount.amount)
-
-    const invoice = await Wallets.addInvoiceForRecipientForUsdWallet({
-      recipientWalletId: walletIdUsd,
-      amount: toCents(centsInput),
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-    expect(request.startsWith("lnbcrt")).toBeTruthy()
-
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-    const {
-      recipientWalletDescriptor: { id: walletId },
-      usdAmount,
-      selfGenerated,
-    } = result
-    expect(usdAmount?.amount).toBe(BigInt(centsInput))
-
-    expect(String(walletId)).toBe(String(walletIdUsd))
-    expect(selfGenerated).toBe(false)
-
-    const decodedInvoice = decodeInvoice(request)
-    if (decodedInvoice instanceof Error) throw decodedInvoice
-
-    const { amount } = decodedInvoice
-    expect(amount).toBe(sats)
-  })
-
-  it("adds a public invoice", async () => {
-    const invoice = await Wallets.addInvoiceNoAmountForRecipient({
-      recipientWalletId: walletIdUsd,
-    })
-    if (invoice instanceof Error) throw invoice
-    const { paymentRequest: request } = invoice.lnInvoice
-    expect(request.startsWith("lnbcrt1")).toBeTruthy()
-
-    const result = await walletInvoices.findByPaymentHash(getHash(request))
-    if (result instanceof Error) throw result
-    const {
-      recipientWalletDescriptor: { id: walletId },
-      usdAmount,
-      selfGenerated,
-    } = result
-    expect(String(walletId)).toBe(String(walletIdUsd))
-    expect(selfGenerated).toBe(false)
-    expect(usdAmount?.amount).toBe(undefined)
-  })
 })
 
 describe("Wallet - rate limiting test", () => {
