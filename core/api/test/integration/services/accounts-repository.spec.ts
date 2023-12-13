a8cb9f064c1aa02751c5ecba772d0e36def1d12d
vindard <17693119+vindard@users.noreply.github.com>
2023-12-13 17:29:21 -0400

    test(core): refactor AccountsRepository 'findByUsername' tests

--- core/api/test/integration/services/accounts-repository.spec.ts
+++ core/api/test/integration/services/accounts-repository.spec.ts
@@ -1,36 +1,66 @@
+import { checkedToUsername } from "@/domain/accounts"
 import { AccountsRepository } from "@/services/mongoose"
+import { createRandomUserAndBtcWallet } from "test/helpers"
+
+const randomUsername = () => {
+  const num = Math.floor(Math.random() * 10 ** 4)
+  const username = checkedToUsername(`alice${num}`)
+  if (username instanceof Error) throw username
+  return username
+}
 
 const accounts = AccountsRepository()
-const username = "userA" as Username
-let accountIdA: AccountId
-
-beforeAll(async () => {
-  const account = await accounts.persistNew("user-id" as UserId)
-  if (account instanceof Error) throw account
-  accountIdA = account.id
-
-  account.username = username
-  await accounts.update(account)
-})
-
 describe("AccountsRepository", () => {
   describe("findByUsername", () => {
-    it("return true if username already exists", async () => {
+    it("returns valid account for username", async () => {
+      const username = randomUsername()
+
+      // Create user
+      const newWalletDescriptor = await createRandomUserAndBtcWallet()
+      const newAccount = await AccountsRepository().findById(
+        newWalletDescriptor.accountId,
+      )
+      if (newAccount instanceof Error) throw newAccount
+
+      // Set username
+      newAccount.username = username
+      const res = await accounts.update(newAccount)
+      expect(res).not.toBeInstanceOf(Error)
+
+      // Check that username was set
       const account = await accounts.findByUsername(username)
       if (account instanceof Error) throw account
-      expect(account.id).toStrictEqual(accountIdA)
+      expect(account.id).toStrictEqual(newAccount.id)
     })
 
-    it("return true for other capitalization", async () => {
+    it("returns valid account for other capitalization", async () => {
+      const username = randomUsername()
+
+      // Create user
+      const newWalletDescriptor = await createRandomUserAndBtcWallet()
+      const newAccount = await AccountsRepository().findById(
+        newWalletDescriptor.accountId,
+      )
+      if (newAccount instanceof Error) throw newAccount
+
+      // Set username
+      newAccount.username = username
+      const res = await accounts.update(newAccount)
+      expect(res).not.toBeInstanceOf(Error)
+
+      // Check that username query is case insensitive
       const account = await accounts.findByUsername(
         username.toLocaleUpperCase() as Username,
       )
       if (account instanceof Error) throw account
-      expect(account.id).toStrictEqual(accountIdA)
+      expect(account.id).toStrictEqual(newAccount.id)
     })
 
-    it("return false if username does not exist", async () => {
-      const account = await accounts.findByUsername("user" as Username)
+    it("errors if username does not exist", async () => {
+      const username = checkedToUsername("non_user")
+      if (username instanceof Error) throw username
+
+      const account = await accounts.findByUsername(username)
       expect(account).toBeInstanceOf(Error)
     })
   })
