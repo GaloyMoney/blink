2b7400921a65885e3fa13180a1574ad153f1801e
vindard <17693119+vindard@users.noreply.github.com>
2023-12-16 10:24:25 -0400

    test(core): setup ln-receive suite and move 1st case

--- bats/helpers/cli.bash
+++ bats/helpers/cli.bash
@@ -10,6 +10,14 @@ lnd_cli() {
       $@
 }
 
+lnd_outside_cli() {
+  docker exec "${COMPOSE_PROJECT_NAME}-lnd-outside-1-1" \
+    lncli \
+      --macaroonpath /root/.lnd/admin.macaroon \
+      --tlscertpath /root/.lnd/tls.cert \
+      $@
+}
+
 bria_cli() {
  docker exec "${COMPOSE_PROJECT_NAME}-bria-1" bria $@ 
 }
