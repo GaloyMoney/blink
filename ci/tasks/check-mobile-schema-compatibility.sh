ad954b694e9b975977f25e4a64d3995fab9b5351
nicolasburtey <nicolasburtey@users.noreply.github.com>
2023-12-20 23:44:56 +0000

    tests: fix script for checking mobile compatibility (#3736)
    * tests: fix script for checking mobile compatibility
    
    * chore: update script to use supergraphql.graphql instead of the local graphql
    
    ---------
    
    Co-authored-by: Nicolas Burtey <nb@galoy.io>

--- ci/tasks/check-mobile-schema-compatibility.sh
+++ ci/tasks/check-mobile-schema-compatibility.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 curl -fsS https://raw.githubusercontent.com/GaloyMoney/galoy-mobile/main/app/graphql/generated.gql -o main.gql
+curl -fsS https://raw.githubusercontent.com/GaloyMoney/galoy-mobile/main/supergraph.graphql -o supergraph.graphql
 
 echo Checking compatibility with current main branch of galoy-mobile
-npx @graphql-inspector/cli validate ./main.gql repo/core/api/src/graphql/public/schema.graphql --apollo --noStrictFragments
+npx @graphql-inspector/cli validate ./main.gql ./supergraph.graphql --apollo --noStrictFragments
+if [ $? -ne 0 ]; then
+  echo "Schema is not compatible with galoy-mobile"
+  exit 1
+fi
 
 for file in mobile-deployments/deployed-schemas/*
 do
   filename=$(basename "$file")
   echo Checking compatibility with ${filename%.*}
-  npx @graphql-inspector/cli validate $file repo/core/api/src/graphql/public/schema.graphql --apollo --noStrictFragments
+  npx @graphql-inspector/cli validate $file ./supergraph.graphql --apollo --noStrictFragments
+  if [ $? -ne 0 ]; then
+    echo "Schema is not compatible with ${filename%.*}"
+    exit 1
+  fi
 done
