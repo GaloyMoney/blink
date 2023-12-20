65118570a4588f5112f1ea69e8587e029eb694e6
Nicolas Burtey <nb@galoy.io>
2023-12-20 08:40:13 -0600

    tests: fix script for checking mobile compatibility

--- ci/tasks/check-mobile-schema-compatibility.sh
+++ ci/tasks/check-mobile-schema-compatibility.sh
@@ -4,10 +4,18 @@ curl -fsS https://raw.githubusercontent.com/GaloyMoney/galoy-mobile/main/app/gra
 
 echo Checking compatibility with current main branch of galoy-mobile
 npx @graphql-inspector/cli validate ./main.gql repo/core/api/src/graphql/public/schema.graphql --apollo --noStrictFragments
+if [ $? -ne 0 ]; then
+  echo "Schema is not compatible with galoy-mobile"
+  exit 1
+fi
 
 for file in mobile-deployments/deployed-schemas/*
 do
   filename=$(basename "$file")
   echo Checking compatibility with ${filename%.*}
   npx @graphql-inspector/cli validate $file repo/core/api/src/graphql/public/schema.graphql --apollo --noStrictFragments
+  if [ $? -ne 0 ]; then
+    echo "Schema is not compatible with ${filename%.*}"
+    exit 1
+  fi
 done
