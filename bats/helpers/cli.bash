88cc854580e78f2c18bea21b4ace30014590c69c
Arvin <17693119+vindard@users.noreply.github.com>
2023-12-22 11:21:16 -0400

    chore: debug concourse (#3747)
    * build: add callback server to Tiltfile
    
    * refactor: change svix extra-hosts name to 'bats-tests'
    
    * test(core): setup ln-receive suite and move 1st case
    
    * fix(core): add waits for lnd rpc endpoints to be ready
    
    * fix(core): source environment before running bats tests
    
    * fix(core): add missing redis_cli for balance_for_check
    
    * build(core): add init-lightning as api dependency
    
    * chore: add debug logging

--- bats/helpers/cli.bash
+++ bats/helpers/cli.bash
@@ -10,6 +10,14 @@ lnd_cli() {
       $@
 }
 
+lnd_outside_cli() {
+  docker exec "${COMPOSE_PROJECT_NAME}-lnd-outside-1-1" \
+    lncli \
+      --macaroonpath /root/.lnd/admin.macaroon \
+      --tlscertpath /root/.lnd/tls.cert \
+      $@
+}
+
 bria_cli() {
  docker exec "${COMPOSE_PROJECT_NAME}-bria-1" bria $@ 
 }
@@ -20,4 +28,8 @@ tilt_cli() {
 
 mongo_cli() {
   docker exec "${COMPOSE_PROJECT_NAME}-mongodb-1" mongosh --quiet mongodb://localhost:27017/galoy --eval $@
+}
+
+redis_cli() {
+  docker exec "${COMPOSE_PROJECT_NAME}-redis-1" redis-cli $@
 }
--- dev/bin/init-lightning.sh
+++ dev/bin/init-lightning.sh
@@ -24,6 +24,28 @@ retry() {
   false
 }
 
+run_with_lnd() {
+  local func_name="$1"
+  shift  # This will shift away the function name, so $1 becomes the next argument
+
+  if [[ "$func_name" == "lnd_cli" ]]; then
+    lnd_cli "$@"
+  elif [[ "$func_name" == "lnd_outside_cli" ]]; then
+    lnd_outside_cli "$@"
+  elif [[ "$func_name" == "lnd_outside_2_cli" ]]; then
+    lnd_outside_2_cli "$@"
+  else
+    echo "Invalid function name passed!" && return 1
+  fi
+}
+
+lnd_is_ready() {
+  lnd_cli_cmd="$1"
+  echo "Checking $lnd_cli_cmd is ready..."
+  run_with_lnd "$lnd_cli_cmd" getinfo > /dev/null 2>&1
+  return "$?"
+}
+
 mempool_not_empty() {
   echo "Waiting for txn to show up in mempool..."
   local txid="$(bitcoin_cli getrawmempool | jq -r ".[0]")"
@@ -56,6 +78,7 @@ bitcoin_cli -generate "${block_rewards}" > /dev/null 2>&1
 echo "Funded with "${block_rewards}" block rewards"
 
 echo "Funding 'lndOutside1' node..."
+retry 30 1 lnd_is_ready lnd_outside_cli
 amount="1"
 address="$(lnd_outside_cli newaddress p2wkh | jq -r '.address')"
 local_amount="10000000"
@@ -66,6 +89,7 @@ echo "Funded with txid: ${funded_txid}"
 
 # Open channel from lndoutside1 -> lnd1
 echo "Opening channel: lndoutside1 -> lnd1..."
+retry 30 1 lnd_is_ready lnd_cli
 pubkey_lnd1="$(lnd_cli getinfo | jq -r '.identity_pubkey')"
 endpoint_lnd1="${COMPOSE_PROJECT_NAME}-lnd1-1:9735"
 lnd_outside_cli connect "${pubkey_lnd1}@${endpoint_lnd1}" > /dev/null 2>&1 || true
@@ -87,6 +111,7 @@ echo "Channel ${funding_txid} confirmed"
 
 # Open channel with push from lndoutside1 -> lndoutside2
 echo "Opening balanced channel: lndoutside1 -> lndoutside2..."
+retry 30 1 lnd_is_ready lnd_outside_2_cli
 pubkey_lnd_outside_2="$(lnd_outside_2_cli getinfo | jq -r '.identity_pubkey')"
 endpoint_lnd_outside_2="${COMPOSE_PROJECT_NAME}-lnd-outside-2-1:9735"
 lnd_outside_cli connect "${pubkey_lnd_outside_2}@${endpoint_lnd_outside_2}" > /dev/null 2>&1 || true
