0193ce5fc7c112463c5cb8c52c09d8bb1cf084fd
vindard <17693119+vindard@users.noreply.github.com>
2024-01-09 17:49:01 -0400

    test(core): move wallet-from-non-existent-account test to wallets-repo tests

--- core/api/test/legacy-integration/app/accounts/multiple-wallet-account.spec.ts
+++ core/api/test/legacy-integration/app/accounts/multiple-wallet-account.spec.ts
@@ -1,9 +1,6 @@
-import mongoose from "mongoose"
-
 import { Accounts } from "@/app"
 import { WalletCurrency } from "@/domain/shared"
 import { WalletType } from "@/domain/wallets"
-import { WalletsRepository } from "@/services/mongoose"
 
 import {
   createUserAndWalletFromPhone,
@@ -11,18 +8,6 @@ import {
   getUsdWalletIdByPhone,
   randomPhone,
 } from "test/helpers"
-
-it("fail to create an invalid account", async () => {
-  const id = new mongoose.Types.ObjectId()
-
-  const newWallet = await WalletsRepository().persistNew({
-    accountId: id as unknown as AccountId,
-    type: WalletType.Checking,
-    currency: WalletCurrency.Btc,
-  })
-
-  expect(newWallet).toBeInstanceOf(Error)
-})
 
 it("does not add a usd wallet if one exists", async () => {
   const phone = randomPhone()
