9bc63859728b9952b5d77a5c4a5730d7939f8a62
Justin Carter <justin@galoy.io>
2024-01-10 11:21:23 +0100

    refactor(api-keys): extract es-entity-rs boilerplate (#3808)
    * chore: notifications boilerplate
    
    * chore: es-entity-rs boilerplate
    
    * chore(es-entity): add EntityError w deps
    
    * chore: use es-entity in api-keys
    
    * fix(api-keys): add es-entity dep to test

--- core/notifications/src/lib.rs
+++ core/notifications/src/lib.rs
@@ -1,9 +1,4 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![cfg_attr(feature = "fail-on-warnings", deny(clippy::all))]
 
-pub mod app;
-pub mod cli;
-pub mod graphql;
-pub mod identity;
-pub mod scope;
-pub mod server;
+mod settings;
