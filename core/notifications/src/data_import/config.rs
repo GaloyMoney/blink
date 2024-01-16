07f0f25818c1e90e4cd0a319a64a49b0e9682104
bodymindarts <justin@galoy.io>
2024-01-16 11:17:37 +0100

    chore(notifications): remove direct_connection option

--- core/notifications/src/data_import/config.rs
+++ core/notifications/src/data_import/config.rs
@@ -2,8 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct MongoImportConfig {
-    /// This is to connect via port forwarding when testing against staging
-    pub direct_connection: Option<bool>,
     pub execute_import: bool,
     pub connection: String,
 }
@@ -11,7 +9,6 @@ pub struct MongoImportConfig {
 impl Default for MongoImportConfig {
     fn default() -> Self {
         Self {
-            direct_connection: None,
             execute_import: false,
             connection: "mongodb://localhost:27017/galoy".to_string(),
         }
--- core/notifications/src/data_import/mongodb.rs
+++ core/notifications/src/data_import/mongodb.rs
@@ -6,7 +6,6 @@ use mongodb::{
 
 pub async fn get_client(mongodb_config: MongoImportConfig) -> anyhow::Result<Client> {
     let mut client_options = ClientOptions::parse(mongodb_config.connection).await?;
-    client_options.direct_connection = Some(mongodb_config.direct_connection.unwrap_or(false));
     client_options.selection_criteria = Some(SelectionCriteria::ReadPreference(
         ReadPreference::SecondaryPreferred {
             options: ReadPreferenceOptions::default(),
