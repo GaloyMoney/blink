de16ccbb98ba36f30f0792c602eb1ce91e9ce242
Vaibhav <thevaibhavdixit@gmail.com>
2024-01-16 17:09:31 +0530

    chore(notifications): derive Default on MongoImportConfig

--- core/notifications/src/data_import/config.rs
+++ core/notifications/src/data_import/config.rs
@@ -1,16 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Default, Debug, Serialize, Deserialize)]
 pub struct MongoImportConfig {
     pub execute_import: bool,
     pub connection: Option<String>,
-}
-
-impl Default for MongoImportConfig {
-    fn default() -> Self {
-        Self {
-            execute_import: false,
-            connection: None,
-        }
-    }
 }
