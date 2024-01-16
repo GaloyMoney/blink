0e539da8313bb4bf64c91bd3b69c9bbfe1488696
Kartik Shah <krtk.6160@gmail.com>
2024-01-16 23:21:08 +0530

    ci: improve core diff (#3844)
    * ci: improve core diff
    
    * ci: iterate on script
    
    * ci: remove debug echo

--- ci/core/tasks/open-core-bundle-charts-pr.sh
+++ ci/core/tasks/open-core-bundle-charts-pr.sh
@@ -33,7 +33,7 @@ git remote set-url origin ${github_url}
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 
 git checkout ${ref}
-app_src_files=($(buck2 uquery 'inputs(deps("//core/..."))' 2>/dev/null))
+app_src_files=($(buck2 uquery 'inputs(deps("//core/api:")) + inputs(deps("//core/api-ws-server:")) + inputs(deps("//core/api-trigger:")) + inputs(deps("//core/api-exporter:")) + inputs(deps("//core/api-cron:"))' 2>/dev/null))
 
 declare -A relevant_commits
 relevant_commits=()
