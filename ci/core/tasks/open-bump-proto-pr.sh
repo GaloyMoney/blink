cb18038b73a90330cae2f067a43f0dd8e902e702
Kartik Shah <krtk.6160@gmail.com>
2024-01-17 23:02:51 +0530

    ci: update proto sync pipeline and vars

--- ci/core/tasks/open-bump-proto-pr.sh
+++ ci/core/tasks/open-bump-proto-pr.sh
@@ -7,7 +7,7 @@ cd repo
 gh pr close ${BOT_BRANCH} || true
 git checkout ${BRANCH}
 gh pr create \
-  --title "chore(deps): bump ${MODULE} proto" \
+  --title "chore(deps): bump ${PROTO_NAME} proto" \
   --body "" \
   --base ${BRANCH} \
   --head ${BOT_BRANCH} \
