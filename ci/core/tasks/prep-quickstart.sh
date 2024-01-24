1f54ab3c0acda2f05d6223c8dbf9bb83a2459e40
Justin Carter <justin@galoy.io>
2024-01-24 14:35:48 +0100

    chore(quickstart): add notifications to quickstart (#3896)
    * chore(quickstart): add notifications
    
    * chore(notifications): handle optional path

--- ci/core/tasks/prep-quickstart.sh
+++ ci/core/tasks/prep-quickstart.sh
@@ -2,6 +2,7 @@
 
 export api_digest=$(cat ./api-edge-image/digest | sed 's/:/@/')
 export trigger_digest=$(cat ./api-trigger-edge-image/digest | sed 's/:/@/')
+export notifications_digest=$(cat ./notifications-edge-image/digest | sed 's/:/@/')
 export migrate_digest=$(cat ./api-migrate-edge-image/digest | sed 's/:/@/')
 
 pushd repo/quickstart || exit 1
@@ -10,6 +11,7 @@ pushd repo/quickstart || exit 1
 
 ./bin/bump-galoy-image-digest.sh "api" "$api_digest"
 ./bin/bump-galoy-image-digest.sh "trigger" "$trigger_digest"
+./bin/bump-galoy-image-digest.sh "notifications" "$notifications_digest"
 ./bin/bump-mongodb-migrate-image-digest.sh "$migrate_digest"
 ./bin/re-render.sh
 
