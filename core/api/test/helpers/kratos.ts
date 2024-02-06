9625f241a03c4edfd1adac7c51d51bbeb404b80e
vindard <17693119+vindard@users.noreply.github.com>
2024-02-05 20:44:46 -0400

    chore(core): cleanup unused 'kratos' helpers

--- core/api/test/helpers/kratos.ts
+++ core/api/test/helpers/kratos.ts
@@ -30,31 +30,3 @@ export const getEmailCode = async (email: EmailAddress) => {
   const code = message.body.split("code:\n\n")[1].slice(0, 6)
   return code
 }
-
-export const getEmailCount = async (email: EmailAddress) => {
-  const knex = getKratosKnex()
-
-  const table = "courier_messages"
-
-  // make the query
-  const res = await knex.select(["recipient", "body", "created_at"]).from(table)
-
-  await knex.destroy()
-
-  const count = res.filter((item) => item.recipient === email).length
-
-  return count
-}
-
-export const removeIdentities = async () => {
-  const knex = getKratosKnex()
-
-  const table = "identities"
-
-  // truncate the table
-  const resTruncate = await getKratosKnex()(table).truncate()
-
-  knex.destroy()
-
-  console.log({ resTruncate })
-}
