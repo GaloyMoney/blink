ca20f97e8d59905ea4e665ec9111fbedfc3a6fed
vindard <17693119+vindard@users.noreply.github.com>
2024-02-07 17:09:35 -0400

    refactor: move 'getNextPageToken' to services layer

--- core/api/test/unit/domain/authentication/index.spec.ts
+++ core/api/test/unit/domain/authentication/index.spec.ts
@@ -1,4 +1,4 @@
-import { getNextPageToken, getSupportedCountries } from "@/domain/authentication"
+import { getSupportedCountries } from "@/domain/authentication"
 
 describe("getSupportedCountries", () => {
   it("returns supported countries", () => {
@@ -18,21 +18,5 @@ describe("getSupportedCountries", () => {
         supportedAuthChannels: ["sms"],
       },
     ])
-  })
-})
-
-describe("decoding link header", () => {
-  const withNext =
-    '</admin/clients?page_size=5&page_token=euKoY1BqY3J8GVax>; rel="first",</admin/clients?page_size=5&page_token=h9LfEKUiFoLH2R0A>; rel="next"'
-
-  const withoutNext =
-    '</admin/clients?page_size=5&page_token=euKoY1BqY3J8GVax>; rel="first"'
-
-  it("try decoding link successfully", () => {
-    expect(getNextPageToken(withNext)).toBe("h9LfEKUiFoLH2R0A")
-  })
-
-  it("should be undefined when no more next is present", () => {
-    expect(getNextPageToken(withoutNext)).toBe(undefined)
   })
 })
