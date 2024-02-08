9bc955824b8d7cc3f939acf7fc4d5ceaacf04ba4
vindard <17693119+vindard@users.noreply.github.com>
2024-02-08 11:03:35 -0400

    refactor: remove kratos file from spelling config

--- core/api/test/unit/services/kratos/identity.spec.ts
+++ core/api/test/unit/services/kratos/identity.spec.ts
@@ -1,14 +1,14 @@
 import { getNextPageToken } from "@/services/kratos"
 
 describe("decoding link header", () => {
-  const withNext =
-    '</admin/clients?page_size=5&page_token=euKoY1BqY3J8GVax>; rel="first",</admin/clients?page_size=5&page_token=h9LfEKUiFoLH2R0A>; rel="next"'
+  const token = "h9LfEKUiFoLH2R0A"
+  const withNext = `</admin/clients?page_size=5&page_token=euKoY1BqY3J8GVax>; rel="first",</admin/clients?page_size=5&page_token=${token}>; rel="next"`
 
   const withoutNext =
     '</admin/clients?page_size=5&page_token=euKoY1BqY3J8GVax>; rel="first"'
 
   it("try decoding link successfully", () => {
-    expect(getNextPageToken(withNext)).toBe("h9LfEKUiFoLH2R0A")
+    expect(getNextPageToken(withNext)).toBe(token)
   })
 
   it("should be undefined when no more next is present", () => {
