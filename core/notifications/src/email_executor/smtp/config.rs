9067ae3b52c8c26ea98650960f4a1228359a80cf
Vaibhav <thevaibhavdixit@gmail.com>
2024-02-20 16:10:07 +0530

    chore(notifications): use serde_default for email_config (#4036)
    * chore(notifications): use serde_default for email_config
    
    * chore(notifications): use default smtp port

--- core/notifications/src/email_executor/smtp/config.rs
+++ core/notifications/src/email_executor/smtp/config.rs
@@ -2,10 +2,18 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Default, Debug, Deserialize, Serialize)]
 pub struct SmtpConfig {
+    #[serde(default)]
     pub username: String,
     #[serde(default)]
     pub password: String,
+    #[serde(default)]
     pub from_email: String,
+    #[serde(default)]
     pub relay: String,
+    #[serde(default = "default_smtp_port")]
     pub port: u16,
+}
+
+fn default_smtp_port() -> u16 {
+    587
 }
