901986b1deeee3e2110c945b5570457f3cdeacef
bodymindarts <justin@galoy.io>
2024-02-23 08:46:57 +0100

    fix(api-keys): migration must set interim default

--- core/api-keys/migrations/20231215145636_add_scopes_column.sql
+++ core/api-keys/migrations/20231215145636_add_scopes_column.sql
@@ -1,9 +1,12 @@
 ALTER TABLE identity_api_keys
-ADD COLUMN scopes VARCHAR[] NOT NULL;
+ADD COLUMN scopes VARCHAR[] NOT NULL DEFAULT ARRAY['read'];
+
 UPDATE identity_api_keys
-SET scopes = CASE
-    WHEN read_only = true THEN ARRAY['read']
-    ELSE ARRAY['read', 'write']
-END;
+SET scopes = ARRAY['read', 'write']
+WHERE read_only = false;
+
+ALTER TABLE identity_api_keys
+ALTER COLUMN scopes DROP DEFAULT;
+
 ALTER TABLE identity_api_keys
 DROP COLUMN read_only;
