3168463f53eec1be795d5ed422f57c5b79907b79
Justin Carter <justin@galoy.io>
2024-02-23 16:23:06 +0100

    refactor(notifications): more expressive error on ConcurrentModification (#4066)

--- core/notifications/src/user_notification_settings/error.rs
+++ core/notifications/src/user_notification_settings/error.rs
@@ -3,7 +3,24 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum UserNotificationSettingsError {
     #[error("UserNotificationSettingsError - Sqlx: {0}")]
-    Sqlx(#[from] sqlx::Error),
+    Sqlx(sqlx::Error),
     #[error("UserNotificationSettingsError - EntityError: {0}")]
     EntityError(#[from] es_entity::EntityError),
+    #[error("UserNotificationSettingsError - ConcurrentModification")]
+    ConcurrentModification,
+}
+
+impl From<sqlx::Error> for UserNotificationSettingsError {
+    fn from(error: sqlx::Error) -> Self {
+        if let Some(err) = error.as_database_error() {
+            if let Some(constraint) = err.constraint() {
+                if constraint.contains("duplicate key value violates unique constraint")
+                    && constraint.contains("events_id_sequence_key")
+                {
+                    return Self::ConcurrentModification;
+                }
+            }
+        }
+        Self::Sqlx(error)
+    }
 }
