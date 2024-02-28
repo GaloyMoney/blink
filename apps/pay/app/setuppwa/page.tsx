ef4a9a7f9d821274f26186fdf44216652e497789
Siddharth Tiwari <siddharthtiwarikreplind@gmail.com>
2024-02-28 16:04:08 +0530

    chore(pay): updated setup page and currency dropdown

--- apps/pay/app/setuppwa/page.tsx
+++ apps/pay/app/setuppwa/page.tsx
@@ -45,36 +45,53 @@ const SetupPwa = () => {
 
   if (!usernameFromLocal) {
     return (
-      <div className="setup-pwa">
-        <form className="username-form" autoComplete="off" onSubmit={handleSubmit}>
-          <h4>Welcome to Blink POS application.</h4>
-          <label htmlFor="username">
-            To use the app, enter the Blink username you would like to receive payments
-            for.
-          </label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              setUsername(event.target.value)
-            }
-            placeholder="username"
-            required
-          />
-          <label htmlFor="display" style={{ alignSelf: "flex-start" }}>
-            Enter your currency
-          </label>
-          <Suspense>
-            <CurrencyDropdown
-              name="display"
-              style={{ height: "42px", width: "100%" }}
-              onSelectedDisplayCurrencyChange={(newDisplayCurrency) => {
-                setSelectedDisplayCurrency(newDisplayCurrency)
-              }}
-            />
-          </Suspense>
-          <button>Submit</button>
+      <div
+        className="flex flex-col justify-center items-center h-screen"
+        style={{ maxWidth: "90%", margin: "0 auto" }}
+      >
+        <h4>Welcome to Blink POS application.</h4>
+
+        <form
+          className="flex flex-col justify-center items-center"
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col justify-center items-center w-full gap-2 rounded-md ">
+            <p>
+              To use the app, enter the Blink username you would like to receive payments
+              for.
+            </p>
+            <div className="flex flex-col w-full">
+              <label className="m-1" htmlFor="username">
+                Blink username
+              </label>
+              <input
+                className="w-full p-1.5 border-2 rounded-md bg-[var(--lighterGrey)]"
+                type="text"
+                name="username"
+                value={username}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  setUsername(event.target.value)
+                }
+                placeholder="username"
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label className="m-1" htmlFor="display">
+                Currency
+              </label>
+              <Suspense fallback={<div>Loading...</div>}>
+                <CurrencyDropdown
+                  name="display"
+                  onSelectedDisplayCurrencyChange={(newDisplayCurrency) => {
+                    setSelectedDisplayCurrency(newDisplayCurrency)
+                  }}
+                />
+              </Suspense>
+            </div>
+          </div>
+          <button className="print-paycode-button w-1/2 m-2">Submit</button>
         </form>
       </div>
     )
--- apps/pay/components/currency/currency-dropdown.tsx
+++ apps/pay/components/currency/currency-dropdown.tsx
@@ -1,79 +1,108 @@
 "use client"
-import React, { useEffect } from "react"
-
+import React, { useEffect, useState } from "react"
+import Select, {
+  CSSObjectWithLabel,
+  SingleValue,
+  StylesConfig,
+  OptionProps,
+} from "react-select"
 import { useSearchParams } from "next/navigation"
 
 import { useCurrencyListQuery } from "../../lib/graphql/generated"
 
-export default function CurrencyDropdown({
-  onSelectedDisplayCurrencyChange,
-  name,
-  showOnlyFlag = false,
-}: {
+type OptionType = {
+  value: string
+  label: string
+}
+
+type CurrencyDropdownProps = {
   onSelectedDisplayCurrencyChange?: (newDisplayCurrency: string) => void
   name?: string
   style?: React.CSSProperties
   showOnlyFlag?: boolean
-}) {
+}
+
+const CurrencyDropdown: React.FC<CurrencyDropdownProps> = ({
+  onSelectedDisplayCurrencyChange,
+  name,
+  showOnlyFlag = false,
+}) => {
   const searchParams = useSearchParams()
   const display = searchParams?.get("display")
 
   const { data: currencyData } = useCurrencyListQuery()
 
-  const [selectedDisplayCurrency, setSelectedDisplayCurrency] = React.useState("USD")
-  const [isDropDownOpen, setIsDropDownOpen] = React.useState(false)
+  const [selectedOption, setSelectedOption] = useState<OptionType | null>(null)
 
   useEffect(() => {
-    const newDisplay =
-      display && typeof display === "string"
-        ? display
-        : localStorage.getItem("display") ?? "USD"
-    setSelectedDisplayCurrency(newDisplay)
+    const defaultDisplay = display || localStorage.getItem("display") || "USD"
+    const initialCurrency = currencyData?.currencyList?.find(
+      ({ id }) => id === defaultDisplay,
+    )
+    setSelectedOption(
+      initialCurrency
+        ? {
+            value: initialCurrency.id,
+            label: `${initialCurrency.id} - ${initialCurrency.name}`,
+          }
+        : null,
+    )
+  }, [display, currencyData?.currencyList])
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  const handleOnChange = (option: SingleValue<OptionType>) => {
+    const newDisplayCurrency = option ? option.value : "USD"
+    setSelectedOption(option as OptionType)
+    if (onSelectedDisplayCurrencyChange) {
+      onSelectedDisplayCurrencyChange(newDisplayCurrency)
+    }
+  }
+
+  const options: OptionType[] =
+    currencyData?.currencyList?.map((currency) => ({
+      value: currency.id,
+      label:
+        showOnlyFlag && currency.flag
+          ? `${currency.flag}`
+          : `${currency.id} - ${currency.name} ${currency.flag}`,
+    })) || []
+
+  const customStyles: StylesConfig<OptionType, false> = {
+    container: (base: CSSObjectWithLabel) => {
+      return {
+        ...base,
+        width: "100%",
+      } as CSSObjectWithLabel
+    },
+    control: (base: CSSObjectWithLabel) => {
+      return {
+        ...base,
+        backgroundColor: "var(--lighterGrey)",
+        border: "none",
+        boxShadow: "none",
+      } as CSSObjectWithLabel
+    },
+    option: (base: CSSObjectWithLabel, state: OptionProps<OptionType, false>) => {
+      return {
+        ...base,
+        backgroundColor: state.isFocused ? "var(--primaryColor)" : undefined,
+        color: state.isFocused ? "white" : "black",
+      } as CSSObjectWithLabel
+    },
+  }
 
   return (
-    // TODO auto-complete input can be better here Instead of select dropdown
-    <select
-      className="bg-slate-200 border-none p-2 w-20 rounded-md"
+    <Select
+      className="react-select-container"
+      classNamePrefix="react-select"
       name={name ?? "display"}
-      required
-      value={selectedDisplayCurrency}
-      onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-        const currencyId = event.target.value
-        const newDisplayCurrency = currencyData?.currencyList?.find(
-          (item) => item.id === currencyId,
-        )
-        if (newDisplayCurrency) {
-          setSelectedDisplayCurrency(newDisplayCurrency.id)
-        }
-        if (onSelectedDisplayCurrencyChange) {
-          onSelectedDisplayCurrencyChange(newDisplayCurrency?.id ?? "USD")
-        }
-        setIsDropDownOpen(false)
-      }}
-      onFocus={() => {
-        setIsDropDownOpen(true)
-      }}
-      onBlur={() => {
-        setIsDropDownOpen(false)
-      }}
-    >
-      {currencyData?.currencyList?.map((option) => {
-        const fullLabel = `${option.id} - ${option.name} ${
-          option.flag ? option.flag : ""
-        }`
-        const flagOnlyLabel = option.flag ? option.flag : option.id
-        const isSelected = selectedDisplayCurrency === option.id
-        return (
-          <option key={option.id} value={option.id}>
-            {isDropDownOpen && fullLabel}
-            {!isDropDownOpen &&
-              (isSelected ? (showOnlyFlag ? flagOnlyLabel : option.id) : fullLabel)}
-          </option>
-        )
-      })}
-    </select>
+      value={selectedOption}
+      onChange={handleOnChange}
+      options={options}
+      isSearchable
+      placeholder="Select a currency..."
+      styles={customStyles}
+    />
   )
 }
+
+export default CurrencyDropdown
