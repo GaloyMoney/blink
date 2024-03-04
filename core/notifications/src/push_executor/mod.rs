9c77df82ed1d650331979d70b21f9cf514fb27ac
Vaibhav <thevaibhavdixit@gmail.com>
2024-03-05 00:44:32 +0530

    chore(notifications): handle SenderIdMismatch error (#4124)
    * chore(notifications): handle SenderIdMismatch error
    
    * chore(notifications): single StaleDeviceToken error

--- core/notifications/src/push_executor/mod.rs
+++ core/notifications/src/push_executor/mod.rs
@@ -54,7 +54,7 @@ impl PushExecutor {
         let mut n_deliveries = 0;
         for device_token in settings.push_device_tokens() {
             match self.fcm.send(&device_token, &msg, event.deep_link()).await {
-                Err(FcmError::UnrecognizedDeviceToken(e) | FcmError::InvalidDeviceToken(e)) => {
+                Err(FcmError::StaleDeviceToken(e)) => {
                     n_errs += 1;
                     n_removed_tokens += 1;
                     error!("BadRequest sending to device: {}", e);
