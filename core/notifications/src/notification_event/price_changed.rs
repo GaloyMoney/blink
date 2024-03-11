3a5c376a00df18b42a83c2d8c1066e0ce6e53870
Vaibhav <thevaibhavdixit@gmail.com>
2024-03-11 17:02:10 +0530

    chore(deps): bump chrono in third-party rust

--- core/notifications/src/notification_event/price_changed.rs
+++ core/notifications/src/notification_event/price_changed.rs
@@ -40,7 +40,7 @@ pub struct PriceChanged {
 
 impl PriceChanged {
     const NOTIFICATION_THRESHOLD: ChangePercentage = ChangePercentage(5.0);
-    const COOL_OFF_PERIOD: chrono::Duration = chrono::Duration::days(2);
+    const COOL_OFF_PERIOD: Option<chrono::Duration> = chrono::Duration::try_days(2);
 
     pub fn should_notify(&self, last_trigger: Option<chrono::DateTime<chrono::Utc>>) -> bool {
         if self.change_percent >= Self::NOTIFICATION_THRESHOLD
@@ -49,7 +49,8 @@ impl PriceChanged {
             match last_trigger {
                 Some(last_trigger_time) => {
                     let time_since_last_trigger = chrono::Utc::now() - last_trigger_time;
-                    time_since_last_trigger > Self::COOL_OFF_PERIOD
+                    time_since_last_trigger
+                        > Self::COOL_OFF_PERIOD.expect("COOL_OFF_PERIOD should always be valid")
                 }
                 None => true,
             }
