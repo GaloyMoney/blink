86c781063197f3e45d27daa619922e4faf024f48
Arvin <17693119+vindard@users.noreply.github.com>
2024-04-04 04:15:08 -0400

    refactor(core): move notification steps out of payment send locks (#4232)
    * refactor(core): move notifications steps outside ln-intraledger lock
    
    * refactor(core): remove recipient user fetch step
    
    * refactor(core): move notifications steps outside ln-external lock
    
    * refactor: move notifications steps outside intraledger lock
    
    * refactor: remove unnecessary user fetch from ln-send
    
    * fix: send notification if pay-attempt errors on service
    
    * refactor(core): move notitification out of onchain-intraledger lock
    
    * refactor(core): move notitification out of onchain-external lock

--- core/api/src/app/payments/index.types.d.ts
+++ core/api/src/app/payments/index.types.d.ts
@@ -2,3 +2,44 @@ type PaymentSendResult = {
   status: PaymentSendStatus
   transaction: WalletTransaction
 }
+
+type PaymentSendAttemptResultTypeObj =
+  typeof import("./ln-send-result").PaymentSendAttemptResultType
+type PaymentSendAttemptResultType =
+  PaymentSendAttemptResultTypeObj[keyof PaymentSendAttemptResultTypeObj]
+
+type IntraLedgerSendAttemptResult =
+  | {
+      type: PaymentSendAttemptResultTypeObj["Ok"]
+      journalId: LedgerJournalId
+    }
+  | {
+      type: PaymentSendAttemptResultTypeObj["AlreadyPaid"]
+    }
+  | {
+      type: PaymentSendAttemptResultTypeObj["Error"]
+      error: ApplicationError
+    }
+
+type LnSendAttemptResult =
+  | {
+      type: PaymentSendAttemptResultTypeObj["Ok"]
+      journalId: LedgerJournalId
+    }
+  | {
+      type: PaymentSendAttemptResultTypeObj["Pending"]
+      journalId: LedgerJournalId
+    }
+  | {
+      type: PaymentSendAttemptResultTypeObj["AlreadyPaid"]
+      journalId: LedgerJournalId
+    }
+  | {
+      type: PaymentSendAttemptResultTypeObj["ErrorWithJournal"]
+      journalId: LedgerJournalId
+      error: ApplicationError
+    }
+  | {
+      type: PaymentSendAttemptResultTypeObj["Error"]
+      error: ApplicationError
+    }
