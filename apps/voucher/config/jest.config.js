e398067a7c378a6cacf7921e969af714096cfb1e
siddharth <siddharthtiwarikreplind@gmail.com>
2024-04-12 02:17:47 +0530

    chore(voucher): removed unused files

--- apps/voucher/config/jest.config.js
+++ apps/voucher/config/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   roots: ["<rootDir>/test/unit"],
   rootDir: "../",
   testEnvironment: "node",
-  // setupFilesAfterEnv: ["<rootDir>/config/jest.setup.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
   },
