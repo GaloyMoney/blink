bf3e4698251931882f28174b987d5243504f1fba
Vaibhav <thevaibhavdixit@gmail.com>
2024-04-15 01:55:03 +0530

    feat(notifications): bitcoin price down notification

--- core/notifications/src/notification_event/price_changed.rs
+++ core/notifications/src/notification_event/price_changed.rs
@@ -44,9 +44,7 @@ impl PriceChanged {
     const COOL_OFF_PERIOD: chrono::Duration = chrono::Duration::days(2);
 
     pub fn should_notify(&self, last_trigger: Option<chrono::DateTime<chrono::Utc>>) -> bool {
-        if self.change_percent >= Self::NOTIFICATION_THRESHOLD
-            && self.direction == PriceChangeDirection::Up
-        {
+        if self.change_percent >= Self::NOTIFICATION_THRESHOLD {
             match last_trigger {
                 Some(last_trigger_time) => {
                     let time_since_last_trigger = chrono::Utc::now() - last_trigger_time;
@@ -71,11 +69,15 @@ impl NotificationEvent for PriceChanged {
     }
 
     fn to_localized_push_msg(&self, locale: GaloyLocale) -> LocalizedPushMessage {
-        let title = t!("price_changed.title", locale = locale.as_ref()).to_string();
+        let (title_key, body_key) = match self.direction {
+            PriceChangeDirection::Up => ("price_changed.up.title", "price_changed.up.body"),
+            PriceChangeDirection::Down => ("price_changed.down.title", "price_changed.down.body"),
+        };
+        let title = t!(title_key, locale = locale.as_ref()).to_string();
         let body = t!(
-            "price_changed.body",
+            body_key,
             locale = locale.as_ref(),
-            percent_increase = format!("{:.1}", self.change_percent.0),
+            change_percent = format!("{:.1}", self.change_percent.0),
             price = self.price.to_string()
         )
         .to_string();
@@ -112,19 +114,6 @@ mod tests {
     }
 
     #[test]
-    fn price_changed_should_not_notify_on_price_drops() {
-        let price_changed = PriceChanged {
-            price: PriceOfOneBitcoin {
-                minor_units: 100_000,
-                currency: "USD".parse().unwrap(),
-            },
-            direction: PriceChangeDirection::Down,
-            change_percent: ChangePercentage(5.1),
-        };
-        assert!(!price_changed.should_notify(None));
-    }
-
-    #[test]
     fn price_changed_should_not_notify_when_percentage_under_threshold() {
         let price_changed = PriceChanged {
             price: PriceOfOneBitcoin {
@@ -151,7 +140,7 @@ mod tests {
     }
 
     #[test]
-    fn price_changed_push_notification() {
+    fn price_changed_up_push_notification() {
         let price_changed = PriceChanged {
             price: PriceOfOneBitcoin {
                 minor_units: 100_000,
@@ -166,6 +155,25 @@ mod tests {
         assert_eq!(
             localized_message.body,
             "Bitcoin is up 5.1% in the last day to $1,000!"
+        );
+    }
+
+    #[test]
+    fn price_changed_down_push_notification() {
+        let price_changed = PriceChanged {
+            price: PriceOfOneBitcoin {
+                minor_units: 100_000,
+                currency: "USD".parse().unwrap(),
+            },
+            direction: PriceChangeDirection::Down,
+            change_percent: ChangePercentage(5.13),
+        };
+        let localized_message =
+            price_changed.to_localized_push_msg(GaloyLocale::from("en".to_string()));
+        assert_eq!(localized_message.title, "Bitcoin is on the move!");
+        assert_eq!(
+            localized_message.body,
+            "Bitcoin is down 5.1% in the last day to $1,000!"
         );
     }
 }
