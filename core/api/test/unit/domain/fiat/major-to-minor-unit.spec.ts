1c4ea28ce564377c9660c80567aa8320530e4c0e
Juan P Lopez <dolcalmi@gmail.com>
2024-04-14 10:46:11 -0500

    fix(core): add bignumber to solve js math issues (#4290)

--- core/api/test/unit/domain/fiat/major-to-minor-unit.spec.ts
+++ core/api/test/unit/domain/fiat/major-to-minor-unit.spec.ts
@@ -2,14 +2,23 @@ import { majorToMinorUnit } from "@/domain/fiat"
 
 describe("majorToMinorUnit", () => {
   it("should handle float correctly", () => {
-    const amount = 68.85
+    let amount = 68.85
     let displayCurrency = "USD" as DisplayCurrency
     let result = majorToMinorUnit({ amount, displayCurrency })
     expect(result).toBe(6885)
 
+    amount = 0.000638418984375
+    result = majorToMinorUnit({ amount, displayCurrency })
+    expect(result).toBe(0.0638418984375)
+
+    amount = 68.85
     displayCurrency = "JPY" as DisplayCurrency
     result = majorToMinorUnit({ amount, displayCurrency })
     expect(result).toBe(68.85)
+
+    amount = 0.000638418984375
+    result = majorToMinorUnit({ amount, displayCurrency })
+    expect(result).toBe(0.000638418984375)
   })
 
   it("should handle bigint correctly", () => {
