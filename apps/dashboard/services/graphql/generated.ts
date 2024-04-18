325f42ca43ebf689eb7bf2a9012348733e5c5483
bodymindarts <justin@galoy.io>
2024-04-18 15:56:08 +0200

    fix(notifications): update schema

--- apps/dashboard/services/graphql/generated.ts
+++ apps/dashboard/services/graphql/generated.ts
@@ -1730,7 +1730,7 @@ export type SettlementViaOnChain = {
 
 export type StatefulNotification = {
   readonly __typename: 'StatefulNotification';
-  readonly acknowledgeAt?: Maybe<Scalars['Timestamp']['output']>;
+  readonly acknowledgedAt?: Maybe<Scalars['Timestamp']['output']>;
   readonly body: Scalars['String']['output'];
   readonly createdAt: Scalars['Timestamp']['output'];
   readonly deepLink?: Maybe<Scalars['String']['output']>;
@@ -4671,7 +4671,7 @@ export interface SignedDisplayMajorAmountScalarConfig extends GraphQLScalarTypeC
 }
 
 export type StatefulNotificationResolvers<ContextType = any, ParentType extends ResolversParentTypes['StatefulNotification'] = ResolversParentTypes['StatefulNotification']> = {
-  acknowledgeAt?: Resolver<Maybe<ResolversTypes['Timestamp']>, ParentType, ContextType>;
+  acknowledgedAt?: Resolver<Maybe<ResolversTypes['Timestamp']>, ParentType, ContextType>;
   body?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   createdAt?: Resolver<ResolversTypes['Timestamp'], ParentType, ContextType>;
   deepLink?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
