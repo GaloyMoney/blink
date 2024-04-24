df0a730ad9ea642f4dc497082661c0ca92f961c3
Siddharth <siddharthtiwarikreplind@gmail.com>
2024-04-24 13:50:45 +0530

    chore(voucher): passing env from server (#4347)

--- apps/voucher/app/withdraw/[voucher-secret]/page.tsx
+++ apps/voucher/app/withdraw/[voucher-secret]/page.tsx
@@ -1,15 +1,9 @@
-"use client"
 import React from "react"
-import Link from "next/link"
+
+import VoucherSecret from "./voucher-secret"
 
 import { env } from "@/env"
-import { useGetWithdrawLinkQuery, Status } from "@/lib/graphql/generated"
-import Button from "@/components/button"
-import LinkDetails from "@/components/link-details"
-import InfoComponent from "@/components/info-component"
-import FundsPaid from "@/components/funds-paid"
-import PageLoadingComponent from "@/components/loading/page-loading-component"
-import Heading from "@/components/heading"
+
 const { NEXT_PUBLIC_VOUCHER_URL } = env
 
 interface Params {
@@ -20,58 +14,7 @@ interface Params {
 
 // this page shows the LNURLw screen after success in fund transfer to escrow account
 export default function Page({ params: { "voucher-secret": voucherSecret } }: Params) {
-  const { loading, error, data } = useGetWithdrawLinkQuery({
-    variables: { voucherSecret },
-    context: {
-      endpoint: "SELF",
-    },
-  })
-
-  if (loading) {
-    return <PageLoadingComponent />
-  }
-  if (error) {
-    return <div>Error: {error.message}</div>
-  }
-  if (!data) {
-    return <div>No data</div>
-  }
-
   return (
-    <div className="top_page_container">
-      {data.getWithdrawLink?.status === Status.Paid ? (
-        <>
-          <FundsPaid />
-        </>
-      ) : (
-        <>
-          <Heading>Please Withdraw your funds</Heading>
-          <LinkDetails withdrawLink={data.getWithdrawLink}></LinkDetails>
-          <Link
-            style={{ width: "90%" }}
-            href={`${NEXT_PUBLIC_VOUCHER_URL}/withdraw/${voucherSecret}/lnurl`}
-          >
-            <Button>
-              <span>LNURLw Link</span>{" "}
-            </Button>
-          </Link>
-
-          <Link
-            style={{ width: "90%" }}
-            href={`${NEXT_PUBLIC_VOUCHER_URL}/withdraw/${voucherSecret}/onchain`}
-          >
-            <Button>
-              <span>On Chain</span>{" "}
-            </Button>
-          </Link>
-
-          <InfoComponent>
-            You can withdraw funds from supported LNURL wallets or through on-chain
-            transactions. However, please note that on-chain transactions will incur
-            transaction fees.
-          </InfoComponent>
-        </>
-      )}
-    </div>
+    <VoucherSecret voucherSecret={voucherSecret} voucherUrl={NEXT_PUBLIC_VOUCHER_URL} />
   )
 }
