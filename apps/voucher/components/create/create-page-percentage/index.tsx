47959177021e9a116715012d6205833c48892de0
siddharth <siddharthtiwarikreplind@gmail.com>
2024-05-09 17:00:40 +0530

    chore(voucher): updated commision percentage page

--- apps/voucher/components/create/create-page-percentage/index.tsx
+++ apps/voucher/components/create/create-page-percentage/index.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 
-import styles from "../create-link.module.css"
-
 import Numpad from "@/components/num-pad"
 import { formatOperand } from "@/utils/helpers"
 import Button from "@/components/button"
@@ -18,19 +16,23 @@ export default function CreatePagePercentage({
   setCurrentPage,
 }: Props) {
   return (
-    <>
-      <p>Commission Percentage</p>
-      <div className="text-3xl font-semibold">{formatOperand(commissionPercentage)}%</div>
-      <Numpad
-        currentAmount={commissionPercentage}
-        setCurrentAmount={setCommissionPercentage}
-        unit="PERCENTAGE"
-      />
-      <div className={styles.commission_and_submit_buttons}>
-        <Button style={{ width: "80%" }} onClick={() => setCurrentPage("AMOUNT")}>
+    <div className="flex flex-col sm:h-full h-[calc(100dvh-6rem)] justify-between w-full">
+      <div className="flex flex-col justify-center align-middle">
+        <p className="m-auto">Commission Percentage</p>
+        <div className="text-4xl text-center mt-5 font-semibold">
+          {formatOperand(commissionPercentage)}%
+        </div>
+      </div>
+      <div className="w-full m-0 flex flex-col justify-center align-middle sm:mt-28">
+        <Numpad
+          currentAmount={commissionPercentage}
+          setCurrentAmount={setCommissionPercentage}
+          unit="PERCENTAGE"
+        />
+        <Button className="w-10/12 m-auto mt-5" onClick={() => setCurrentPage("AMOUNT")}>
           Set commission
         </Button>
       </div>
-    </>
+    </div>
   )
 }
