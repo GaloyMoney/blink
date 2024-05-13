bb53fe7c8d2b2ec0103e3fee317f8635f60829a6
siddharth <siddharthtiwarikreplind@gmail.com>
2024-05-13 12:16:50 +0530

    chore(voucher): remove async from root layout

--- apps/voucher/app/layout.tsx
+++ apps/voucher/app/layout.tsx
@@ -12,23 +12,26 @@ const { NEXT_PUBLIC_CORE_URL, NEXT_PUBLIC_VOUCHER_URL } = env
 
 const inter = Inter_Tight({ subsets: ["latin"], display: "auto" })
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const coreGqlUrl = NEXT_PUBLIC_CORE_URL
+  const voucherUrl = NEXT_PUBLIC_VOUCHER_URL
+
   return (
     <SessionProvider>
       <ApolloWrapper
         config={{
-          coreGqlUrl: NEXT_PUBLIC_CORE_URL,
-          voucherUrl: NEXT_PUBLIC_VOUCHER_URL,
+          coreGqlUrl,
+          voucherUrl,
         }}
       >
-        <html lang="en">
-          <CurrencyProvider>
+        <CurrencyProvider>
+          <html lang="en">
             <body className={inter.className}>
               <Navigation />
               {children}
             </body>
-          </CurrencyProvider>
-        </html>
+          </html>
+        </CurrencyProvider>
       </ApolloWrapper>
     </SessionProvider>
   )
