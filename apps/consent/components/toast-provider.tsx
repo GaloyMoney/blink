c649fa154fab7376a0ea2e0f90102bd9f213f02b
Juan P Lopez <dolcalmi@gmail.com>
2025-02-19 01:36:57 -0500

    fix: consent lint issues

---
+++ apps/consent/components/toast-provider.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { ToastContainer } from "react-toastify"
+
+export default function ToastProvider() {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  )
+}
