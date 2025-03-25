ff8a166cb0ff444778f938e15a32612bc6d4b924
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-03-25 10:19:08 -0600

    chore: value changed to usernameOrPhone

--- core/api/src/app/accounts/get-default-wallet-for-username-phone.ts
+++ core/api/src/app/accounts/get-default-wallet-for-username-phone.ts
@@ -5,17 +5,17 @@ import { checkedToPhoneNumber } from "@/domain/users"
 import { AccountsRepository, UsersRepository } from "@/services/mongoose"
 
 export const getDefaultWalletByUsernameOrPhone = async (
-  value: Username | PhoneNumber,
+  usernameOrPhone: Username | PhoneNumber,
   walletCurrency?: WalletCurrency,
 ): Promise<Wallet | RepositoryError> => {
-  const checkedUsername = checkedToUsername(value)
+  const checkedUsername = checkedToUsername(usernameOrPhone)
   if (!(checkedUsername instanceof Error)) {
     const account = await AccountsRepository().findByUsername(checkedUsername)
     if (account instanceof Error) return account
-    return getWalletFromAccount(account, walletCurrency, value)
+    return getWalletFromAccount(account, walletCurrency, usernameOrPhone)
   }
 
-  const checkedPhoneNumber = checkedToPhoneNumber(value)
+  const checkedPhoneNumber = checkedToPhoneNumber(usernameOrPhone)
   if (checkedPhoneNumber instanceof Error) return checkedPhoneNumber
 
   const user = await UsersRepository().findByPhone(checkedPhoneNumber)
@@ -24,5 +24,5 @@ export const getDefaultWalletByUsernameOrPhone = async (
   const account = await AccountsRepository().findByUserId(user.id)
   if (account instanceof Error) return account
 
-  return getWalletFromAccount(account, walletCurrency, value)
+  return getWalletFromAccount(account, walletCurrency, usernameOrPhone)
 }
