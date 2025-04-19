fe7da7faa3b30c686481e31dc6ce3581994b6a74
Juan P Lopez <dolcalmi@gmail.com>
2025-04-19 12:41:02 -0500

    chore: update callback cmd (#4809)
    * chore: update update api is up method
    
    * chore: update callback cmd
    
    * chore: build before run dev env

--- bats/ci_setup_suite.bash
+++ bats/ci_setup_suite.bash
@@ -6,6 +6,11 @@ source "${REPO_ROOT}/bats/helpers/_common.bash"
 TILT_PID_FILE="${BATS_ROOT_DIR}/.tilt_pid"
 
 setup_suite() {
+  buck2 build \
+    //core/api //core/api-ws-server //core/api-trigger //core/api-exporter \
+    //apps/dashboard //apps/consent //apps/pay //apps/admin-panel //apps/map //apps/voucher \
+    //core/api-keys //core/notifications \
+    //bats/helpers/callback:run //bats/helpers/subscriber:run //bats/helpers/totp:generate
   background buck2 run //dev:up -- --bats=True > "${REPO_ROOT}/bats/.e2e-tilt.log"
   echo $! > "$TILT_PID_FILE"
   await_notifications_is_up
@@ -23,13 +28,13 @@ teardown_suite() {
 }
 
 await_api_is_up() {
-  server_is_up() {
+  api_is_up() {
     exec_graphql 'anon' 'globals'
     network="$(graphql_output '.data.globals.network')"
     [[ "${network}" = "regtest" ]] || exit 1
   }
 
-  retry 360 5 server_is_up
+  retry 360 5 api_is_up
 }
 
 await_pay_is_up() {
