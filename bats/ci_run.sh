9acfbb2a66d35a7a7230b4814163103ebdfcbc21
Juan P Lopez <dolcalmi@gmail.com>
2025-04-21 09:23:23 -0500

    test: remove apps builds from ci run (#4824)

--- bats/ci_run.sh
+++ bats/ci_run.sh
@@ -9,8 +9,8 @@ buck2 build //core/api-keys:api-keys //core/notifications:notifications
 echo "Running api builds..."
 buck2 build //core/api:api //core/api-ws-server:api-ws-server //core/api-trigger:api-trigger //core/api-exporter:api-exporter --verbose 4
 
-echo "Running apps builds..."
-buck2 build //apps/dashboard:dashboard //apps/consent:consent //apps/pay:pay-ci //apps/admin-panel:admin-panel //apps/map:map //apps/voucher:voucher --verbose 4
+# echo "Running apps builds..."
+# buck2 build //apps/dashboard:dashboard //apps/consent:consent //apps/pay:pay-ci //apps/admin-panel:admin-panel //apps/map:map //apps/voucher:voucher --verbose 4
 
 echo "Running bats helpers builds..."
 buck2 build //bats/helpers/callback:run //bats/helpers/subscriber:run //bats/helpers/totp:generate --verbose 4
