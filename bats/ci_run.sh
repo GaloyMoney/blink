882f2296f38715a9e8fc975f5016bcd7cf463792
Juan P Lopez <dolcalmi@gmail.com>
2025-04-20 21:00:48 -0500

    chore: add verbose to ci_run (#4818)

--- bats/ci_run.sh
+++ bats/ci_run.sh
@@ -7,13 +7,13 @@ echo "Running rust builds..."
 buck2 build //core/api-keys:api-keys //core/notifications:notifications
 
 echo "Running api builds..."
-buck2 build //core/api:api //core/api-ws-server:api-ws-server //core/api-trigger:api-trigger //core/api-exporter:api-exporter
+buck2 build //core/api:api //core/api-ws-server:api-ws-server //core/api-trigger:api-trigger //core/api-exporter:api-exporter --verbose 4
 
 echo "Running apps builds..."
-buck2 build //apps/dashboard:dashboard //apps/consent:consent //apps/pay:pay-ci //apps/admin-panel:admin-panel //apps/map:map //apps/voucher:voucher
+buck2 build //apps/dashboard:dashboard //apps/consent:consent //apps/pay:pay-ci //apps/admin-panel:admin-panel //apps/map:map //apps/voucher:voucher --verbose 4
 
 echo "Running bats helpers builds..."
-buck2 build //bats/helpers/callback:run //bats/helpers/subscriber:run //bats/helpers/totp:generate
+buck2 build //bats/helpers/callback:run //bats/helpers/subscriber:run //bats/helpers/totp:generate --verbose 4
 
 echo "Running bats tests..."
 if [ "$1" != "" ]; then
