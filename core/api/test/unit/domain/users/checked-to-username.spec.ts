455ad705814bb105569bd63b5e43a15727ac8774
Juan P Lopez <dolcalmi@gmail.com>
2025-04-23 14:12:38 -0500

    fix: add removed unit test

--- core/api/test/unit/domain/users/checked-to-username.spec.ts
+++ core/api/test/unit/domain/users/checked-to-username.spec.ts
@@ -11,6 +11,11 @@ describe("username-check", () => {
     expect(username).toBeInstanceOf(Error)
   })
 
+  it("Fails if starting with 1", () => {
+    const username = checkedToUsername("1ab")
+    expect(username).toBeInstanceOf(Error)
+  })
+
   it("Fails if starting with +", () => {
     const username = checkedToUsername("+57300123456")
     expect(username).toBeInstanceOf(Error)
